f82cd0dd36ba808fe5d79de07b83afd3b579c1ad
Jamie Winsor <jamie@vialstudios.com>
2015-11-18 07:26:21 -0800

    properly specify pkg_version for cacerts

--- cacerts/plan.sh
+++ cacerts/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=cacerts
 pkg_derivation=chef
-pkg_version=
+pkg_version=$(date --date="$build_date" "+%Y.%m.%d")
 pkg_license=('MPL1.1 GPLv2.0 LGPLv2.1')
 pkg_source=http://curl.haxx.se/ca/cacert.pem
 pkg_shasum=nopenopebucketofnope
@@ -9,7 +9,6 @@ pkg_gpg_key=3853DA6B
 unpack() {
 	# We are hijacking unpack to get the version from inside the CA Certs
 	local build_date=$(cat $BLDR_SRC_CACHE/$pkg_filename | grep 'Certificate data from Mozilla' | sed 's/^## Certificate data from Mozilla as of: //')
-	pkg_version=$(date --date="$build_date" "+%Y.%m.%d")
 	pkg_dirname=${pkg_name}-${pkg_version}
 	mkdir -p $BLDR_SRC_CACHE/$pkg_dirname
 	cp $BLDR_SRC_CACHE/$pkg_filename $BLDR_SRC_CACHE/$pkg_dirname
