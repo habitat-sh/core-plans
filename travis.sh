17c030a0812561e65f111f58d2ca23e781917bb2
Nathan L Smith <smith@chef.io>
2016-07-12 16:16:36 -0500

    don't run pre-commit on auto
    Signed-off-by: Nathan L Smith <smith@chef.io>

--- travis.sh
+++ travis.sh
@@ -1,27 +1,15 @@
 #!/bin/bash
 # Script to run when testing core-plans on Travis CI
-set -x
 
-# debug info
-echo "TRAVIS_BRANCH=$TRAVIS_BRANCH"
-echo "TRAVIS_COMMIT=$TRAVIS_COMMIT"
-echo "TRAVIS_COMMIT_RANGE=$TRAVIS_COMMIT_RANGE"
-echo "HEAD=$(git rev-parse HEAD)"
-
-PARENT_COMMIT=$(git log --pretty=%P -n 1 "$TRAVIS_COMMIT")
-echo "PARENT_COMMIT=$PARENT_COMMIT"
-## end debug
+# Don't run if we're on the auto branch. Using the right range for pre-commit
+# has been problematic, so we're just going to skip it until somebody figures
+# it out.
+[[ "$TRAVIS_BRANCH" = "auto" ]] && exit 0;
 
 pre-commit install
 
-# Retrieve the canonical range.
-# If we're on the auto branch, the origin should be auto
-if [[ "$TRAVIS_BRANCH" = "auto" ]]; then
-  ORIGIN=auto
 # The TRAVIS_COMMIT env var is always a merge commit, so we want to test from
 # the original commit all the way up until HEAD.
-else
-  ORIGIN=$(echo "$TRAVIS_COMMIT_RANGE" | cut -f1 -d'.')
-fi
+ORIGIN=$(echo "$TRAVIS_COMMIT_RANGE" | cut -f1 -d'.')
 
 pre-commit run --origin "$ORIGIN" --source HEAD
