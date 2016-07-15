9ba54d66a7878080a0ca7b005f80f65714a5618c
Nathan L Smith <smith@chef.io>
2016-07-15 14:01:21 -0500

    Don't copy in upload message script
    I wanted to use this in a studio but I didn't have `pbcopy` in there so
    I made it do less.
    
    Signed-off-by: Nathan L Smith <smith@chef.io>

--- bin/copy-depot-upload-message.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Take the last build information and put it into a message that you can paste
-# into GitHub as a comment. Copy it to the clipboard.
-source ./results/last_build.env
-echo "[$pkg_ident](https://app.habitat.sh/#/pkgs/$pkg_ident) has been built and uploaded to the depot. :sparkling_heart:" | pbcopy
---
+++ bin/depot-upload-message.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Take the last build information and put it into a message that you can paste
+# into GitHub as a comment.
+source ./results/last_build.env
+echo "[$pkg_ident](https://app.habitat.sh/#/pkgs/$pkg_ident) has been built and uploaded to the depot. :sparkling_heart:"
