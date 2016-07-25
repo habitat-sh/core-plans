0486f061158d7cd493050d86a024760036286a98
jtimberman <joshua@chef.io>
2016-07-25 11:45:59 -0600

    Add a plan for OpenSSH
    This is so we have an SSH binary that can be used by other packages such as core/git so we don't make an assumption that the underlying OS / distribution has SSH available. Also includes sshd configuration.
    
    Signed-off-by: jtimberman <joshua@chef.io>

---
+++ openssh/plan.sh
@@ -0,0 +1,26 @@
+pkg_origin=core
+pkg_name=openssh
+pkg_version=7.2p2
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_license=('bsd')
+pkg_source=http://ftp3.usa.openbsd.org/pub/OpenBSD/OpenSSH/portable/${pkg_name}-${pkg_version}.tar.gz
+pkg_shasum=a72781d1a043876a224ff1b0032daa4094d87565a68528759c1c2cab5482548c
+pkg_bin_dirs=(bin sbin libexec)
+pkg_include_dirs=(include)
+pkg_lib_dirs=(lib)
+pkg_deps=(core/glibc core/openssl core/zlib)
+pkg_build_deps=(core/coreutils core/gcc core/make)
+
+do_build() {
+  ./configure --prefix=${pkg_prefix} \
+              --sysconfdir=${pkg_svc_path}/config \
+              --localstatedir=${pkg_svc_path}/var \
+              --datadir=${pkg_svc_data_path} \
+              --with-privsep-user=hab \
+              --with-privsep-path=${pkg_svc_var_path}/empty
+  make
+}
+
+do_install() {
+  make install-nosysconf
+}
