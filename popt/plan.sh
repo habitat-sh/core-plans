42f7dd6dc1bef25c754018f27941ea1775ade66f
Kristian Vlaardingerbroek <kristian.vlaardingerbroek@gmail.com>
2016-07-29 21:03:46 +0200

    Add plans for RPM, NSS, NSPR and Popt
    Signed-off-by: Kristian Vlaardingerbroek <kristian.vlaardingerbroek@gmail.com>

---
+++ popt/plan.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+pkg_name=popt
+pkg_origin=core
+pkg_version=1.16
+pkg_descriptyon="Popt is a C library for parsing command line parameters"
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_license=("MIT")
+pkg_upstream_url=http://rpm5.org
+pkg_source=http://rpm5.org/files/${pkg_name}/${pkg_name}-${pkg_version}.tar.gz
+pkg_shasum=e728ed296fe9f069a0e005003c3d6b2dde3d9cad453422a10d6558616d304cc8
+pkg_deps=(core/glibc)
+pkg_build_deps=(core/gcc core/make)
+pkg_include_dirs=(include)
+pkg_lib_dirs=(lib)
---
+++ rpm/plan.sh
@@ -0,0 +1,23 @@
+pkg_name=rpm
+pkg_origin=core
+pkg_version=4.12.0.1
+pkg_license=("GPL-2.0" "LGPL-2.0")
+pkg_description="RPM Package Manager"
+pkg_upstream_url=http://www.rpm.org/
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_upstream_url=http://rpm.org
+pkg_source=http://www.rpm.org/releases/${pkg_name}-4.12.x/${pkg_name}-${pkg_version}.tar.bz2
+pkg_shasum=77ddd228fc332193c874aa0b424f41db1ff8b7edbb6a338703ef747851f50229
+pkg_deps=(core/glibc core/zlib rarenerd/nspr rarenerd/nss core/file rarenerd/popt core/libarchive core/db)
+pkg_build_deps=(core/gcc core/make)
+pkg_bin_dirs=(bin)
+pkg_include_dirs=(include)
+pkg_lib_dirs=(lib)
+
+do_build() {
+  ./configure --prefix="${pkg_prefix}" \
+              --with-external-db       \
+              --without-lua
+
+  make
+}
