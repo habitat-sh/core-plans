f3e7227d2e6a9b12fd96150036746107c05fcd71
bdangit <me@bdang.it>
2016-08-04 09:44:53 -0700

    updated to latest jdk8u102
    Signed-off-by: bdangit <me@bdang.it>

--- jdk8/plan.sh
+++ jdk8/plan.sh
@@ -1,12 +1,13 @@
 pkg_origin=core
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_name=jdk8
-pkg_version=8u92
+pkg_version=8u102
 pkg_source=http://download.oracle.com/otn-pub/java/jdk/${pkg_version}-b14/jdk-${pkg_version}-linux-x64.tar.gz
-pkg_shasum=79a3f25e9b466cb9e969d1772ea38550de320c88e9119bf8aa11ce8547c39987
+pkg_shasum=7cfbe0bc0391a4abe60b3e9eb2a541d2315b99b9cb3a24980e618a89229e04b7
 pkg_filename=jdk-${pkg_version}-linux-x64.tar.gz
 pkg_license=('Oracle Binary Code License Agreement for the Java SE Platform Products and JavaFX')
 pkg_description=('Oracle Java Development Kit. This package is made available to you to allow you to run your applications as provided in and subject to the terms of the Oracle Binary Code License Agreement for the Java SE Platform Products and JavaFX, found at http://www.oracle.com/technetwork/java/javase/terms/license/index.html')
+pkg_upstream_url=http://www.oracle.com/technetwork/java/javase/overview/index.html
 pkg_deps=(core/glibc)
 pkg_build_deps=(core/patchelf core/file)
 pkg_bin_dirs=(bin jre/bin)
@@ -24,29 +25,29 @@ download_file() {
   local dst="$2"
   local sha="$3"
 
-  pushd $HAB_CACHE_SRC_PATH > /dev/null
+  pushd "$HAB_CACHE_SRC_PATH" > /dev/null
   if [[ -f $dst && -n "$sha" ]]; then
     build_line "Found previous file '$dst', attempting to re-use"
-    if verify_file $dst $sha; then
+    if verify_file "$dst" "$sha"; then
       build_line "Using cached and verified '$dst'"
       return 0
     else
       build_line "Clearing previous '$dst' file and re-attempting download"
-      rm -fv $dst
+      rm -fv "$dst"
     fi
   fi
 
   build_line "Downloading '$url' to '$dst'"
-  $_wget_cmd --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie"  $url -O $dst
+  $_wget_cmd --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie"  "$url" -O "$dst"
   build_line "Downloaded '$dst'";
   popd > /dev/null
 }
 
 do_unpack() {
   local unpack_file="$HAB_CACHE_SRC_PATH/$pkg_filename"
-  mkdir $source_dir
-  pushd $source_dir >/dev/null
-  tar xz --strip-components=1 -f $unpack_file
+  mkdir "$source_dir"
+  pushd "$source_dir" >/dev/null
+  tar xz --strip-components=1 -f "$unpack_file"
 
   popd > /dev/null
   return 0
@@ -57,15 +58,15 @@ do_build() {
 }
 
 do_install() {
-  cd $source_dir
-  cp -r * $pkg_prefix
+  cd "$source_dir" || exit
+  cp -r ./* "$pkg_prefix"
 
   build_line "Setting interpreter for '${pkg_prefix}/bin/java' '$(pkg_path_for glibc)/lib/ld-linux-x86-64.so.2'"
   build_line "Setting rpath for '${pkg_prefix}/bin/java' to '$LD_RUN_PATH'"
 
   export LD_RUN_PATH=$LD_RUN_PATH:$pkg_prefix/lib/amd64/jli
 
-  find $pkg_prefix/bin $pkg_prefix/jre/bin -type f -executable \
-    -exec sh -c "file -i '{}' | grep -q 'x-executable; charset=binary'" \; \
-    -exec patchelf --interpreter "$(pkg_path_for glibc)/lib/ld-linux-x86-64.so.2" --set-rpath ${LD_RUN_PATH} {} \;
+  find "$pkg_prefix"/bin "$pkg_prefix"/jre/bin -type f -executable \
+    -exec sh -c 'file -i "$1" | grep -q "x-executable; charset=binary"' _ {} \; \
+    -exec patchelf --interpreter "$(pkg_path_for glibc)/lib/ld-linux-x86-64.so.2" --set-rpath "$LD_RUN_PATH" {} \;
 }
