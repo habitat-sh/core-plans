dfa856e0cf1debe3f5a8ed49b71970479bd37128
i-agrawal <ishan.agrawal@relevancelab.com>
2016-09-07 18:16:14 -0700

    added new httpd plan with configurability
    Signed-off-by: i-agrawal <ishan.agrawal@relevancelab.com>

--- httpd/plan.sh
+++ httpd/plan.sh
@@ -12,18 +12,21 @@ pkg_build_deps=(core/patch core/make core/gcc)
 pkg_bin_dirs=(bin)
 pkg_lib_dirs=(lib)
 pkg_expose=(80 443)
-pkg_svc_run="bin/httpd -DFOREGROUND -f $pkg_svc_config_path/httpd.conf"
 pkg_svc_user="root"
+pkg_svc_group="root"
 
 do_build() {
-  ./configure --prefix="$pkg_prefix" \
-              --with-expat="$(pkg_path_for expat)" \
-              --with-iconv="$(pkg_path_for libiconv)" \
-              --with-pcre="$(pkg_path_for pcre)" \
-              --with-apr="$(pkg_path_for apr)" \
-              --with-apr-util="$(pkg_path_for apr-util)" \
-              --with-z="$(pkg_path_for zlib)" \
-              --enable-ssl --with-ssl="$(pkg_path_for openssl)" \
-              --enable-modules=most --enable-mods-shared=most
-  make
+    ./configure --prefix="$pkg_prefix" \
+                --with-expat="$(pkg_path_for core/expat)" \
+                --with-iconv="$(pkg_path_for core/libiconv)" \
+                --with-pcre="$(pkg_path_for core/pcre)" \
+                --with-apr="$(pkg_path_for core/apr)" \
+                --with-apr-util="$(pkg_path_for core/apr-util)" \
+                --with-z="$(pkg_path_for core/zlib)" \
+                --with-ssl="$(pkg_path_for core/openssl)" \
+                --enable-modules="none" \
+                --enable-mods-static="unixd" \
+                --enable-mods-shared="reallyall" \
+                --enable-mpms-shared="prefork event worker"
+    make
 }
