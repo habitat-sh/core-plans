91512cd74a7018292c690cb51316c3a021c2b823
i-agrawal <ishan.agrawal@relevancelab.com>
2016-09-12 13:53:03 -0700

    adding path to include and DCO Signed-Off-By
    Signed-off-by: i-agrawal <ishan.agrawal@relevancelab.com>

--- httpd/plan.sh
+++ httpd/plan.sh
@@ -10,6 +10,7 @@ pkg_shasum=b71a13f56b8061c6b4086fdcc9ffdddd904449735eadec0f0e2947e33eec91d7
 pkg_deps=(core/glibc core/expat core/libiconv core/apr core/apr-util core/pcre core/zlib core/openssl)
 pkg_build_deps=(core/patch core/make core/gcc)
 pkg_bin_dirs=(bin)
+pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
 pkg_expose=(80 443)
 pkg_svc_run="$pkg_prefix/bin/httpd -DFOREGROUND -f $pkg_svc_config_path/httpd.conf"
