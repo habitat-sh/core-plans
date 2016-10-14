1f1e6e6d62f15dbafa212e670c12801ec9721bb7
Elliott Davis <edavis@chef.io>
2016-10-14 16:11:32 -0500

    Added plan for local-lib and cpanminus

--- cpanminus/plan.sh
+++ cpanminus/plan.sh
@@ -9,8 +9,8 @@ pkg_source=https://github.com/miyagawa/${pkg_name}/archive/${pkg_version}.tar.gz
 pkg_filename=${pkg_name}-${pkg_version}.tar.gz
 pkg_dirname=${pkg_name}-${pkg_version}
 pkg_shasum=48a747c040689445f7db0edd169da0abd709a37cfece3ceecff0816c09beab0e
-pkg_deps=(core/glibc core/perl)
-pkg_build_deps=(core/gcc core/make core/coreutils core/perl)
+pkg_deps=(core/glibc core/perl core/local-lib)
+pkg_build_deps=(core/gcc core/make core/coreutils core/perl core/local-lib)
 pkg_lib_dirs=(lib)
 pkg_bin_dirs=(bin)
 
@@ -19,5 +19,11 @@ do_build() {
 }
 
 do_install() {
+  # Load local::lib into our current perl include chain
+  eval "$(perl -I$(pkg_path_for core/local-lib)/lib/perl5 -Mlocal::lib=$(pkg_path_for core/local-lib))"
+  # Create a new lib dir in our pacakge for cpanm to house all of its libs
+  eval $(perl -Mlocal::lib=${pkg_prefix})
+
+  # cpanm prioritizes the local::lib location for an install dir
   cat cpanm | perl - App::cpanminus
 }
