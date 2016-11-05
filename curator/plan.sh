5ba3bb9e6f6cc2112ebc45e839d1492c2828a130
Nathan L Smith <smith@chef.io>
2016-11-05 11:52:47 -0500

    Add elasticsearch curator
    Signed-off-by: Nathan L Smith <smith@chef.io>

---
+++ curator/plan.sh
@@ -0,0 +1,45 @@
+pkg_name=curator
+pkg_origin=core
+pkg_version=4.1.2
+pkg_description="Elasticsearch Curator helps you curate, or manage your indices."
+pkg_upstream_url=https://github.com/elastic/curator
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_license=('Apache-2.0')
+pkg_source=nosuchfile.tgz
+pkg_deps=(
+  core/python2
+)
+pkg_build_deps=(
+  core/virtualenv
+)
+pkg_bin_dirs=(bin)
+
+do_download() {
+  return 0
+}
+
+do_verify() {
+  return 0
+}
+
+do_unpack() {
+  return 0
+}
+
+do_prepare() {
+  localedef -i en_US -f UTF-8 en_US.UTF-8
+  export LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+  virtualenv "$pkg_prefix"
+  # shellcheck source=/dev/null
+  source "$pkg_prefix/bin/activate"
+}
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  pip install "elasticsearch-curator==$pkg_version"
+  # Write out versions of all pip packages to package
+  pip freeze > "$pkg_prefix/requirements.txt"
+}
