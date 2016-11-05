843cf156d742b9ed1568aec9058f9ab2ce26cc9d
George Marshall <george@georgemarshall.name>
2016-11-05 01:11:03 -0700

    Update tzdata to 2016i and added do_install step

--- tzdata/plan.sh
+++ tzdata/plan.sh
@@ -1,21 +1,32 @@
 pkg_name=tzdata
 pkg_origin=core
-pkg_version=2015f
+pkg_version=2016i
+pkg_description="Sources for time zone and daylight saving time data"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('gpl')
 pkg_source=http://www.iana.org/time-zones/repository/releases/${pkg_name}${pkg_version}.tar.gz
-pkg_shasum=959f81b541e042ecb13c50097d264ae92ff03a57979c478dbcf24d5da242531d
+pkg_shasum=b6966ec982ef64fe48cebec437096b4f57f4287519ed32dde59c86d3a1853845
+pkg_upstream_url=http://www.iana.org/time-zones
 
-# This is an incomplete plan, but is used by glibc to install timezone data.
-
-# Re-override the defaults as this plan is sourced externally
-pkg_filename="$(basename $pkg_source)"
-pkg_dirname="${pkg_name}-${pkg_version}"
+timezones=(
+  'africa'
+  'antarctica'
+  'asia'
+  'australasia'
+  'europe'
+  'northamerica'
+  'southamerica'
+  'pacificnew'
+  'etcetera'
+  'backward'
+  'systemv'
+  'factory'
+)
 
 do_unpack() {
   mkdir -p "$HAB_CACHE_SRC_PATH/$pkg_dirname"
-  pushd $HAB_CACHE_SRC_PATH/$pkg_dirname > /dev/null
-    tar xzf "$HAB_CACHE_SRC_PATH/$pkg_filename"
+  pushd "$HAB_CACHE_SRC_PATH/$pkg_dirname" > /dev/null
+    tar xf "$HAB_CACHE_SRC_PATH/$pkg_filename"
   popd > /dev/null
 }
 
@@ -24,5 +35,10 @@ do_build() {
 }
 
 do_install() {
-  return 0
+  zic -y ./yearistype -d "$pkg_prefix/share/zoneinfo" "${timezones[@]}"
+  zic -y ./yearistype -d "$pkg_prefix/share/zoneinfo/posix" "${timezones[@]}"
+  zic -y ./yearistype -d "$pkg_prefix/share/zoneinfo/right" -L leapseconds "${timezones[@]}"
+
+  zic -y ./yearistype -d "$pkg_prefix/share/zoneinfo" -p America/New_York
+  install -m444 -t "$pkg_prefix/share/zoneinfo" -v iso3166.tab zone1970.tab zone.tab
 }
