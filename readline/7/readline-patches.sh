80a5e4aee49c98de9f7087e6aceae3b7cbcbcb8b
Lilian A. Moraru <lilian.moraru90@gmail.com>
2016-11-06 00:08:12 +0000

    Update readline to version 7.0 and split readline 6 into another plan
    Signed-off-by: Lilian A. Moraru <lilian.moraru90@gmail.com>

---
+++ readline/7/readline-patches.sh
@@ -0,0 +1,13 @@
+source "${PLAN_CONTEXT}/../base/readline-patches.sh"
+
+# All official patch file URLs
+_patch_files=(
+  # Next line is kept as an example
+  # ${_patch_url_base}-001
+)
+
+# All official patch file shasums
+_patch_shasums=(
+  # Next line is kept as an example
+  # 1a79bbb6eaee750e0d6f7f3d059b30a45fc54e8e388a8e05e9c3ae598590146f
+)
