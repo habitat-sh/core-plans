321f99ff56db8e37d07c18b6f58ea5f5f6b8bbc1
Mike Fiedler <miketheman@gmail.com>
2016-11-11 19:45:27 -0500

    jemalloc 4.3.1 (updated)
    See ChangeLog for highlights:
    https://github.com/jemalloc/jemalloc/blob/master/ChangeLog
    
    - Replaces the need for customized `do_build` and simplifies some of the
      build dependencies.
    - Adds a `do_check` to run tests.
    - Fixes metadata.
    
    Refs #262
    
    Signed-off-by: Mike Fiedler <miketheman@gmail.com>

--- jemalloc/plan.sh
+++ jemalloc/plan.sh
@@ -1,33 +1,29 @@
 pkg_name=jemalloc
-pkg_description="jemalloc"
-pkg_upstream_url=http://www.canonware.com/jemalloc/
+pkg_description="malloc implementation emphasizing fragmentation avoidance"
+pkg_upstream_url="http://jemalloc.net/"
 pkg_origin=core
-pkg_version=4.2.1
+pkg_version=4.3.1
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('BSD-2-Clause')
-pkg_source=https://github.com/jemalloc/jemalloc/archive/${pkg_version}.tar.gz
-pkg_shasum=38abd5c3798dee4bd0e63e082502358cd341b831b038bb443e89370df888a3eb
+pkg_source=https://github.com/jemalloc/jemalloc/releases/download/$pkg_version/${pkg_name}-${pkg_version}.tar.bz2
+pkg_shasum=f7bb183ad8056941791e0f075b802e8ff10bd6e2d904e682f87c8f6a510c278b
 pkg_dirname=${pkg_name}-${pkg_version}
 pkg_deps=(core/glibc)
 
 pkg_build_deps=(
-    core/autoconf
-    core/coreutils
-    core/gcc
-    core/make
+  core/gcc
+  core/make
 )
-pkg_lib_dirs=(lib)
+pkg_bin_dirs=(bin)
 pkg_include_dirs=(include)
+pkg_lib_dirs=(lib)
 
-do_prepare() {
-    autoconf
-}
-
-do_build() {
-    ./configure --prefix=$pkg_prefix && make
+do_check() {
+  make check
 }
 
 do_install() {
-    make install_bin install_include install_lib
-    install -Dm644 COPYING "${pkg_prefix}/share/licenses/license.txt"
+  # Default `install` includes doc that we do not need
+  make install_bin install_include install_lib
+  install -Dm644 COPYING "${pkg_prefix}/share/licenses/license.txt"
 }
