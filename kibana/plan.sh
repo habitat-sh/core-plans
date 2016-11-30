5522e7a0ee14eda60855c782d7ce0f4cd2d83695
larryebaum <larryebaum@users.noreply.github.com>
2016-11-30 12:19:43 -0800

    update to 4.6.1, update hooks
    Signed-off-by: larryebaum <larryebaum@users.noreply.github.com>

--- kibana/plan.sh
+++ kibana/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=kibana
-pkg_version=4.5.1
+pkg_version=4.6.1
 pkg_origin=core
 pkg_license=('Apache-2.0')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="Kibana is an open source (Apache Licensed), browser based analytics and search dashboard for Elasticsearch. Kibana is a snap to setup and start using. Kibana strives to be easy to get started with, while also being flexible and powerful, just like Elasticsearch."
 pkg_upstream_url=https://www.elastic.co/products/kibana
 pkg_source=https://github.com/elastic/${pkg_name}/archive/v${pkg_version}.tar.gz
-pkg_shasum=417c4757167d4fdacd04fab9738505a35ed35c687a62f8c9de5d9ec8bd861438
+pkg_shasum=58dc3f82cdd62708034169db64c342a48674065673e2115d410509f83fe59c9e
 pkg_filename=${pkg_name}-${pkg_version}.tar.gz
 pkg_deps=(core/node)
 pkg_build_deps=(core/node core/coreutils core/python2 core/make core/gcc core/git)
@@ -15,7 +15,7 @@ pkg_expose=(5601)
 do_prepare() {
   # The `/usr/bin/env` path is hardcoded, so we'll add a symlink if needed.
   if [[ ! -r /usr/bin/env ]]; then
-    ln -sv $(pkg_path_for coreutils)/bin/env /usr/bin/env
+    ln -sv "$(pkg_path_for coreutils)/bin/env" /usr/bin/env
     _clean_env=true
   fi
 }
@@ -26,8 +26,10 @@ do_build () {
 }
 
 do_install() {
-  cp -R ./* $pkg_prefix/
-  mkdir -p ${pkg_prefix}/bin
+  cp -rv ./* "$pkg_prefix/"
+  mkdir -p "${pkg_prefix}/bin"
+# Delete the /config directory created by Kibana installer; habitat lays down /config/kibana.yml
+  rm -rv "$pkg_prefix/config/"
 }
 
 do_end() {
