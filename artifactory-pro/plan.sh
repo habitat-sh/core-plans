8d71599e59672a88da4e1904a7d9c4b156cc15aa
Seth Chisamore <schisamo@chef.io>
2016-12-07 10:34:47 -0500

    Update Artifactory to latest of 4.14.3
    Signed-off-by: Seth Chisamore <schisamo@chef.io>

--- artifactory-pro/plan.sh
+++ artifactory-pro/plan.sh
@@ -1,10 +1,10 @@
 pkg_origin=core
 pkg_name=artifactory-pro
-pkg_version=4.8.1
+pkg_version=4.14.3
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("JFrog Artifactory EULA")
 pkg_source=https://dl.bintray.com/jfrog/${pkg_name}/org/artifactory/pro/jfrog-${pkg_name}/${pkg_version}/jfrog-${pkg_name}-${pkg_version}.zip
-pkg_shasum=481f755a51faa33492829becb5f00ed3e07a392a0669a02d4d874db13dbcbc3f
+pkg_shasum=470f7cafcc94ffd181b75aac70e033f2414d69ca3cd66fc03cf621c9b8a3368e
 pkg_deps=(core/bash core/jre8)
 pkg_build_deps=(core/unzip)
 pkg_expose=(8081)
