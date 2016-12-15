58547ec1b2def6467bfdfcbe72b7a851d553b146
Scott Macfarlane <macfarlane.scott@gmail.com>
2016-12-14 17:37:14 -0800

    Fix build errors
    * Fix shellcheck errors
    * Limit build targets to x86_64
    * Add libaio support
    * Add jemalloc support

--- libcap-ng/plan.sh
+++ libcap-ng/plan.sh
@@ -5,6 +5,7 @@ pkg_source=http://people.redhat.com/sgrubb/$pkg_name/$pkg_name-$pkg_version.tar.
 pkg_shasum=c21af997445cd4107a55d386f955c5ea6f6e96ead693e9151277c0ab5f97d05f
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="The libcap-ng library is intended to make programming with posix capabilities much easier than the traditional libcap library"
+pkg_upstream_url="https://people.redhat.com/sgrubb/libcap-ng/"
 pkg_license=('GPL-2.0' 'LGPL-2.1')
 pkg_bin_dirs=(bin)
 pkg_lib_dirs=(lib)
@@ -13,6 +14,6 @@ pkg_build_deps=(core/make core/gcc)
 pkg_deps=(core/glibc)
 
 do_build() {
-  ./configure --prefix=$pkg_prefix --enable-static=no --with-python=no
+  ./configure --prefix="${pkg_prefix}" --enable-static=no --with-python=no
   make
 }
--- qemu/plan.sh
+++ qemu/plan.sh
@@ -5,6 +5,7 @@ pkg_source=http://wiki.qemu-project.org/download/${pkg_name}-${pkg_version}.tar.
 pkg_shasum=326e739506ba690daf69fc17bd3913a6c313d9928d743bd8eddb82f403f81e53
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="QEMU is a generic and open source machine emulator and virtualizer."
+pkg_upstream_url="http://www.qemu.org"
 pkg_license=('GPL-2.0')
 pkg_bin_dirs=(bin)
 pkg_lib_dirs=(lib)
@@ -25,7 +26,9 @@ pkg_deps=(
   core/gcc-libs
   core/glib
   core/glibc
-  smacfarlane/libcap-ng
+  core/jemalloc
+  core/libaio
+  core/libcap-ng
   core/lzo
   core/ncurses
   core/pcre
@@ -33,35 +36,28 @@ pkg_deps=(
   core/python2
   core/snappy
   core/util-linux
-  smacfarlane/vde2
+  core/vde2
   core/zlib
 )
 
 do_build() {
   mkdir build
-  cd build
+  cd build || exit
 
   # QEMU uses its own CFLAGS, etc. so we need to inject our environment into
   #   those variables.
   export CFLAGS+=" -fPIC"
   export QEMU_CFLAGS=$CFLAGS
-  ../configure --prefix=${pkg_prefix} 
-
+  ../configure --prefix="${pkg_prefix}" \
+    --target-list=x86_64-softmmu
   make
 }
 
 do_install() {
-  cd build
+  cd build || exit
   make install
 }
 
 do_strip() {
-# do_default_strip bombs with the following error:
-#   strip: Unable to recognise the format of the input file 
-#     `/hab/pkgs/core/qemu/2.7.0/20161213044729/share/qemu/palcode-clipper'
-# where:
-#  file /hab/pkgs/core/qemu/2.7.0/20161213044729/share/qemu/palcode-clipper
-#     /hab/pkgs/core/qemu/2.7.0/20161213044729/share/qemu/palcode-clipper: 
-#     ELF 64-bit LSB executable, Alpha (unofficial), version 1 (SYSV), statically linked, not stripped
   return 0
 }
