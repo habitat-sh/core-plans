01bc947cfc0c3445600a91995f52cfb2de4ed6cd
Seth Chisamore <schisamo@chef.io>
2016-12-21 16:33:20 -0500

    Update Logstash to 5.1.1
    Signed-off-by: Seth Chisamore <schisamo@chef.io>

--- logstash/plan.sh
+++ logstash/plan.sh
@@ -1,12 +1,12 @@
 pkg_origin=core
 pkg_name=logstash
-pkg_version=5.0.2
+pkg_version=5.1.1
 pkg_description="Logstash is an open source, server-side data processing pipeline that ingests data from a multitude of sources simultaneously, transforms it, and then sends it to your favorite 'stash.'"
 pkg_upstream_url=https://github.com/elastic/logstash
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=(Apache-2.0)
 pkg_source=https://artifacts.elastic.co/downloads/${pkg_name}/${pkg_name}-${pkg_version}.tar.gz
-pkg_shasum=eff45f965118b6ef767f719d85f6dbca438ea2daa5e901907a32fa5bf1a70d9c
+pkg_shasum=9ce438ec331d3311acc55f22553a3f5a7eaea207b8aa2863164bb2767917de1f
 pkg_deps=(core/bash core/jre8 core/jruby1)
 pkg_build_deps=(core/bash)
 pkg_bin_dirs=(bin)
@@ -19,14 +19,25 @@ do_build() {
 do_install() {
   mkdir -p "$pkg_prefix"
   cp -r ./* "$pkg_prefix"
+
+  # Ensure default `data/queue' dir exists and is writable (even though
+  # we override it in `hooks/run') to work around:
+  #
+  #   https://github.com/elastic/logstash/issues/6378
+  #
+  mkdir -p "$pkg_prefix/data/queue"
+  chmod 0777 "$pkg_prefix/data" "$pkg_prefix/data/queue"
+
   # The `config/' directory is special in Habitat so we'll just move
   # the various config files that ship with Logstash into `settings'
   # as Logstash refers to this location as SETTINGS_DIR.
   mv "$pkg_prefix/config" "$pkg_prefix/settings"
-  rm -rf "$pkg_prefix/data"
-  rm -rf "$pkg_prefix/vendor/jruby"
-  fix_interpreter "${pkg_prefix}/bin/*" core/bash bin/sh
 
+  # Remove non x86_64-Linux vendored JNI native extensions or `strip' will
+  # throw `Unable to recognise the format of the input file' errors
+  find "$pkg_prefix/vendor/jruby/lib/jni/" -mindepth 1 -maxdepth 1 -type d -not -name 'x86_64-Linux' -exec rm -rf {} \;
+
+  fix_interpreter "${pkg_prefix}/bin/*" core/bash bin/sh
   # Ensure we only print to the console
   cp "${PLAN_CONTEXT}/log4j2.properties" "$pkg_prefix/settings/"
 }
