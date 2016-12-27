4e473f2ce37c62bcb45535377fa5adf7e280c222
Elliott Davis <edavis@chef.io>
2016-12-27 17:27:49 -0600

    Upgrade node to latest LTS release
    Signed-off-by: Elliott Davis <edavis@chef.io>

--- node7/plan.sh
+++ node7/plan.sh
@@ -1,6 +1,6 @@
-pkg_name=node
+pkg_name=node7
 pkg_origin=core
-pkg_version=6.9.5
+pkg_version=7.5.0
 pkg_description="Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine."
 pkg_upstream_url=https://nodejs.org/
 pkg_license=('MIT')
@@ -9,7 +9,7 @@ pkg_source=https://nodejs.org/dist/v${pkg_version}/node-v${pkg_version}.tar.gz
 # the archive contains a 'v' version # prefix, but the default value of
 # pkg_dirname is node-${pkg_version} (without the v). This tweak makes build happy
 pkg_dirname=node-v${pkg_version}
-pkg_shasum=f7e9ab702c5d1f5a3521199c04cc670fda3cf4b0e48548b09ac7ac874ccb504a
+pkg_shasum=0da8e0288b5c0f136e650b7119219968720caf88b5a67ef0591555113f0844c2
 pkg_deps=(core/glibc core/gcc-libs core/coreutils)
 pkg_build_deps=(core/python2 core/gcc core/make)
 pkg_bin_dirs=(bin)
