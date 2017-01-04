687175d5ebec993468bf6196e94c8eafb47bbd41
Chris Alfano <chris@jarv.us>
2017-01-04 03:03:19 -0500

    Bump gnatsd version and use GOPATH correctly
    Signed-off-by: Chris Alfano <chris@jarv.us>

--- gnatsd/plan.sh
+++ gnatsd/plan.sh
@@ -1,35 +1,46 @@
 pkg_name=gnatsd
 pkg_origin=core
-pkg_version=0.9.4
+pkg_version=0.9.6
 pkg_description="A High Performance NATS Server written in Go."
 pkg_upstream_url=https://github.com/nats-io/gnatsd
 pkg_license=('MIT')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source=https://github.com/nats-io/gnatsd/archive/v${pkg_version}.tar.gz
-pkg_shasum=6465161c52e3e703f88cfe37182904d956234c4346f9ad6eb74903eff0190c98
-pkg_dirname=${pkg_name}-${pkg_version}
-pkg_filename=${pkg_name}-${pkg_version}.tar.gz
-pkg_deps=(core/go)
+pkg_shasum=18d6d1b014bfd262da101e15ed914e194b51b47e3e1a8ca4e8743c742d65310c
+pkg_deps=(core/glibc)
 pkg_build_deps=(core/go core/coreutils core/gcc core/make)
 pkg_bin_dirs=(bin)
-pkg_svc_run="${pkg_name}-${pkg_version}"
+pkg_svc_run="${pkg_name}"
 
-do_build() {
-  export GOPATH=/hab/cache/src/gnatsd-0.9.4
-  mkdir -p ./src/github.com/nats-io/gnatsd
 
-  cp -r auth ./src/github.com/nats-io/gnatsd
-  cp -r server ./src/github.com/nats-io/gnatsd
-  cp -r logger ./src/github.com/nats-io/gnatsd
-  cp -r conf ./src/github.com/nats-io/gnatsd
-  cp -r util ./src/github.com/nats-io/gnatsd
-  cp -r vendor ./src/github.com/nats-io/gnatsd
-  go build
+do_begin() {
+  export GOPATH="/hab/cache"
+  parent_go_path="${GOPATH}/src/github.com/nats-io"
 }
 
+do_clean() {
+  do_default_clean
+  rm -rf "${parent_go_path}"
+  return $?
+}
+
+do_prepare() {
+  mkdir -p "${parent_go_path}"
+  ln -s "${PWD}" "${parent_go_path}/${pkg_name}"
+  return $?
+}
+
+do_build() {
+  pushd "${parent_go_path}/${pkg_name}" > /dev/null
+  go build
+  local code=$?
+  popd > /dev/null
+
+  return $code
+}
 
 do_install() {
   mkdir -p "${pkg_prefix}/bin"
-  cp -R . "${pkg_prefix}"
-  ln -s "${pkg_prefix}/${pkg_name}-${pkg_version}" "${pkg_prefix}/bin/${pkg_name}-${pkg_version}"
+  cp  "${pkg_name}" "${pkg_prefix}/bin/${pkg_name}"
+  return $?
 }
