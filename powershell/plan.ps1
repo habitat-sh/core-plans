3527a53960b4ef8da7b2cb0fd1342e212fe6f3e4
Matt Wrock <matt@mattwrock.com>
2017-01-06 12:16:56 -0800

    adds a powershell plan for powershell
    Signed-off-by: Matt Wrock <matt@mattwrock.com>

---
+++ powershell/plan.ps1
@@ -0,0 +1,34 @@
+$pkg_name="powershell"
+$pkg_origin="core"
+$pkg_version="6.0.0-alpha.14"
+$pkg_license=@("MIT")
+$pkg_upstream_url="https://msdn.microsoft.com/powershell"
+$pkg_description="PowerShell is a cross-platform (Windows, Linux, and macOS) automation and configuration tool/framework that works well with your existing tools and is optimized for dealing with structured data (e.g. JSON, CSV, XML, etc.), REST APIs, and object models. It includes a command-line shell, an associated scripting language and a framework for processing cmdlets."
+$pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+$pkg_source="https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-alpha.14/powershell-$pkg_version-win7-x64.zip"
+$pkg_shasum="689e59c8a97a7f6f136104a56be397d9456d46069aa2c1121bbda421c14852f8"
+$pkg_filename="powershell-$pkg_version-win7-x64.zip"
+$pkg_bin_dirs=@("bin")
+
+function Invoke-Unpack {
+  Expand-Archive -Path "$HAB_CACHE_SRC_PATH/$pkg_filename" -DestinationPath "$HAB_CACHE_SRC_PATH/$pkg_dirname"
+}
+
+function Invoke-Install {
+  Copy-Item . "$pkg_prefix/bin" -Recurse -Force
+}
+
+function Invoke-Check() {
+  $versionTable = ./powershell.exe -command '$PSVersionTable'
+  $passed = $false
+
+  $versionTable | % {
+    if($_.Trim().StartsWith('GitCommitId')) {
+        $passed = $_.Trim().EndsWith($pkg_version)
+    }
+  }
+
+  if(!$passed) {
+    Write-Error "Check failed to confirm powershell version as $pkg_version"
+  }
+}
