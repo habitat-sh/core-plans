f14dcc4d3dcc78ec13213279d15340b666aaf0c0
Jamie Winsor <jamie@vialstudios.com>
2017-01-17 02:44:42 -0800

    Remove dep on jdk8 in Tomcat8 plan
    Signed-off-by: Jamie Winsor <jamie@vialstudios.com>

--- tomcat8/plan.sh
+++ tomcat8/plan.sh
@@ -1,18 +1,14 @@
 pkg_name=tomcat8
 pkg_description="An open source implementation of the Java Servlet, JavaServer Pages, Java Expression Language and Java WebSocket technologies."
 pkg_origin=core
-pkg_version=8.5.5
+pkg_version=8.5.9
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('Apache-2.0')
 pkg_description="The Apache Tomcat software is an open source implementation of the Java Servlet, JavaServer Pages, Java Expression Language and Java WebSocket technologies."
 pkg_upstream_url="http://tomcat.apache.org/"
-pkg_source=http://apache.mirrors.pair.com/tomcat/tomcat-8/v${pkg_version}/bin/apache-tomcat-${pkg_version}.tar.gz
-pkg_shasum=123ba0c010267d54caf984872d3cb449ae29bbd956fa17467185e0c2b3ecae45
-
-pkg_deps=(
-  core/jdk8
-  core/coreutils
-)
+pkg_source=http://archive.apache.org/dist/tomcat/tomcat-8/v${pkg_version}/bin/apache-tomcat-${pkg_version}.tar.gz
+pkg_shasum=d72234baa373234aa9ed78e8331ac1ce47d2e07a262dafce35d17389825bc8b7
+pkg_deps=(core/coreutils)
 pkg_expose=(8080 8443)
 
 # The default implementation extracts your tarball source file into HAB_CACHE_SRC_PATH. The
