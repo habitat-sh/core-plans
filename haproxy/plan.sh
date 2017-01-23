4ddf51f4b49caa43c48b6d85a2f25ddf44e1e110
Michael Ducy <michael@chef.io>
2017-01-23 18:57:29 -0500

    Bump haproxy to 1.6.11, add status page support
    Signed-off-by: Michael Ducy <michael@chef.io>

--- haproxy/plan.sh
+++ haproxy/plan.sh
@@ -1,14 +1,14 @@
 pkg_origin=core
 pkg_name=haproxy
 pkg_description="The Reliable, High Performance TCP/HTTP Load Balancer"
-pkg_version=1.6.5
+pkg_version=1.6.11
 pkg_maintainer='The Habitat Maintainers <humans@habitat.sh>'
 pkg_license=('GPL-2.0' 'LGPL-2.1')
 pkg_source=http://www.haproxy.org/download/1.6/src/haproxy-${pkg_version}.tar.gz
 pkg_upstream_url="http://git.haproxy.org/git/haproxy-1.6.git/"
-pkg_shasum=c4b3fb938874abbbbd52782087117cc2590263af78fdce86d64e4a11acfe85de
+pkg_shasum=62fe982edb102a9f55205792bc14b0d05745cc7993cd6bee5d73cd3c5ae16ace
 pkg_svc_run='haproxy -f config/haproxy.conf -db'
-pkg_expose=(8080)
+pkg_expose=(80 9000)
 pkg_deps=(core/zlib core/pcre core/openssl)
 pkg_build_deps=(
   core/coreutils
