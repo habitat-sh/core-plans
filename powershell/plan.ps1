82bf4991b6f9231bb9de55b861cb984965c1df7b
Matt Wrock <matt@mattwrock.com>
2017-02-03 18:26:48 -0800

    bumping powershell to 5.0.0-alpha.15
    Signed-off-by: Matt Wrock <matt@mattwrock.com>

--- powershell/plan.ps1
+++ powershell/plan.ps1
@@ -1,13 +1,13 @@
 $pkg_name="powershell"
 $pkg_origin="core"
-$pkg_version="6.0.0-alpha.14"
+$pkg_version="6.0.0-alpha.15"
 $pkg_license=@("MIT")
 $pkg_upstream_url="https://msdn.microsoft.com/powershell"
 $pkg_description="PowerShell is a cross-platform (Windows, Linux, and macOS) automation and configuration tool/framework that works well with your existing tools and is optimized for dealing with structured data (e.g. JSON, CSV, XML, etc.), REST APIs, and object models. It includes a command-line shell, an associated scripting language and a framework for processing cmdlets."
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-$pkg_source="https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-alpha.14/powershell-$pkg_version-win7-x64.zip"
-$pkg_shasum="689e59c8a97a7f6f136104a56be397d9456d46069aa2c1121bbda421c14852f8"
-$pkg_filename="powershell-$pkg_version-win7-x64.zip"
+$pkg_source="https://github.com/PowerShell/PowerShell/releases/download/v$pkg_version/PowerShell_$pkg_version-win7-win2k8r2-x64.zip"
+$pkg_shasum="d3384b9c8c2b5152b113791193a89dfe4ab01ee94caf8f995633906258fd14c3"
+$pkg_filename="powershell-$pkg_version-win7-win2k8r2-x64.zip"
 $pkg_bin_dirs=@("bin")
 
 function Invoke-Unpack {
