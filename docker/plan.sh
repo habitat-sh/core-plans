00db2e1e99749b162824a3263c70a7e64a4a4217
Julian C. Dunn <jdunn@chef.io>
2017-02-14 23:42:51 -0500

    Update to Docker 1.13.1.
    Signed-off-by: Julian C. Dunn <jdunn@chef.io>

--- docker/plan.sh
+++ docker/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=docker
 pkg_description="The Docker Engine"
 pkg_origin=core
-pkg_version=1.12.3
+pkg_version=1.13.1
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('Apache-2')
 pkg_source=https://get.docker.com/builds/Linux/x86_64/${pkg_name}-${pkg_version}.tgz
 pkg_upstream_url=https://docs.docker.com/engine/installation/binaries/
-pkg_shasum=626601deb41d9706ac98da23f673af6c0d4631c4d194a677a9a1a07d7219fa0f
+pkg_shasum=97892375e756fd29a304bd8cd9ffb256c2e7c8fd759e12a55a6336e15100ad75
 pkg_dirname=docker
 pkg_deps=()
 pkg_build_deps=()
