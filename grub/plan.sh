aa0c577f25605ba4534da112072fbfeb9627aa9d
Scott Macfarlane <macfarlane.scott@gmail.com>
2017-02-21 08:29:04 -0800

    Make requested changes

--- grub/plan.sh
+++ grub/plan.sh
@@ -1,27 +1,27 @@
 pkg_name=grub
 pkg_origin=core
 pkg_version=2.02-beta3
-pkg_source=http://git.savannah.gnu.org/cgit/${pkg_name}.git
+pkg_source=https://git.savannah.gnu.org/cgit/${pkg_name}.git
 pkg_shasum=TODO
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="GNU GRUB is a Multiboot boot loader."
 pkg_upstream_url=https://www.gnu.org/software/grub/
-pkg_license=('GPLv3')
+pkg_license=('GPL-3.0')
 pkg_bin_dirs=(bin)
 pkg_build_deps=(
-  core/make
-  core/gcc
-  core/diffutils
-  core/bison
-  core/flex
-  core/binutils
-  core/gettext
-  core/m4
-  core/cacerts
-  core/git
-  core/python
   core/autoconf
   core/automake
+  core/binutils
+  core/bison
+  core/cacerts
+  core/diffutils
+  core/flex
+  core/gcc
+  core/gettext
+  core/git
+  core/m4
+  core/make
+  core/python
 )
 pkg_deps=(core/glibc)
 
@@ -30,25 +30,30 @@ do_download() {
   export GIT_SSL_CAINFO
 
   pushd "${HAB_CACHE_SRC_PATH}"
+  
   if [[ ! -d ${pkg_name} ]]; then
-    git clone $pkg_source ${pkg_name}
+    git clone --branch ${pkg_version} ${pkg_source} ${pkg_name}
+  else 
+    pushd ${pkg_name} 
+    git checkout -f ${pkg_version}
+    popd 
   fi
-  pushd ${pkg_name}
-  git checkout $pkg_version
 
-  sed -i "s/#! \/usr\/bin\/env bash/#!\/bin\/bash/" autogen.sh
-  popd
+  sed -i "s/#! \/usr\/bin\/env bash/#!\/bin\/bash/" ${pkg_name}/autogen.sh
+
   pkg_filename=${pkg_name}-${pkg_version}.tar.bz2
 
   tar -cjf "${HAB_CACHE_SRC_PATH}/${pkg_filename}" \
       --transform "s,^\./grub,grub-${pkg_version}," "./${pkg_name}" \
       --exclude-vcs
-  pkg_shasum=$(trim "$(sha256sum "${HAB_CACHE_SRC_PATH}/${pkg_filename}" | cut -d " " -f 1)")
   popd
+}
+
+do_verify() {
+  return 0
 }
 
 do_build() {
   ./autogen.sh
-  ./configure --prefix="${pkg_prefix}"
-  make
+  do_default_build
 }
