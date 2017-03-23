19955f432a56489e6523972074c5f8f358cd7a26
Fletcher Nichol <fnichol@nichol.ca>
2017-03-23 11:29:50 -0600

    Update to Rust 1.16.0.
    https://blog.rust-lang.org/2017/03/16/Rust-1.16.html
    
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- rust/plan.ps1
+++ rust/plan.ps1
@@ -1,12 +1,12 @@
 $pkg_name="rust"
 $pkg_origin="core"
-$pkg_version="1.15.1"
+$pkg_version="1.16.0"
 $pkg_description="Safe, concurrent, practical language"
 $pkg_upstream_url="https://www.rust-lang.org/"
 $pkg_license=@("Apache-2.0", "MIT")
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_source="https://static.rust-lang.org/dist/rust-$pkg_version-x86_64-pc-windows-msvc.msi"
-$pkg_shasum="2febd69fc10095170c8784309d0118cb3f0c3e57c88ef3029aaa616ab8e4e1ed"
+$pkg_shasum="2dff0d8e445614d6da00aa9931a003658a99ca5bdb8fe43893e0df164953915f"
 $pkg_deps=@("core/visual-cpp-redist-2013", "core/visual-cpp-build-tools-2015")
 $pkg_build_deps=@("core/lessmsi")
 $pkg_bin_dirs=@("bin")
