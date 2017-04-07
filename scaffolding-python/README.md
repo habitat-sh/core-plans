aba42078cf6dc6c422d465ff3b3a66cfdb1a4196
Ryan Hass <rhass@users.noreply.github.com>
2017-04-07 16:15:02 -0700

    Fix python scaffolding and properly support pip.
    This fixes issues with the clean phase which would bomb out in most
    cases. Additionally, we have added support for building packages via
    `pip install`.
    
    Under the covers this leverages venv for Python 3.6, and depends on pip
    in the venv to install the applications with the correct interpreter
    paths for the shebangs.
    
    - Update aws-cli version and use python-scaffolding
    
    This updates the version of aws-cli to the current release and
    leverages scaffolding to provide a working example of how to use the
    python-scaffolding.
    
    - Improve scaffolding-python docs and fix small bug.
    
    This fixes a small bug in the pip arguments constructor whereby
    the package name and package version should not exist in the variable.
    
    Additionally, the documentation has been expanded to reflect the new
    pip features.
    
    - Use venv for setuptools installed packages.
    
    This breaks out the calls to setuptools into a new function and
    leverages venv to ensure the correct relative PYTHONPATH is set when
    running the application.
    
    - Automagically set pkg variables from setup.py.
    
    Added a new `scaffolding_python_prepare` callback which attempts to
    set `pkg_description` and `pkg_upstream_url` if a `setup.py` is found
    and the values are defined.
    
    - Clarify runtime dep resolution for python-scaffolding.
    
    - Port python scaffolding to python2 with virutalenv
    
    This ports the features and functionality from scaffolding-python to
    the scaffolding-python2 package. The primary difference in the
    scaffolding.sh is the default usage of `virtualenv` instead of the
    `venv` which is built-in to Python 3.6 and later.
    
    - Add support for embedded plans to python-scaffolding
    
    This adds support for embedded plans into `habitat` or the root level
    of the source. Some additional refactoring was done to clean up the
    scaffolding to use the new `disable_callbacks` helper function.
    
    Signed-off-by: Ryan Hass <rhass@users.noreply.github.com>

---
+++ scaffolding-python/README.md
@@ -0,0 +1,55 @@
+# Python Scaffolding
+
+## Getting Started with Scaffolding
+See https://www.habitat.sh/docs/concepts-scaffolding/ to learn how to get started with Scaffolding.
+
+## Variables
+| Variable | Type | Value | Default |
+| -------- | ---- | ----- | ------- |
+|`pkg_scaffolding_python_runtime_pkg_name`| **String** | (Optional) Origin and Package Name for Python package which is used for runtime (pkg_deps). | `"core/python"` |
+|`pkg_scaffolding_python_pip_name`| **String** | (Optional) Name of package on pypi which can differ from the hab package name.| `"$pkg_name"` |
+|`pkg_scaffolding_python_pip_version`| **String** | (Optional) Version of package on pypy which can differen from hab package version.| `"$pkg_version"` |
+|`pkg_scaffolding_python_venv_cmd`| **String** | (Optional) Base command to execute for the python virtual env. | `"python -m venv"` |
+
+## Callbacks
+### Scaffolding
+#### scaffolding_python_clean
+If a `setup.py` is found, it attemtps to performs `python setup.py clean --all` to recursively clean the compiled byte code. If one is not found, it will run a find and delete all `*.pyc` and `*.pyo` files.
+
+#### scaffolding_python_build
+This makes multiple calls.
+
+1. Calls `scaffolding_python_venv` to create a venv with symlinks to the python interpreter.
+2. It will attempt to use a `setup.py` and if one is found it will run `scaffolding_python_setuptools_install`. If a `setup.py` is not found this will `return 0` as there is nothing to build.
+
+#### scaffolding_python_prepare
+The default behavior of this callback is to attempt to set the values of `pkg_description` and `pkg_upstream_url` to the values defined in `setup.py` should one be available.
+
+#### scaffolding_python_install
+Installs the application into `$pkg_prefix.`
+
+If a `setup.py` is found, this will call `scaffolding_python_setuptools_install`. If `setup.py` is not found otherwise it will call `scaffolding_python_pip_install` will run against `$pkg_name` and `$pkg_version`. The package name and version on pip can differ from the habitat package name and version, and an override can be specified via the `pkg_scaffolding_python_pip_name` and `pkg_scaffolding_python_pip_version` variables.
+
+#### scaffolding_python_venv
+Creates a venv for the package. This is automatically called for all installs to ensure the interpreter is set correctly.
+
+#### scaffolding_python_pip_install
+Installs packages via pip, and accepts three input parameters.
+ 1. Package name on pypi.
+ 2. Package version on pypi.
+ 3. (Optional) Arguments to pass to pip. Defaults to: "--prefix=$pkg_prefix --force-reinstall --no-compile"
+#### scaffolding_python_setuptools_install
+Builds and installs packages via `setup.py` into a venv. The venv is used to provide a relative path to python and the PYTHONPATH for the installation.
+
+#### scaffolding_python_strip
+Calls `do_default_strip` and then `scaffolding_python_clean`.
+
+### Default Overrides
+The following default callbacks have overrides:
+* `do_default_download`Calls [scaffolding_python_download](#scaffolding_python_download)
+* `do_default_clean` - Calls [scaffolding_python_clean](#scaffolding_python_clean)
+* `do_default_verify` - NOP -- Returns 0
+* `do_default_unpack` - NOP -- Returns 0
+* `do_default_prepare` - Calls [scaffolding_python_prepare](#scaffolding_python_prepare)
+* `do_default_build`- Calls [scaffolding_python_build](#scaffolding_python_build)
+* `do_default_install` - Calls [scaffolding_python_install](#scaffolding_python_install)
--- scaffolding-python/lib/scaffolding.sh
+++ scaffolding-python/lib/scaffolding.sh
@@ -11,62 +11,205 @@
 # }
 
 #
+# Plan Default Variables
+#
+pkg_bin_dirs=(${pkg_bin_dirs[@]:-"bin"})
+pkg_interpreters=(bin/python)
+
+#
 # Scaffolding Variables
 #
 
-scaffolding_python_pkg="core/python"
+# This variable allows a user to set the habitat package name to be different
+# from the package name on pypi if needed.
+pkg_scaffolding_python_pip_name="${pkg_scaffolding_python_pip_name:-$pkg_name}"
+# This variable allows a user to set the habitat package version to be different
+# from the package version on pypi if needed.
+pkg_scaffolding_python_pip_version="${pkg_scaffolding_python_pip_version:-$pkg_version}"
+# Name of python package to add to the build and runtime deps.
+pkg_scaffolding_python_runtime_pkg_name="core/python"
+# Default venv command to use.
+pkg_scaffolding_python_venv_cmd="python -m venv"
 
 #
 # Scaffolding Callback Functions
 #
 
+# Helper function to check for a setup.py file.
+_setup_py_exists() {
+  [[ -f "$SRC_PATH/setup.py" ]]
+}
+
 # The default_begin phase is executed prior to loading the scaffolding. As such
 # we have a scaffolding specific callback to allow us to run anything we need
 # to execute before download and build. This callback executes immediately
 # after the scaffolding is loaded.
-scaffolding_load() {
-  pkg_deps=(${pkg_deps[@]} $scaffolding_python_pkg)
-}
-
-
-# Recursively clean GOPATH and dependencies.
-do_default_clean() {
-  scaffolding_python_clean
-}
-
-scaffolding_python_clean() {
-  pushd "$pkg_dirname"
-  if [[ -f "setup.py" ]]; then
-    python setup.py clean --all
-  else
-    find . \( -type f -name "*.pyc" -or -name "*.pyo" \) -delete
+_scaffolding_load() {
+  pkg_deps=(${pkg_deps[@]} $pkg_scaffolding_python_runtime_pkg_name)
+  if _resolve_run_dependencies; then
+    build_line "Resolved Scaffolding Runtime Dependencies"
   fi
-  popd
+
+  if [[ "$pkg_source" == "pip" ]]; then
+    _scaffolding_python_pip_only="true"
+  fi
 }
 
+# Attempt to retrieve values from setup.py so we can assign the return value to
+# package variables.
+# Accepts option names without the preceeding `--` part.
+# Returns 0 for valid options.
+# Returns 5 for non-existant setup.py, undefined setup parameters, or invalid
+# arguments. This is so that the default `trap` does not run `_on_exit` since
+# we want to check the return of this function when it is used.
+_scaffolding_python_fetch_setup_info() {
+  local option
+  local value
+  option="$1"
+  if _setup_py_exists; then
+    value="$(python setup.py --"$option")"
+    # Python with emit a non-zero return status if setup.py does not exist, or
+    # if the option passed is invalid.
+    # For valid but undefined values it will return "UNKNOWN", with the
+    # exception of `--version` for which it will return `0.0.0`.
+    if [[ $? -ne 0 ]] || [[ "$value" == "UNKNOWN" ]] || [[ "$value" == "0.0.0" ]]; then
+      return 5
+    else
+      printf "%s" "$value"
+    fi
+  fi
+}
+
+# Fetch and assign the pkg_upstream_url if it is not set in the plan, and is
+# defined in the setup.py.
+_scaffolding_python_setup_pkg_upstream_url() {
+  local url
+  url="$(_scaffolding_python_fetch_setup_info "url")"
+
+  if [[ $? -eq 0 ]]; then
+    build_line "Setting pkg_upstream_url=$url"
+    pkg_upstream_url=${pkg_upstream_url:-"$url"}
+  fi
+}
+
+# Fetch and assign the pkg_description if it is not set in the plan, and is
+# defined in the setup.py. Attempts to use `long_description`, and falls back
+# to description if `long_description` is undefined in the setup.py.
+_scaffolding_python_setup_pkg_description() {
+  local name
+  description="$(_scaffolding_python_fetch_setup_info "long-description" || _scaffolding_python_fetch_setup_info "description")"
+
+  if [[ $? -eq 0 ]]; then
+    build_line "Setting pkg_description=$description"
+    pkg_description=${pkg_description:-"$description"}
+  fi
+}
+scaffolding_python_venv() {
+  if [[ ! -f "$pkg_prefix/bin/activate" ]]; then
+    build_line "Creating package venv"
+    $pkg_scaffolding_python_venv_cmd \
+      --symlinks \
+      "$pkg_prefix"
+  fi
+
+  if source "$pkg_prefix/bin/activate"; then
+    build_line "Activated venv"
+  fi
+}
+
+
+# Attempts to automatically set various pkg_* variables.
+scaffolding_python_prepare() {
+  _scaffolding_python_setup_pkg_description
+  _scaffolding_python_setup_pkg_upstream_url
+}
+
+# Removes bytecode and other stuff we do not need to package up.
+scaffolding_python_clean() {
+  if [[ $(_setup_py_exists) ]]; then
+    pushd "$SRC_PATH" >/dev/null
+    python setup.py clean --all
+    popd >/dev/null
+  else
+    find "$SRC_PATH" \( -type f -name "*.pyc" -or -name "*.pyo" \) -delete
+  fi
+}
+
+# Run setup.py if it exists, otherwise return so pip can do the install.
 scaffolding_python_build() {
-  python setup.py build
+  scaffolding_python_venv
+  if [[ $(_setup_py_exists) ]]; then
+    build_line "Running setup.py"
+    "$pkg_prefix/bin/python" setup.py build --executable="$pkg_prefix/bin/python"
+  else
+    return 0
+  fi
 }
 
+# This can be called directly to install multiple pip packages.
+# Usage: `scaffolding_python_pip_install "mypackage "myversion"`
+# OR `scaffolding_python_pip_install "mypackage" "myversion" "--other-pip-arg"`
 scaffolding_python_pip_install() {
   local name
   local version
+  local args
   name="$1"
   version="$2"
+  args="${3:-"--prefix=$pkg_prefix --force-reinstall --no-compile"}"
 
-  pip install "$name==$version"
+  if [[ -n "$DEBUG" ]]; then
+    args="$args -vvv"
+  else
+    args="$args --quiet"
+  fi
+
+  build_line "Running pip install"
+  # Disable shellcheck rule to ensure the pip install command is expanded
+  # with the correct syntax.
+  # shellcheck disable=SC2086
+  pip install $name==$version $args
+  # Write out versions of all pip packages to package
+  pip freeze > "$pkg_prefix/requirements.txt"
 }
 
-scaffolding_python_install() {
-  if [[ -f "${pkg_dirname}/setup.py" ]]; then
-    python setup.py install \
-      --prefix="$pkg_prefix" \
+# Execute `setup.py install` and attempts to not create zip file eggs.
+# Accepts optional argument to overide the default `install_args`.
+# shellcheck disable=SC2120
+scaffolding_python_setuptools_install() {
+    local install_cmd
+    local install_args
+    install_cmd="$pkg_prefix/bin/python setup.py install"
+    install_args=(${1:-"--prefix=$pkg_prefix"})
+
+    if [[ -z "$DEBUG" ]]; then
+      install_args+=("--quiet")
+    else
+      install_args+=("--verbose")
+    fi
+
+    $install_cmd \
+      "${install_args[@]}" \
       --old-and-unmanageable || \
-    python setup.py install \
-      --prefix="$pkg_prefix"
+    $install_cmd \
+      "${install_args[@]}"
+}
+
+# Attempt to install the python package via setup.py without egg files.
+# We do not need the eggs since we are already keeping everything contained
+# within the hab package.
+scaffolding_python_install() {
+  if [[ -n "$_scaffolding_python_pip_only" ]]; then
+    scaffolding_python_pip_install "$pkg_scaffolding_python_pip_name" "$pkg_scaffolding_python_pip_version"
+  else
+    # shellcheck disable=SC2119
+    scaffolding_python_setuptools_install
   fi
 }
 
+# Run do_default_strip to ensure any native C extentions are stripped.
+# After this completes, we clean the bytecode since we don't need to keep it
+# in the hab package and remove tests since they won't be executed at this
+# stage.
 scaffolding_python_strip() {
  do_default_strip
  scaffolding_python_clean
@@ -75,6 +218,10 @@ scaffolding_python_strip() {
  find "$pkg_prefix" \
    \( -type d -name test -o -name tests \) \
    -exec rm -rf '{}' +
+}
+
+do_default_prepare() {
+  scaffolding_python_prepare
 }
 
 do_default_build() {
