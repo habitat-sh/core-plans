736a48783d199f640900ad1a5310bf25e3c90e8f
Nathan L Smith <smith@chef.io>
2017-04-08 23:43:10 -0500

    Scaffolding scaffolding to scaffold the scaffolding
    All of the scaffolding plans look a lot alike. Here is a scaffolding for
    them.
    
    Signed-off-by: Nathan L Smith <smith@chef.io>

---
+++ scaffolding-base/plan.sh
@@ -0,0 +1,26 @@
+pkg_name=scaffolding-base
+pkg_origin=core
+pkg_version="0.1.0"
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_license=('Apache-2.0')
+pkg_source=nope
+
+do_download() {
+  return 0
+}
+
+do_verify() {
+  return 0
+}
+
+do_unpack() {
+  return 0
+}
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  install -D -m 0644 "$PLAN_CONTEXT/lib/scaffolding.sh" "$pkg_prefix/lib/scaffolding.sh"
+}
