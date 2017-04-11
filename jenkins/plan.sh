f863244acc560580e57ce24ae747a81adcbe6ece
Paul Adams <paul.adams@endocode.com>
2017-04-11 10:35:31 +0200

    Add the package description

--- jenkins/plan.sh
+++ jenkins/plan.sh
@@ -2,6 +2,7 @@ pkg_origin=endocode
 pkg_name=jenkins
 pkg_version=2.46.1
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_description="The leading open source automation server, Jenkins provides hundreds of plugins to support building, deploying and automating any project."
 pkg_license=('mit')
 pkg_upstream_url=https://jenkins.io/
 pkg_source=http://mirrors.jenkins.io/war-stable/${pkg_version}/jenkins.war
