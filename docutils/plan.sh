1298f87fe1e78889559cf09cfe2c859219269e48
Fletcher Nichol <fnichol@nichol.ca>
2017-04-11 23:53:46 +0000

    [docutils] Update out of date pkg_source URL.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- docutils/plan.sh
+++ docutils/plan.sh
@@ -8,7 +8,7 @@ pkg_license=(
   'Python-2.0',
   'Docutils Public Domain Dedication'
 )
-pkg_source=http://heanet.dl.sourceforge.net/project/docutils/docutils/${pkg_version}/docutils-${pkg_version}.tar.gz
+pkg_source=https://downloads.sourceforge.net/project/${pkg_name}/${pkg_name}/${pkg_version}/${pkg_name}-${pkg_version}.tar.gz
 pkg_shasum=c7db717810ab6965f66c8cf0398a98c9d8df982da39b4cd7f162911eb89596fa
 pkg_deps=()
 pkg_build_deps=(
