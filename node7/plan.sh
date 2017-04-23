f2a3acaabe44e082f17e050e34bb0cb8e12d0f87
Jamie Winsor <jamie@vialstudios.com>
2017-04-23 13:40:59 -0700

    Fix node7 checksum
    Signed-off-by: Jamie Winsor <jamie@vialstudios.com>

--- node7/plan.sh
+++ node7/plan.sh
@@ -2,7 +2,7 @@ source "../node/plan.sh"
 
 pkg_name=node7
 pkg_version=7.6.0
-pkg_shasum=809E80265E332FE1A8268E5A73EB219C356810FE86C69FD2D931C52E07211970
+pkg_shasum=809e80265e332fe1a8268e5a73eb219c356810fe86c69fd2d931c52e07211970
 pkg_source=https://nodejs.org/dist/v${pkg_version}/node-v${pkg_version}.tar.gz
 
 # the archive contains a 'v' version # prefix, but the default value of
