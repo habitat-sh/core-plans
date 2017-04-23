ab61da84456f0aec1b94af1cc001477bab88b818
John Kerry <jk185160@ncr.com>
2017-04-23 13:07:46 -0400

    upping the node7 version to 7.6.0
    Signed-off-by: John Kerry <jk185160@ncr.com>

--- node7/plan.sh
+++ node7/plan.sh
@@ -1,8 +1,8 @@
 source "../node/plan.sh"
 
 pkg_name=node7
-pkg_version=7.5.0
-pkg_shasum=0da8e0288b5c0f136e650b7119219968720caf88b5a67ef0591555113f0844c2
+pkg_version=7.6.0
+pkg_shasum=809E80265E332FE1A8268E5A73EB219C356810FE86C69FD2D931C52E07211970
 pkg_source=https://nodejs.org/dist/v${pkg_version}/node-v${pkg_version}.tar.gz
 
 # the archive contains a 'v' version # prefix, but the default value of
