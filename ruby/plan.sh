b134d594efccaa55c187199ba667afbcd07828d1
Fletcher Nichol <fnichol@nichol.ca>
2017-04-23 23:02:48 -0600

    [ruby] Update to version 2.3.4.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- ruby/plan.sh
+++ ruby/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=ruby
 pkg_origin=core
-pkg_version=2.3.3
+pkg_version=2.3.4
 pkg_description="A dynamic, open source programming language with a focus on \
   simplicity and productivity. It has an elegant syntax that is natural to \
   read and easy to write."
@@ -8,7 +8,7 @@ pkg_license=("Ruby")
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source=https://cache.ruby-lang.org/pub/${pkg_name}/${pkg_name}-${pkg_version}.tar.gz
 pkg_upstream_url=https://www.ruby-lang.org/en/
-pkg_shasum=241408c8c555b258846368830a06146e4849a1d58dcaf6b14a3b6a73058115b7
+pkg_shasum=98e18f17c933318d0e32fed3aea67e304f174d03170a38fd920c4fbe49fec0c3
 pkg_deps=(core/glibc core/ncurses core/zlib core/openssl core/libyaml core/libffi core/readline)
 pkg_build_deps=(core/coreutils core/diffutils core/patch core/make core/gcc core/sed)
 pkg_lib_dirs=(lib)
