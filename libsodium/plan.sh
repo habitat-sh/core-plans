d5c2ecd4ed5010c6298c7f3a64bca3ac340fd088
Justin Carter <justin@starkandwayne.com>
2017-05-07 19:34:35 +0200

    Update libsodium to 1.0.11
    Signed-off-by: Justin Carter <justin@starkandwayne.com>

--- libsodium/plan.sh
+++ libsodium/plan.sh
@@ -1,11 +1,11 @@
 pkg_name=libsodium
 pkg_distname=$pkg_name
 pkg_origin=core
-pkg_version=1.0.8
+pkg_version=1.0.11
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('libsodium')
 pkg_source=https://download.libsodium.org/libsodium/releases/${pkg_name}-${pkg_version}.tar.gz
-pkg_shasum=c0f191d2527852641e0a996b7b106d2e04cbc76ea50731b2d0babd3409301926
+pkg_shasum=a14549db3c49f6ae2170cbbf4664bd48ace50681045e8dbea7c8d9fb96f9c765
 pkg_dirname=${pkg_distname}-${pkg_version}
 pkg_deps=(core/glibc)
 pkg_build_deps=(core/autoconf core/automake core/diffutils core/patch core/make core/gcc core/sed)
