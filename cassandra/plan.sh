62a7e087e68d963d6a5296dcfa9be687ffa72e60
Maxime Vidori <maxime.vidori@container-solutions.com>
2017-05-08 14:01:05 +0200

    Default configuration for Cassandra
    Only single node for the moment. Default configuration is set up, minimal
    config for habitat.
    
    Signed-off-by: Maxime Vidori <maxime.vidori@container-solutions.com>

---
+++ cassandra/plan.sh
@@ -0,0 +1,36 @@
+pkg_name=apache-cassandra
+pkg_origin=core
+pkg_version=3.10
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_description="Open source distributed realtime computation system."
+pkg_upstream_url="http://storm.apache.org/index.html"
+pkg_license=("Apache-2.0")
+pkg_source="http://apache.40b.nl/cassandra/${pkg_version}/${pkg_name}-${pkg_version}-bin.tar.gz"
+pkg_shasum=c09c3f92d4f80d5639e3f1624c9eec45d25793bbb6b3e3640937b68a9c6d107f
+pkg_deps=(
+  core/jre8
+  core/python
+)
+pkg_bin_dirs=(bin)
+pkg_lib_dirs=(lib)
+pkg_exports=(
+  [http-port]=network.port
+  [transport-port]=transport.port
+)
+pkg_exposes=(http-port transport-port)
+
+do_build() {
+  return 0
+}
+
+do_strip() {
+  return 0
+}
+
+do_install() {
+  install -vDm644 LICENSE.txt "$pkg_prefix/LICENSE.txt"
+  install -vDm644 NOTICE.txt "$pkg_prefix/NOTICE.txt"
+
+  cp -a bin lib  "$pkg_prefix"
+  rm "$pkg_prefix/bin/"*.bat
+}
