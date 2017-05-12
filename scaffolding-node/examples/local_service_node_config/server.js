f0be3cb2da9a8cf2438d679b1b7e0d1935f303a0
Fletcher Nichol <fnichol@nichol.ca>
2017-05-11 22:01:13 -0600

    [scaffolding-node] Updates to Node Scaffolding package.
    Node Scaffolding is a Habitat package which helps you build your
    Node.js-based **web applications**, **services**, and **processes**
    (hereafter referred to as "apps") into a package which runs consistently
    on a wide range of containers, virtual machines, or servers via the
    Habitat Supervisor. The Supervisor will facilitate clustering, discovery
    of external services such as databases, dynamically update configuration
    and credentials, coordinate reliable rolling updates, and a lot more!
    
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- scaffolding-node/examples/local_service_node_config/server.js
+++
@@ -1,7 +0,0 @@
-var config = require("config")
-
-require("http").createServer(function (request, response) {
-  response.end("Hello World\n");
-}).listen(config.get("port"))
-
-console.log("Server listening on " + config.get("port"));
