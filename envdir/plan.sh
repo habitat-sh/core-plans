d3c83505186edc042d65e298386a37d1237ca2ac
Justin Carter <justin@starkandwayne.com>
2017-05-16 09:24:21 +0200

    Add wal-e to core plans
    Signed-off-by: Justin Carter <justin@starkandwayne.com>

---
+++ envdir/plan.sh
@@ -0,0 +1,37 @@
+pkg_name=envdir
+pkg_version=0.7.0
+pkg_origin=core
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_license=('MIT')
+pkg_description="Envdir runs another program with a modified environment according to files in a specified directory."
+pkg_upstream_url="https://github.com/jezdez/envdir"
+pkg_source=https://github.com/jezdez/envdir/archive/${pkg_version}.tar.gz
+pkg_deps=(core/python/3.5.2)
+pkg_bin_dirs=(bin)
+
+do_download() {
+  return 0
+}
+
+do_verify() {
+  return 0
+}
+
+do_unpack() {
+  return 0
+}
+
+do_prepare() {
+  pyvenv "$pkg_prefix"
+  source "$pkg_prefix/bin/activate"
+}
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  pip install "$pkg_name==$pkg_version"
+  # Write out versions of all pip packages to package
+  pip freeze > "$pkg_prefix/requirements.txt"
+}
