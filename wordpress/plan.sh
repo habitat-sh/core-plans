c7f15af72cb910161d073afd04e43ea40bfdb697
ramonskie <ramonmakkelie@gmail.com>
2017-05-16 13:08:04 +0200

    redo changes
    Signed-off-by: ramonskie <ramonmakkelie@gmail.com>

--- wordpress/plan.sh
+++ wordpress/plan.sh
@@ -9,6 +9,7 @@ pkg_license=('Apache-2.0')
 pkg_source="https://wordpress.org/${pkg_name}-${pkg_version}.tar.gz"
 pkg_shasum="c11ce7580f21dfbca70dd6f817d3376385be6d34cf4d86f233eae3acb5fd87fd"
 pkg_description="installs wordpress"
+pkg_upstream_url="https://wordpress.org/"
 
 source_dir=$HAB_CACHE_SRC_PATH/${pkg_name}
 
