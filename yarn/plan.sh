f54b9f58d5d12f286fe92073fcfc238058d6ab11
Fletcher Nichol <fnichol@nichol.ca>
2017-05-18 16:29:02 -0600

    [yarn] Update to 0.24.5.
    https://github.com/yarnpkg/yarn/releases/tag/v0.24.5
    
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- yarn/plan.sh
+++ yarn/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=yarn
 pkg_origin=core
-pkg_version=0.24.3
+pkg_version=0.24.5
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="Yarn is a package manager for your code. It allows you to use and share code with other developers from around the world. Yarn does this quickly, securely, and reliably so you don’t ever have to worry."
 pkg_upstream_url=https://yarnpkg.com/
 pkg_license=('BSD-2-Clause')
 pkg_source="https://yarnpkg.com/downloads/$pkg_version/yarn-v$pkg_version.tar.gz"
-pkg_shasum=0bcb35837d089fe3ae66448284971518f2a6c8e3257209e26a9496c9b8d5b353
+pkg_shasum=a7492431eedee0203faeac64e75f484e4911ab707c96c5bee4c7b97bf19c102c
 pkg_bin_dirs=(bin)
 pkg_build_deps=()
 pkg_deps=(
