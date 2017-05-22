f1b3308e3b4dbcdce220912fe356c62bb9828aea
Justin Carter <justin@starkandwayne.com>
2017-05-22 14:09:38 +0200

    Update hab svc load command for latest hab-sup
    Signed-off-by: Justin Carter <justin@starkandwayne.com>

--- postgresql/config/functions.sh
+++ postgresql/config/functions.sh
@@ -94,7 +94,7 @@ bootstrap_replica_via_wale() {
 
 stop_wale_service() {
   if hab sup status | grep wal-e; then
-    hab service unload core/wal-e
+    hab svc unload core/wal-e
   fi
 }
 
@@ -102,5 +102,5 @@ start_wale_service() {
   echo "Starting wal-e to perform regular backups"
   mkdir -p /hab/svc/wal-e/
   cp {{pkg.svc_config_path}}/wal-e.toml /hab/svc/wal-e/user.toml
-  hab service load core/wal-e --group {{svc.group}}
+  hab svc load core/wal-e --group {{svc.group}}
 }
