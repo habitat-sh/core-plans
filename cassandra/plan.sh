1c6e355b563f04cfc532af334bb9d6538e86080e
Maxime Vidori <maxime.vidori@container-solutions.com>
2017-05-24 10:21:08 +0200

    Use bindings instead of using whole members
    Signed-off-by: Maxime Vidori <maxime.vidori@container-solutions.com>

--- cassandra/plan.sh
+++ cassandra/plan.sh
@@ -1,11 +1,11 @@
-pkg_name=apache-cassandra
+pkg_name=cassandra
 pkg_origin=core
 pkg_version=3.10
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="Open source distributed realtime computation system."
 pkg_upstream_url="https://cassandra.apache.org/"
 pkg_license=("Apache-2.0")
-pkg_source="http://apache.40b.nl/cassandra/${pkg_version}/${pkg_name}-${pkg_version}-bin.tar.gz"
+pkg_source="http://apache.40b.nl/cassandra/${pkg_version}/apache-${pkg_name}-${pkg_version}-bin.tar.gz"
 pkg_shasum=c09c3f92d4f80d5639e3f1624c9eec45d25793bbb6b3e3640937b68a9c6d107f
 pkg_deps=(
   core/jre8
@@ -13,6 +13,7 @@ pkg_deps=(
 )
 pkg_bin_dirs=(bin)
 pkg_lib_dirs=(lib)
+pkg_dirname=apache-${pkg_name}-${pkg_version}
 
 # thrift port is for legacy client connection, need to be enabled to be used
 pkg_exports=(
@@ -23,6 +24,10 @@ pkg_exports=(
 pkg_exposes=(
   port
   # don't expose "ssl-port", "thrift-port" as they may be non existent depending on config
+)
+
+pkg_binds=(
+  [cassandra]=""
 )
 
 do_build() {
