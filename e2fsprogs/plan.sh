f69a5b18684409ec88442fa49b6691eae6cf91a4
Scott Macfarlane <macfarlane.scott@gmail.com>
2017-05-23 21:23:20 -0700

    Add packages to support image exporter
    Signed-off-by: Scott Macfarlane <macfarlane.scott@gmail.com>

---
+++ e2fsprogs/plan.sh
@@ -0,0 +1,10 @@
+pkg_name=e2fsprogs
+pkg_origin=core
+pkg_version="1.43.4"
+pkg_source="https://git.kernel.org/pub/scm/fs/ext2/e2fsprogs.git/snapshot/e2fsprogs-${pkg_version}.tar.gz"
+pkg_shasum="206d27b4ea64fdee562d10a3d08ce213d2b2b76be69d65a9e7da6de4c67c28e5"
+pkg_deps=(core/glibc)
+pkg_build_deps=(core/make core/gcc)
+pkg_lib_dirs=(lib)
+pkg_include_dirs=(include)
+pkg_bin_dirs=(bin sbin)
