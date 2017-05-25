d60f9c2a5f373614644237f46457e99b6833d933
Scott Macfarlane <macfarlane.scott@gmail.com>
2017-05-25 08:18:02 -0700

    Remove unneccesary functions, update make to use nproc
    Signed-off-by: Scott Macfarlane <macfarlane.scott@gmail.com>

--- linux/plan.sh
+++ linux/plan.sh
@@ -8,38 +8,11 @@ pkg_shasum="1ece864a5b0fbf448f6e01439968de7476c3bf57595c1c74cb96e9a2e3adbd0f"
 pkg_deps=(core/glibc)
 pkg_build_deps=(core/make core/gcc core/perl core/elfutils core/bc core/diffutils)
 
-do_begin() {
-  return 0
-}
-
-do_download() {
-  do_default_download
-}
-
-do_verify() {
-  do_default_verify
-}
-
-do_clean() {
-  do_default_clean
-}
-
-do_unpack() {
-  do_default_unpack
-}
-
-do_prepare() {
-  return 0
-}
 
 do_build() {
   make defconfig INSTALL_PATH="$pkg_prefix"
   sed "s/=m/=y/" -i .config
-  make
-}
-
-do_check() {
-  return 0
+  make -j$(nproc)
 }
 
 do_install() {
@@ -47,12 +20,3 @@ do_install() {
   mkdir -p ${pkg_prefix}/boot
   cp -a arch/x86/boot/bzImage ${pkg_prefix}/boot/
 }
-
-do_strip() {
-  do_default_strip
-}
-
-do_end() {
-  return 0
-}
-
