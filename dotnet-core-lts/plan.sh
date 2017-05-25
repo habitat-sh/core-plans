198beaba31eb3990da280a2e643d143544a22b07
W. Duncan Fraser <duncan@wduncanfraser.com>
2017-05-25 13:38:31 -0500

    Created LTS release channel plans for dotnet core runtime and sdk
    Signed-off-by: W. Duncan Fraser <duncan@wduncanfraser.com>

---
+++ dotnet-core-lts/plan.sh
@@ -0,0 +1,54 @@
+pkg_name=dotnet-core-lts
+pkg_origin=core
+pkg_version=1.0.5
+pkg_license=('MIT')
+pkg_upstream_url=https://www.microsoft.com/net/core
+pkg_description=".NET Core is a blazing fast, lightweight and modular platform
+  for creating web applications and services that run on Windows,
+  Linux and Mac. LTS release channel."
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_source="https://dotnetcli.blob.core.windows.net/dotnet/Runtime/${pkg_version}/dotnet-debian-x64.${pkg_version}.tar.gz"
+pkg_shasum=55481b0254a72d8c342ba6ccca3908ffb5c99d7eeb54f83dec6cc93c6b4cc3ae
+pkg_filename="dotnet-debian-x64.${pkg_version}.tar.gz"
+pkg_deps=(
+  core/curl
+  core/gcc-libs
+  core/glibc
+  core/icu52
+  core/krb5
+  core/libunwind
+  core/lttng-ust
+  core/openssl
+  core/util-linux
+  core/zlib
+)
+pkg_build_deps=(
+  core/patchelf
+)
+pkg_bin_dirs=(bin)
+
+do_unpack() {
+  # Extract into $pkg_dirname instead of straight into $HAB_CACHE_SRC_PATH.
+  mkdir -p "$HAB_CACHE_SRC_PATH/$pkg_dirname"
+  tar xfz "$HAB_CACHE_SRC_PATH/$pkg_filename" -C "$HAB_CACHE_SRC_PATH/$pkg_dirname"
+}
+
+do_prepare() {
+  find -type f -name 'dotnet' \
+    -exec patchelf --interpreter "$(pkg_path_for glibc)/lib/ld-linux-x86-64.so.2" --set-rpath "$LD_RUN_PATH" {} \;
+  find -type f -name '*.so*' \
+    -exec patchelf --set-rpath "$LD_RUN_PATH" {} \;
+}
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  cp -a . "$pkg_prefix/bin"
+  chmod o+r -R "$pkg_prefix/bin"
+}
+
+do_strip() {
+  return 0
+}
