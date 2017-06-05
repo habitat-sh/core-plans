4ce0a264ff99fa2da7391c40fbc56d1acf55413a
John Jelinek <jjelinek@containerstore.com>
2017-06-05 16:27:14 -0500

    Bump gradle version to 3.5.0
    Signed-off-by: John Jelinek <jjelinek@containerstore.com>

--- gradle/plan.sh
+++ gradle/plan.sh
@@ -1,8 +1,8 @@
 pkg_name=gradle
 pkg_origin=core
-pkg_version=3.2.1
+pkg_version=3.5
 pkg_source=https://services.gradle.org/distributions/${pkg_name}-${pkg_version}-bin.zip
-pkg_shasum=9843a3654d3e57dce54db06d05f18b664b95c22bf90c6becccb61fc63ce60689
+pkg_shasum=0b7450798c190ff76b9f9a3d02e18b33d94553f708ebc08ebe09bdf99111d110
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="A powerful build system for the JVM"
 pkg_upstream_url=http://gradle.org
@@ -15,10 +15,10 @@ pkg_deps=(core/glibc core/jre8 core/coreutils core/bash-static core/gcc-libs cor
 do_build() {
   mkdir patching
   pushd patching
-  jar xf ../lib/native-platform-linux-amd64-0.11.jar
+  jar xf ../lib/native-platform-linux-amd64-0.14.jar
   patchelf --set-rpath "${LD_RUN_PATH}" net/rubygrapefruit/platform/linux-amd64/libnative-platform.so
-  jar cf native-platform-linux-amd64-0.11.jar .
-  mv native-platform-linux-amd64-0.11.jar ../lib/
+  jar cf native-platform-linux-amd64-0.14.jar .
+  mv native-platform-linux-amd64-0.14.jar ../lib/
   popd
   rm -rf patching
   fix_interpreter bin/gradle core/coreutils bin/env
