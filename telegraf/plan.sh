4cf5ce0e2714a14b2620718987a26b2e2ec48f57
Jake Plimack <jake.plimack@gmail.com>
2017-06-27 16:33:32 -0600

    telegraf package
    Signed-off-by: Jake Plimack <jake.plimack@gmail.com>

--- telegraf/plan.sh
+++ telegraf/plan.sh
@@ -7,11 +7,32 @@ pkg_description="telegraf - client for InfluxDB"
 pkg_upstream_url="https://github.com/influxdata/telegraf/"
 pkg_scaffolding="core/scaffolding-go"
 pkg_svc_run="bin/telegraf"
+pkg_bin_dirs=(bin)
+
+do_begin() {
+    export GOBIN="${GOPATH}/bin"
+}
 
 do_download() {
     git clone --branch "${pkg_version}" https://github.com/influxdata/telegraf || true
     cd telegraf && \
         git pull origin $pkg_version
+}
+
+do_verify() {
+    do_default_verify
+}
+
+do_clean() {
+    return 0
+}
+
+do_unpack() {
+    do_default_unpack
+}
+
+do_prepare() {
+    return 0
 }
 
 do_build() {
@@ -20,7 +41,19 @@ do_build() {
     make
 }
 
+do_check() {
+    return 0
+}
+
 do_install() {
-    mkdir -p $pkg_prefix/bin
-    cp /bin/telegraf $pkg_prefix/bin
+    mkdir -p ${pkg_prefix}/bin
+    cp /bin/telegraf ${pkg_prefix}/bin
+}
+
+do_strip() {
+    do_default_strip
+}
+
+do_end() {
+    return 0
 }
