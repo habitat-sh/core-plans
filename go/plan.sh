1b26331106b07fd08e60045753f9e1b66541801f
Chris Alfano <chris@jarv.us>
2017-08-11 01:57:03 -0400

    Bump go version to 1.8.3
    Signed-off-by: Chris Alfano <chris@jarv.us>

--- go/plan.sh
+++ go/plan.sh
@@ -1,13 +1,13 @@
 pkg_name=go
 pkg_origin=core
-pkg_version=1.8
+pkg_version=1.8.3
 pkg_description="Go is an open source programming language that makes it easy to
   build simple, reliable, and efficient software."
 pkg_upstream_url=https://golang.org/
 pkg_license=('BSD')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source=https://storage.googleapis.com/golang/go${pkg_version}.src.tar.gz
-pkg_shasum=406865f587b44be7092f206d73fc1de252600b79b3cacc587b74b5ef5c623596
+pkg_shasum=5f5dea2447e7dcfdc50fa6b94c512e58bfba5673c039259fd843f68829d99fa6
 pkg_dirname=go
 pkg_deps=(core/glibc core/iana-etc core/cacerts)
 pkg_build_deps=(core/coreutils core/inetutils core/bash core/patch core/gcc core/go17 core/perl)
