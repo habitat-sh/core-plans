0f9605302f84475431c5363be3e36fbfb8a45fd8
Christian Nunciato <cnunciato@chef.io>
2017-08-11 16:29:08 -0700

    Update core/node to latest LTS (6.11.2)
    Signed-off-by: Christian Nunciato <cnunciato@chef.io>

--- node/plan.sh
+++ node/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=node
 pkg_origin=core
-pkg_version=6.10.3
+pkg_version=6.11.2
 pkg_description="Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine."
 pkg_upstream_url=https://nodejs.org/
 pkg_license=('MIT')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source=https://nodejs.org/dist/v${pkg_version}/node-v${pkg_version}.tar.gz
-pkg_shasum=a8f679f595fd921305c28d126935ad59b4419ac8474a99997a31e01ab50acd3d
+pkg_shasum=20146ed51b638404665737ed8a25cc06e96d7d7259eb90a4bdec4730a78002a6
 pkg_deps=(core/glibc core/gcc-libs core/coreutils)
 pkg_build_deps=(core/python2 core/gcc core/grep core/make)
 pkg_bin_dirs=(bin)
