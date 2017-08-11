c120b9c47214f3991586a225b6f7ee3204321284
jtimberman <joshua@chef.io>
2017-08-11 10:28:11 -0600

    Add telegraf plan using static binary
    This plan builds on the work started by @kplimack in #639.
    
    Telegraf project provides precompiled static binaries.  They're
    referenced and linked on the Github project "releases" page rather
    than the Influx project portal's "downloads" page for telegraf. The
    "downloads" page has only the dynamically linked binary, which has a
    libc interpreter outside aa path that Habitat knows about (the
    standard /lib location for Linux 64 bit machines).
    
    This plan will make use of the statically compiled binary so we
    don't have to:
    
    1. Build from source, or
    2. Use patchelf to change the interpreter of the dynamically built
       binary
    
    As telegraf has highly customizable config, it's not maintainable to
    template the configuration, so it is recommended users create a
    package that depends on this one. This is described in greater detail
    in the README.md.
    
    Signed-off-by: Joshua Timberman <joshua@chef.io>

---
+++ telegraf/plan.sh
@@ -0,0 +1,21 @@
+pkg_name=telegraf
+pkg_origin=core
+pkg_version="1.3.5"
+pkg_license=('MIT')
+pkg_description="telegraf - client for InfluxDB"
+pkg_upstream_url="https://github.com/influxdata/telegraf/"
+pkg_source="https://dl.influxdata.com/${pkg_name}/releases/${pkg_name}-${pkg_version}-static_linux_amd64.tar.gz"
+pkg_shasum="3c44bf10fc689e723c6f62f28babd8317331af507d1178100cdb16ee15b5a490"
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_svc_run="telegraf --config $pkg_svc_config_path/telegraf.conf"
+pkg_build_deps=(core/wget core/tar)
+pkg_deps=()
+pkg_bin_dirs=(bin)
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  install -vD "${HAB_CACHE_SRC_PATH}/telegraf/telegraf" "${pkg_prefix}/bin/telegraf"
+}
