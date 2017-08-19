5eeec66267caf103c81937347ec77ef34f762352
Scott Hain <shain@chef.io>
2017-08-18 18:59:30 -0700

    Update tomcat7 def, still a WIP
    Signed-off-by: Scott Hain <shain@chef.io>

--- tomcat7/plan.sh
+++ tomcat7/plan.sh
@@ -6,52 +6,28 @@ pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('Apache-2.0')
 pkg_description="The Apache Tomcat software is an open source implementation of the Java Servlet, JavaServer Pages, Java Expression Language and Java WebSocket technologies."
 pkg_upstream_url="http://tomcat.apache.org/"
-pkg_source=http://archive.apache.org/dist/tomcat/tomcat-7/v${pkg_version}/bin/apache-tomcat-${pkg_version}.tar.gz
+pkg_filename=apache-tomcat-${pkg_version}.tar.gz
+pkg_source=http://archive.apache.org/dist/tomcat/tomcat-7/v${pkg_version}/bin/${pkg_filename}.tar.gz
+pkg_dirname=apache-tomcat-${pkg_version}
 pkg_shasum=0e814d6ad7d5b90e29c79887137420d3bc413540f9faa60d98f11e6c8a8fea85
-pkg_deps=(core/coreutils)
+pkg_deps=(core/bash core/coreutils core/jdk7)
 pkg_exports=(
   [port]=server.port
 )
 pkg_exposes=(port)
 
-# The default implementation extracts your tarball source file into HAB_CACHE_SRC_PATH. The
-# supported archives are: .tar, .tar.bz2, .tar.gz, .tar.xz, .rar, .zip, .Z, .7z. If the file
-# archive could not be found or was not supported, then a message will be printed to stderr
-# with additional information.
 
-do_unpack() {
-  local source_dir=$HAB_CACHE_SRC_PATH/${pkg_name}-${pkg_version}
-  local unpack_file="$HAB_CACHE_SRC_PATH/$pkg_filename"
-
-  mkdir "$source_dir"
-  pushd "$source_dir" >/dev/null
-  tar xz --strip-components=1 -f "$unpack_file"
-
-  popd > /dev/null
-}
-
-# The default implementation is to update the prefix path for the configure script to
-# use $pkg_prefix and then run make to compile the downloaded source. This means the
-# script in the default implementation does ./configure --prefix=$pkg_prefix && make. You
-# should override this behavior if you have additional configuration changes to make or
-# other software to build and install as part of building your package.
 do_build() {
-    return 0
+  for file in $(cd $CACHE_PATH/bin; ls *.sh -1)
+  do
+    build_line "Fixing bash interpreter on $file"
+    fix_interpreter "$CACHE_PATH/bin/$file" core/bash bin/bash
+  done
+  return 0
 }
 
-# The default implementation is to run make install on the source files and place the compiled
-# binaries or libraries in HAB_CACHE_SRC_PATH/$pkg_dirname, which resolves to a path like
-# /hab/cache/src/packagename-version/. It uses this location because of do_build() using the
-# --prefix option when calling the configure script. You should override this behavior if you
-# need to perform custom installation steps, such as copying files from HAB_CACHE_SRC_PATH
-# to specific directories in your package, or installing pre-built binaries into your package.
 do_install() {
-    build_line "Performing install"
-    mkdir -p "${pkg_prefix}/tc"
-    cp -vR ./* "${pkg_prefix}/tc"
-
-    # default permissions included in the tarball don't give any world access
-    find "${pkg_prefix}/tc" -type d -exec chmod -v 755 {} +
-    find "${pkg_prefix}/tc" -type f -exec chmod -v 644 {} +
-    find "${pkg_prefix}/tc" -type f -name '*.sh' -exec chmod -v 755 {} +
+    build_line "Performing install from $CACHE_PATH to ${pkg_prefix}"
+    cp -r "$CACHE_PATH" "${pkg_prefix}/tc/"
+    return 0
 }
