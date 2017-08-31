f40b3a1a61d9e3af4c41ac2b5ed9ed1d72bb1b2b
Fletcher Nichol <fnichol@nichol.ca>
2017-08-31 12:40:25 -0600

    Update to Rust 1.20.0.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- rust/plan.ps1
+++ rust/plan.ps1
@@ -1,12 +1,12 @@
 ﻿$pkg_name="rust"
 $pkg_origin="core"
-$pkg_version="1.18.0"
+$pkg_version="1.20.0"
 $pkg_description="Safe, concurrent, practical language"
 $pkg_upstream_url="https://www.rust-lang.org/"
 $pkg_license=@("Apache-2.0", "MIT")
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_source="https://static.rust-lang.org/dist/rust-$pkg_version-x86_64-pc-windows-msvc.msi"
-$pkg_shasum="51023c40b17617b525f0f1fd6f2dd994c3700261158cb186e3fbf54ca21eb52b"
+$pkg_shasum="de1635d760d7e92984eedb7ef900dfc3c2b8341cd49135269ae416657f4af836"
 $pkg_deps=@("core/visual-cpp-redist-2013", "core/visual-cpp-build-tools-2015")
 $pkg_build_deps=@("core/lessmsi")
 $pkg_bin_dirs=@("bin")
