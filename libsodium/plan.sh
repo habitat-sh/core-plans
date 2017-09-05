45550d54167f1c6a0495fc5254a5062761cc4bcd
eeyun <ihenry@chef.io>
2017-09-05 09:41:35 -0400

    Update libsodium to available version
    Signed-off-by: eeyun <ihenry@chef.io>

--- libsodium/plan.sh
+++ libsodium/plan.sh
@@ -1,11 +1,11 @@
 pkg_name=libsodium
 pkg_distname=$pkg_name
 pkg_origin=core
-pkg_version=1.0.11
+pkg_version=1.0.13
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('libsodium')
 pkg_source=https://download.libsodium.org/libsodium/releases/${pkg_name}-${pkg_version}.tar.gz
-pkg_shasum=a14549db3c49f6ae2170cbbf4664bd48ace50681045e8dbea7c8d9fb96f9c765
+pkg_shasum=9c13accb1a9e59ab3affde0e60ef9a2149ed4d6e8f99c93c7a5b97499ee323fd
 pkg_dirname=${pkg_distname}-${pkg_version}
 pkg_deps=(core/glibc)
 pkg_build_deps=(core/autoconf core/automake core/diffutils core/patch core/make core/gcc core/sed)
