7394d92fa683f95e28f454199507d6c24b4bd582
Scott Hain <shain@chef.io>
2017-09-05 14:09:24 -0700

    Updated some more fixes for permissions and correct paths
    Signed-off-by: Scott Hain <shain@chef.io>

--- tomcat7/plan.sh
+++ tomcat7/plan.sh
@@ -7,7 +7,7 @@ pkg_license=('Apache-2.0')
 pkg_description="The Apache Tomcat software is an open source implementation of the Java Servlet, JavaServer Pages, Java Expression Language and Java WebSocket technologies."
 pkg_upstream_url="http://tomcat.apache.org/"
 pkg_filename=apache-tomcat-${pkg_version}.tar.gz
-pkg_source=http://archive.apache.org/dist/tomcat/tomcat-7/v${pkg_version}/bin/${pkg_filename}.tar.gz
+pkg_source=http://archive.apache.org/dist/tomcat/tomcat-7/v${pkg_version}/bin/${pkg_filename}
 pkg_dirname=apache-tomcat-${pkg_version}
 pkg_shasum=0e814d6ad7d5b90e29c79887137420d3bc413540f9faa60d98f11e6c8a8fea85
 pkg_deps=(core/bash core/coreutils core/jdk7)
@@ -28,6 +28,8 @@ do_build() {
 
 do_install() {
     build_line "Performing install from $CACHE_PATH to ${pkg_prefix}"
-    cp -r "$CACHE_PATH" "${pkg_prefix}/tc/"
+    cp -R "$CACHE_PATH" "${pkg_prefix}/tc/"
+    # tomcat has a small issue where it uncompresses all the files with 0600 permissions
+    chmod -R a+r "${pkg_prefix}/tc/"
     return 0
 }
