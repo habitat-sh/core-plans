e655896e54b8c23ce87b7c02b31643394d7cc158
Blake Irvin <dev+blake.irvin@smartb.eu>
2017-09-08 18:23:08 +0200

    satisfy Travis
    Signed-off-by: Blake Irvin <blakeirvin@me.com>

--- journalbeat/plan.sh
+++ journalbeat/plan.sh
@@ -1,9 +1,11 @@
 pkg_svc_user="root"
 pkg_name="journalbeat"
+pkg_description="Journalbeat is a log shipper from systemd/journald to Logstash/Elasticsearch"
 pkg_origin="core"
 pkg_version="5.5.2"
 pkg_maintainer="smartB Engineering <dev@smartb.eu>"
 pkg_license=('Apache 2')
+pkg_upstream_url="https://github.com/mheese/journalbeat"
 pkg_source="https://github.com/mheese/$pkg_name/archive/v$pkg_version.tar.gz"
 pkg_shasum="3cc431a199226b14e318610ee5cfd08d2f3109dc9850591f73a28219c56753c9"
 pkg_deps=(
@@ -34,7 +36,8 @@ do_clean() {
 do_prepare() {
   mkdir -p "${parent_go_path}"
   ln -s "${PWD}" "${parent_go_path}/${pkg_name}"
-  export C_INCLUDE_PATH="$(pkg_path_for smartb/systemd)/include"
+  export C_INCLUDE_PATH
+  C_INCLUDE_PATH="$(pkg_path_for smartb/systemd)/include"
   return $?
 }
 
