d816480890c35498fb2560e5a319113550055bf1
Scott Macfarlane <macfarlane.scott@gmail.com>
2017-09-11 12:06:39 -0700

    Update Linux to 4.13.1
    * Update linux-headers to match version of linux package
    * Add kernel configuration based on Arch Linux kernel config
    * Update do_build to allow for building of scripts/tools directories
    
    Signed-off-by: Scott Macfarlane <macfarlane.scott@gmail.com>

--- linux/plan.sh
+++ linux/plan.sh
@@ -1,22 +1,93 @@
 pkg_name=linux
 pkg_origin=core
-pkg_version="4.11.1"
+pkg_version="4.13.1"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('gplv2')
-pkg_source="https://cdn.kernel.org/pub/linux/kernel/v4.x/${pkg_name}-${pkg_version}.tar.gz"
-pkg_shasum="1ece864a5b0fbf448f6e01439968de7476c3bf57595c1c74cb96e9a2e3adbd0f"
+pkg_source="https://cdn.kernel.org/pub/linux/kernel/v4.x/${pkg_name}-${pkg_version}.tar.xz"
+pkg_description="The Linux kernel"
+pkg_upstream_url="https://www.kernel.org/"
+pkg_shasum="decee7a2de34aea921fce3e7934f520790fe43176de29bd9718a84419ca6e1ce"
 pkg_deps=(core/glibc)
-pkg_build_deps=(core/make core/gcc core/perl core/elfutils core/bc core/diffutils)
+pkg_build_deps=(
+  core/bc
+  core/diffutils
+  core/elfutils
+  core/findutils
+  core/gcc
+  core/inetutils
+  core/make
+  core/perl
+  core/openssl
+)
 
+do_begin() {
+  if [[ ! -e /bin/pwd ]]; then
+    hab pkg binlink core/coreutils pwd -d /bin
+    _pwd_binlink=true
+  fi
+}
+
+do_prepare() {
+  make mrproper
+  cp "${PLAN_CONTEXT}/config/config.x86_64" "${HAB_CACHE_SRC_PATH}/${pkg_dirname}/.config"
+}
 
 do_build() {
-  make defconfig INSTALL_PATH="$pkg_prefix"
-  sed "s/=m/=y/" -i .config
-  make -j "$(nproc)"
+  # Some software in tools/scripts requires external libraries to compile.
+  #  The resulting binaries are not packaged so their dependencies are listed
+  #  as build dependencies. To allow them to build successfully and run
+  #  temporarily set LD_LIBRARY_PATH to all of the pkg_build_deps lib directories.
+
+  set_ld_library_path
+
+  # These line numbers can change between kernel versions, but changes will only break
+  #  builds that have CONFIG_ options set that require building scripts/ or tools/
+
+  # Let the inline test build (CONFIG_STACK_VALIDATION) know where libelf lives
+  sed -i "932s|-xc|$LDFLAGS -xc|" Makefile
+
+  # Override the defaults for building scripts and tools.
+  #  scripts/sign-file and tools/objtool need openssl and elfutils.
+  sed -i "306s|$| $LDFLAGS|" Makefile
+  sed -i "98s|\$(hostc_flags)|\$(hostc_flags) \$(HOSTLDFLAGS)|" scripts/Makefile.host
+  sed -i "55s|\$(LDFLAGS)|\$(LDFLAGS) \$(HOSTLDFLAGS)|" tools/objtool/Makefile
+
+  HOST_EXTRACFLAGS="${CFLAGS}" make -j "$(nproc)" bzImage modules
+
+  unset LD_LIBRARY_PATH
 }
 
 do_install() {
-  make INSTALL_MOD_PATH="$pkg_prefix" modules_install
+  make INSTALL_MOD_PATH="${pkg_prefix}" modules_install
   mkdir -p "${pkg_prefix}/boot"
   cp -a arch/x86/boot/bzImage "${pkg_prefix}/boot/"
+
+  # make modules_install symlinks lib/modules/$pkg_version/{build,source} to the cache directory
+  find "${pkg_prefix}" -type l -delete
+}
+
+do_end() {
+  if [[ -v $_pwd_binlink ]]; then
+    rm -f /bin/pwd
+  fi
+}
+
+set_ld_library_path() {
+  local ld_library_path_part=()
+
+  for dep in "${pkg_build_deps[@]}"; do
+    local dep_path
+    dep_path=$(pkg_path_for "$dep");
+
+    if [[ -f "$dep_path/LD_RUN_PATH" ]]; then
+      local data
+      local trimmed
+      data=$(cat "$dep_path/LD_RUN_PATH")
+      trimmed=$(trim "$data")
+      ld_library_path_part+=("$trimmed")
+    fi
+  done
+
+  LD_LIBRARY_PATH=$(join_by ':' "${ld_library_path_part[@]}")
+  export LD_LIBRARY_PATH
 }
