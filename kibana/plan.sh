820a23e11ee8296e3f3de1e8d3f733a5f7044b9e
Graham Weldon <graham@grahamweldon.com>
2017-09-28 10:17:47 +0900

    Upgrade kibana
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- kibana/plan.sh
+++ kibana/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=kibana
-pkg_version=4.6.1
+pkg_version=5.6.1
 pkg_origin=core
 pkg_license=('Apache-2.0')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="Kibana is a browser based analytics and search dashboard for Elasticsearch."
 pkg_upstream_url=https://www.elastic.co/products/kibana
 pkg_source=https://github.com/elastic/${pkg_name}/archive/v${pkg_version}.tar.gz
-pkg_shasum=58dc3f82cdd62708034169db64c342a48674065673e2115d410509f83fe59c9e
+pkg_shasum=12ab79f6db3318d5bb718f09daa13fcdaf34f37395b8eeb0f414b3a35614c2b7
 pkg_filename=${pkg_name}-${pkg_version}.tar.gz
 pkg_deps=(core/node)
 pkg_build_deps=(
@@ -46,10 +46,10 @@ do_build () {
 }
 
 do_install() {
-  cp -r ./* "$pkg_prefix/"
+  cp -r ./* "${pkg_prefix}/"
   # Delete the /config directory created by Kibana installer; habitat lays down
   # /config/kibana.yml
-  rm -rv "$pkg_prefix/config/"
+  rm -rv "${pkg_prefix}/config/"
 }
 
 do_strip() {
