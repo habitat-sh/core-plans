c523b57eb8d1aa48ef48321ef84c5a4d3ed58d99
Nell Shamrell <nellshamrell@gmail.com>
2017-10-02 18:48:28 -0700

    remove unnecessary bin dir
    Signed-off-by: Nell Shamrell <nellshamrell@gmail.com>

--- scaffolding-node/plan.sh
+++ scaffolding-node/plan.sh
@@ -7,7 +7,6 @@ pkg_description="Habitat Plan Scaffolding for Node.js Applications"
 pkg_upstream_url="https://github.com/habitat-sh/core-plans/tree/master/scaffolding-node"
 pkg_deps=(core/tar core/rq core/jq-static core/gawk core/curl core/bc)
 pkg_build_deps=(core/node core/coreutils)
-pkg_bin_dirs=(bin)
 
 do_build() {
   return 0
