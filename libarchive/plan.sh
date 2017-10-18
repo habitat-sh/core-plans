8b80d6878b299333b3a06a5bf6fb6d9ccd21179d
Jamie Winsor <jamie@vialstudios.com>
2017-10-18 09:32:28 -0700

    Bump libarchive to 3.3.2
    Signed-off-by: Jamie Winsor <jamie@vialstudios.com>

--- libarchive/plan.sh
+++ libarchive/plan.sh
@@ -1,11 +1,13 @@
 pkg_name=libarchive
 pkg_distname=$pkg_name
 pkg_origin=core
-pkg_version=3.2.0
+pkg_version=3.3.2
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_description="Multi-format archive and compression library"
+pkg_upstream_url=https://www.libarchive.org
 pkg_license=('BSD')
 pkg_source=http://www.libarchive.org/downloads/${pkg_distname}-${pkg_version}.tar.gz
-pkg_shasum=7bce45fd71ff01dc20d19edd78322d4965583d81b8bed8e26cacb65d6f5baa87
+pkg_shasum=ed2dbd6954792b2c054ccf8ec4b330a54b85904a80cef477a1c74643ddafa0ce
 pkg_dirname=${pkg_distname}-${pkg_version}
 pkg_deps=(core/glibc core/openssl core/zlib core/bzip2 core/xz)
 pkg_build_deps=(core/gcc core/coreutils core/make)
@@ -14,7 +16,7 @@ pkg_lib_dirs=(lib)
 
 do_build() {
     ./configure \
-      --prefix=$pkg_prefix \
+      --prefix="$pkg_prefix" \
       --without-xml2 \
       --without-lzo2
   make
