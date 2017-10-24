43a431bc62244c0061ca84fd865739d2d92fd0a7
Blake Irvin <blake.irvin@gmail.com>
2017-10-24 20:35:47 +0200

    clean up after code review
    Signed-off-by: Blake Irvin <blakeirvin@me.com>

--- journalbeat/plan.sh
+++ journalbeat/plan.sh
@@ -10,13 +10,12 @@ pkg_source="https://github.com/mheese/$pkg_name/archive/v$pkg_version.tar.gz"
 pkg_shasum="3cc431a199226b14e318610ee5cfd08d2f3109dc9850591f73a28219c56753c9"
 pkg_deps=(
   core/lz4
-  smartb/systemd
+  core/systemd
 )
 pkg_build_deps=(
   core/go
   core/git
   core/gcc
-  smartb/systemd
 )
 pkg_bin_dirs=(bin)
 pkg_include_dirs=(include)
@@ -30,15 +29,13 @@ do_begin() {
 do_clean() {
   do_default_clean
   rm -rf "${parent_go_path}"
-  return $?
 }
 
 do_prepare() {
   mkdir -p "${parent_go_path}"
   ln -s "${PWD}" "${parent_go_path}/${pkg_name}"
   export C_INCLUDE_PATH
-  C_INCLUDE_PATH="$(pkg_path_for smartb/systemd)/include"
-  return $?
+  C_INCLUDE_PATH="$(pkg_path_for core/systemd)/include"
 }
 
 do_build() {
@@ -52,5 +49,4 @@ do_build() {
 do_install() {
   mkdir -p "${pkg_prefix}/bin"
   cp "${pkg_name}" "${pkg_prefix}/bin/${pkg_name}"
-  return $?
 }
