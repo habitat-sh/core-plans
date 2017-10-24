f79da0835171731ddc638d7ef103261582aff8c4
Stephan Renatus <srenatus@chef.io>
2017-10-24 09:28:03 +0200

    [wget] bump: 1.19.1
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

--- wget/plan.sh
+++ wget/plan.sh
@@ -1,11 +1,13 @@
 pkg_name=wget
 pkg_distname=$pkg_name
 pkg_origin=core
-pkg_version=1.18
-pkg_license=('gplv3+')
+pkg_version=1.19.1
+pkg_license=('GPL-3.0+')
+pkg_description="GNU Wget is a free software package for retrieving files using HTTP, HTTPS, FTP and FTPS the most widely-used Internet protocols."
+pkg_upstream_url=https://www.gnu.org/software/wget/
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_source=http://ftp.gnu.org/gnu/$pkg_distname/${pkg_distname}-${pkg_version}.tar.xz
-pkg_shasum=b5b55b75726c04c06fe253daec9329a6f1a3c0c1878e3ea76ebfebc139ea9cc1
+pkg_source=https://ftp.gnu.org/gnu/$pkg_distname/${pkg_distname}-${pkg_version}.tar.xz
+pkg_shasum=0c950b9671881222a4d385b013c9604e98a8025d1988529dfca0e93617744cd2
 pkg_deps=(core/glibc core/libidn core/zlib core/openssl core/cacerts)
 pkg_build_deps=(core/coreutils core/diffutils core/patch core/make core/gcc core/sed core/grep core/pkg-config)
 pkg_bin_dirs=(bin)
@@ -16,7 +18,7 @@ do_prepare() {
 
 do_build() {
   ./configure \
-    --prefix=$pkg_prefix \
+    --prefix="$pkg_prefix" \
     --with-ssl=openssl \
     --without-libuuid
   make
@@ -25,7 +27,7 @@ do_build() {
 do_install() {
   do_default_install
 
-  cat <<EOF >> $pkg_prefix/etc/wgetrc
+  cat <<EOF >> "$pkg_prefix/etc/wgetrc"
 
 # Default root CA certs location
 ca_certificate=$(pkg_path_for cacerts)/ssl/certs/cacert.pem
@@ -35,7 +37,7 @@ EOF
 _wget_common_prepare() {
   # Purge the codebase (mostly tests & build Perl scripts) of the hardcoded
   # reliance on `/usr/bin/env`.
-  grep -lr '/usr/bin/env' . | while read f; do
+  grep -lr '/usr/bin/env' . | while read -r f; do
     sed -e "s,/usr/bin/env,$(pkg_path_for coreutils)/bin/env,g" -i "$f"
   done
 }
