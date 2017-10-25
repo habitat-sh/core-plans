9b65b618757f7ffc364775a06260f966c14d5465
Stephan Renatus <srenatus@chef.io>
2017-10-25 11:52:34 +0200

    [ed] bump 1.14.2
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

--- ed/plan.sh
+++ ed/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=ed
 pkg_origin=core
-pkg_version=1.13
+pkg_version=1.14.2
 pkg_description="The standard text editor."
 pkg_upstream_url="https://www.gnu.org/software/ed/"
 pkg_license=('GPL-3.0')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="https://ftp.gnu.org/gnu/ed/ed-${pkg_version}.tar.lz"
-pkg_shasum=cd66c54a53cd6ef35a217556e7b2b2fdd973ca2708f4fc41636b0bc06388c7d3
+pkg_shasum=f57962ba930d70d02fc71d6be5c5f2346b16992a455ab9c43be7061dec9810db
 pkg_deps=(core/glibc)
 pkg_build_deps=(core/gcc core/make core/lzip core/diffutils)
 pkg_bin_dirs=(bin)
