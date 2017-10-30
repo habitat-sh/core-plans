c2cb788661341cafc778bdd45df1e20499d76ae4
Nell Shamrell <nellshamrell@gmail.com>
2017-10-30 16:45:00 -0700

    upgrades node version
    Signed-off-by: Nell Shamrell <nellshamrell@gmail.com>

--- node/plan.sh
+++ node/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=node
 pkg_origin=core
-pkg_version=6.11.3
+pkg_version=6.11.5
 pkg_description="Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine."
 pkg_upstream_url=https://nodejs.org/
 pkg_license=('MIT')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source=https://nodejs.org/dist/v${pkg_version}/node-v${pkg_version}.tar.gz
-pkg_shasum=c60562a7bc316f137297b7aa23ae354e3ac6779fe8ddf46fe43a75b6b92443e8
+pkg_shasum=c4aed94e82dbf246a1c9e0705c3054f0c0f3d9c4d8d025d877e0ef1f7b6cde4c
 pkg_deps=(core/glibc core/gcc-libs)
 pkg_build_deps=(core/python2 core/gcc core/grep core/make)
 pkg_bin_dirs=(bin)
