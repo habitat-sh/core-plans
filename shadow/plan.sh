12de555020b089c27db524bf096e77a638ab1dab
Fletcher Nichol <fnichol@nichol.ca>
2017-10-30 22:07:05 +0000

    [shadow] Update Plan to match current standards.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- shadow/plan.sh
+++ shadow/plan.sh
@@ -5,6 +5,8 @@ pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('bsd')
 pkg_source=https://github.com/shadow-maint/${pkg_name}/releases/download/${pkg_version}/${pkg_name}-${pkg_version}.tar.xz
 pkg_shasum=fc8c858381ad577a5c25ff5beb6ee60a34f8719c73e4e7c61e74188b4e54b741
+pkg_description="Password and account management tool suite"
+pkg_upstream_url="https://github.com/shadow-maint/shadow"
 pkg_deps=(core/glibc core/attr core/acl)
 pkg_build_deps=(core/coreutils core/diffutils core/patch core/make core/gcc)
 pkg_bin_dirs=(bin)
@@ -13,12 +15,13 @@ do_prepare() {
   # Allow dots in usernames.
   #
   # Thanks to: http://sources.gentoo.org/cgi-bin/viewvc.cgi/gentoo-x86/sys-apps/shadow/files/shadow-4.1.3-dots-in-usernames.patch
-  patch -p1 -i $PLAN_CONTEXT/dots-in-usernames.patch
+  patch -p1 -i "$PLAN_CONTEXT/dots-in-usernames.patch"
 
   # Disable the installation of the `groups` program as Coreutils provides a
   # better version.
   #
   # Thanks to: http://www.linuxfromscratch.org/lfs/view/stable/chapter06/shadow.html
+  # shellcheck disable=SC2016
   sed -i 's/groups$(EXEEXT) //' src/Makefile.in
   find man -name Makefile.in -exec sed -i 's/groups\.1 / /' {} \;
 
@@ -32,7 +35,7 @@ do_prepare() {
 
 do_build() {
   ./configure \
-    --prefix=$pkg_prefix \
+    --prefix="$pkg_prefix" \
     --with-acl \
     --with-attr \
     --with-group-name-max-length=32 \
@@ -50,11 +53,11 @@ do_install() {
 
   # Move all binaries in `sbin/` into `bin/` as this isn't handled by
   # `./configure`.
-  mv $pkg_prefix/sbin/* $pkg_prefix/bin/
-  rm -rf $pkg_prefix/sbin
+  mv "$pkg_prefix/sbin"/* "$pkg_prefix/bin/"
+  rm -rf "$pkg_prefix/sbin"
 
   # Install the license
-  install -Dm644 COPYING $pkg_prefix/share/licenses/COPYING
+  install -Dm644 COPYING "$pkg_prefix/share/licenses/COPYING"
 }
 
 
