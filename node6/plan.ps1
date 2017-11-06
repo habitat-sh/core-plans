b07411412c590fe0d7991fd0caad865bca39ab59
eeyun <ihenry@chef.io>
2017-11-06 11:13:50 -0500

    Added node6 as currently LTS and created node9 package for node current
    Signed-off-by: eeyun <ihenry@chef.io>

---
+++ node6/plan.ps1
@@ -0,0 +1,26 @@
+$pkg_name="node6"
+$pkg_origin="core"
+$pkg_version="6.11.5"
+$pkg_description="Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine."
+$pkg_upstream_url="https://nodejs.org/"
+$pkg_license=@("MIT")
+$pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+$pkg_source="https://nodejs.org/dist/v$pkg_version/node-v$pkg_version-x64.msi"
+$pkg_shasum="c4aed94e82dbf246a1c9e0705c3054f0c0f3d9c4d8d025d877e0ef1f7b6cde4c"
+$pkg_build_deps=@("core/lessmsi")
+$pkg_bin_dirs=@("bin")
+
+function Invoke-Unpack {
+  write-host
+  lessmsi x (Resolve-Path "$HAB_CACHE_SRC_PATH/$pkg_filename").Path
+  mkdir "$HAB_CACHE_SRC_PATH/$pkg_dirname"
+  Move-Item "node-v$pkg_version-x64/SourceDir/nodejs" "$HAB_CACHE_SRC_PATH/$pkg_dirname"
+}
+
+function Invoke-Install {
+  Copy-Item "$HAB_CACHE_SRC_PATH/$pkg_dirname/nodejs/*" "$pkg_prefix/bin" -Recurse
+}
+
+function Invoke-Check() {
+  (& "$HAB_CACHE_SRC_PATH/$pkg_dirname/nodejs/node" --version) -eq "v$pkg_version"
+}
--- node6/plan.sh
+++ node6/plan.sh
@@ -1,14 +1,14 @@
 source "../node/plan.sh"
 
-pkg_name=node8
+pkg_name=node6
 pkg_origin=core
-pkg_version=8.6.0
+pkg_version=6.11.5
 pkg_description="Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine."
-pkg_license=('MIT')
 pkg_upstream_url=https://nodejs.org/
+pkg_license=('MIT')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source=https://nodejs.org/dist/v${pkg_version}/node-v${pkg_version}.tar.gz
-pkg_shasum=80551d47440c602c5825a3f6611e13cb0c57d277234dbff693af433a88351a08
+pkg_shasum=c4aed94e82dbf246a1c9e0705c3054f0c0f3d9c4d8d025d877e0ef1f7b6cde4c
 
 # the archive contains a 'v' version # prefix, but the default value of
 # pkg_dirname is node-${pkg_version} (without the v). This tweak makes build happy
