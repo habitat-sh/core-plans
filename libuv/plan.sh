371c5cb4b5570528f6743cf56546d53fb0ce3adf
Steven Danna <steve@chef.io>
2017-11-17 00:28:41 +0000

    [libuv] Bump to 1.16.1 and fix minor build issues
    NOTE: The build has the folllowing warning output
    
        Unescaped left brace in regex is deprecated, passed through in
        regex; marked by <-- HERE in m/\${ <-- HERE ([^ \t=:+{}]+)}/ at
        /hab/pkgs/core/automake/1.15/201705132 14259/bin/automake line 3936.
    
    I believe this is actually a problem in automake 1.5.
    
    Signed-off-by: Steven Danna <steve@chef.io>

--- libuv/plan.sh
+++ libuv/plan.sh
@@ -1,11 +1,11 @@
 pkg_name=libuv
 pkg_origin=core
-pkg_version="1.11.0"
+pkg_version="1.16.1"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('MIT')
 pkg_source="http://dist.$pkg_name.org/dist/v$pkg_version/$pkg_name-v$pkg_version.tar.gz"
 pkg_dirname="${pkg_name}-v$pkg_version"
-pkg_shasum="0f686994dcea6cb5cd3f50e35d5fdda07211b4b3586516df7c39bdbf19acb9a7"
+pkg_shasum="d64aafa9ad969391248a2dc7ef14b7da128be0b3f2d6ca5c18e13a93d64c785d"
 pkg_deps=(core/glibc)
 pkg_build_deps=(
   core/autoconf
@@ -22,9 +22,22 @@ pkg_include_dirs=(include)
 pkg_description="libuv is a multi-platform support library with a focus on asynchronous I/O."
 pkg_upstream_url="http://libuv.org/"
 
+do_prepare() {
+  if [[ ! -r /usr/bin/file ]]; then
+    ln -sv "$(pkg_path_for file)/bin/file" /usr/bin/file
+    _clean_file=true
+  fi
+}
+
 do_build() {
   sh autogen.sh
   do_default_build
+}
+
+do_end() {
+  if [[ -n "$_clean_file" ]]; then
+    rm -fv /usr/bin/file
+  fi
 }
 
 do_check() {
