941ac21a4ce908ff97e265757f4d045adf1a124d
Romain Sertelon <romain@sertelon.fr>
2017-11-18 18:47:28 +0100

    [libtasn1] Creates package
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

---
+++ libtasn1/plan.sh
@@ -0,0 +1,44 @@
+pkg_name=libtasn1
+pkg_origin=core
+pkg_version="4.13"
+pkg_description="ASN.1 implementation"
+pkg_upstream_url="https://www.gnu.org/software/libtasn1/"
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_license=('LGPL-2.1-or-later')
+pkg_source="https://ftp.gnu.org/gnu/${pkg_name}/${pkg_name}-${pkg_version}.tar.gz"
+pkg_shasum="7e528e8c317ddd156230c4e31d082cd13e7ddeb7a54824be82632209550c8cca"
+pkg_deps=(
+  core/glibc
+)
+pkg_build_deps=(
+  core/bison
+  core/busybox-static
+  core/diffutils
+  core/file
+  core/gcc
+  core/make
+  core/pkg-config
+  core/util-linux
+)
+
+pkg_bin_dirs=(bin)
+pkg_lib_dirs=(lib)
+pkg_include_dirs=(include)
+pkg_pconfig_dirs=(lib/pkgconfig)
+
+do_prepare() {
+  if [[ ! -r /usr/bin/file ]]; then
+    ln -sv "$(pkg_path_for file)/bin/file" /usr/bin/file
+    _clean_file=true
+  fi
+}
+
+do_check() {
+  make check
+}
+
+do_end() {
+  if [[ -n "$_clean_file" ]]; then
+    rm -fv /usr/bin/file
+  fi
+}
