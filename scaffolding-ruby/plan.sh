4365af60f1f4dee24fc530b8aa0da0f3de76bfb0
Nell Shamrell <nellshamrell@gmail.com>
2017-11-27 16:19:21 -0800

    updates ruby scaffolding version and changelog
    Signed-off-by: Nell Shamrell <nellshamrell@gmail.com>

--- scaffolding-ruby/plan.sh
+++ scaffolding-ruby/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=scaffolding-ruby
 pkg_origin=core
-pkg_version="0.8.7"
+pkg_version="0.8.8"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('Apache-2.0')
 pkg_description="Habitat Plan Scaffolding for Ruby Applications"
