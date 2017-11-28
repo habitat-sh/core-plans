206cb2b18520981eff280d9d2951da0ec0cb8dd5
Salim Afiune <afiune@chef.io>
2017-11-28 12:40:23 -0500

    Upgrade protobuf-cpp to 3.5.0
    Signed-off-by: Salim Afiune <afiune@chef.io>

--- protobuf-cpp/plan.sh
+++ protobuf-cpp/plan.sh
@@ -1,13 +1,13 @@
 pkg_name=protobuf-cpp
 pkg_distname=protobuf
 pkg_origin=core
-pkg_version=3.4.1
+pkg_version=3.5.0
 pkg_license=('BSD')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="Google's language-neutral, platform-neutral, extensible mechanism for serializing structured data."
 pkg_upstream_url=https://github.com/google/${pkg_distname}
 pkg_source=https://github.com/google/${pkg_distname}/releases/download/v${pkg_version}/${pkg_name}-${pkg_version}.tar.gz
-pkg_shasum=2bb34b4a8211a30d12ef29fd8660995023d119c99fbab2e5fe46f17528c9cc78
+pkg_shasum=8dd6b051c2b39ab95dfe1f53cc5e1662ceb852d856db57d85751696859698551
 pkg_dirname=${pkg_distname}-${pkg_version}
 pkg_deps=(core/gcc core/zlib)
 pkg_build_deps=(core/make)
