413adf852a21ccda5f279acab2ad809f41d8bb04
Graham Weldon <graham@grahamweldon.com>
2017-11-29 14:28:14 +0900

    Kibana version up to 6.0.0
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- kibana/plan.sh
+++ kibana/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=kibana
-pkg_version=5.6.4
+pkg_version=6.0.0
 pkg_origin=core
 pkg_license=('Apache-2.0')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="Kibana is a browser based analytics and search dashboard for Elasticsearch."
 pkg_upstream_url=https://www.elastic.co/products/kibana
 pkg_source=https://github.com/elastic/${pkg_name}/archive/v${pkg_version}.tar.gz
-pkg_shasum=8592c1a89b55a2bbba717d82503c3ea219933953591951964531f8d708431fe5
+pkg_shasum=85778c3d09bdc68369f37b887b4295d62e030ec29bf81c9f6bd17f0494177d56
 pkg_filename=${pkg_name}-${pkg_version}.tar.gz
 pkg_deps=(core/node)
 pkg_build_deps=(
