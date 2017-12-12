26a0944a63a4169c952cc3bc63bd9e2357c87f6c
Romain Sertelon <romain@sertelon.fr>
2017-12-12 23:10:59 +0100

    Improves autoconf package
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- autoconf/plan.sh
+++ autoconf/plan.sh
@@ -2,11 +2,21 @@ pkg_name=autoconf
 pkg_origin=core
 pkg_version=2.69
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_license=('gplv2+')
-pkg_source=http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.xz
-pkg_shasum=64ebcec9f8ac5b2487125a86a7760d2591ac9e1d3dbd59489633f9de62a57684
-pkg_deps=(core/m4 core/perl)
-pkg_build_deps=(core/coreutils core/diffutils core/patch core/make core/gcc core/sed core/gawk)
+pkg_description="Autoconf is an extensible package of M4 macros that produce shell scripts to automatically configure software source code packages"
+pkg_upstream_url="https://www.gnu.org/software/autoconf/autoconf.html"
+pkg_license=('GPL-2.0')
+pkg_source="http://ftp.gnu.org/gnu/${pkg_name}/${pkg_name}-${pkg_version}.tar.xz"
+pkg_shasum="64ebcec9f8ac5b2487125a86a7760d2591ac9e1d3dbd59489633f9de62a57684"
+pkg_deps=(
+  core/m4
+  core/perl
+)
+pkg_build_deps=(
+  core/diffutils
+  core/inetutils
+  core/gcc
+  core/make
+)
 pkg_bin_dirs=(bin)
 
 
