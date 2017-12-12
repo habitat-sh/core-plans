98404cae4658db2863fdf809cf0919e812087edb
Romain Sertelon <romain@sertelon.fr>
2017-12-12 23:04:17 +0100

    Improves attr
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- attr/plan.sh
+++ attr/plan.sh
@@ -2,18 +2,28 @@ pkg_name=attr
 pkg_origin=core
 pkg_version=2.4.47
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_license=('gplv2+')
-pkg_source=http://download.savannah.gnu.org/releases/$pkg_name/$pkg_name-${pkg_version}.src.tar.gz
-pkg_shasum=25772f653ac5b2e3ceeb89df50e4688891e21f723c460636548971652af0a859
-pkg_deps=(core/glibc)
-pkg_build_deps=(core/coreutils core/diffutils core/patch core/make core/gcc core/gettext)
+pkg_description="Commands for Manipulating Filesystem Extended Attributes"
+pkg_upstream_url="https://savannah.nongnu.org/projects/attr/"
+pkg_license=('GPL-2.0')
+pkg_source="http://download.savannah.gnu.org/releases/${pkg_name}/${pkg_name}-${pkg_version}.src.tar.gz"
+pkg_shasum="25772f653ac5b2e3ceeb89df50e4688891e21f723c460636548971652af0a859"
+pkg_deps=(
+  core/glibc
+)
+pkg_build_deps=(
+  core/coreutils
+  core/diffutils
+  core/make
+  core/gcc
+  core/gettext
+)
 pkg_bin_dirs=(bin)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
 
 do_install() {
   make install install-dev install-lib
-  chmod -v 755 $pkg_prefix/lib/libattr.so
+  chmod -v 755 "${pkg_prefix}/lib/libattr.so"
 }
 
 
