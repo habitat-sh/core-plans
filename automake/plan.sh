d9aa93d66c4df43b620a62e6191558f027890ff1
Romain Sertelon <romain@sertelon.fr>
2017-12-13 22:19:12 +0100

    [automake] package improvements
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- automake/plan.sh
+++ automake/plan.sh
@@ -2,13 +2,24 @@ pkg_name=automake
 pkg_origin=core
 pkg_version=1.15
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_license=('gplv2+')
-pkg_source=http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.xz
-pkg_shasum=9908c75aabd49d13661d6dcb1bc382252d22cc77bf733a2d55e87f2aa2db8636
-pkg_deps=(core/perl)
-pkg_build_deps=(core/coreutils core/diffutils core/patch core/make core/gcc core/autoconf)
+pkg_license=("GPL-2.0")
+pkg_description="Automake is a tool for automatically generating Makefile.in files compliant with the GNU Coding Standards"
+pkg_upstream_url="https://www.gnu.org/software/automake/"
+pkg_source="http://ftp.gnu.org/gnu/${pkg_name}/${pkg_name}-${pkg_version}.tar.xz"
+pkg_shasum="9908c75aabd49d13661d6dcb1bc382252d22cc77bf733a2d55e87f2aa2db8636"
+pkg_deps=(
+  core/perl
+)
+pkg_build_deps=(
+  core/autoconf
+  core/bison
+  core/coreutils
+  core/diffutils
+  core/flex
+  core/gcc
+  core/make
+)
 pkg_bin_dirs=(bin)
-
 
 # ----------------------------------------------------------------------------
 # **NOTICE:** What follows are implementation details required for building a
