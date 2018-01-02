bf423d527021aa7d9b028258b46d63f18cb0684d
Christopher P. Maher <defilan@gmail.com>
2018-01-02 11:34:27 -0800

    Updated to 5.8.0
    Signed-off-by: Christopher P. Maher <defilan@gmail.com>

--- artifactory-pro/plan.sh
+++ artifactory-pro/plan.sh
@@ -1,13 +1,13 @@
 pkg_origin=core
 pkg_name=artifactory-pro
-pkg_version=5.5.2
+pkg_version=5.8.0
 pkg_description="Artifactory is an advanced binary repository manager for use by build tools (like Maven and Gradle), dependency management tools (like Ivy and NuGet) and build servers (like Jenkins, Hudson, TeamCity and Bamboo).
 Repository managers serve two purposes: they act as highly configurable proxies between your organization and external repositories and they also provide build servers with a deployment destination for your internally generated artifacts."
 pkg_upstream_url=https://www.jfrog.com/artifactory/
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("JFrog Artifactory EULA")
 pkg_source=https://dl.bintray.com/jfrog/${pkg_name}/org/artifactory/pro/jfrog-${pkg_name}/${pkg_version}/jfrog-${pkg_name}-${pkg_version}.zip
-pkg_shasum=333ea04274a06de8d0937b2a04390e35eda5498c5ecbe7ac8ba2ff2af5fe1cfd
+pkg_shasum=096925328734eeb5acaf49a777c189055bdad123adb700282ad721a8a9ef23c8
 pkg_deps=(core/bash core/jre8)
 pkg_exports=(
   [port]=port
