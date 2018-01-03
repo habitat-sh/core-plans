c10b863ce19156259163ef90b386be850afc6d75
Mark Anderson <mark@chef.io>
2018-01-03 15:29:28 -0800

    Purge unneeded files
    Signed-off-by: Mark Anderson <mark@chef.io>

--- libpq/README.md
+++ libpq/README.md
@@ -7,4 +7,9 @@ libpq and the psql command line client utility.
 
 This is a subset of the postgresql habitat package, leaving out much
 of the server side code and postgis support. It is intended to be a
-lightweight package for client side applications.
+lightweight package for client side applications, providing both the
+build and runtime dependencies for clients.
+
+Postgresql-client was considered as a name, to follow mysql-client, but in
+the end the libpq-dev debian package came the closest to what this
+provides.
--- libpq/plan.sh
+++ libpq/plan.sh
@@ -27,6 +27,30 @@ pkg_bin_dirs=(bin)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
 
+
+# These commands only make sense for if there's a postgres server
+# running locally, and in that case can use the versions that came
+# with that install
+
+server_execs=(
+    ecpg
+    initdb
+    pg_archivecleanup
+    pg_config
+    pg_controldata
+    pg_resetxlog
+    pg_rewind
+    pg_test_fsync
+    pg_test_timing
+    pg_upgrade
+    pg_xlogdump
+)
+
+server_includes=(
+    postgresql/informix
+    postgresql/server
+)
+
 do_build() {
     # ld manpage: "If -rpath is not used when linking an ELF
     # executable, the contents of the environment variable LD_RUN_PATH
@@ -53,4 +77,21 @@ do_install() {
     # not all of the include files are needed for client side, but for simplicit's sake we install them all.
     make -C src/include install
     make -C src/interfaces install
+
+    # Clean up files needed only for server installs
+    # this shrinks the package by about 60%
+    echo "Purging unneeded execs"
+    for unneeded in "${server_execs[@]}"
+    do
+       target="$pkg_prefix/bin/${unneeded}"
+       echo "rm -f ${target}"
+       rm -f "${target}"
+    done
+    echo "Purging unneeded includes"
+    for unneeded in "${server_includes[@]}"
+    do
+       target="$pkg_prefix/include/${unneeded}"
+       echo "rm -rf ${target}"
+       rm -rf "${target}"
+    done
 }
