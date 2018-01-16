a051e6394f0365c7d9fd08c43de206d34b1d61a6
Fletcher Nichol <fnichol@nichol.ca>
2018-01-16 19:50:47 +0000

    [gcc] Upgrade to version 7.2.0.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- gcc/plan.sh
+++ gcc/plan.sh
@@ -1,15 +1,15 @@
 pkg_name=gcc
 pkg_distname=$pkg_name
 pkg_origin=core
-pkg_version=5.2.0
+pkg_version=7.2.0
 pkg_description="The GNU Compiler Collection"
 pkg_upstream_url="https://gcc.gnu.org/"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('GPL-2.0')
-pkg_source=http://ftp.gnu.org/gnu/$pkg_distname/${pkg_distname}-${pkg_version}/${pkg_distname}-${pkg_version}.tar.bz2
-pkg_shasum=5f835b04b5f7dd4f4d2dc96190ec1621b8d89f2dc6f638f9f8bc1b1014ba8cad
+pkg_source=http://ftp.gnu.org/gnu/$pkg_distname/${pkg_distname}-${pkg_version}/${pkg_distname}-${pkg_version}.tar.xz
+pkg_shasum=1cf7adf8ff4b5aa49041c8734bbcf1ad18cc4c94d0029aae0f4e48841088479a
 pkg_deps=(core/glibc core/zlib core/gmp core/mpfr core/libmpc core/binutils)
-pkg_build_deps=(core/coreutils core/diffutils core/patch core/make core/gcc core/gawk core/m4 core/texinfo core/perl core/inetutils core/expect core/dejagnu)
+pkg_build_deps=(core/coreutils core/diffutils core/patch core/file core/make core/gcc core/gawk core/m4 core/texinfo core/perl core/inetutils core/expect core/dejagnu)
 pkg_bin_dirs=(bin)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
@@ -39,8 +39,12 @@ do_prepare() {
   build_line "Updating CFLAGS=$CFLAGS"
 
   # Set `CXXFLAGS` for the c++ code
-  export CXXFLAGS="$CXXFLAGS $CFLAGS"
+  export CXXFLAGS="$CFLAGS"
   build_line "Setting CXXFLAGS=$CXXFLAGS"
+
+  # Set `CPPFLAGS` which is set by the build system
+  export CPPFLAGS="$CFLAGS"
+  build_line "Setting CPPFLAGS=$CPPFLAGS"
 
   # Ensure gcc can find the headers for zlib
   CPATH="$(pkg_path_for zlib)/include"
@@ -86,6 +90,12 @@ do_prepare() {
   #
   # Thanks to: https://projects.archlinux.org/svntogit/packages.git/tree/trunk/PKGBUILD?h=packages/gcc
   sed -i '/m64=/s/lib64/lib/' gcc/config/i386/t-linux64
+
+  # Update all references to the `/usr/bin/file` absolute path with `file`
+  # which will be on `$PATH` due to file being a build dependency.
+  grep -lr /usr/bin/file * | while read f; do
+    sed -i -e "s,/usr/bin/file,file,g" "$f"
+  done
 
   # Build up the build cflags that will be set for multiple environment
   # variables in the `make` command
@@ -137,6 +147,7 @@ do_build() {
       --enable-shared \
       --enable-threads=posix \
       --enable-install-libiberty \
+      --enable-vtable-verify \
       --disable-werror \
       --disable-multilib \
       --with-system-zlib \
@@ -189,19 +200,26 @@ do_check() {
     #
     # g++:
     #
-    #  XPASS: g++.dg/tls/thread_local-order2.C  -std=c++11 execution test
-    #  XPASS: g++.dg/tls/thread_local-order2.C  -std=c++14 execution test
     #  FAIL: c-c++-common/tsan/thread_leak1.c   -O0  output pattern test
     #  FAIL: c-c++-common/tsan/thread_leak1.c   -O2  output pattern test
     #
     # libstdc++:
     #
-    #  FAIL: 22_locale/messages/13631.cc execution test
-    #  FAIL: 22_locale/messages/members/char/1.cc execution test
-    #  FAIL: 22_locale/messages/members/char/2.cc execution test
-    #  FAIL: 22_locale/messages/members/char/wrapped_env.cc execution test
-    #  FAIL: 22_locale/messages/members/char/wrapped_locale.cc execution test
-    #  FAIL: 22_locale/messages_byname/named_equivalence.cc execution test
+    #  FAIL: 22_locale/codecvt/encoding/wchar_t/2.cc execution test
+    #  FAIL: 22_locale/codecvt/encoding/wchar_t/3.cc execution test
+    #  FAIL: 22_locale/codecvt/in/wchar_t/2.cc execution test
+    #  FAIL: 22_locale/codecvt/length/wchar_t/2.cc execution test
+    #  FAIL: 22_locale/codecvt/length/wchar_t/3.cc execution test
+    #  FAIL: 22_locale/codecvt/max_length/wchar_t/2.cc execution test
+    #  FAIL: 22_locale/codecvt/max_length/wchar_t/3.cc execution test
+    #  FAIL: 22_locale/codecvt/out/wchar_t/2.cc execution test
+    #  FAIL: 22_locale/codecvt/out/wchar_t/7.cc execution test
+    #  FAIL: 22_locale/ctype/widen/wchar_t/2.cc execution test
+    #  FAIL: experimental/filesystem/iterators/directory_iterator.cc execution test
+    #  FAIL: experimental/filesystem/iterators/recursive_directory_iterator.cc execution test
+    #  FAIL: experimental/filesystem/operations/exists.cc execution test
+    #  FAIL: experimental/filesystem/operations/is_empty.cc execution test
+    #  FAIL: experimental/filesystem/operations/temp_directory_path.cc execution test
 
     make -k check || true
     unset LIBRARY_PATH
@@ -288,5 +306,5 @@ wrap_binary() {
 # significantly altered. Thank you!
 # ----------------------------------------------------------------------------
 if [[ "$STUDIO_TYPE" = "stage1" ]]; then
-  pkg_build_deps=(core/m4)
+  pkg_build_deps=(core/m4 core/file)
 fi
