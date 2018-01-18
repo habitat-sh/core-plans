bf2277880ab25e6837f7974096f59d562b7c1438
Travis Elliott Davis <edavis@chef.io>
2018-01-18 13:24:08 -0600

    Make Sccache a service
    Signed-off-by: Travis Elliott Davis <edavis@chef.io>

--- sccache/plan.sh
+++ sccache/plan.sh
@@ -10,6 +10,8 @@ pkg_upstream_url="https://github.com/mozilla/sccache"
 pkg_description="sccache is ccache with cloud storage"
 pkg_deps=(core/glibc core/gcc-libs core/openssl)
 pkg_build_deps=(core/rust core/gcc core/pkg-config core/openssl core/make)
+pkg_svc_user="root"
+pkg_svc_group="root"
 
 do_build() {
   cargo build --release
