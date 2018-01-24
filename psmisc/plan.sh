a90b2eaea44b5fa2612e2630173ec52648310276
Fletcher Nichol <fnichol@nichol.ca>
2018-01-24 06:54:00 +0000

    [psmisc] Upgrade to version 23.1.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- psmisc/plan.sh
+++ psmisc/plan.sh
@@ -1,20 +1,13 @@
 pkg_name=psmisc
 pkg_origin=core
-pkg_version=22.21
+pkg_version=23.1
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('gpl')
-pkg_source=http://downloads.sourceforge.net/psmisc/${pkg_name}-${pkg_version}.tar.gz
-pkg_shasum=97323cad619210845b696d7d722c383852b2acb5c49b5b0852c4f29c77a8145a
+pkg_source=http://downloads.sourceforge.net/psmisc/${pkg_name}-${pkg_version}.tar.xz
+pkg_shasum=2e84d474cf75dfbe3ecdacfb797bbfab71a35c7c2639d1b9f6d5f18b2149ba30
 pkg_deps=(core/glibc core/ncurses)
 pkg_build_deps=(core/coreutils core/diffutils core/patch core/make core/gcc)
 pkg_bin_dirs=(bin)
-
-do_prepare() {
-  do_default_prepare
-
-  # Thanks to: https://raw.githubusercontent.com/NixOS/nixpkgs/release-15.09/pkgs/os-specific/linux/psmisc/0001-Typo-in-fuser-makes-M-on-all-the-time.patch
-  patch -p1 -i $PLAN_CONTEXT/0001-Typo-in-fuser-makes-M-on-all-the-time.patch
-}
 
 
 # ----------------------------------------------------------------------------
