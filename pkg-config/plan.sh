b3215b5c8958630ed5c86b7b03aea9d67ea5bacb
Fletcher Nichol <fnichol@nichol.ca>
2018-01-24 05:22:11 +0000

    [pkg-config] Upgrade to version 0.29.2.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- pkg-config/plan.sh
+++ pkg-config/plan.sh
@@ -1,10 +1,10 @@
 pkg_name=pkg-config
 pkg_origin=core
-pkg_version=0.29
+pkg_version=0.29.2
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('gplv2+')
 pkg_source=http://pkgconfig.freedesktop.org/releases/${pkg_name}-${pkg_version}.tar.gz
-pkg_shasum=c8507705d2a10c67f385d66ca2aae31e81770cc0734b4191eb8c489e864a006b
+pkg_shasum=6fc69c01688c9458a57eb9a1664c9aba372ccda420a02bf4429fe610e7e7d591
 pkg_deps=(core/glibc)
 pkg_build_deps=(core/coreutils core/diffutils core/patch core/make core/gcc)
 pkg_bin_dirs=(bin)
