0d68e4b53e45ed3bfb76ee276564a6d2c5b4261e
Fletcher Nichol <fnichol@nichol.ca>
2018-01-26 05:31:44 +0000

    [expat] Upgrade to version 2.2.5.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- expat/plan.sh
+++ expat/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=expat
 pkg_origin=core
-pkg_version=2.2.2
+pkg_version=2.2.5
 pkg_description="Expat library: Fast XML parser in C"
 pkg_upstream_url="https://libexpat.github.io/"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('MIT')
 pkg_source="https://downloads.sourceforge.net/project/${pkg_name}/${pkg_name}/${pkg_version}/${pkg_name}-${pkg_version}.tar.bz2"
-pkg_shasum=4376911fcf81a23ebd821bbabc26fd933f3ac74833f74924342c29aad2c86046
+pkg_shasum=d9dc32efba7e74f788fcc4f212a43216fc37cf5f23f4c2339664d473353aedf6
 pkg_deps=(core/glibc core/gcc-libs)
 pkg_build_deps=(core/coreutils core/diffutils core/patch core/make core/gcc)
 pkg_bin_dirs=(bin)
@@ -14,6 +14,9 @@ pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
 
 do_check() {
+  # Remove shebang line containing `/usr/bin/env` in test helper
+  sed -i 's,^#!.*bash$,,' run.sh
+
   # Set `LDFLAGS` for the c++ test code to find libstdc++
   make check LDFLAGS="$LDFLAGS -lstdc++"
 }
