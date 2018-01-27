c6295011d4e2d257eda4b07d86fb4fcd549c4c35
Romain Sertelon <romain@sertelon.fr>
2018-01-28 00:13:02 +0100

    [unzip] Improves package
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- unzip/plan.sh
+++ unzip/plan.sh
@@ -1,27 +1,34 @@
 pkg_name=unzip
-pkg_distname=$pkg_name
 pkg_origin=core
 pkg_version=6.0
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_license=('zlib')
-pkg_source=http://downloads.sourceforge.net/infozip/unzip60.tar.gz
-pkg_shasum=036d96991646d0449ed0aa952e4fbe21b476ce994abc276e49d30e686708bd37
+pkg_license=('Zlib')
+pkg_description="UnZip is an extraction utility for archives compressed in .zip format (also called 'zipfiles')."
+pkg_upstream_url="https://sourceforge.net/projects/infozip/"
+pkg_source="https://downloads.sourceforge.net/infozip/unzip60.tar.gz"
+pkg_shasum="036d96991646d0449ed0aa952e4fbe21b476ce994abc276e49d30e686708bd37"
 pkg_dirname=unzip60
-pkg_deps=(core/glibc core/bzip2)
-pkg_build_deps=(core/make core/gcc)
+pkg_deps=(
+  core/bzip2
+  core/glibc
+)
+pkg_build_deps=(
+  core/gcc
+  core/make
+)
 pkg_bin_dirs=(bin)
 
 do_build() {
-  DEFINES='-DACORN_FTYPE_NFS -DWILD_STOP_AT_DIR -DLARGE_FILE_SUPPORT \
+  DEFINES="-DACORN_FTYPE_NFS -DWILD_STOP_AT_DIR -DLARGE_FILE_SUPPORT \
     -DUNICODE_SUPPORT -DUNICODE_WCHAR -DUTF8_MAYBE_NATIVE -DNO_LCHMOD \
-    -DDATE_FORMAT=DF_YMD -DUSE_BZIP2 -DNOMEMCPY -DNO_WORKING_ISPRINT'
-  make -f unix/Makefile prefix=$pkg_prefix D_USE_BZ2=-DUSE_BZIP2 L_BZ2=-lbz2 \
+    -DDATE_FORMAT=DF_YMD -DUSE_BZIP2 -DNOMEMCPY -DNO_WORKING_ISPRINT"
+  make -f unix/Makefile prefix="${pkg_prefix}" D_USE_BZ2=-DUSE_BZIP2 L_BZ2=-lbz2 \
           LF2="$LDFLAGS" CF="$CFLAGS $CPPFLAGS -I. $DEFINES" \
           unzips
 }
 
 do_install() {
-  make -f unix/Makefile prefix=$pkg_prefix install
+  make -f unix/Makefile prefix="${pkg_prefix}" install
 }
 
 
