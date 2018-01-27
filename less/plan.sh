15f1a6907fdf494bd55ac35a822d33c5cc5fca66
Tim Smith <tsmith@chef.io>
2018-01-26 19:42:04 -0800

    Resolve warnings in Travis
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- less/plan.sh
+++ less/plan.sh
@@ -1,4 +1,5 @@
 pkg_name=less
+pkg_description="a terminal pager program used to view (but not change) the contents of a text file"
 pkg_origin=core
 pkg_version=487
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
@@ -11,7 +12,7 @@ pkg_bin_dirs=(bin)
 
 do_build() {
   ./configure \
-    --prefix=$pkg_prefix \
+    --prefix="$pkg_prefix" \
     --sysconfdir=/etc \
     --with-regex=pcre
   make
