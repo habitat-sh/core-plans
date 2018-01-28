15a68b276a60bc84a375e34cd2052b117d446ff8
Romain Sertelon <romain@sertelon.fr>
2018-01-28 20:20:15 +0100

    [check] Updates to 0.12.0
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- check/plan.sh
+++ check/plan.sh
@@ -1,23 +1,59 @@
 pkg_name=check
 pkg_origin=core
-pkg_version=0.10.0
-pkg_license=('lgplv2+')
+pkg_version=0.12.0
+pkg_license=('LGPL-2.1-or-later')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_source=http://downloads.sourceforge.net/sourceforge/$pkg_name/${pkg_name}-${pkg_version}.tar.gz
-pkg_shasum=f5f50766aa6f8fe5a2df752666ca01a950add45079aa06416b83765b1cf71052
-pkg_deps=(core/glibc core/gawk)
-pkg_build_deps=(core/coreutils core/diffutils core/patch core/make core/gcc core/sed)
+pkg_description="A unit test framework for C"
+pkg_upstream_url="https://libcheck.github.io/check"
+pkg_source="https://github.com/libcheck/check/releases/download/${pkg_version}/check-${pkg_version}.tar.gz"
+pkg_shasum="464201098bee00e90f5c4bdfa94a5d3ead8d641f9025b560a27755a83b824234"
+pkg_deps=(
+  core/gawk
+  core/glibc
+)
+pkg_build_deps=(
+  core/coreutils
+  core/diffutils
+  core/file
+  core/gcc
+  core/make
+  core/sed
+)
 pkg_bin_dirs=(bin)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
+pkg_pconfig_dirs=(lib/pkgconfig)
+
+do_prepare() {
+  if [[ ! -r /usr/bin/file ]]; then
+    ln -sv "$(pkg_path_for "core/file")/bin/file" /usr/bin/file
+    _clean_file=true
+  fi
+}
 
 do_install() {
   do_default_install
 
   # Clean up extra files
-  rm -rfv $pkg_prefix/share/info/dir $pkg_prefix/share/doc/check/*ChangeLog*
+  rm -rfv "${pkg_prefix}/share/info/dir" "${pkg_prefix}/share/doc/check/*ChangeLog*"
 }
 
+do_check() {
+  # Fix interpreters for test scripts
+  _interpreter="$(pkg_path_for "core/coreutils")/bin/env"
+  sed -e "s#\#\!/usr/bin/env#\#\!${_interpreter}#" -i "checkmk/test/check_checkmk"
+  for f in tests/test_*; do
+    sed -e "s#\#\!/usr/bin/env#\#\!${_interpreter}#" -i "$f"
+  done
+
+  make check
+}
+
+do_end() {
+  if [[ -n "$_clean_file" ]]; then
+    rm -fv /usr/bin/file
+  fi
+}
 
 # ----------------------------------------------------------------------------
 # **NOTICE:** What follows are implementation details required for building a
@@ -28,4 +64,10 @@ do_install() {
 # ----------------------------------------------------------------------------
 if [[ "$STUDIO_TYPE" = "stage1" ]]; then
   pkg_build_deps=(core/gcc core/coreutils core/sed core/diffutils core/make core/patch)
+  do_prepare() {
+    return 0
+  }
+  do_end() {
+    return 0
+  }
 fi
