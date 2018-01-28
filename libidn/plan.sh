8301eefd2d9549dd32b4c5f991def1883300423a
Romain Sertelon <romain@sertelon.fr>
2018-01-28 18:18:25 +0100

    [libidn] Updates to 1.33
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- libidn/plan.sh
+++ libidn/plan.sh
@@ -1,24 +1,27 @@
 pkg_name=libidn
 pkg_origin=core
 pkg_version=1.33
-pkg_description="Library for internationalized domain names"
-pkg_upstream_url=https://www.gnu.org/software/libidn/
-pkg_license=('LGPL-2.0')
+pkg_license=('LGPL-2.0-or-later')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_source=http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.gz
-pkg_shasum=44a7aab635bb721ceef6beecc4d49dfd19478325e1b47f3196f7d2acc4930e19
-pkg_deps=(core/glibc)
+pkg_description="GNU Libidn is a fully documented implementation of the Stringprep, Punycode and IDNA 2003 specifications"
+pkg_upstream_url="https://www.gnu.org/software/libidn/"
+pkg_source="http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.gz"
+pkg_shasum="44a7aab635bb721ceef6beecc4d49dfd19478325e1b47f3196f7d2acc4930e19"
+pkg_deps=(
+  core/glibc
+)
 pkg_build_deps=(
   core/coreutils
   core/diffutils
   core/file
-  core/gcc
-  core/make
   core/patch
+  core/make
+  core/gcc
 )
 pkg_bin_dirs=(bin)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
+pkg_pconfig_dirs=(lib/pkgconfig)
 
 do_prepare() {
   if [[ ! -r /usr/bin/file ]]; then
@@ -37,6 +40,7 @@ do_end() {
   fi
 }
 
+
 # ----------------------------------------------------------------------------
 # **NOTICE:** What follows are implementation details required for building a
 # first-pass, "stage1" toolchain and environment. It is only used when running
@@ -45,5 +49,5 @@ do_end() {
 # significantly altered. Thank you!
 # ----------------------------------------------------------------------------
 if [[ "$STUDIO_TYPE" = "stage1" ]]; then
-  pkg_build_deps=(core/gcc core/coreutils core/diffutils core/make core/patch)
+  pkg_build_deps=(core/gcc core/coreutils core/diffutils core/make core/patch core/file)
 fi
