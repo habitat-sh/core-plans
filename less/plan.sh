80c98058e72e42dd2d6003416782a14050c95f42
Fletcher Nichol <fnichol@nichol.ca>
2018-02-18 20:09:30 +0000

    [less] Upgrade to version 530.
    Release notes since 487:
    
    * Don't output terminal init sequence if using -F and file fits on one
    screen.
    * When using -S, mark truncated lines with a special character. The
    character can be changed or disabled via the new --rscroll option.
    * New command M marks the last line displayed on the screen.
    * New command ESC-m removes a line mark.
    * Status column (enabled via -J) now shows mark letters.
    * Status column shows search matches even if highlighting is disabled via
    -G.
    * A second ESC-u command will clear search match markers in the status
    column.
    * Do same ANSI escape code filtering for tag matching that we do for
    searching, to help when viewing syntax-highlighted code.
    * Catch SIGTERM and clean up before exiting.
    * Fix bug initializing default charset on Windows.
    * Handle keypad ENTER key correctly if it sends something other than
    newline.
    * Fix buffering bug when using stdin with a LESSOPEN pipe.
    * On Windows, allow 'u' in -D option to enable underlining.
    * On Windows, use underline in sgr mode.
    * On Windows, convert UTF-8 to multibyte if console is not UTF-8.
    * Update Unicode tables to 2017-03-08.
    * Pass-thru Unicode formating chars (Cf type) instead of treating them as
    binary chars. But treat them as binary if -U is set.
    * Fix erroneous binary file warning when UTF-8 file contains ANSI SGR
    sequences.
    * Fix bugs when using LESSOPEN and switching between stdin and other
    files.
    * Fix some bugs handling filenames containing shell metacharacters.
    * Fix some memory leaks.
    * Allow some debugging environment variables to be set in lesskey file.
    * Code improvements:
    * Use ANSI prototypes in funcs.h declarations.
    * Fix some const mismatches.
    * Remove archaic "register" in variable declarations.
    
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- less/plan.sh
+++ less/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=less
 pkg_origin=core
-pkg_version=487
+pkg_version=530
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="a terminal pager program used to view (but not change) the contents of a text file"
 pkg_license=('gplv3+')
 pkg_upstream_url="http://www.greenwoodsoftware.com/less/index.html"
 pkg_source=http://www.greenwoodsoftware.com/$pkg_name/${pkg_name}-${pkg_version}.tar.gz
-pkg_shasum=f3dc8455cb0b2b66e0c6b816c00197a71bf6d1787078adeee0bcf2aea4b12706
+pkg_shasum=503f91ab0af4846f34f0444ab71c4b286123f0044a4964f1ae781486c617f2e2
 pkg_deps=(core/glibc core/ncurses core/pcre)
 pkg_build_deps=(core/coreutils core/diffutils core/patch core/make core/gcc)
 pkg_bin_dirs=(bin)
