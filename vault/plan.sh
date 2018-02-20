991ad31f97cf9ca3ce0da5e0c04719686198f3ee
Christopher P. Maher <chris@mahercode.io>
2018-02-20 01:21:07 -0800

    Updating Vault to 0.9.3
    Signed-off-by: Christopher P. Maher <chris@mahercode.io>

--- vault/plan.sh
+++ vault/plan.sh
@@ -1,12 +1,12 @@
 pkg_origin=core
 pkg_name=vault
-pkg_version=0.9.1
+pkg_version=0.9.3
 pkg_description="A tool for managing secrets."
 pkg_maintainer='The Habitat Maintainers <humans@habitat.sh>'
 pkg_license=("MPL-2.0")
 pkg_upstream_url=https://www.vaultproject.io/
 pkg_source=https://releases.hashicorp.com/vault/${pkg_version}/vault_${pkg_version}_linux_amd64.zip
-pkg_shasum=6308013ee0d6278e98cdfe8d6de0162102a8d25f3bcd1e3737bf7b022a9f6702
+pkg_shasum=4ba8b9dafb903d20b4c87e8954015a07a6995f53a9650ac02b39e6cae502645e
 pkg_filename=${pkg_name}-${pkg_version}_linux_amd64.zip
 pkg_deps=()
 pkg_build_deps=(core/unzip)
