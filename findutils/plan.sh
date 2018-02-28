432d7eba9ee09b140937342b3a7fe301427c028d
Fletcher Nichol <fnichol@nichol.ca>
2018-02-28 22:48:56 +0000

    [findutils] Upgrade to version 4.6.0.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- findutils/plan.sh
+++ findutils/plan.sh
@@ -1,30 +1,23 @@
 pkg_name=findutils
 pkg_origin=core
-pkg_version=4.4.2
+pkg_version=4.6.0
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('gplv3+')
 pkg_source=http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.gz
-pkg_shasum=434f32d171cbc0a5e72cfc5372c6fc4cb0e681f8dce566a0de5b6fccd702b62a
+pkg_shasum=ded4c9f73731cd48fec3b6bdaccce896473b6d8e337e9612e16cf1431bb1169d
 pkg_deps=(core/glibc)
 pkg_build_deps=(core/coreutils core/diffutils core/patch core/make core/gcc core/sed)
 pkg_bin_dirs=(bin)
-
-do_prepare() {
-  do_default_prepare
-
-  # Fix a few hardcoded, absolute paths in the codebase.
-  #
-  # Thanks to: https://github.com/NixOS/nixpkgs/blob/release-15.09/pkgs/tools/misc/findutils/default.nix#L28
-  patch -p1 -i $PLAN_CONTEXT/updatedb-path.patch
-  patch -p1 -i $PLAN_CONTEXT/xargs-echo-path.patch
-  patch -p1 -i $PLAN_CONTEXT/disable-test-canonicalize.patch
-}
 
 do_build() {
   ./configure \
     --prefix=$pkg_prefix \
     --localstatedir=$pkg_svc_var_path/locate
   make
+}
+
+do_check() {
+  make check
 }
 
 
