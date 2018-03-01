5e4a81f3ed921e2be809c00f232adb43899f8c89
Fletcher Nichol <fnichol@nichol.ca>
2018-03-01 20:04:56 +0000

    [mg] Upgrade to version 20171014.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- mg/plan.sh
+++ mg/plan.sh
@@ -1,10 +1,10 @@
 pkg_name=mg
 pkg_origin=core
-pkg_version=20160118
+pkg_version=20171014
 pkg_license=('publicdomain')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source=http://homepage.boetes.org/software/$pkg_name/${pkg_name}-${pkg_version}.tar.gz
-pkg_shasum=26450b2564bec0b0afc465fd24a1917dc31508c5500c3a36823b9c763a2b8636
+pkg_shasum=51519698f3f44acd984d7805e4e315ded50c15aba8222521f88756fd67745341
 pkg_deps=(core/glibc core/ncurses core/libbsd)
 pkg_build_deps=(core/coreutils core/diffutils core/patch core/make core/gcc core/sed core/pkg-config core/clens)
 pkg_bin_dirs=(bin)
@@ -17,6 +17,7 @@ do_prepare() {
       -e "s,@libbsd_prefix@,$(pkg_path_for libbsd),g" \
     | patch -p1
 }
+
 do_build() {
   make \
     prefix=$pkg_prefix \
