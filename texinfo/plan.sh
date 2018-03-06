8809fc49d1b6f2495e3726b68ed2673d6fa7e8e1
Fletcher Nichol <fnichol@nichol.ca>
2018-03-06 20:26:12 +0000

    [texinfo] Update & modernize Plan style.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- texinfo/plan.sh
+++ texinfo/plan.sh
@@ -2,11 +2,29 @@ pkg_name=texinfo
 pkg_origin=core
 pkg_version=6.5
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_description="\
+Texinfo is the official documentation format of the GNU project. It was \
+invented by Richard Stallman and Bob Chassell many years ago, loosely based on \
+Brian Reid's Scribe and other formatting languages of the time. It is used by \
+many non-GNU projects as well.\
+"
+pkg_upstream_url="http://www.gnu.org/software/texinfo/"
 pkg_license=('gplv3+')
-pkg_source=http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.xz
-pkg_shasum=77774b3f4a06c20705cc2ef1c804864422e3cf95235e965b1f00a46df7da5f62
-pkg_deps=(core/glibc core/ncurses core/perl)
-pkg_build_deps=(core/coreutils core/diffutils core/patch core/make core/gcc core/sed)
+pkg_source="http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.xz"
+pkg_shasum="77774b3f4a06c20705cc2ef1c804864422e3cf95235e965b1f00a46df7da5f62"
+pkg_deps=(
+  core/glibc
+  core/ncurses
+  core/perl
+)
+pkg_build_deps=(
+  core/coreutils
+  core/diffutils
+  core/patch
+  core/make
+  core/gcc
+  core/sed
+)
 pkg_bin_dirs=(bin)
 
 do_check() {
@@ -22,5 +40,12 @@ do_check() {
 # significantly altered. Thank you!
 # ----------------------------------------------------------------------------
 if [[ "$STUDIO_TYPE" = "stage1" ]]; then
-  pkg_build_deps=(core/gcc core/coreutils core/sed core/diffutils core/make core/patch)
+  pkg_build_deps=(
+    core/gcc
+    core/coreutils
+    core/sed
+    core/diffutils
+    core/make
+    core/patch
+  )
 fi
