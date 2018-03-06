16bfb4e0f5566585e08961af1f775d66270c5ef0
Fletcher Nichol <fnichol@nichol.ca>
2018-03-06 19:29:23 +0000

    [db] Update & modernize Plan style.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- db/plan.sh
+++ db/plan.sh
@@ -2,13 +2,25 @@ pkg_name=db
 pkg_origin=core
 pkg_version=5.3.28
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_description="Berkeley DB is a family of embedded key-value database libraries providing scalable high-performance data management services to applications."
-pkg_license=('custom')
-pkg_source=http://download.oracle.com/berkeley-db/${pkg_name}-${pkg_version}.tar.gz
+pkg_description="\
+Berkeley DB is a family of embedded key-value database libraries providing \
+scalable high-performance data management services to applications.\
+"
 pkg_upstream_url="http://www.oracle.com/technetwork/database/database-technologies/berkeleydb/overview/index.html"
-pkg_shasum=e0a992d740709892e81f9d93f06daf305cf73fb81b545afe72478043172c3628
-pkg_deps=(core/glibc core/gcc-libs)
-pkg_build_deps=(core/coreutils core/diffutils core/patch core/make core/gcc)
+pkg_license=('custom')
+pkg_source="http://download.oracle.com/berkeley-db/${pkg_name}-${pkg_version}.tar.gz"
+pkg_shasum="e0a992d740709892e81f9d93f06daf305cf73fb81b545afe72478043172c3628"
+pkg_deps=(
+  core/glibc
+  core/gcc-libs
+)
+pkg_build_deps=(
+  core/coreutils
+  core/diffutils
+  core/patch
+  core/make
+  core/gcc
+)
 pkg_bin_dirs=(bin)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
@@ -44,5 +56,8 @@ do_install() {
 # significantly altered. Thank you!
 # ----------------------------------------------------------------------------
 if [[ "$STUDIO_TYPE" = "stage1" ]]; then
-  pkg_build_deps=(core/gcc core/coreutils)
+  pkg_build_deps=(
+    core/gcc
+    core/coreutils
+  )
 fi
