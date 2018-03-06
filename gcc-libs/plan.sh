62cdb5b0d3d9f13ba84d7d8787509ff064fac67d
Fletcher Nichol <fnichol@nichol.ca>
2018-03-06 00:19:52 +0000

    [gcc-libs] Update & modernize Plan style.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- gcc-libs/plan.sh
+++ gcc-libs/plan.sh
@@ -1,17 +1,25 @@
 source ../gcc/plan.sh
 
 pkg_name=gcc-libs
+pkg_origin=core
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_description="Runtime libraries shipped by GCC."
+pkg_upstream_url="https://gcc.gnu.org/"
+pkg_license=('GPL-2.0')
 
 # The shared libraries only depend on core/glibc
-pkg_deps=(core/glibc)
+pkg_deps=(
+  core/glibc
+)
 # Add the same version of the full gcc package as a build dep
-pkg_build_deps=(core/gcc/$pkg_version core/patchelf)
+pkg_build_deps=(
+  core/gcc/$pkg_version
+  core/patchelf
+)
 
 # Zero out the bin and include paths, as we're only shipping shared libs
 pkg_bin_dirs=()
 pkg_include_dirs=()
-
 
 # The list of GCC libraries to copy
 _gcc_libs=(
@@ -31,23 +39,28 @@ _gcc_libs=(
 )
 
 do_install() {
-  mkdir -pv $pkg_prefix/lib
+  mkdir -pv "$pkg_prefix/lib"
   for lib in "${_gcc_libs[@]}"; do
-    cp -av $(pkg_path_for gcc)/lib/${lib}.* $pkg_prefix/lib/
+    cp -av "$(pkg_path_for gcc)/lib/${lib}".* "$pkg_prefix/lib"/
   done
-  rm -fv $pkg_prefix/lib/*.spec $pkg_prefix/lib/*.py
+  rm -fv "$pkg_prefix/lib"/*.spec "$pkg_prefix/lib"/*.py
 
-  mkdir -pv $pkg_prefix/share/licenses
-  cp -av $(pkg_path_for gcc)/share/licenses/RUNTIME.LIBRARY.EXCEPTION \
-    $pkg_prefix/share/licenses/
+  mkdir -pv "$pkg_prefix/share/licenses"
+  cp -av "$(pkg_path_for gcc)"/share/licenses/RUNTIME.LIBRARY.EXCEPTION \
+    "$pkg_prefix/share/licenses/"
 
   # Due to the copy-from-package trick above, the resulting `RUNPATH` entries
   # have more path entries than are actually being used (for mpfr, libmpc,
   # etc), so we'll use `patchelf` trim these unused path entries for each
   # shared library.
-  find $pkg_prefix/lib -type f -name '*.so.*' \
-    -exec patchelf --set-rpath $(pkg_path_for glibc)/lib:$pkg_prefix/lib {} \;
-  find $pkg_prefix/lib -type f -name '*.so.*' -exec patchelf --shrink-rpath {} \;
+  find "$pkg_prefix/lib" \
+    -type f \
+    -name '*.so.*' \
+    -exec patchelf --set-rpath "$(pkg_path_for glibc)/lib:$pkg_prefix/lib" {} \;
+  find "$pkg_prefix/lib" \
+    -type f \
+    -name '*.so.*' \
+    -exec patchelf --shrink-rpath {} \;
 }
 
 # Turn the remaining default phases into no-ops
