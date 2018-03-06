282ed497906519a474026062b42bc035bbd24e79
Fletcher Nichol <fnichol@nichol.ca>
2018-03-06 19:08:40 +0000

    [gawk] Update & modernize Plan style.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- gawk/plan.sh
+++ gawk/plan.sh
@@ -2,11 +2,27 @@ pkg_name=gawk
 pkg_origin=core
 pkg_version=4.2.0
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_description="\
+The awk utility interprets a special-purpose programming language that makes \
+it possible to handle simple data-reformatting jobs with just a few lines of \
+code.\
+"
+pkg_upstream_url="http://www.gnu.org/software/gawk/"
 pkg_license=('gplv3+')
-pkg_source=http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.gz
-pkg_shasum=c88046c6e8396ee548bcb941e16def809b7b55b60a1044b5dd254094f347c7d9
-pkg_deps=(core/glibc core/mpfr)
-pkg_build_deps=(core/coreutils core/diffutils core/patch core/make core/gcc core/sed)
+pkg_source="http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.gz"
+pkg_shasum="c88046c6e8396ee548bcb941e16def809b7b55b60a1044b5dd254094f347c7d9"
+pkg_deps=(
+  core/glibc
+  core/mpfr
+)
+pkg_build_deps=(
+  core/coreutils
+  core/diffutils
+  core/patch
+  core/make
+  core/gcc
+  core/sed
+)
 pkg_bin_dirs=(bin)
 pkg_interpreters=(bin/awk bin/gawk)
 
@@ -23,5 +39,9 @@ do_check() {
 # significantly altered. Thank you!
 # ----------------------------------------------------------------------------
 if [[ "$STUDIO_TYPE" = "stage1" ]]; then
-  pkg_build_deps=(core/gcc core/coreutils core/sed)
+  pkg_build_deps=(
+    core/gcc
+    core/coreutils
+    core/sed
+  )
 fi
