46343a806075e5ca4c6a9ba9ee0a012a1384d081
Fletcher Nichol <fnichol@nichol.ca>
2018-03-06 23:35:23 +0000

    [unzip] Update & modernize Plan style.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- unzip/plan.sh
+++ unzip/plan.sh
@@ -2,9 +2,12 @@ pkg_name=unzip
 pkg_origin=core
 pkg_version=6.0
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_license=('Zlib')
-pkg_description="UnZip is an extraction utility for archives compressed in .zip format (also called 'zipfiles')."
+pkg_description="\
+UnZip is an extraction utility for archives compressed in .zip format (also \
+called 'zipfiles').\
+"
 pkg_upstream_url="https://sourceforge.net/projects/infozip/"
+pkg_license=('Zlib')
 pkg_source="https://downloads.sourceforge.net/infozip/unzip60.tar.gz"
 pkg_shasum="036d96991646d0449ed0aa952e4fbe21b476ce994abc276e49d30e686708bd37"
 pkg_dirname=unzip60
@@ -22,9 +25,14 @@ do_build() {
   DEFINES="-DACORN_FTYPE_NFS -DWILD_STOP_AT_DIR -DLARGE_FILE_SUPPORT \
     -DUNICODE_SUPPORT -DUNICODE_WCHAR -DUTF8_MAYBE_NATIVE -DNO_LCHMOD \
     -DDATE_FORMAT=DF_YMD -DUSE_BZIP2 -DNOMEMCPY -DNO_WORKING_ISPRINT"
-  make -f unix/Makefile prefix="${pkg_prefix}" D_USE_BZ2=-DUSE_BZIP2 L_BZ2=-lbz2 \
-          LF2="$LDFLAGS" CF="$CFLAGS $CPPFLAGS -I. $DEFINES" \
-          unzips
+  make \
+    -f unix/Makefile \
+    prefix="${pkg_prefix}" \
+    D_USE_BZ2=-DUSE_BZIP2 \
+    L_BZ2=-lbz2 \
+    LF2="$LDFLAGS" \
+    CF="$CFLAGS $CPPFLAGS -I. $DEFINES" \
+    unzips
 }
 
 do_install() {
@@ -40,5 +48,14 @@ do_install() {
 # significantly altered. Thank you!
 # ----------------------------------------------------------------------------
 if [[ "$STUDIO_TYPE" = "stage1" ]]; then
-  pkg_build_deps=(core/gcc core/pkg-config core/coreutils core/sed core/grep core/diffutils core/make core/patch)
+  pkg_build_deps=(
+    core/gcc
+    core/pkg-config
+    core/coreutils
+    core/sed
+    core/grep
+    core/diffutils
+    core/make
+    core/patch
+  )
 fi
