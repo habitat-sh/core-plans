799fdd02e304abc1c89052adb94446969f018056
Fletcher Nichol <fnichol@nichol.ca>
2018-03-06 00:58:25 +0000

    [sed] Update & modernize Plan style.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- sed/plan.sh
+++ sed/plan.sh
@@ -2,11 +2,28 @@ pkg_name=sed
 pkg_origin=core
 pkg_version=4.4
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_description="\
+sed (stream editor) is a non-interactive command-line text editor. sed is \
+commonly used to filter text, i.e., it takes text input, performs some \
+operation (or set of operations) on it, and outputs the modified text. sed is \
+typically used for extracting part of a file using pattern matching or \
+substituting multiple occurrences of a string within a file.\
+"
+pkg_upstream_url="https://www.gnu.org/software/sed/"
 pkg_license=('gplv3')
-pkg_source=http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.xz
-pkg_shasum=cbd6ebc5aaf080ed60d0162d7f6aeae58211a1ee9ba9bb25623daa6cd942683b
-pkg_deps=(core/glibc core/acl)
-pkg_build_deps=(core/coreutils core/diffutils core/patch core/make core/gcc)
+pkg_source="http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.xz"
+pkg_shasum="cbd6ebc5aaf080ed60d0162d7f6aeae58211a1ee9ba9bb25623daa6cd942683b"
+pkg_deps=(
+  core/glibc
+  core/acl
+)
+pkg_build_deps=(
+  core/coreutils
+  core/diffutils
+  core/patch
+  core/make
+  core/gcc
+)
 pkg_bin_dirs=(bin)
 
 do_prepare() {
@@ -40,5 +57,7 @@ do_check() {
 # significantly altered. Thank you!
 # ----------------------------------------------------------------------------
 if [[ "$STUDIO_TYPE" = "stage1" ]]; then
-  pkg_build_deps=(core/gcc)
+  pkg_build_deps=(
+    core/gcc
+  )
 fi
