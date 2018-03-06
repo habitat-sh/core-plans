4284ca2364a62106d82c8f6a244f606385344eeb
Fletcher Nichol <fnichol@nichol.ca>
2018-03-06 23:17:18 +0000

    [cacerts] Update & modernize Plan style.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- cacerts/plan.sh
+++ cacerts/plan.sh
@@ -1,10 +1,12 @@
 pkg_name=cacerts
 pkg_origin=core
-pkg_version=_set_from_downloaded_cacerts_file_
+pkg_description="\
+The Mozilla CA certificate store in PEM format (around 250KB uncompressed).
+"
+pkg_upstream_url="https://curl.haxx.se/docs/caextract.html"
 pkg_license=('MPL-2.0')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_source=http://curl.haxx.se/ca/cacert.pem
-pkg_shasum=nopenopebucketofnope
+pkg_source="http://curl.haxx.se/ca/cacert.pem"
 pkg_deps=()
 pkg_build_deps=()
 
