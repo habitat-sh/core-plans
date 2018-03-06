4fa70cb8109f2b7fcb5ff055168d1594fb97a3fc
Fletcher Nichol <fnichol@nichol.ca>
2018-03-06 19:11:39 +0000

    [libtool] Update & modernize Plan style.
    Note that `core/coreutils` was removed from `pkg_build_deps` as it was
    already present in `pkg_deps`.
    
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- libtool/plan.sh
+++ libtool/plan.sh
@@ -3,11 +3,28 @@ pkg_origin=core
 pkg_version=2.4.6
 pkg_license=('gplv2+')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_source=http://ftp.gnu.org/gnu/${pkg_name}/${pkg_name}-${pkg_version}.tar.gz
-pkg_filename=${pkg_name}-${pkg_version}.tar.gz
-pkg_shasum=e3bd4d5d3d025a36c21dd6af7ea818a2afcd4dfc1ea5a17b39d7854bcd0c06e3
-pkg_deps=(core/glibc core/coreutils core/sed core/grep core/binutils)
-pkg_build_deps=(core/coreutils core/diffutils core/patch core/make core/gcc core/m4)
+pkg_description="\
+GNU libtool is a generic library support script. Libtool hides the complexity \
+of using shared libraries behind a consistent, portable interface.\
+"
+pkg_upstream_url="http://www.gnu.org/software/libtool"
+pkg_source="http://ftp.gnu.org/gnu/${pkg_name}/${pkg_name}-${pkg_version}.tar.gz"
+pkg_shasum="e3bd4d5d3d025a36c21dd6af7ea818a2afcd4dfc1ea5a17b39d7854bcd0c06e3"
+pkg_filename="${pkg_name}-${pkg_version}.tar.gz"
+pkg_deps=(
+  core/glibc
+  core/coreutils
+  core/sed
+  core/grep
+  core/binutils
+)
+pkg_build_deps=(
+  core/diffutils
+  core/patch
+  core/make
+  core/gcc
+  core/m4
+)
 pkg_bin_dirs=(bin)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
@@ -40,5 +57,8 @@ do_build() {
 # significantly altered. Thank you!
 # ----------------------------------------------------------------------------
 if [[ "$STUDIO_TYPE" = "stage1" ]]; then
-  pkg_build_deps=(core/gcc core/m4)
+  pkg_build_deps=(
+    core/gcc
+    core/m4
+  )
 fi
