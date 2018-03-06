391661001802991b0f27fe52b9acad6ac1359c0b
Fletcher Nichol <fnichol@nichol.ca>
2018-03-06 18:02:19 +0000

    [bash-static] Update & modernize Plan style.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- bash-static/plan.sh
+++ bash-static/plan.sh
@@ -1,11 +1,25 @@
 source ../bash/plan.sh
 
 pkg_name=bash-static
+pkg_origin=core
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_dirname=${pkg_distname}-$_base_version
+pkg_description="\
+ Bash is the GNU Project's shell. Bash is the Bourne Again SHell. Bash is an \
+sh-compatible shell that incorporates useful features from the Korn shell \
+(ksh) and C shell (csh). It is intended to conform to the IEEE POSIX \
+P1003.2/ISO 9945.2 Shell and Tools standard. It offers functional \
+improvements over sh for both programming and interactive use. In addition, \
+most sh scripts can be run by Bash without modification.\
+"
+pkg_upstream_url="http://www.gnu.org/software/bash/bash.html"
+pkg_license=('gplv3+')
+pkg_dirname=${_distname}-$_base_version
 
 # Throw the run deps into build deps as this will be static
-pkg_build_deps=("${pkg_deps[@]}" "${pkg_build_deps[@]}")
+pkg_build_deps=(
+  "${pkg_deps[@]}"
+  "${pkg_build_deps[@]}"
+)
 # Empty out the run deps array
 pkg_deps=()
 
@@ -15,11 +29,11 @@ do_begin() {
 
 do_build() {
   ./configure \
-    --prefix=$pkg_prefix \
+    --prefix="$pkg_prefix" \
     --with-curses \
     --enable-readline \
     --without-bash-malloc \
-    --with-installed-readline=$(pkg_path_for core/readline) \
+    --with-installed-readline="$(pkg_path_for core/readline)" \
     LDFLAGS="-static $LDFLAGS"
 
   # Link `-ltinfow` rather than `-lcurses` as some symbols appear to have moved
