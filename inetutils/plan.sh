e49d121b63d53bd13146bfc1f427ded914b22f91
Fletcher Nichol <fnichol@nichol.ca>
2018-03-06 19:37:38 +0000

    [inetutils] Update & modernize Plan style.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- inetutils/plan.sh
+++ inetutils/plan.sh
@@ -2,11 +2,29 @@ pkg_name=inetutils
 pkg_origin=core
 pkg_version=1.9.4
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_description="\
+Inetutils is a collection of common network programs. It includes: an ftp \
+client and server, a telnet client and server, an rsh client and server, an \
+rlogin client and server, a tftp client and server, and much more...\
+"
+pkg_upstream_url="http://www.gnu.org/software/inetutils/"
 pkg_license=('gplv3+')
-pkg_source=http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.xz
-pkg_shasum=849d96f136effdef69548a940e3e0ec0624fc0c81265296987986a0dd36ded37
-pkg_deps=(core/glibc core/libcap core/ncurses)
-pkg_build_deps=(core/coreutils core/diffutils core/patch core/make core/gcc core/sed core/grep)
+pkg_source="http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.xz"
+pkg_shasum="849d96f136effdef69548a940e3e0ec0624fc0c81265296987986a0dd36ded37"
+pkg_deps=(
+  core/glibc
+  core/libcap
+  core/ncurses
+)
+pkg_build_deps=(
+  core/coreutils
+  core/diffutils
+  core/patch
+  core/make
+  core/gcc
+  core/sed
+  core/grep
+)
 pkg_bin_dirs=(bin)
 
 do_build() {
@@ -22,7 +40,7 @@ do_build() {
   #   this codebase, such as `telnetd`, `ftpd`, etc.--a dedicated Plan for
   #   any of these service components is much preferred
   ./configure \
-    --prefix=$pkg_prefix \
+    --prefix="$pkg_prefix" \
     --disable-logger \
     --disable-whois \
     --disable-rcp \
@@ -37,7 +55,7 @@ do_install() {
   do_default_install
 
   # `libexec/` directory is not used
-  rm -rfv $pkg_prefix/libexec
+  rm -rfv "$pkg_prefix/libexec"
 }
 
 
@@ -49,5 +67,10 @@ do_install() {
 # significantly altered. Thank you!
 # ----------------------------------------------------------------------------
 if [[ "$STUDIO_TYPE" = "stage1" ]]; then
-  pkg_build_deps=(core/gcc core/coreutils core/sed core/grep)
+  pkg_build_deps=(
+    core/gcc
+    core/coreutils
+    core/sed
+    core/grep
+  )
 fi
