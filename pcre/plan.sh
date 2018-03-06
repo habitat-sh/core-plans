62cffe810dfb536911f3cddeb60b8a9d5bee1f30
Fletcher Nichol <fnichol@nichol.ca>
2018-03-06 17:21:55 +0000

    [pcre] Update & modernize Plan style.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- pcre/plan.sh
+++ pcre/plan.sh
@@ -2,13 +2,28 @@ pkg_name=pcre
 pkg_origin=core
 pkg_version=8.41
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_description="A library that implements Perl 5-style regular expressions"
+pkg_description="\
+The PCRE library is a set of functions that implement regular expression \
+pattern matching using the same syntax and semantics as Perl 5. PCRE has its \
+own native API, as well as a set of wrapper functions that correspond to the \
+POSIX regular expression API. The PCRE library is free, even for building \
+proprietary software.\
+"
 pkg_upstream_url="http://www.pcre.org/"
 pkg_license=('bsd')
 pkg_source="https://ftp.pcre.org/pub/${pkg_name}/${pkg_name}-${pkg_version}.tar.bz2"
-pkg_shasum=e62c7eac5ae7c0e7286db61ff82912e1c0b7a0c13706616e94a7dd729321b530
-pkg_deps=(core/glibc core/gcc-libs)
-pkg_build_deps=(core/coreutils core/diffutils core/patch core/make core/gcc)
+pkg_shasum="e62c7eac5ae7c0e7286db61ff82912e1c0b7a0c13706616e94a7dd729321b530"
+pkg_deps=(
+  core/glibc
+  core/gcc-libs
+)
+pkg_build_deps=(
+  core/coreutils
+  core/diffutils
+  core/patch
+  core/make
+  core/gcc
+)
 pkg_bin_dirs=(bin)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
@@ -45,5 +60,8 @@ do_install() {
 # significantly altered. Thank you!
 # ----------------------------------------------------------------------------
 if [[ "$STUDIO_TYPE" = "stage1" ]]; then
-  pkg_build_deps=(core/gcc core/coreutils)
+  pkg_build_deps=(
+    core/gcc
+    core/coreutils
+  )
 fi
