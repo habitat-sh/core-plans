d6d302213ff3979578fb3c9d1fcbcb6540a011b7
Fletcher Nichol <fnichol@nichol.ca>
2018-03-06 19:23:42 +0000

    [gdbm] Update & modernize Plan style.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- gdbm/plan.sh
+++ gdbm/plan.sh
@@ -2,18 +2,31 @@ pkg_name=gdbm
 pkg_origin=core
 pkg_version=1.14.1
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_description="\
+GNU dbm is a set of database routines that use extensible hashing. It works \
+similar to the standard UNIX dbm routines.\
+"
+pkg_upstream_url="http://www.gnu.org/software/gdbm/gdbm.html"
 pkg_license=('gplv3+')
-pkg_source=http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.gz
-pkg_shasum=cdceff00ffe014495bed3aed71c7910aa88bf29379f795abc0f46d4ee5f8bc5f
-pkg_deps=(core/glibc)
-pkg_build_deps=(core/coreutils core/diffutils core/patch core/make core/gcc)
+pkg_source="http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.gz"
+pkg_shasum="cdceff00ffe014495bed3aed71c7910aa88bf29379f795abc0f46d4ee5f8bc5f"
+pkg_deps=(
+  core/glibc
+)
+pkg_build_deps=(
+  core/coreutils
+  core/diffutils
+  core/patch
+  core/make
+  core/gcc
+)
 pkg_bin_dirs=(bin)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
 
 do_build() {
   ./configure \
-    --prefix=$pkg_prefix \
+    --prefix="$pkg_prefix" \
     --enable-libgdbm-compat
   make
 }
@@ -26,10 +39,10 @@ do_install() {
   do_default_install
 
   # create symlinks for compatibility
-  install -dm755 ${pkg_prefix}/include/gdbm
-  ln -sf ../gdbm.h ${pkg_prefix}/include/gdbm/gdbm.h
-  ln -sf ../ndbm.h ${pkg_prefix}/include/gdbm/ndbm.h
-  ln -sf ../dbm.h  ${pkg_prefix}/include/gdbm/dbm.h
+  install -dm755 "$pkg_prefix/include/gdbm"
+  ln -sf ../gdbm.h "$pkg_prefix/include/gdbm/gdbm.h"
+  ln -sf ../ndbm.h "$pkg_prefix/include/gdbm/ndbm.h"
+  ln -sf ../dbm.h  "$pkg_prefix/include/gdbm/dbm.h"
 }
 
 
@@ -41,5 +54,8 @@ do_install() {
 # significantly altered. Thank you!
 # ----------------------------------------------------------------------------
 if [[ "$STUDIO_TYPE" = "stage1" ]]; then
-  pkg_build_deps=(core/gcc core/coreutils)
+  pkg_build_deps=(
+    core/gcc
+    core/coreutils
+  )
 fi
