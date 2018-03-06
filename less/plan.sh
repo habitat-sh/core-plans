ba63ec9bda60d796bdcf7f4fe5118fc5b8542941
Fletcher Nichol <fnichol@nichol.ca>
2018-03-06 19:40:00 +0000

    [less] Update & modernize Plan style.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- less/plan.sh
+++ less/plan.sh
@@ -2,13 +2,26 @@ pkg_name=less
 pkg_origin=core
 pkg_version=530
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_description="a terminal pager program used to view (but not change) the contents of a text file"
-pkg_license=('gplv3+')
+pkg_description="\
+A terminal pager program used to view (but not change) the contents of a text \
+file.\
+"
 pkg_upstream_url="http://www.greenwoodsoftware.com/less/index.html"
-pkg_source=http://www.greenwoodsoftware.com/$pkg_name/${pkg_name}-${pkg_version}.tar.gz
-pkg_shasum=503f91ab0af4846f34f0444ab71c4b286123f0044a4964f1ae781486c617f2e2
-pkg_deps=(core/glibc core/ncurses core/pcre)
-pkg_build_deps=(core/coreutils core/diffutils core/patch core/make core/gcc)
+pkg_license=('gplv3+')
+pkg_source="http://www.greenwoodsoftware.com/$pkg_name/${pkg_name}-${pkg_version}.tar.gz"
+pkg_shasum="503f91ab0af4846f34f0444ab71c4b286123f0044a4964f1ae781486c617f2e2"
+pkg_deps=(
+  core/glibc
+  core/ncurses
+  core/pcre
+)
+pkg_build_deps=(
+  core/coreutils
+  core/diffutils
+  core/patch
+  core/make
+  core/gcc
+)
 pkg_bin_dirs=(bin)
 
 do_build() {
@@ -28,5 +41,8 @@ do_build() {
 # significantly altered. Thank you!
 # ----------------------------------------------------------------------------
 if [[ "$STUDIO_TYPE" = "stage1" ]]; then
-  pkg_build_deps=(core/gcc core/coreutils)
+  pkg_build_deps=(
+    core/gcc
+    core/coreutils
+  )
 fi
