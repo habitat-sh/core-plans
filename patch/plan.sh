810dcbf5d0b96698763e8401ad8a20981fccd9cb
Fletcher Nichol <fnichol@nichol.ca>
2018-03-06 20:24:01 +0000

    [patch] Update & modernize Plan style.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- patch/plan.sh
+++ patch/plan.sh
@@ -2,11 +2,26 @@ pkg_name=patch
 pkg_origin=core
 pkg_version=2.7.6
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_description="\
+Patch takes a patch file containing a difference listing produced by the diff \
+program and applies those differences to one or more original files, producing \
+patched versions.\
+"
+pkg_upstream_url="https://www.gnu.org/software/patch/"
 pkg_license=('gplv3+')
-pkg_source=http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.xz
-pkg_shasum=ac610bda97abe0d9f6b7c963255a11dcb196c25e337c61f94e4778d632f1d8fd
-pkg_deps=(core/glibc core/attr)
-pkg_build_deps=(core/coreutils core/diffutils core/make core/gcc core/sed)
+pkg_source="http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.xz"
+pkg_shasum="ac610bda97abe0d9f6b7c963255a11dcb196c25e337c61f94e4778d632f1d8fd"
+pkg_deps=(
+  core/glibc
+  core/attr
+)
+pkg_build_deps=(
+  core/coreutils
+  core/diffutils
+  core/make
+  core/gcc
+  core/sed
+)
 pkg_bin_dirs=(bin)
 
 do_check() {
@@ -22,5 +37,11 @@ do_check() {
 # significantly altered. Thank you!
 # ----------------------------------------------------------------------------
 if [[ "$STUDIO_TYPE" = "stage1" ]]; then
-  pkg_build_deps=(core/gcc core/coreutils core/sed core/diffutils core/make)
+  pkg_build_deps=(
+    core/gcc
+    core/coreutils
+    core/sed
+    core/diffutils
+    core/make
+  )
 fi
