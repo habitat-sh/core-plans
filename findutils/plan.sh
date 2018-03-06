7101d9cf117035094cfd194359c5ffe123451f63
Fletcher Nichol <fnichol@nichol.ca>
2018-03-06 20:06:42 +0000

    [findutils] Update & modernize Plan style.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- findutils/plan.sh
+++ findutils/plan.sh
@@ -2,17 +2,33 @@ pkg_name=findutils
 pkg_origin=core
 pkg_version=4.6.0
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_description="\
+The GNU Find Utilities are the basic directory searching utilities of the GNU \
+operating system. These programs are typically used in conjunction with other \
+programs to provide modular and powerful directory search and file locating \
+capabilities to other commands.\
+"
+pkg_upstream_url="http://www.gnu.org/software/findutils"
 pkg_license=('gplv3+')
-pkg_source=http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.gz
-pkg_shasum=ded4c9f73731cd48fec3b6bdaccce896473b6d8e337e9612e16cf1431bb1169d
-pkg_deps=(core/glibc)
-pkg_build_deps=(core/coreutils core/diffutils core/patch core/make core/gcc core/sed)
+pkg_source="http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.gz"
+pkg_shasum="ded4c9f73731cd48fec3b6bdaccce896473b6d8e337e9612e16cf1431bb1169d"
+pkg_deps=(
+  core/glibc
+)
+pkg_build_deps=(
+  core/coreutils
+  core/diffutils
+  core/patch
+  core/make
+  core/gcc
+  core/sed
+)
 pkg_bin_dirs=(bin)
 
 do_build() {
   ./configure \
-    --prefix=$pkg_prefix \
-    --localstatedir=$pkg_svc_var_path/locate
+    --prefix="$pkg_prefix" \
+    --localstatedir="$pkg_svc_var_path/locate"
   make
 }
 
@@ -29,5 +45,10 @@ do_check() {
 # significantly altered. Thank you!
 # ----------------------------------------------------------------------------
 if [[ "$STUDIO_TYPE" = "stage1" ]]; then
-  pkg_build_deps=(core/gcc core/coreutils core/sed core/diffutils)
+  pkg_build_deps=(
+    core/gcc
+    core/coreutils
+    core/sed
+    core/diffutils
+  )
 fi
