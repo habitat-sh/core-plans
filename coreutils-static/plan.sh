48a410f8c97dbf00828f4e3d58e1127cd163a2d0
Fletcher Nichol <fnichol@nichol.ca>
2018-03-06 01:22:48 +0000

    [coreutils-static] Update & modernize Plan style.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- coreutils-static/plan.sh
+++ coreutils-static/plan.sh
@@ -1,13 +1,28 @@
 source ../coreutils/plan.sh
 
 pkg_name=coreutils-static
+pkg_origin=core
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_dirname=${pkg_distname}-${pkg_version}
+pkg_description="\
+The GNU Core Utilities are the basic file, shell and text manipulation \
+utilities of the GNU operating system. These are the core utilities which are \
+expected to exist on every operating system.\
+"
+pkg_upstream_url="https://www.gnu.org/software/coreutils/"
+pkg_license=('GPL-3.0')
+pkg_dirname=${_distname}-${pkg_version}
 
 # Throw the run deps into build deps as this will be static
-pkg_build_deps=("${pkg_deps[@]}" "${pkg_build_deps[@]}")
+pkg_build_deps=(
+  "${pkg_deps[@]}"
+  "${pkg_build_deps[@]}"
+)
 # Empty out the run deps array
 pkg_deps=()
+
+do_prepare() {
+  PLAN_CONTEXT=$PLAN_CONTEXT/../coreutils _patch_files
+}
 
 do_build() {
   # Uses the `--enable-single-binary` flag to build a single binary file
@@ -15,7 +30,7 @@ do_build() {
   #
   # Thanks to: https://lists.gnu.org/archive/html/coreutils/2014-06/msg00036.html
   FORCE_UNSAFE_CONFIGURE=1 ./configure \
-    --prefix=$pkg_prefix \
+    --prefix="$pkg_prefix" \
     --enable-single-binary \
     LDFLAGS="-static $LDFLAGS"
   make
