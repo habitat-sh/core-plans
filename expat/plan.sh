629d5e41b8045efc10785a277ba7aa23e7313f4a
Fletcher Nichol <fnichol@nichol.ca>
2018-03-06 19:25:58 +0000

    [expat] Modernize Plan.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- expat/plan.sh
+++ expat/plan.sh
@@ -1,14 +1,26 @@
 pkg_name=expat
 pkg_origin=core
 pkg_version=2.2.5
-pkg_description="Expat library: Fast XML parser in C"
-pkg_upstream_url="https://libexpat.github.io/"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_description="\
+Expat is a stream-oriented XML parser library written in C. Expat excels with \
+files too large to fit RAM, and where performance and flexibility are crucial.\
+"
+pkg_upstream_url="https://libexpat.github.io/"
 pkg_license=('MIT')
 pkg_source="https://downloads.sourceforge.net/project/${pkg_name}/${pkg_name}/${pkg_version}/${pkg_name}-${pkg_version}.tar.bz2"
-pkg_shasum=d9dc32efba7e74f788fcc4f212a43216fc37cf5f23f4c2339664d473353aedf6
-pkg_deps=(core/glibc core/gcc-libs)
-pkg_build_deps=(core/coreutils core/diffutils core/patch core/make core/gcc)
+pkg_shasum="d9dc32efba7e74f788fcc4f212a43216fc37cf5f23f4c2339664d473353aedf6"
+pkg_deps=(
+  core/glibc
+  core/gcc-libs
+)
+pkg_build_deps=(
+  core/coreutils
+  core/diffutils
+  core/patch
+  core/make
+  core/gcc
+)
 pkg_bin_dirs=(bin)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
@@ -37,5 +49,8 @@ do_install() {
 # significantly altered. Thank you!
 # ----------------------------------------------------------------------------
 if [[ "$STUDIO_TYPE" = "stage1" ]]; then
-  pkg_build_deps=(core/gcc core/coreutils)
+  pkg_build_deps=(
+    core/gcc
+    core/coreutils
+  )
 fi
