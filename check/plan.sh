fcc1858516ce9104231b85f36671d5a9594341fc
Fletcher Nichol <fnichol@nichol.ca>
2018-03-06 23:12:43 +0000

    [check] Update & modernize Plan style.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- check/plan.sh
+++ check/plan.sh
@@ -1,10 +1,15 @@
 pkg_name=check
 pkg_origin=core
 pkg_version=0.12.0
-pkg_license=('LGPL-2.1-or-later')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_description="A unit test framework for C"
+pkg_description="\
+Check is a unit testing framework for C. It features a simple interface for \
+defining unit tests, putting little in the way of the developer. Tests are run \
+in a separate address space, so both assertion failures and code errors that \
+cause segmentation faults or other signals can be caught.\
+"
 pkg_upstream_url="https://libcheck.github.io/check"
+pkg_license=('LGPL-2.1-or-later')
 pkg_source="https://github.com/libcheck/check/releases/download/${pkg_version}/check-${pkg_version}.tar.gz"
 pkg_shasum="464201098bee00e90f5c4bdfa94a5d3ead8d641f9025b560a27755a83b824234"
 pkg_deps=(
@@ -25,23 +30,16 @@ pkg_lib_dirs=(lib)
 pkg_pconfig_dirs=(lib/pkgconfig)
 
 do_prepare() {
-  if [[ ! -r /usr/bin/file ]]; then
-    ln -sv "$(pkg_path_for "core/file")/bin/file" /usr/bin/file
-    _clean_file=true
-  fi
-}
-
-do_install() {
-  do_default_install
-
-  # Clean up extra files
-  rm -rfv "${pkg_prefix}/share/info/dir" "${pkg_prefix}/share/doc/check/*ChangeLog*"
+  # Update all references to the `/usr/bin/file` absolute path with `file`
+  # which will be on `$PATH` due to file being a build dependency.
+  sed -i -e "s,/usr/bin/file,file,g" ./configure
 }
 
 do_check() {
   # Fix interpreters for test scripts
   _interpreter="$(pkg_path_for "core/coreutils")/bin/env"
-  sed -e "s#\#\!/usr/bin/env#\#\!${_interpreter}#" -i "checkmk/test/check_checkmk"
+  sed -e "s#\#\!/usr/bin/env#\#\!${_interpreter}#" \
+    -i "checkmk/test/check_checkmk"
   for f in tests/test_*; do
     sed -e "s#\#\!/usr/bin/env#\#\!${_interpreter}#" -i "$f"
   done
@@ -49,11 +47,14 @@ do_check() {
   make check
 }
 
-do_end() {
-  if [[ -n "$_clean_file" ]]; then
-    rm -fv /usr/bin/file
-  fi
+do_install() {
+  do_default_install
+
+  # Clean up extra files
+  rm -rfv "${pkg_prefix}/share/info/dir" \
+    "${pkg_prefix}/share/doc/check/*ChangeLog*"
 }
+
 
 # ----------------------------------------------------------------------------
 # **NOTICE:** What follows are implementation details required for building a
@@ -63,11 +64,13 @@ do_end() {
 # significantly altered. Thank you!
 # ----------------------------------------------------------------------------
 if [[ "$STUDIO_TYPE" = "stage1" ]]; then
-  pkg_build_deps=(core/gcc core/coreutils core/sed core/diffutils core/make core/patch)
-  do_prepare() {
-    return 0
-  }
-  do_end() {
-    return 0
-  }
+  pkg_build_deps=(
+    core/gcc
+    core/coreutils
+    core/sed
+    core/diffutils
+    core/make
+    core/patch
+    core/file
+  )
 fi
