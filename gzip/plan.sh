de9036fa9868e835db47491963023e113f1b7b34
Fletcher Nichol <fnichol@nichol.ca>
2018-03-06 20:18:32 +0000

    [gzip] Update & modernize Plan style.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- gzip/plan.sh
+++ gzip/plan.sh
@@ -2,11 +2,28 @@ pkg_name=gzip
 pkg_origin=core
 pkg_version=1.9
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_description="\
+GNU Gzip is a popular data compression program originally written by Jean-loup \
+Gailly for the GNU project.\
+"
+pkg_upstream_url="https://www.gnu.org/software/gzip/"
 pkg_license=('gplv3+')
-pkg_source=http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.xz
-pkg_shasum=ae506144fc198bd8f81f1f4ad19ce63d5a2d65e42333255977cf1dcf1479089a
-pkg_deps=(core/glibc core/less core/grep)
-pkg_build_deps=(core/coreutils core/diffutils core/patch core/make core/gcc core/sed core/xz)
+pkg_source="http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.xz"
+pkg_shasum="ae506144fc198bd8f81f1f4ad19ce63d5a2d65e42333255977cf1dcf1479089a"
+pkg_deps=(
+  core/glibc
+  core/less
+  core/grep
+)
+pkg_build_deps=(
+  core/coreutils
+  core/diffutils
+  core/patch
+  core/make
+  core/gcc
+  core/sed
+  core/xz
+)
 pkg_bin_dirs=(bin)
 
 do_prepare() {
@@ -46,5 +63,11 @@ do_check() {
 # significantly altered. Thank you!
 # ----------------------------------------------------------------------------
 if [[ "$STUDIO_TYPE" = "stage1" ]]; then
-  pkg_build_deps=(core/gcc core/coreutils core/sed core/diffutils core/xz)
+  pkg_build_deps=(
+    core/gcc
+    core/coreutils
+    core/sed
+    core/diffutils
+    core/xz
+  )
 fi
