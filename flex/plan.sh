c0d6ca9097b3057d53736b37f1c364c50d5807b0
Fletcher Nichol <fnichol@nichol.ca>
2018-03-06 17:17:15 +0000

    [flex] Update & modernize Plan style.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- flex/plan.sh
+++ flex/plan.sh
@@ -2,11 +2,26 @@ pkg_name=flex
 pkg_origin=core
 pkg_version=2.6.4
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_description="\
+flex is a tool for generating scanners: programs which recognize lexical \
+patterns in text.\
+"
+pkg_upstream_url="https://github.com/westes/flex"
 pkg_license=('custom')
 pkg_source="https://github.com/westes/$pkg_name/releases/download/v${pkg_version}/${pkg_name}-${pkg_version}.tar.gz"
-pkg_shasum=e87aae032bf07c26f85ac0ed3250998c37621d95f8bd748b31f15b33c45ee995
-pkg_deps=(core/glibc)
-pkg_build_deps=(core/coreutils core/diffutils core/patch core/make core/gcc core/m4 core/bison)
+pkg_shasum="e87aae032bf07c26f85ac0ed3250998c37621d95f8bd748b31f15b33c45ee995"
+pkg_deps=(
+  core/glibc
+)
+pkg_build_deps=(
+  core/coreutils
+  core/diffutils
+  core/patch
+  core/make
+  core/gcc
+  core/m4
+  core/bison
+)
 pkg_bin_dirs=(bin)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
@@ -21,7 +36,7 @@ do_install() {
 
   # A few programs do not know about `flex` yet and try to run its predecessor,
   # `lex`
-  ln -sv flex $pkg_prefix/bin/lex
+  ln -sv flex "$pkg_prefix/bin/lex"
 }
 
 
@@ -33,5 +48,10 @@ do_install() {
 # significantly altered. Thank you!
 # ----------------------------------------------------------------------------
 if [[ "$STUDIO_TYPE" = "stage1" ]]; then
-  pkg_build_deps=(core/gcc core/m4 core/coreutils core/bison)
+  pkg_build_deps=(
+    core/gcc
+    core/m4
+    core/coreutils
+    core/bison
+  )
 fi
