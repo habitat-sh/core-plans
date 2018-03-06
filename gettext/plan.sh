90146663f3ed4970cce1d2689a1cc5d9bc6f53a7
Fletcher Nichol <fnichol@nichol.ca>
2018-03-06 20:16:17 +0000

    [gettext] Update & modernize Plan style.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- gettext/plan.sh
+++ gettext/plan.sh
@@ -2,19 +2,40 @@ pkg_name=gettext
 pkg_origin=core
 pkg_version=0.19.8
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_description="GNU internationalization library."
+pkg_upstream_url="http://www.gnu.org/software/gettext/"
 pkg_license=('gplv2+' 'lgpl2+')
-pkg_source=http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.gz
-pkg_shasum=3da4f6bd79685648ecf46dab51d66fcdddc156f41ed07e580a696a38ac61d48f
-pkg_deps=(core/glibc core/gcc-libs core/acl core/xz)
-pkg_build_deps=(core/coreutils core/diffutils core/patch core/make core/gcc core/sed core/findutils)
+pkg_source="http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.gz"
+pkg_shasum="3da4f6bd79685648ecf46dab51d66fcdddc156f41ed07e580a696a38ac61d48f"
+pkg_deps=(
+  core/glibc
+  core/gcc-libs
+  core/acl
+  core/xz
+)
+pkg_build_deps=(
+  core/coreutils
+  core/diffutils
+  core/patch
+  core/make
+  core/gcc
+  core/sed
+  core/findutils
+)
 pkg_bin_dirs=(bin)
 pkg_lib_dirs=(lib)
 pkg_include_dirs=(include)
 
+do_prepare() {
+  do_default_prepare
+
+  patch -p1 -i "$PLAN_CONTEXT/disable-test.patch"
+}
+
 do_build() {
   ./configure \
-    --prefix=$pkg_prefix
-  make -j$(nproc)
+    --prefix="$pkg_prefix"
+  make -j"$(nproc)"
 }
 
 do_check() {
@@ -30,5 +51,11 @@ do_check() {
 # significantly altered. Thank you!
 # ----------------------------------------------------------------------------
 if [[ "$STUDIO_TYPE" = "stage1" ]]; then
-  pkg_build_deps=(core/gcc core/coreutils core/sed core/diffutils core/findutils)
+  pkg_build_deps=(
+    core/gcc
+    core/coreutils
+    core/sed
+    core/diffutils
+    core/findutils
+  )
 fi
