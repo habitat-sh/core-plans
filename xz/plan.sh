68d5c3b29a78858ca45078f98d955e5c861aba9e
Fletcher Nichol <fnichol@nichol.ca>
2018-03-06 20:12:41 +0000

    [xz] Update & modernize Plan style.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- xz/plan.sh
+++ xz/plan.sh
@@ -1,14 +1,29 @@
 pkg_name=xz
-pkg_distname=$pkg_name
+_distname="$pkg_name"
 pkg_origin=core
 pkg_version=5.2.3
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_description="\
+XZ Utils is free general-purpose data compression software with a high \
+compression ratio. XZ Utils were written for POSIX-like systems, but also work \
+on some not-so-POSIX systems. XZ Utils are the successor to LZMA Utils.\
+"
+pkg_upstream_url="http://tukaani.org/xz/"
 pkg_license=('gpl2+' 'lgpl2+')
-pkg_source=http://tukaani.org/${pkg_distname}/${pkg_distname}-${pkg_version}.tar.gz
-pkg_shasum=71928b357d0a09a12a4b4c5fafca8c31c19b0e7d3b8ebb19622e96f26dbf28cb
-pkg_dirname=${pkg_distname}-${pkg_version}
-pkg_deps=(core/glibc)
-pkg_build_deps=(core/coreutils core/diffutils core/patch core/make core/gcc core/sed)
+pkg_source="http://tukaani.org/${_distname}/${_distname}-${pkg_version}.tar.gz"
+pkg_shasum="71928b357d0a09a12a4b4c5fafca8c31c19b0e7d3b8ebb19622e96f26dbf28cb"
+pkg_dirname="${_distname}-${pkg_version}"
+pkg_deps=(
+  core/glibc
+)
+pkg_build_deps=(
+  core/coreutils
+  core/diffutils
+  core/patch
+  core/make
+  core/gcc
+  core/sed
+)
 pkg_bin_dirs=(bin)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
@@ -26,5 +41,10 @@ do_check() {
 # significantly altered. Thank you!
 # ----------------------------------------------------------------------------
 if [[ "$STUDIO_TYPE" = "stage1" ]]; then
-  pkg_build_deps=(core/gcc core/coreutils core/sed core/diffutils)
+  pkg_build_deps=(
+    core/gcc
+    core/coreutils
+    core/sed
+    core/diffutils
+  )
 fi
