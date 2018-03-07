eb7a1e5c460d864d28c5fef9c12126bf3501a47e
Fletcher Nichol <fnichol@nichol.ca>
2018-03-07 01:54:38 +0000

    [bats] Update & modernize Plan style.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- bats/plan.sh
+++ bats/plan.sh
@@ -1,13 +1,15 @@
 pkg_name=bats
 pkg_origin=core
-pkg_version='0.4.0'
-pkg_maintainer='The Habitat Maintainers <humans@habitat.sh>'
+pkg_version=0.4.0
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_description="\
+Bats is a TAP-compliant testing framework for Bash. It provides a simple way \
+to verify that the UNIX programs you write behave as expected.\
+"
+pkg_upstream_url="https://github.com/sstephenson/bats"
 pkg_license=('MIT')
-pkg_description="Bash Automated Testing System"
-pkg_upstream_url='https://github.com/sstephenson/bats'
-pkg_filename="v$pkg_version.tar.gz"
-pkg_source="https://github.com/sstephenson/bats/archive/$pkg_filename"
-pkg_shasum='480d8d64f1681eee78d1002527f3f06e1ac01e173b761bc73d0cf33f4dc1d8d7'
+pkg_source="https://github.com/sstephenson/bats/archive/v$pkg_version.tar.gz"
+pkg_shasum="480d8d64f1681eee78d1002527f3f06e1ac01e173b761bc73d0cf33f4dc1d8d7"
 pkg_deps=(
   core/bash
   core/coreutils
@@ -17,6 +19,10 @@ pkg_bin_dirs=(bin)
 do_build() {
   fix_interpreter 'install.sh' core/coreutils bin/env
   fix_interpreter 'libexec/*' core/coreutils bin/env
+}
+
+do_check() {
+  ./bin/bats --tap test
 }
 
 do_install() {
