b7e7ef3effdf986a1672657dc35b27e6faa44b61
Fletcher Nichol <fnichol@nichol.ca>
2018-03-07 00:02:49 +0000

    [musl] Update & modernize Plan style.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- musl/plan.sh
+++ musl/plan.sh
@@ -1,10 +1,14 @@
 pkg_name=musl
 pkg_origin=core
 pkg_version=1.1.19
-pkg_license=('MIT')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_description="musl is a new standard library to power a new generation of Linux-based devices. musl is lightweight, fast, simple, free, and strives to be correct in the sense of standards-conformance and safety."
+pkg_description="\
+musl is a new standard library to power a new generation of Linux-based \
+devices. musl is lightweight, fast, simple, free, and strives to be correct \
+in the sense of standards-conformance and safety.\
+"
 pkg_upstream_url="https://www.musl-libc.org/"
+pkg_license=('MIT')
 pkg_source="http://www.musl-libc.org/releases/${pkg_name}-${pkg_version}.tar.gz"
 pkg_shasum="db59a8578226b98373f5b27e61f0dd29ad2456f4aa9cec587ba8c24508e4c1d9"
 pkg_deps=()
@@ -22,8 +26,9 @@ pkg_lib_dirs=(lib)
 
 do_prepare() {
   stack_size="2097152"
-  build_line "Setting default stack size to '$stack_size' from default of '81920'"
-  sed -i "s/#define DEFAULT_STACK_SIZE .*/#define DEFAULT_STACK_SIZE $stack_size/" \
+  build_line "Setting default stack size to '$stack_size' from default '81920'"
+  sed \
+    -i "s/#define DEFAULT_STACK_SIZE .*/#define DEFAULT_STACK_SIZE $stack_size/" \
     src/internal/pthread_impl.h
 }
 
@@ -50,5 +55,12 @@ do_install() {
 # significantly altered. Thank you!
 # ----------------------------------------------------------------------------
 if [[ "$STUDIO_TYPE" = "stage1" ]]; then
-  pkg_build_deps=(core/gcc core/coreutils core/sed core/diffutils core/make core/patch)
+  pkg_build_deps=(
+    core/gcc
+    core/coreutils
+    core/sed
+    core/diffutils
+    core/make
+    core/patch
+  )
 fi
