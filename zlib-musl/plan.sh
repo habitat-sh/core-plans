f10f203895d5276d69764ae151c520c5e497d8b9
Fletcher Nichol <fnichol@nichol.ca>
2018-03-07 00:15:44 +0000

    [zlib-musl] Update & modernize Plan style.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- zlib-musl/plan.sh
+++ zlib-musl/plan.sh
@@ -1,8 +1,17 @@
 source ../zlib/plan.sh
 
 pkg_name=zlib-musl
+pkg_origin=core
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_deps=(core/musl)
+pkg_description="\
+Compression library implementing the deflate compression method found in gzip \
+and PKZIP.\
+"
+pkg_upstream_url="http://www.zlib.net/"
+pkg_license=('zlib')
+pkg_deps=(
+  core/musl
+)
 
 do_prepare() {
   export CC=musl-gcc
