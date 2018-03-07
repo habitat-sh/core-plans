48eac60c961797be7c56475cda4f5e55f40eba4d
Fletcher Nichol <fnichol@nichol.ca>
2018-03-07 00:12:24 +0000

    [busybox] Update & modernize Plan style.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- busybox/plan.sh
+++ busybox/plan.sh
@@ -1,13 +1,21 @@
 pkg_name=busybox
-pkg_distname=$pkg_name
+_distname="$pkg_name"
 pkg_origin=core
 pkg_version=1.28.1
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_description="\
+BusyBox is the Swiss Army Knife of embedded Linux. BusyBox combines tiny \
+versions of many common UNIX utilities into a single small executable. It \
+provides replacements for most of the utilities you usually find in GNU \
+fileutils, shellutils, etc.\
+"
+pkg_upstream_url="https://www.busybox.net/"
 pkg_license=('gplv2')
-pkg_source=http://www.busybox.net/downloads/${pkg_distname}-${pkg_version}.tar.bz2
-pkg_shasum=98fe1d3c311156c597cd5cfa7673bb377dc552b6fa20b5d3834579da3b13652e
-
-pkg_deps=(core/glibc)
+pkg_source="http://www.busybox.net/downloads/${_distname}-${pkg_version}.tar.bz2"
+pkg_shasum="98fe1d3c311156c597cd5cfa7673bb377dc552b6fa20b5d3834579da3b13652e"
+pkg_deps=(
+  core/glibc
+)
 pkg_build_deps=(
   core/bash
   core/bison
@@ -29,32 +37,32 @@ pkg_build_deps=(
   core/wget
   core/xz
 )
-
 pkg_bin_dirs=(bin)
 pkg_interpreters=(bin/ash bin/awk bin/env bin/sh bin/bash)
 
 do_prepare() {
-  create_config
+  _create_config
 }
 
 do_build() {
-  make -j$(nproc)
+  make -j"$(nproc)"
 }
 
 do_install() {
-  install -Dm755 busybox $pkg_prefix/bin/busybox
+  install -Dm755 busybox "$pkg_prefix/bin/busybox"
 
   # Generate the symlinks back to the `busybox` executable
-  for l in $($pkg_prefix/bin/busybox --list); do
-    ln -sv busybox $pkg_prefix/bin/$l
+  for l in $("$pkg_prefix/bin/busybox" --list); do
+    ln -sv busybox "$pkg_prefix/bin/$l"
   done
 }
 
-create_config() {
+_create_config() {
   # To update to a new version, run `make defconfig` to generate a new
   # `.config` file and add the following replacement tokens below.
   build_line "Customizing busybox configuration..."
-  cat $PLAN_CONTEXT/config \
+  # shellcheck disable=SC2002
+  cat "$PLAN_CONTEXT/config" \
     | sed \
       -e "s,@pkg_prefix@,$pkg_prefix,g" \
       -e "s,@pkg_svc_var@,$pkg_svc_var_path,g" \
