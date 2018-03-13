3dc3252c9f16405f9ea816400415d5db62ae0250
Fletcher Nichol <fnichol@nichol.ca>
2018-03-13 02:57:45 +0000

    [linux-headers] Upgrade to version 4.15.9.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- linux-headers/plan.sh
+++ linux-headers/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=linux-headers
 pkg_origin=core
-pkg_version=4.15.7
+pkg_version=4.15.9
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="The Linux kernel headers"
 pkg_upstream_url="https://kernel.org"
 pkg_license=('gplv2')
 pkg_source="https://www.kernel.org/pub/linux/kernel/v4.x/linux-${pkg_version}.tar.xz"
-pkg_shasum="cd44df4b23a3e0edc14be63df95d768b9600b31c35be05fb89f93226907fc8c6"
+pkg_shasum="dda015b2042e71c6d0df56de553846df1252eac486514000c76b741cde6d4492"
 pkg_dirname="linux-$pkg_version"
 pkg_deps=()
 pkg_build_deps=(
