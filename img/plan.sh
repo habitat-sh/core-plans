a867aa65bf18cda9aeecc41ce5ab220db05ed699
Ian Henry <ihenry@chef.io>
2018-03-15 12:13:35 -0400

    [img] version bump
    Signed-off-by: Ian Henry <ihenry@chef.io>

--- img/plan.sh
+++ img/plan.sh
@@ -1,12 +1,12 @@
 pkg_name="img"
 pkg_origin="core"
-pkg_version=0.2.4
+pkg_version=0.3.0
 pkg_description="Standalone, daemon-less, unprivileged Dockerfile and OCI compatible container image builder."
 pkg_upstream_url="https://github.com/genuinetools/img"
 pkg_license=('MIT')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="https://github.com/genuinetools/img"
-pkg_shasum="73549dc3eb29005dae5248385c6b40310a323b817714853d35d7194abfa357b8"
+pkg_shasum="37fb69555f178370a67568e0004c7e67a048673fd26e7ab29e327c3ccdb57ace"
 pkg_bin_dirs=(bin)
 pkg_scaffolding=core/scaffolding-go
 pkg_deps=(core/runc core/git)
