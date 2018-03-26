f243684dfa57b615ae65a811042389f57ee0ada8
Christopher P. Maher <chris@mahercode.io>
2018-03-26 10:31:12 -0700

    Adding description
    Signed-off-by: Christopher P. Maher <chris@mahercode.io>

--- libtalloc/plan.sh
+++ libtalloc/plan.sh
@@ -3,6 +3,7 @@ pkg_origin=core
 pkg_version="2.1.12"
 pkg_upstream_url=https://talloc.samba.org
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_description="Talloc is a hierarchical, reference counted memory pool system with destructors. It is the core memory allocator used in Samba."
 pkg_license=("GPL-3.0")
 pkg_source="https://www.samba.org/ftp/talloc/talloc-${pkg_version}.tar.gz"
 pkg_dirname="talloc-${pkg_version}"
