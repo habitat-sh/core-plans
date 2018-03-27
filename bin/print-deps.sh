8d7ee0fc06ff6c40a045d679414506432bbc65b3
Fletcher Nichol <fnichol@nichol.ca>
2018-03-27 00:18:14 +0000

    [infra] Extract `print-deps.sh` from Ruby programs.
    This change extracts a small but common Bash program that is used by:
    
    * `bin/build-dependent-order.rb`
    * `bin/build-order.rb`
    
    into `bin/print-deps.sh`. This program takes a `plan.sh` file, loads it
    in a minimal way similar to the build program, and echos out:
    
    * The `$pkg_origin/$pkg_name` on the first line
    * All the buildtime and runtime dependency package identifiers on the
    second line, split by whitespace
    
    Future work could add a pre-commit lint to all Plans that they must
    allow `bin/print-deps.sh` to load without errors, thus ensuring that no
    core plans have side effects when sourced.
    
    Additionally, in the process of extracting this logic and testing it
    against the full core Plans set, I realized that Scaffolding packages
    were not fully considered as the existing code pre-dated this feature.
    While accounting for Scaffolding in here `print-deps.sh` may not be
    100% correct, it did satisfy the current needs for core Plans
    specifically.
    
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

---
+++ bin/print-deps.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# shellcheck disable=SC2034,SC2154,SC1090
+
+# Only set exit-on-error. No other values should be set.
+set -e
+
+# Simulate a stage1 build to get any special base plan build dependency lists
+STUDIO_TYPE=stage1
+# Some base plans use this value if they need to be built twice, simulate first
+# pas
+FIRST_PASS=true
+# Provide an absolute `PLAN_CONTEXT` path for any Plans that require it
+PLAN_CONTEXT="$(pwd)/$(dirname "$1")"
+
+# Change directory into the `PLAN_CONTEXT` directory, just like the build
+# progra
+cd "$(dirname "$1")"
+
+# Load the Plan
+source "$(basename "$1")"
+
+# Echo the `<pkg_origin>/<pkg_name>` package identifier on the first line
+echo "${pkg_origin:-${HAB_ORIGIN:-unknown}}/${pkg_name}"
+# Echo all buildtime and runtime dependent package identifiers on the second
+# line
+echo "${pkg_build_deps[*]} ${pkg_deps[*]} $pkg_scaffolding"
+
+exit 0
