edf8d5e50bff060fab7c76ce861e37d9d0289466
Fletcher Nichol <fnichol@nichol.ca>
2018-03-29 20:52:38 +0000

    [elfutils] Upgrade to version 0.170.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- elfutils/plan.sh
+++ elfutils/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=elfutils
 pkg_origin=core
-pkg_version=0.166
+pkg_version=0.170
 pkg_license=('GPL-3.0')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="elfutils is a collection of various binary tools such as
@@ -8,7 +8,7 @@ pkg_description="elfutils is a collection of various binary tools such as
   manipulate ELF files."
 pkg_upstream_url=https://fedorahosted.org/elfutils/
 pkg_source=https://fedorahosted.org/releases/e/l/$pkg_name/$pkg_version/$pkg_name-$pkg_version.tar.bz2
-pkg_shasum=3c056914c8a438b210be0d790463b960fc79d234c3f05ce707cbff80e94cba30
+pkg_shasum=1f844775576b79bdc9f9c717a50058d08620323c1e935458223a12f249c9e066
 pkg_deps=(
   core/glibc
   core/zlib
