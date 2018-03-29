5e0bae6742ca30a8af1fbbd4d058531298b46472
Stephan Renatus <srenatus@chef.io>
2018-03-29 14:23:41 +0200

    [nghtt2p] bump: 1.24.0 -> 1.31.0
    Release notes for 1.31.0 are here:
    
    http://nghttp2.org/blog/2018/02/27/nghttp2-v1-31-0/
    
    But we're skipping some versions in this bump.
    
    I've built core/curl against this version locally; and it seemed to work fine:
    
        [5][default:/src:2]# /hab/pkgs/srenatus/curl/7.54.1/20180329122510/bin/curl -V
        curl 7.54.1 (x86_64-pc-linux-gnu) libcurl/7.54.1 OpenSSL/1.0.2l zlib/1.2.8 nghttp2/1.31.0
        Release-Date: 2017-06-14
        Protocols: dict file ftp ftps gopher http https imap imaps pop3 pop3s smb smbs smtp smtps telnet tftp
        Features: IPv6 Largefile NTLM NTLM_WB SSL libz TLS-SRP HTTP2 UnixSockets HTTPS-proxy
    
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

--- nghttp2/plan.sh
+++ nghttp2/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=nghttp2
 pkg_origin=core
-pkg_version=1.24.0
+pkg_version=1.31.0
 pkg_description="nghttp2 is an open source HTTP/2 C Library."
 pkg_upstream_url=https://nghttp2.org/
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('MIT')
 pkg_source=https://github.com/${pkg_name}/${pkg_name}/releases/download/v${pkg_version}/${pkg_name}-${pkg_version}.tar.gz
-pkg_shasum=5058da99c94764ff39f4a7046b9c8f0b6bafa10ec2fc096945a5d9d693654840
+pkg_shasum=6a2d02c441cf8d4279aea3c98d22763f8464808c3955db5c308291fe59d17cab
 pkg_dirname=${pkg_name}-${pkg_version}
 pkg_build_deps=(
   core/make
