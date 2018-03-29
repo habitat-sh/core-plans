8be8f90d77e9bed866599c71c7a3911a5b69feaa
Fletcher Nichol <fnichol@nichol.ca>
2018-03-29 20:53:15 +0000

    [go14] Disable CGO to support newer compilers.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- go14/plan.sh
+++ go14/plan.sh
@@ -17,7 +17,7 @@ do_prepare() {
   build_line "Setting GOOS=$GOOS"
   export GOARCH=amd64
   build_line "Setting GOARCH=$GOARCH"
-  export CGO_ENABLED=1
+  export CGO_ENABLED=0
   build_line "Setting CGO_ENABLED=$CGO_ENABLED"
 
   GOROOT="$(pwd)"
