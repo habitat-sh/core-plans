3b61808790e7b17a42035197e4e781f04edb20de
Nell Shamrell <nellshamrell@gmail.com>
2018-04-03 11:04:35 -0700

    corrects download link for docker 17
    Signed-off-by: Nell Shamrell <nellshamrell@gmail.com>

--- docker17/plan.sh
+++ docker17/plan.sh
@@ -4,9 +4,9 @@ pkg_origin=core
 pkg_version=17.12.1
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('Apache-2')
-pkg_source=https://download.docker.com/linux/static/stable/x86_64/${pkg_name}-${pkg_version}-ce.tgz
+pkg_source=https://download.docker.com/linux/static/stable/x86_64/docker-${pkg_version}-ce.tgz
 pkg_upstream_url=https://docs.docker.com/engine/installation/binaries/
-pkg_shasum=1270dce1bd7e1838d62ae21d2505d87f16efc1d9074645571daaefdfd0c14054+
+pkg_shasum=1270dce1bd7e1838d62ae21d2505d87f16efc1d9074645571daaefdfd0c14054
 pkg_dirname=docker
 pkg_bin_dirs=(bin)
 
