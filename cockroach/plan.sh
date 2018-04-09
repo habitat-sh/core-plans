571cd4076730dff62f78a2790b048e1230e6553b
Chris Alfano <chris@jarv.us>
2018-04-09 12:31:09 -0400

    Bump cockroach version to 1.1.7
    Signed-off-by: Chris Alfano <chris@jarv.us>

--- cockroach/plan.sh
+++ cockroach/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=cockroach
 pkg_origin=core
-pkg_version=1.0.6
+pkg_version=1.1.7
 pkg_description="CockroachDB is a cloud-native SQL database for building global, scalable cloud services that survive disasters."
 pkg_upstream_url=https://github.com/cockroachdb/cockroach
 pkg_license=('Apache-2.0')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source=https://binaries.cockroachdb.com/cockroach-v${pkg_version}.src.tgz
-pkg_shasum=1f9b867385f6d99e8ff0f15b66e6bf7205b14617030db7482641627472eae0c8
+pkg_shasum=f6ce7290ba1eac5dd5e94d1ef44cd3c1f52ef01622d0b4a8c8797c0b7cb53b00
 pkg_dirname="${pkg_name}-v${pkg_version}"
 pkg_deps=(
   core/coreutils
@@ -18,6 +18,7 @@ pkg_build_deps=(
   core/go/1.8
   core/gcc
   core/cmake
+  core/autoconf
 )
 pkg_bin_dirs=(bin)
 
