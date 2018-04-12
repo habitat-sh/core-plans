1461fd8a427e61965db5a192bea805891921b181
Elliott Davis <elliott@excellent.io>
2018-04-12 13:25:07 -0500

    Upgrade CMake to latest version and use system libs
    Currently CMake relies on an internal version of curl and friends, which doesn't work in habitat.
    
    For this to work, I believe we need an upgraded version of GCC.
    
    Signed-off-by: Elliott Davis <elliott@excellent.io>

--- cmake/plan.sh
+++ cmake/plan.sh
@@ -1,25 +1,25 @@
 pkg_name=cmake
 pkg_origin=core
-_base_version=3.10
-pkg_version=${_base_version}.2
+_base_version=3.11
+pkg_version=${_base_version}.0
 pkg_maintainer='The Habitat Maintainers <humans@habitat.sh>'
 pkg_license=('BSD-3-Clause')
 pkg_description="CMake is an open-source, cross-platform family of tools designed to build, test and package software"
 pkg_upstream_url="https://cmake.org/"
 pkg_source="https://cmake.org/files/v${_base_version}/cmake-${pkg_version}.tar.gz"
-pkg_shasum=80d0faad4ab56de07aa21a7fc692c88c4ce6156d42b0579c6962004a70a3218b
+pkg_shasum=c313bee371d4d255be2b4e96fd59b11d58bc550a7c78c021444ae565709a656b
 pkg_deps=(
   core/glibc
   core/gcc-libs
+  core/curl
+  core/libarchive
+  edavis/libuv
 )
 pkg_build_deps=(
   core/coreutils
   core/diffutils
   core/make
   core/gcc
-  core/curl
-  core/zlib
-  core/bzip2
 )
 
 pkg_lib_dirs=(lib)
@@ -27,8 +27,12 @@ pkg_include_dirs=(include)
 pkg_bin_dirs=(bin)
 
 do_build() {
-  ./bootstrap
-  ./configure --prefix="${pkg_prefix}"
+  ./bootstrap \
+    --system-libs \
+    --prefix="${pkg_prefix}" \
+    --no-system-jsoncpp \
+    --no-system-librhash \
+    --parallel="$(nproc)"
   make -j "$(nproc)"
 }
 
