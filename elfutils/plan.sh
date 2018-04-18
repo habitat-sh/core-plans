6dc2c1b1c6c29844c36e1513d468b9287c937cb2
Nell Shamrell <nellshamrell@gmail.com>
2018-04-18 12:39:09 -0700

    change back to using sourceware to go directly to source rather than through redirect
    Signed-off-by: Nell Shamrell <nellshamrell@gmail.com>

--- elfutils/plan.sh
+++ elfutils/plan.sh
@@ -6,8 +6,8 @@ pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="elfutils is a collection of various binary tools such as
   eu-objdump, eu-readelf, and other utilities that allow you to inspect and
   manipulate ELF files."
-pkg_upstream_url=https://fedorahosted.org/elfutils/
-pkg_source=https://fedorahosted.org/releases/e/l/$pkg_name/$pkg_version/$pkg_name-$pkg_version.tar.bz2
+pkg_upstream_url=https://sourceware.org/elfutils/
+pkg_source=https://sourceware.org/${pkg_name}/ftp/${pkg_version}/$pkg_name-$pkg_version.tar.bz2
 pkg_shasum=1f844775576b79bdc9f9c717a50058d08620323c1e935458223a12f249c9e066
 pkg_deps=(
   core/glibc
