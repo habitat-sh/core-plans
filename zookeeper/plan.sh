17f2e78d5bebf7067bd28a031c6a7c1d38d42cba
Romain Sertelon <romain@sertelon.fr>
2018-04-19 15:55:22 +0200

    [zookeeper] Updates to 3.4.11
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- zookeeper/plan.sh
+++ zookeeper/plan.sh
@@ -1,16 +1,20 @@
 pkg_name=zookeeper
 pkg_origin=core
-pkg_version=3.4.9
-pkg_source=http://apache.mirrors.ionfish.org/zookeeper/${pkg_name}-${pkg_version}/${pkg_name}-${pkg_version}.tar.gz
-pkg_shasum=e7f340412a61c7934b5143faef8d13529b29242ebfba2eba48169f4a8392f535
+pkg_version=3.4.11
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="The Apache ZooKeeper system for distributed coordination is a high-performance service for building distributed applications."
 pkg_upstream_url="https://zookeeper.apache.org"
 pkg_license=('Apache-2.0')
+pkg_source="http://apache.mirrors.ionfish.org/zookeeper/${pkg_name}-${pkg_version}/${pkg_name}-${pkg_version}.tar.gz"
+pkg_shasum="f6bd68a1c8f7c13ea4c2c99f13082d0d71ac464ffaf3bf7a365879ab6ad10e84"
+pkg_build_deps=()
+pkg_deps=(
+  core/bash-static
+  core/coreutils
+  core/jre8
+)
 pkg_bin_dirs=(bin)
 pkg_lib_dirs=(lib)
-pkg_build_deps=()
-pkg_deps=(core/jre8 core/coreutils core/bash-static)
 pkg_exports=(
   [port]=zookeeper.clientPort
 )
