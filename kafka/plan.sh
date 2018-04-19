53fdf3fd506831f602201019a086c333e4a7dcfb
Romain Sertelon <romain@sertelon.fr>
2018-04-19 14:58:47 +0200

    [kafka] Updates to 0.10.2.1
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- kafka/plan.sh
+++ kafka/plan.sh
@@ -1,14 +1,19 @@
 pkg_name=kafka
 pkg_origin=core
-pkg_version=0.10.1.0
-pkg_source=http://mirrors.gigenet.com/apache/${pkg_name}/${pkg_version}/${pkg_name}_2.11-${pkg_version}.tgz
-pkg_shasum=6d9532ae65c9c8126241e7b928b118aaa3a694dab08069471f0e61f4f0329390
-pkg_dirname=${pkg_name}_2.11-${pkg_version}
+pkg_version=0.10.2.1
+pkg_source="http://mirrors.gigenet.com/apache/${pkg_name}/${pkg_version}/${pkg_name}_2.11-${pkg_version}.tgz"
+pkg_shasum="b86f75c8f078bc818031568155dd442ba6c1ed849663d0a7da9870efc96be461"
+pkg_dirname="${pkg_name}_2.11-${pkg_version}"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="A distributed streaming platform"
+pkg_upstream_url="https://kafka.apache.org/"
 pkg_license=('Apache-2.0')
 pkg_bin_dirs=(bin)
-pkg_deps=(core/jre8 core/coreutils core/bash-static)
+pkg_deps=(
+  core/bash-static
+  core/coreutils
+  core/jre8
+)
 pkg_binds=(
   [zookeeper]="port"
 )
@@ -18,5 +23,5 @@ do_build() {
 }
 
 do_install() {
-  cp -R libs bin "$pkg_prefix"
+  cp -R libs bin "${pkg_prefix}"
 }
