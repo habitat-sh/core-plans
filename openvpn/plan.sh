635babd63539025c3ad97ed45f9f5774b81d0e61
Romain Sertelon <romain@sertelon.fr>
2018-04-19 16:36:20 +0200

    [openvpn] Fixes run and updates to 2.4.6
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- openvpn/plan.sh
+++ openvpn/plan.sh
@@ -1,22 +1,54 @@
 pkg_name=openvpn
 pkg_origin=core
-pkg_version=2.3.11
+pkg_version=2.4.6
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_license=('gplv2')
-pkg_source=https://swupdate.openvpn.org/community/releases/${pkg_name}-${pkg_version}.tar.gz
-pkg_shasum=9117a4434fd35e61cf94f9ee7ef84b7aecbc6fa556f779ff599560f219756163
-pkg_deps=(core/glibc core/openssl core/lzo)
-pkg_build_deps=(core/gcc core/coreutils core/make core/busybox-static)
-pkg_bin_dirs=(bin)
+pkg_license=('GPL-2.0')
+pkg_description="OpenVPN is an open source VPN daemon"
+pkg_upstream_url="https://openvpn.net/"
+pkg_source="https://swupdate.openvpn.org/community/releases/${pkg_name}-${pkg_version}.tar.gz"
+pkg_shasum="738dbd37fcf8eb9382c53628db22258c41ba9550165519d9200e8bebaef4cbe2"
+pkg_deps=(
+  core/glibc
+  core/lz4
+  core/lzo
+  core/openssl
+  core/zlib
+)
+pkg_build_deps=(
+  core/busybox-static
+  core/coreutils
+  core/cmake
+  core/file
+  core/gcc
+  core/git
+  core/make
+  core/pkg-config
+)
+pkg_bin_dirs=(sbin)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
+
+do_prepare() {
+  if [[ ! -r /usr/bin/file ]]; then
+    ln -sv "$(pkg_path_for file)/bin/file" /usr/bin/file
+    _clean_file=true
+  fi
+}
 
 do_build() {
   ./configure \
     --disable-plugin-auth-pam \
-    --prefix=${pkg_prefix} \
-    --exec-prefix=${pkg_prefix} \
-    --sbindir=${pkg_prefix}/bin \
+    --prefix"=${pkg_prefix}" \
     --enable-iproute2
   make
+}
+
+do_check() {
+  make check
+}
+
+do_end() {
+  if [[ -n "$_clean_file" ]]; then
+    rm -fv /usr/bin/file
+  fi
 }
