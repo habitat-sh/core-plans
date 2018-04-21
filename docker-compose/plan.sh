42a5bfd0f9285f285643df73b0b14ed95574978f
Romain Sertelon <romain@sertelon.fr>
2018-04-21 20:48:44 +0200

    [docker-compose] Update to 1.21.0
    Also, version is now fixed.
    
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- docker-compose/plan.sh
+++ docker-compose/plan.sh
@@ -1,30 +1,19 @@
 pkg_name=docker-compose
 pkg_origin=core
+pkg_version=1.21.0
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('Apache-2.0')
 pkg_description="Define and run multi-container applications with Docker"
-pkg_upstream_url=https://docs.docker.com/compose/install/
+pkg_upstream_url="https://docs.docker.com/compose/install/"
 pkg_build_deps=(
-  core/gawk
-  core/sed
 )
 pkg_deps=(
   core/python
 )
 pkg_bin_dirs=(bin)
 
-pkg_version() {
-  export LC_ALL=en_US LANG=en_US
-  pip search --disable-pip-version-check docker-compose | grep '^docker-compose (' | awk -F'[()]' '{print $2}'
-}
-
-do_before() {
-  update_pkg_version
-}
-
 do_prepare() {
   python -m venv "$pkg_prefix"
-  # shellcheck source=/dev/null
   source "$pkg_prefix/bin/activate"
 }
 
