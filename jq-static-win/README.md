439ba800b27e79b55d6f439f672e60de94af0983
skylerto <skylerclayne@gmail.com>
2018-04-24 11:11:48 -0400

    jq for windows
    Signed-off-by: skylerto <skylerclayne@gmail.com>

---
+++ jq-static-win/README.md
@@ -0,0 +1,9 @@
+# Habitat package: jq-static-win
+
+## Description
+
+Provide a brief description of the `jq-static-win` plan / purpose.
+
+## Usage
+
+Describe the general usage for the `jq-static-win` plan
---
+++ jq-static-win/plan.ps1
@@ -0,0 +1,28 @@
+$pkg_name="jq-static-win"
+$pkg_origin="skylerto"
+$pkg_version="1.5"
+$pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+$pkg_license=@("Apache-2.0")
+$pkg_source="https://github.com/stedolan/jq/releases/download/jq-$pkg_version/jq-win64.exe"
+# $pkg_filename="$pkg_name-$pkg_version.zip"
+# $pkg_shasum="TODO"
+# $pkg_deps=@()
+# $pkg_build_deps=@()
+# $pkg_lib_dirs=@("lib")
+# $pkg_include_dirs=@("include")
+# $pkg_bin_dirs=@("bin")
+# $pkg_svc_run="MyBinary.exe"
+# $pkg_exports=@{
+#   host="srv.address"
+#   port="srv.port"
+#   ssl-port="srv.ssl.port"
+# }
+# $pkg_exposes=@("port", "ssl-port")
+# $pkg_binds=@{
+#   database="port host"
+# }
+# $pkg_binds_optional=@{
+#   storage="port host"
+# }
+# $pkg_description="Some description."
+# $pkg_upstream_url="http://example.com/project-name"
