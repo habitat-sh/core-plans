c361e8977b751fbad42ab4b4f01ee4c30d0a801c
Christopher P. Maher <chris@mahercode.io>
2018-04-24 00:06:42 -0700

    Adding README and updating to 0.10.0
    Signed-off-by: Christopher P. Maher <chris@mahercode.io>

--- vault/plan.sh
+++ vault/plan.sh
@@ -1,12 +1,12 @@
 pkg_origin=core
 pkg_name=vault
-pkg_version=0.9.5
+pkg_version=0.10.0
 pkg_description="A tool for managing secrets."
 pkg_maintainer='The Habitat Maintainers <humans@habitat.sh>'
 pkg_license=("MPL-2.0")
 pkg_upstream_url=https://www.vaultproject.io/
 pkg_source=https://releases.hashicorp.com/vault/${pkg_version}/vault_${pkg_version}_linux_amd64.zip
-pkg_shasum=f6dbc9fdac00598d2a319c9b744b85bf17d9530298f93d29ef2065bc751df099
+pkg_shasum=a6b4b6db132f3bbe6fbb77f76228ffa45bd55a5a1ab83ff043c2c665c3f5a744
 pkg_filename=${pkg_name}-${pkg_version}_linux_amd64.zip
 pkg_deps=()
 pkg_build_deps=(core/unzip)
