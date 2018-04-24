543efb1e8a3b1a30e4516efefc806dc0711e52f4
skylerto <skylerclayne@gmail.com>
2018-04-24 18:57:14 +0000

    remove phases and rename downloaded exe
    Signed-off-by: skylerto <skylerclayne@gmail.com>

--- jq-static/plan.ps1
+++ jq-static/plan.ps1
@@ -4,14 +4,11 @@ $pkg_version="1.5"
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_license=@("Apache-2.0")
 $pkg_source="https://github.com/stedolan/jq/releases/download/jq-$pkg_version/jq-win64.exe"
-$pkg_filename="jq.exe"
 $pkg_shasum="ebecd840ba47efbf66822868178cc721a151060937f7ac406e3d31bd015bde94"
 $pkg_bin_dirs=@("bin")
 
-function Invoke-Verify { }
-
-function Invoke-Build { }
+function Invoke-Unpack { }
 
 function Invoke-Install {
-  Copy-Item $HAB_CACHE_SRC_PATH/jq.exe $pkg_prefix/bin
+  Copy-Item $HAB_CACHE_SRC_PATH/$pkg_filename $pkg_prefix/bin/jq.exe
 }
