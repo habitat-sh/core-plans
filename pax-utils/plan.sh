ab6c61343c1f717cc0561a796241c07f9b1a5292
Romain Sertelon <romain@sertelon.fr>
2018-05-01 00:43:37 +0200

    [pax-utils] Update to 1.2.3
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- pax-utils/plan.sh
+++ pax-utils/plan.sh
@@ -1,13 +1,13 @@
 pkg_name=pax-utils
-pkg_version=1.1.7
+pkg_version=1.2.3
 pkg_origin=core
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('GPL')
 pkg_description="ELF related utils for ELF 32/64 binaries that can check files
   for security relevant properties"
 pkg_upstream_url='http://hardened.gentoo.org/pax-utils.xml'
-pkg_source=http://distfiles.gentoo.org/distfiles/${pkg_name}-${pkg_version}.tar.xz
-pkg_shasum=bb9bdbf0888de9444b53b78f7b8069af9832bac7cef0588030b8ce49e8ebad10
+pkg_source="http://distfiles.gentoo.org/distfiles/${pkg_name}-${pkg_version}.tar.xz"
+pkg_shasum="d231b1a34aea2b9205c24fada79a8251d4c3fbcce49367fc055fe43e4c9783b2"
 pkg_deps=(
   core/bash
   core/glibc
