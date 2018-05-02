ae2b31f62b147ca4a9eb4065c2eb93472838e0c5
Romain Sertelon <romain@sertelon.fr>
2018-05-02 22:47:25 +0200

    [mono5] Update to 5.10.1.47
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- mono5/plan.sh
+++ mono5/plan.sh
@@ -1,12 +1,14 @@
 pkg_name=mono5
 pkg_origin=core
-pkg_version="5.10.1.6"
+pkg_version="5.10.1.47"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('Microsoft-Patent-Promise-for-Mono')
-pkg_source="https://download.mono-project.com/sources/mono/mono-${pkg_version}.tar.bz2"
-pkg_filename="mono-${pkg_version}.tar.bz2"
-pkg_shasum="e3dca1105d6d58730e81c5af728377927230d7c590d4cb253acb5d40c6522865"
-pkg_dirname="mono-$pkg_version"
+pkg_description="Mono 5.x open source ECMA CLI, C# and .NET implementation."
+pkg_upstream_url="https://www.mono-project.com"
+pkg_dirname="mono-${pkg_version}"
+pkg_filename="${pkg_dirname}.tar.bz2"
+pkg_source="https://download.mono-project.com/sources/mono/${pkg_filename}"
+pkg_shasum="90c237b5288f95f6fdab4ace1e36ab64a6369e2c9fddd462d604fd788e2545da"
 pkg_deps=(
   core/gcc-libs
   core/glibc
@@ -25,13 +27,12 @@ pkg_build_deps=(
   core/pkg-config
   core/python
   core/tzdata
+  core/which
 )
 pkg_lib_dirs=(lib)
 pkg_include_dirs=(include)
 pkg_bin_dirs=(bin)
 pkg_pconfig_dirs=(lib/pkgconfig)
-pkg_description="Mono 5.x open source ECMA CLI, C# and .NET implementation."
-pkg_upstream_url="https://www.mono-project.com"
 
 do_prepare() {
   export with_gnu_ld="yes"
