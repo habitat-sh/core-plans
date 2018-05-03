f80005c11583435e1c0acad139ba25a0021a8938
Ben Dang <me@bdang.it>
2018-05-02 21:52:35 -0700

    attempt to fix
    Signed-off-by: Ben Dang <me@bdang.it>

--- libcxx/plan.sh
+++ libcxx/plan.sh
@@ -1,26 +1,30 @@
 pkg_name=libcxx
 pkg_origin=core
-pkg_version=3.9.0
+pkg_version=5.0.1
 pkg_license=('NCSA')
 pkg_description="A new implementation of the C++ standard library, targeting C++11"
 pkg_upstream_url=http://libcxx.llvm.org/
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_filename=${pkg_name}-${pkg_version}.src.tar.xz
 pkg_source=http://llvm.org/releases/${pkg_version}/${pkg_name}-${pkg_version}.src.tar.xz
-pkg_shasum=d0b38d51365c6322f5666a2a8105785f2e114430858de4c25a86b49f227f5b06
+pkg_shasum=fa8f99dd2bde109daa3276d529851a3bce5718d46ce1c5d0806f46caa3e57c00
 pkg_build_deps=(
+  core/binutils
+  core/clang
   core/cmake
+  core/gcc
   core/llvm
   core/make
 )
 pkg_deps=(
-  core/gcc
+  core/gcc-libs
   core/glibc
-  core/util-linux
 )
 pkg_lib_dirs=(lib)
 pkg_include_dirs=(include)
 pkg_bin_dirs=(bin)
+
+BUILDDIR="_build"
 
 do_download() {
   do_default_download
@@ -28,10 +32,13 @@ do_download() {
   build_line "Downloading libcxxabi"
   download_file http://llvm.org/releases/${pkg_version}/libcxxabi-${pkg_version}.src.tar.xz \
     libcxxabi-${pkg_version}.src.tar.xz \
-    b037a92717856882e05df57221e087d7d595a2ae9f170f7bc1a23ec7a92c8019
+    5a25152cb7f21e3c223ad36a1022faeb8a5ac27c9e75936a5ae2d3ac48f6e854
 }
 
 do_unpack() {
+  # build_line "Copy llvm src"
+  # cp -R "$(pkg_path_for core/llvm)/src/" "${HAB_CACHE_SRC_PATH}/${pkg_dirname}"
+
   mkdir -p "$HAB_CACHE_SRC_PATH/${pkg_dirname}/libcxx"
   pushd "$HAB_CACHE_SRC_PATH/${pkg_dirname}/libcxx"
   tar xf "$HAB_CACHE_SRC_PATH/$pkg_filename" --strip 1
@@ -44,55 +51,75 @@ do_unpack() {
 }
 
 do_prepare() {
-  do_default_prepare
-
   # Without these links, the clang linker fails. I not been able to find any
   # workaround for this. The library paths do not impact the search paths for these files
-  clang_path=$(dirname "$(find "$(pkg_path_for llvm)/lib/clang" -type d -name include)")
-  export clang_path
+  # clang_path=$(dirname "$(find "$(pkg_path_for core/clang)/lib/clang" -type d -name include)")
+  # export clang_path
+  #
+  # build_line "clang_path: $clang_path"
+  #
+  # ln -sf "$(pkg_path_for glibc)/lib/crtn.o"  "$clang_path/crtn.o"
+  # ln -sf "$(pkg_path_for glibc)/lib/crt1.o"  "$clang_path/crt1.o"
+  # ln -sf "$(pkg_path_for glibc)/lib/crti.o"  "$clang_path/crti.o"
+  # ln -sf "$(pkg_path_for gcc)/lib/gcc/x86_64-unknown-linux-gnu/5.2.0/crtbegin.o"  "$clang_path/crtbegin.o"
+  #
+  # CC="$(pkg_path_for core/clang)/bin/clang"
+  # export CC
+  # CXX="$(pkg_path_for core/clang)/bin/clang++"
+  # export CXX
 
-  ln -sf "$(pkg_path_for glibc)/lib/crtn.o"  "$clang_path/crtn.o"
-  ln -sf "$(pkg_path_for glibc)/lib/crt1.o"  "$clang_path/crt1.o"
-  ln -sf "$(pkg_path_for glibc)/lib/crti.o"  "$clang_path/crti.o"
-
-  CC="$(pkg_path_for llvm)/bin/clang"
-  export CC
-  CXX="$(pkg_path_for llvm)/bin/clang++"
-  export CXX
+  mkdir -p "${BUILDDIR}"
 }
 
 do_build() {
-  pushd libcxxabi
+  LLVM_PATH="$(pkg_path_for core/llvm)"
+
+  pushd "${BUILDDIR}"
   cmake \
     -DCMAKE_INSTALL_PREFIX="$pkg_prefix" \
-    -DCMAKE_CXX_FLAGS="$CXXFLAGS --gcc-toolchain=$(pkg_path_for gcc)" \
-    -DCMAKE_C_FLAGS="$CFLAGS --gcc-toolchain=$(pkg_path_for gcc)" \
-    -DLIBCXXABI_LIBCXX_PATH=../libcxx \
-    .
+    -DLLVM_PATH="${LLVM_PATH}" \
+    -DLIBCXX_CXX_ABI=libcxxabi \
+    -DLIBCXX_CXX_ABI_INCLUDE_PATHS="${HAB_CACHE_SRC_PATH}/${pkg_dirname}/libcxxabi/include" \
+    ../libcxx
+
   make
   popd
+  #
+  # pushd libcxxabi
+  # cmake \
+  #   -DCMAKE_INSTALL_PREFIX="$pkg_prefix" \
+  #   -DCMAKE_CXX_FLAGS="$CXXFLAGS --gcc-toolchain=$(pkg_path_for gcc)" \
+  #   -DCMAKE_C_FLAGS="$CFLAGS --gcc-toolchain=$(pkg_path_for gcc)" \
+  #   -DLIBCXXABI_LIBCXX_PATH=../libcxx \
+  #   .
+  # make
+  # popd
+  #
+  # cmake \
+  #   -DCMAKE_INSTALL_PREFIX="$pkg_prefix" \
+  #   -DCMAKE_CXX_FLAGS="$CXXFLAGS --gcc-toolchain=$(pkg_path_for gcc)" \
+  #   -DCMAKE_C_FLAGS="$CFLAGS --gcc-toolchain=$(pkg_path_for gcc)" \
+  #   -DLIBCXX_CXX_ABI=libcxxabi \
+  #   -DLIBCXX_CXX_ABI_LIBRARY_PATH=libcxxabi/lib \
+  #   -DLIBCXX_CXX_ABI_INCLUDE_PATHS=libcxxabi/include \
+  #   libcxx
+  # make
+}
 
-  cmake \
-    -DCMAKE_INSTALL_PREFIX="$pkg_prefix" \
-    -DCMAKE_CXX_FLAGS="$CXXFLAGS --gcc-toolchain=$(pkg_path_for gcc)" \
-    -DCMAKE_C_FLAGS="$CFLAGS --gcc-toolchain=$(pkg_path_for gcc)" \
-    -DLIBCXX_CXX_ABI=libcxxabi \
-    -DLIBCXX_CXX_ABI_LIBRARY_PATH=libcxxabi/lib \
-    -DLIBCXX_CXX_ABI_INCLUDE_PATHS=libcxxabi/include \
-    libcxx
-  make
+do_check() {
+  pushd "${BUILDDIR}"
+  make check-libcxx # check-cxx check-cxxabi
+  popd
 }
 
 do_install() {
-  pushd libcxxabi
-  make install-libcxxabi
-  popd
-
+  pushd "${BUILDDIR}"
   make install-libcxx
+  popd
 }
 
-do_end() {
-  rm "$clang_path/crtn.o"
-  rm "$clang_path/crt1.o"
-  rm "$clang_path/crti.o"
-}
+# do_end() {
+#   rm "$clang_path/crtn.o"
+#   rm "$clang_path/crt1.o"
+#   rm "$clang_path/crti.o"
+# }
