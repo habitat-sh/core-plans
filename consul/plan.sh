c81166e0e97e308d701d3954aacac37e72a747df
Christopher P. Maher <chris@mahercode.io>
2018-05-14 16:53:01 -0700

    Version bump, nothing special
    Signed-off-by: Christopher P. Maher <chris@mahercode.io>

--- consul/plan.sh
+++ consul/plan.sh
@@ -1,12 +1,12 @@
 pkg_origin=core
 pkg_name=consul
-pkg_version=1.0.7
+pkg_version=1.1.0
 pkg_maintainer='The Habitat Maintainers <humans@habitat.sh>'
 pkg_license=("MPL-2.0")
 pkg_description="Consul is a tool for service discovery, monitoring and configuration."
 pkg_upstream_url=https://www.consul.io/
 pkg_source=https://releases.hashicorp.com/${pkg_name}/${pkg_version}/${pkg_name}_${pkg_version}_linux_amd64.zip
-pkg_shasum=6c2c8f6f5f91dcff845f1b2ce8a29bd230c11397c448ce85aae6dacd68aa4c14
+pkg_shasum=09c40c8b5be868003810064916d8460bff334ccfb59a5046390224b27e052c45
 pkg_filename=${pkg_name}-${pkg_version}_linux_amd64.zip
 pkg_deps=()
 pkg_build_deps=(core/unzip)
