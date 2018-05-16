a888f0704319e233093df6a40428b9f433403a0e
Elliott Davis <elliott@excellent.io>
2018-05-16 10:24:06 -0500

    Update RabbitMQ to 3.7.5
    * Upgrade erlang and elixir since they are requrired deps
    * Set locale to UTF-8 from latin1 to squelch warnings
    * Change to new ini config file format
    
    Signed-off-by: Elliott Davis <elliott@excellent.io>

--- elixir/plan.sh
+++ elixir/plan.sh
@@ -1,12 +1,12 @@
 pkg_origin=core
 pkg_name=elixir
-pkg_version=1.5.1
+pkg_version=1.6.5
 pkg_description="A dynamic, functional language designed for building scalable and maintainable applications. Elixir leverages the Erlang VM, known for running low-latency, distributed and fault-tolerant systems, while also being successfully used in web development and the embedded software domain."
 pkg_upstream_url=http://elixir-lang.org
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('Apache-2.0')
 pkg_source="https://github.com/elixir-lang/elixir/archive/v${pkg_version}.tar.gz"
-pkg_shasum=9a903dc71800c6ce8f4f4b84a1e4849e3433e68243958fd6413a144857b61f6a
+pkg_shasum=defe2bed953ee729addf1121db3fa42a618ef1d6c57a1f489da03b0e7a626e89
 pkg_deps=(core/busybox core/cacerts core/coreutils core/openssl core/erlang)
 pkg_build_deps=(core/busybox core/make)
 pkg_bin_dirs=(bin)
--- rabbitmq/plan.sh
+++ rabbitmq/plan.sh
@@ -1,18 +1,18 @@
 pkg_name=rabbitmq
 pkg_distname=${pkg_name}-server
 pkg_origin=core
-pkg_version=3.6.12
+pkg_version=3.7.5
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('MPL')
 pkg_description="Open source multi-protocol messaging broker"
 pkg_upstream_url="https://www.rabbitmq.com"
-pkg_source=http://www.rabbitmq.com/releases/rabbitmq-server/v${pkg_version}/rabbitmq-server-${pkg_version}.tar.xz
-pkg_shasum=c8e5a8ed5aae6167aa0050f41bd9f26d50eb16efd8abd1a1b0b01fcbccb458b9
+pkg_source=https://github.com/rabbitmq/rabbitmq-server/releases/download/v${pkg_version}/rabbitmq-server-${pkg_version}.tar.xz
+pkg_shasum=54fbe57b9c05caee6b50b79e22c269a265a3783205ebd2a2f05a6901d378040d
 pkg_dirname=${pkg_distname}-${pkg_version}
 pkg_deps=(
   core/coreutils
   core/glibc
-  core/erlang19
+  core/erlang
 )
 pkg_build_deps=(
   core/bash
@@ -29,13 +29,13 @@ pkg_build_deps=(
   core/rsync
   core/unzip
   core/zip
+  core/elixir
 )
 pkg_include_dirs=(include)
 pkg_bin_dirs=(sbin)
 pkg_exports=(
-  [port]=rabbitmq.listen_port
+  [port]=listeners.tcp.default
 )
-
 pkg_exposes=(port)
 
 do_prepare() {
@@ -49,6 +49,10 @@ do_prepare() {
   build_line "Setting RMQ_LIBDIR=$RMQ_LIBDIR"
   export RMQ_ERLAPP_DIR=""
   build_line "Setting RMQ_ERLAPP_DIR=$RMQ_ERLAPP_DIR"
+  export LANG="en_US.utf8"
+  export LANGUAGE="en_US:"
+  export LC_ALL=en_US.UTF-8
+  build_line "Setting locale to en_US.utf8"
 }
 
 do_build() {
