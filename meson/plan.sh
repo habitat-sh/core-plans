241c7d8495308895e560e44087409d4fdd79ac74
Scott Macfarlane <smacfarlane@chef.io>
2018-05-21 07:03:56 -0700

    Update Meson to 0.46.1
    This also removes the post-install cleaning of rpath that Meson performs, which interferes with Habitats build process.
    
    Signed-off-by: Scott Macfarlane <smacfarlane@chef.io>

--- meson/plan.sh
+++ meson/plan.sh
@@ -1,15 +1,18 @@
 pkg_name=meson
 pkg_origin=core
-pkg_version=0.43.0
+pkg_version=0.46.1
 pkg_description="The Meson Build System"
 pkg_upstream_url="http://mesonbuild.com/"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('Apache-2.0')
 pkg_source="https://github.com/mesonbuild/${pkg_name}/archive/${pkg_version}.tar.gz"
-pkg_shasum=324894427dcd29f6156fe06b046c6ad1b998470714debd7c5705902f21aaaa73
+pkg_shasum=2d917692d2cc194e12295f00469fbdf3c045e85d0295e5e59ced69115920ffa0
 pkg_deps=(
   core/python
   core/ninja
+)
+pkg_build_deps=(
+  core/patch
 )
 pkg_bin_dirs=(bin)
 pkg_lib_dirs=(lib)
@@ -21,6 +24,7 @@ do_setup_environment() {
 
 do_prepare() {
   mkdir -p "${pkg_prefix}/lib/python3.6/site-packages"
+  patch -p0 < "$PLAN_CONTEXT/patches/000_fix_rpath.patch"
 }
 
 do_build() {
