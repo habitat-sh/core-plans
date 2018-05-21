96a0c7d5b89a01a6d390e1e5e7ad7b958387f60d
Scott Macfarlane <smacfarlane@chef.io>
2018-05-21 06:39:30 -0700

    Update systemd to 238
    This version changes the build system from make to meson/ninja.
    Signed-off-by: Scott Macfarlane <smacfarlane@chef.io>

--- systemd/plan.sh
+++ systemd/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=systemd
 pkg_origin=core
-pkg_version="233"
+pkg_version="238"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="systemd is an init system used in Linux distributions to \
 bootstrap the user space. Subsequently to booting, it is used to manage system \
@@ -8,7 +8,7 @@ processes."
 pkg_license=('LGPL-2.1')
 pkg_source="https://github.com/systemd/${pkg_name}/archive/v${pkg_version}.tar.gz"
 pkg_upstream_url="https://github.com/systemd/systemd"
-pkg_shasum="8b3e99da3d4164b66581830a7f2436c0c8fe697b5fbdc3927bdb960646be0083"
+pkg_shasum="bbc8599bab2e3c4273886dfab12464e488ecdaf20b8284949e50f8858de3e022"
 pkg_bin_dirs=(bin)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib var/lib usr/lib)
@@ -17,54 +17,52 @@ pkg_deps=(
   core/libcap
   core/lz4
   core/util-linux
+  core/xz
 )
 pkg_svc_user=root
 pkg_svc_group=root
 pkg_build_deps=(
-  core/autoconf
-  core/automake
-  core/cpanminus
+  core/coreutils
   core/dbus
-  core/expat
   core/gcc
   core/gcc-libs
   core/gettext
-  core/glibc
   core/gperf
-  core/intltool
-  core/libtool
-  core/libxslt
-  core/local-lib
-  core/lz4
   core/m4
-  core/make
-  core/perl
+  core/meson
+  core/ninja
   core/pkg-config
-  core/util-linux
-  core/xz
 )
 
 do_prepare() {
-  eval "$(perl -I"$(pkg_path_for core/local-lib)/lib/perl5" -Mlocal::lib="$(pkg_path_for core/local-lib)")"
-  eval "$(perl -I"$(pkg_path_for core/intltool)/lib/lib/perl5" -Mlocal::lib="$(pkg_path_for core/intltool)/lib")"
+  if [[ ! -f /usr/bin/env ]]; then
+    ln -s "$(pkg_path_for core/coreutils)/bin/env" /usr/bin/env
+  fi
+
+  export LANG=en_US.utf8
+  export LC_ALL=en_US.utf8
+  # Systemd needs itself in rpath
+  export LD_RUN_PATH="${LD_RUN_PATH}:${pkg_prefix}/lib:${pkg_prefix}/lib/systemd"
 }
 
 do_build() {
-  export ACLOCAL_FLAGS
-  ACLOCAL_FLAGS="-I $(pkg_path_for core/pkg-config)/share/aclocal \
-    -I$(pkg_path_for core/libtool)/share/aclocal \
-    -I$(pkg_path_for core/intltool)/share/aclocal \
-    -I$(pkg_path_for core/gettext)/share/aclocal"
-  ./autogen.sh
-  ./configure \
-    --prefix="${pkg_prefix}" \
-    --disable-manpages \
-    --without-python \
-    --with-rootprefix="${pkg_prefix}" \
-    --libdir="${pkg_prefix}/usr/lib" \
-    --with-rootlibdir="${pkg_prefix}/lib" \
-    --with-dbuspolicydir="${pkg_prefix}/etc/dbus-1/system.d" \
-    --with-dbussystemservicedir="${pkg_prefix}/share/dbus-1/system-services" \
-    --with-dbussessionservicedir="${pkg_prefix}/share/dbus-1/services"
-  make
+  # meson_options.txt
+  local meson_opts=(
+    "--prefix=${pkg_prefix}"
+    "-Dman=false"
+    "-Dhtml=false"
+    "-Dpython=false"
+    "-Drootprefix=${pkg_prefix}"
+    "-Drootlibdir=${pkg_prefix}/lib"
+    "-Ddbuspolicydir=${pkg_prefix}/etc/dbus-1/system.d"
+    "-Ddbussessionservicedir=${pkg_prefix}/etc/dbus-1/services"
+    "-Ddbussystemservicedir=${pkg_prefix}/etc/dbus-1/system-services"
+    "-Dtests=false"
+  )
+  meson build "${meson_opts[@]}"
+  ninja -C build
+}
+
+do_install() {
+  ninja -C build install
 }
