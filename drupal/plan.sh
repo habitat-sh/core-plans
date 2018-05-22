67600898100409b1450f8c29e1e7b5582cc45c25
Paul Adams <paul@baggerspion.net>
2018-05-22 15:37:19 -0500

    OK, I should at least make this actually build
    Signed-off-by: Paul Adams <paul@baggerspion.net>

--- drupal/plan.sh
+++ drupal/plan.sh
@@ -2,7 +2,7 @@ pkg_name=drupal
 pkg_origin=core
 pkg_version="8.5.3"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_license=("Apache-2.0")
+pkg_license=("gplv2+")
 pkg_source="https://ftp.drupal.org/files/projects/$pkg_name-$pkg_version.tar.gz"
 pkg_shasum="7fc105f9e04ac5ccd337ff784aeaba6b48841490d7a80fd8ddb002fac0def47b"
 pkg_deps=(core/nginx)
@@ -10,6 +10,7 @@ pkg_binds=(
   [php]="port local_only"
 )
 pkg_svc_user="root"
+pkg_description="Drupal is a free and open source content-management framework written in PHP."
 
 do_build() {
   return 0
