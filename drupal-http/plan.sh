db8cd68fae34ff4cba1be308c2da7cf883254ec7
Paul Adams <paul@baggerspion.net>
2018-05-23 15:17:25 -0500

    Completed restructuring, as per @themightychris' advice. Thanks
    Signed-off-by: Paul Adams <paul@baggerspion.net>

--- drupal-http/plan.sh
+++ drupal-http/plan.sh
@@ -5,6 +5,8 @@ pkg_version="8.5.3"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("gplv2+")
 pkg_source="https://ftp.drupal.org/files/projects/${pkg_distname}-${pkg_version}.tar.gz"
+pkg_filename="${pkg_distname}-${pkg_version}.tar.gz"
+pkg_dirname="${pkg_distname}-${pkg_version}"
 pkg_shasum="7fc105f9e04ac5ccd337ff784aeaba6b48841490d7a80fd8ddb002fac0def47b"
 pkg_deps=(core/nginx)
 pkg_binds=(
@@ -19,5 +21,5 @@ do_build() {
 }
 
 do_install() {
-  cp -r "$CACHE_PATH" "$pkg_prefix/drupal"
+  cp -r "$CACHE_PATH" "${pkg_prefix}/drupal"
 }
