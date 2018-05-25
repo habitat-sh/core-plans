c39c1805931e239efbee349ed6fc4cda9d9d2db2
W. Duncan Fraser <wdf@alasconnect.com>
2018-05-25 13:24:38 -0500

    Updated concourse and fly for 3.13.0 Removed redundant cd from fly plan
    Signed-off-by: W. Duncan Fraser <wdf@alasconnect.com>

--- concourse/plan.sh
+++ concourse/plan.sh
@@ -1,11 +1,11 @@
 pkg_name=concourse
 pkg_origin=core
-pkg_version="3.6.0"
+pkg_version="3.13.0"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('Apache-2.0')
 pkg_source="https://github.com/${pkg_name}/${pkg_name}/releases/download/v${pkg_version}/${pkg_name}_linux_amd64"
 pkg_filename="${pkg_name}_linux_amd64"
-pkg_shasum="87132e62f227e776b10178c21c83eb9f0049e4b53adc4e4dbb7eb06dfbb2e3b1"
+pkg_shasum="cd232c5dd7a47616f5c8037a90c7b945edae8c6588bf5df833dd632af08a1d4b"
 pkg_deps=(core/glibc)
 pkg_build_deps=(core/patchelf)
 pkg_bin_dirs=(bin)
