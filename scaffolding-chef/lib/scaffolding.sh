b1680597e73678f6d6fbecd3c00551a22b9bf826
danielsdeleo <dan@chef.io>
2018-05-25 15:05:51 -0500

    Appease shellcheck on chef scaffolding
    Signed-off-by: Daniel DeLeo <dan@chef.io>

--- scaffolding-chef/lib/scaffolding.sh
+++ scaffolding-chef/lib/scaffolding.sh
@@ -12,8 +12,16 @@ if [ -z "${scaffold_policy_name+x}" ]; then
 fi
 
 scaffolding_load() {
-  pkg_deps=("chef/chef-client" "core/cacerts" ${pkg_deps[@]})
-  pkg_build_deps=("chef/chef-dk" "core/git" ${pkg_build_deps[@]})
+  parent_deps="${pkg_deps[*]}"
+  pkg_deps=("chef/chef-client" "core/cacerts")
+  for i in $parent_deps; do
+    pkg_deps+=(i)
+  done
+  parent_build_deps="${pkg_build_deps[*]}"
+  pkg_build_deps=("chef/chef-dk" "core/git")
+  for i in $parent_build_deps; do
+    pkg_build_deps+=(i)
+  done
   pkg_svc_user="root"
   pkg_svc_run="set_just_so_you_will_render"
 }
@@ -32,11 +40,11 @@ do_default_unpack() {
 
 do_default_build_service() {
   ## Create hooks
-  mkdir -p $pkg_prefix/hooks
-  chown 0644 $pkg_prefix/hooks
+  mkdir -p "$pkg_prefix/hooks"
+  chown 0644 "$pkg_prefix/hooks"
 
   # Run hook
-  cat << EOF >> $pkg_prefix/hooks/run
+  cat << EOF >> "$pkg_prefix/hooks/run"
 #!/bin/sh
 
 export SSL_CERT_FILE="{{pkgPathFor "core/cacerts"}}/ssl/cert.pem"
@@ -46,10 +54,10 @@ cd {{pkg.path}}
 exec 2>&1
 exec chef-client -z -i {{cfg.interval}} -s {{cfg.splay}} -l {{cfg.log_level}} -c $pkg_svc_config_path/client-config.rb
 EOF
-  chown 0755 $pkg_prefix/hooks/run
+  chown 0755 "$pkg_prefix/hooks/run"
 
   # Init hook
-  cat << EOF >> $pkg_prefix/hooks/init
+  cat << EOF >> "$pkg_prefix/hooks/init"
 #!/bin/sh
 
 export SSL_CERT_FILE="{{pkgPathFor "core/cacerts"}}/ssl/cert.pem"
@@ -58,7 +66,7 @@ cd {{pkg.path}}
 exec 2>&1
 exec chef-client -z -l {{cfg.log_level}} -c $pkg_svc_config_path/client-config.rb
 EOF
-  chown 0755 $pkg_prefix/hooks/init
+  chown 0755 "$pkg_prefix/hooks/init"
 }
 
 do_default_build() {
@@ -76,20 +84,20 @@ do_default_build() {
       fi
     fi
   fi
-  rm -f $_policyfile_path/*.lock.json
-  policyfile=$_policyfile_path/$scaffold_policy_name.rb
-  for x in `cat $policyfile | grep include_policy | awk -F "," '{print $1}' | awk -F '"' '{print $2}' | tr -d " "`; do
-    chef install $_policyfile_path/$x.rb
+  rm -f "$_policyfile_path"/*.lock.json
+  policyfile="$_policyfile_path/$scaffold_policy_name.rb"
+  for x in $(grep include_policy "$policyfile" | awk -F "," '{print $1}' | awk -F '"' '{print $2}' | tr -d " "); do
+    chef install "$_policyfile_path/$x.rb"
   done
-  chef install $policyfile
+  chef install "$policyfile"
 }
 
 do_default_install() {
-  chef export $_policyfile_path/$scaffold_policy_name.lock.json $pkg_prefix
+  chef export "$_policyfile_path/$scaffold_policy_name.lock.json" "$pkg_prefix"
 
-  mkdir -p $pkg_prefix/config
-  chown 0755 $pkg_prefix/config
-  cat << EOF >> $pkg_prefix/.chef/config.rb
+  mkdir -p "$pkg_prefix/config"
+  chown 0755 "$pkg_prefix/config"
+  cat << EOF >> "$pkg_prefix/.chef/config.rb"
 cache_path "$pkg_svc_data_path/cache"
 node_path "$pkg_svc_data_path/nodes"
 role_path "$pkg_svc_data_path/roles"
@@ -98,24 +106,24 @@ ssl_verify_mode :verify_none
 chef_zero.enabled true
 EOF
 
-  cp $pkg_prefix/.chef/config.rb $pkg_prefix/config/bootstrap-config.rb
-  cat << EOF >> $pkg_prefix/config/bootstrap-config.rb
+  cp "$pkg_prefix/.chef/config.rb $pkg_prefix/config/bootstrap-config.rb"
+  cat << EOF >> "$pkg_prefix/config/bootstrap-config.rb"
 ENV['PATH'] = "/sbin:/usr/sbin:/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:#{ENV['PATH']}"
 EOF
 
-  cp $pkg_prefix/.chef/config.rb $pkg_prefix/config/client-config.rb
-  cat << EOF >> $pkg_prefix/config/client-config.rb
+  cp "$pkg_prefix/.chef/config.rb $pkg_prefix/config/client-config.rb"
+  cat << EOF >> "$pkg_prefix/config/client-config.rb"
 ENV['PATH'] = "{{cfg.env_path_prefix}}:#{ENV['PATH']}"
 
-{{#if cfg.data_collector.enable ~}} 
+{{#if cfg.data_collector.enable ~}}
 data_collector.token "{{cfg.data_collector.token}}"
 data_collector.server_url "{{cfg.data_collector.server_url}}"
 {{/if ~}}
 EOF
-  chown 0644 $pkg_prefix/config/client-config.rb
+  chown 0644 "$pkg_prefix/config/client-config.rb"
 
   ## Create config
-  cat << EOF >> $pkg_prefix/default.toml
+  cat << EOF >> "$pkg_prefix/default.toml"
 interval = 1800
 splay = 180
 log_level = "warn"
@@ -126,10 +134,9 @@ enable = "false"
 token = "set_to_your_token"
 server_url = "set_to_your_url"
 EOF
-  chown 0644 $pkg_prefix/default.toml
+  chown 0644 "$pkg_prefix/default.toml"
 }
 
 do_default_strip() {
   return 0
 }
-
