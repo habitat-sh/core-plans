091cd3bffb68cc882b11c8e8a96795464e603251
W. Duncan Fraser <wdf@alasconnect.com>
2018-05-29 12:19:16 -0800

    Updated nuget for 4.6.2 Added empty invoke-unpack so plan would build
    Signed-off-by: W. Duncan Fraser <wdf@alasconnect.com>

--- nuget/plan.ps1
+++ nuget/plan.ps1
@@ -1,13 +1,15 @@
 $pkg_name="nuget"
 $pkg_origin="core"
-$pkg_version="4.1.0"
+$pkg_version="4.6.2"
 $pkg_license=('Apache-2.0')
 $pkg_upstream_url="https://dist.nuget.org/index.html"
 $pkg_description="NuGet is the package manager for the Microsoft development platform including .NET."
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_source="https://dist.nuget.org/win-x86-commandline/v${pkg_version}/nuget.exe"
-$pkg_shasum="4c1de9b026e0c4ab087302ff75240885742c0faa62bd2554f913bbe1f6cb63a0"
+$pkg_shasum="2c562c1a18d720d4885546083ec8eaad6773a6b80befb02564088cc1e55b304e"
 $pkg_bin_dirs=@("bin")
+
+function Invoke-Unpack { }
 
 function Invoke-Install {
   Copy-Item "$HAB_CACHE_SRC_PATH/nuget.exe" "$pkg_prefix/bin" -Force
