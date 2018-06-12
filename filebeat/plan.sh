ea43ace336eb87f87df23090564bbc1aa9ba6846
Graham Weldon <graham@grahamweldon.com>
2018-06-12 15:44:45 +0900

    Cleanup plan file
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- filebeat/plan.sh
+++ filebeat/plan.sh
@@ -3,36 +3,17 @@ pkg_origin=core
 pkg_version="6.2.4"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("Apache-2.0")
-# pkg_source="https://github.com/elastic/beats/archive/v${pkg_version}.tar.gz"
-# pkg_filename="${pkg_name}-${pkg_version}-linux-x86_64.tar.gz"
-# # pkg_dirname="beats-${pkg_version}"
-# pkg_shasum="87d863cf55863329ca80e76c3d813af2960492f4834d4fea919f1d4b49aaf699"
-# pkg_deps=(core/glibc)
 pkg_build_deps=(core/go core/git core/make core/gcc)
-# pkg_lib_dirs=(lib)
-# pkg_include_dirs=(include)
 pkg_bin_dirs=(bin)
-# pkg_pconfig_dirs=(lib/pconfig)
-# pkg_svc_run="haproxy -f $pkg_svc_config_path/haproxy.conf"
-# pkg_exports=(
-#   [host]=srv.address
-#   [port]=srv.port
-#   [ssl-port]=srv.ssl.port
-# )
-# pkg_exposes=(port ssl-port)
-# pkg_binds=(
-#   [database]="port host"
-# )
 pkg_binds_optional=(
   [kibana]="port"
   [elasticsearch]="http-port"
   [logstash]="port"
 )
-# pkg_interpreters=(bin/bash)
 pkg_svc_user="hab"
 pkg_svc_group="${pkg_svc_user}"
 pkg_description="Lightweight shipper for logfiles"
-# pkg_upstream_url="http://example.com/project-name"
+pkg_upstream_url="https://www.elastic.co/products/beats/filebeat"
 
 do_download() {
   export GOPATH=$(dirname "${HAB_CACHE_SRC_PATH}")
