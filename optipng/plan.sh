9620e5c31e9c4c24b613fa1b8ae95768a6c55fb3
Graham Weldon <graham@grahamweldon.com>
2018-06-20 10:33:34 +0900

    Added new plan for OptiPNG
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

---
+++ optipng/plan.sh
@@ -0,0 +1,14 @@
+pkg_name=optipng
+pkg_origin=core
+pkg_version="0.7.7"
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_license=("Zlib")
+pkg_upstream_url="http://optipng.sourceforge.net/"
+pkg_source="http://prdownloads.sourceforge.net/${pkg_name}/${pkg_name}-${pkg_version}.tar.gz"
+pkg_shasum="4f32f233cef870b3f95d3ad6428bfe4224ef34908f1b42b0badf858216654452"
+pkg_build_deps=(
+  core/make
+  core/gcc
+  core/zlib
+)
+pkg_bin_dirs=(bin)
