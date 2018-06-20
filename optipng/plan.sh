2e4813bdf16958dc1ba32e22a6c35741c8624242
Graham Weldon <graham@grahamweldon.com>
2018-06-20 12:02:39 +0900

    Added pkg_description
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- optipng/plan.sh
+++ optipng/plan.sh
@@ -3,7 +3,6 @@ pkg_origin=core
 pkg_version="0.7.7"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("Zlib")
-pkg_upstream_url="http://optipng.sourceforge.net/"
 pkg_source="http://prdownloads.sourceforge.net/${pkg_name}/${pkg_name}-${pkg_version}.tar.gz"
 pkg_shasum="4f32f233cef870b3f95d3ad6428bfe4224ef34908f1b42b0badf858216654452"
 pkg_build_deps=(
@@ -12,3 +11,5 @@ pkg_build_deps=(
   core/zlib
 )
 pkg_bin_dirs=(bin)
+pkg_description="OptiPNG is a PNG optimizer that recompresses image files to a smaller size, without losing any information."
+pkg_upstream_url="http://optipng.sourceforge.net/"
