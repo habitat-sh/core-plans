1b6b6a4261d6fa6133ec3e0bd12e7f9d590a6259
Graham Weldon <graham@grahamweldon.com>
2018-06-21 11:34:32 +0900

    Remove debug attach
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- pngquant/plan.sh
+++ pngquant/plan.sh
@@ -4,7 +4,6 @@ pkg_version="2.11.7"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("GPL-3.0-only")
 pkg_source="https://github.com/kornelski/${pkg_name}/archive/${pkg_version}.tar.gz"
-# pkg_filename="${pkg_name}-${pkg_version}.tar.gz"
 pkg_shasum="0ca09a1f253b264e5aab8477b7f0e3cde51d9f88ed668b38ae057ced24076bda"
 pkg_deps=(
   core/coreutils
@@ -15,16 +14,13 @@ pkg_build_deps=(
   core/libpng
   core/zlib
   core/pkg-config
-  core/libimagequant
+  rakops/libimagequant
 )
-# pkg_lib_dirs=(lib)
-# pkg_include_dirs=(include)
 pkg_bin_dirs=(bin)
 pkg_description="Lossy PNG compressor"
 pkg_upstream_url="https://pngquant.org"
 
 do_build() {
   fix_interpreter "configure" core/coreutils bin/env
-  attach
   do_default_build
 }
