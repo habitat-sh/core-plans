94d3e99b5fabf4f809b255f762e812abb387ff49
Scott Macfarlane <smacfarlane@chef.io>
2018-06-29 07:48:51 -0700

    Update version
    Signed-off-by: Scott Macfarlane <smacfarlane@chef.io>

--- scaffolding-node/plan.sh
+++ scaffolding-node/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=scaffolding-node
 pkg_origin=core
-pkg_version="0.6.13"
+pkg_version="0.6.14"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('Apache-2.0')
 pkg_description="Habitat Plan Scaffolding for Node.js Applications"
