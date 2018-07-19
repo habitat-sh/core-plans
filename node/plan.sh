6904e8554569812404a6b4bf5dc00dbcc8942e32
Graham Weldon <graham@grahamweldon.com>
2018-07-19 11:04:44 +0900

    NodeJS update to 10.7.0
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- node/plan.sh
+++ node/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=node
 pkg_origin=core
-pkg_version=8.11.3
+pkg_version=10.7.0
 pkg_description="Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine."
 pkg_upstream_url=https://nodejs.org/
 pkg_license=('MIT')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source=https://nodejs.org/dist/v${pkg_version}/node-v${pkg_version}.tar.gz
-pkg_shasum=0d7e795c0579226c8b197353bbb9392cae802f4fefa4787a2c0e678beaf85cce
+pkg_shasum=b9691cbc6e6a2e209a9b8cb88fd942802236dae06652080f582304dbdd505ad2
 pkg_deps=(core/glibc core/gcc-libs core/python2 core/bash)
 pkg_build_deps=(core/gcc core/grep core/make)
 pkg_bin_dirs=(bin)
