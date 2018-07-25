cfaaad3e5a8915e89538f7d0da85844836bf937e
Graham Weldon <graham@grahamweldon.com>
2018-07-25 11:55:00 +0900

    Update strace to 4.23
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- strace/plan.sh
+++ strace/plan.sh
@@ -1,13 +1,16 @@
 pkg_name=strace
 pkg_origin=core
-pkg_version=4.19
-pkg_license=('strace')
+pkg_version=4.23
+pkg_license=("BSD-3-Clause-LBNL")
 pkg_description="strace is a system call tracer for Linux"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_upstream_url="https://strace.io/"
-pkg_source=http://downloads.sourceforge.net/project/strace/strace/${pkg_version}/strace-${pkg_version}.tar.xz
-pkg_shasum=7c93ebc6c29280f47c24a0eb86873a99ccb2cac6512c60a60ba4ef99ab807281
-pkg_deps=(core/glibc core/libunwind)
+pkg_source="https://github.com/strace/strace/releases/download/v4.23/${pkg_name}-${pkg_version}.tar.xz"
+pkg_shasum=7860a6965f1dd832747bd8281a04738274398d32c56e9fbd0a68b1bb9ec09aad
+pkg_deps=(
+  core/glibc
+  core/libunwind
+)
 pkg_bin_dirs=(bin)
 pkg_build_deps=(core/coreutils core/make core/gcc core/diffutils)
 pkg_lib_dirs=(lib)
