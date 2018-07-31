118f2836f4085f682e0411e8576495d156856ed3
Graham Weldon <graham@grahamweldon.com>
2018-07-31 13:41:44 +0900

    Update consul to 1.2.2, quote variables
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- consul/plan.sh
+++ consul/plan.sh
@@ -1,13 +1,13 @@
 pkg_origin=core
 pkg_name=consul
-pkg_version=1.2.1
+pkg_version=1.2.2
 pkg_maintainer='The Habitat Maintainers <humans@habitat.sh>'
 pkg_license=("MPL-2.0")
 pkg_description="Consul is a tool for service discovery, monitoring and configuration."
 pkg_upstream_url=https://www.consul.io/
-pkg_source=https://releases.hashicorp.com/${pkg_name}/${pkg_version}/${pkg_name}_${pkg_version}_linux_amd64.zip
-pkg_shasum=e4146334be453146890023303da3e0c815669e108a18fb7d742745df3414a31a
-pkg_filename=${pkg_name}-${pkg_version}_linux_amd64.zip
+pkg_source="https://releases.hashicorp.com/${pkg_name}/${pkg_version}/${pkg_name}_${pkg_version}_linux_amd64.zip"
+pkg_shasum=7fa3b287b22b58283b8bd5479291161af2badbc945709eb5412840d91b912060
+pkg_filename="${pkg_name}-${pkg_version}_linux_amd64.zip"
 pkg_deps=()
 pkg_build_deps=(core/unzip)
 pkg_bin_dirs=(bin)
@@ -25,7 +25,7 @@ pkg_svc_group="${pkg_svc_user}"
 
 do_unpack() {
   cd "${HAB_CACHE_SRC_PATH}" || exit
-  unzip ${pkg_filename} -d "${pkg_name}-${pkg_version}"
+  unzip "${pkg_filename}" -d "${pkg_name}-${pkg_version}"
 }
 
 do_build() {
