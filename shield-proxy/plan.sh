cdb04b039735de3fbf67e5f1a5510d0d94421662
Graham Weldon <graham@grahamweldon.com>
2018-08-02 09:46:10 +0900

    Add empty pkg_upstream_url
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- shield-proxy/plan.sh
+++ shield-proxy/plan.sh
@@ -5,7 +5,8 @@ pkg_description="Proxy package for the Shield backup and restore tool"
 pkg_license=('Apache-2.0')
 pkg_version=0.10.8
 pkg_svc_user=root
-pkg_svc_group=$pkg_svc_user
+pkg_svc_group="${pkg_svc_user}"
+pkg_upstream_url=""
 pkg_deps=(
   core/nginx
   core/openssl
