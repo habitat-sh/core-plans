d2ce77bf4de3d1f482ce12327873b067058f746d
Graham Weldon <graham@grahamweldon.com>
2018-08-02 09:58:48 +0900

    Add pkg_license to redis3 and redis4
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- redis3/plan.sh
+++ redis3/plan.sh
@@ -5,6 +5,7 @@ pkg_origin=core
 pkg_version="3.2.12"
 pkg_description="Persistent key-value database, with built-in net interface"
 pkg_upstream_url="http://redis.io"
+pkg_license=("BSD-3-Clause")
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="http://download.redis.io/releases/${pkg_dist_name}-${pkg_version}.tar.gz"
 pkg_shasum="98c4254ae1be4e452aa7884245471501c9aa657993e0318d88f048093e7f88fd"
