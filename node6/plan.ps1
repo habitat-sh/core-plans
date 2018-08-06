5a88f79ed93fa0cff2441b79f4c977f374407fde
Nell Shamrell <nellshamrell@gmail.com>
2018-08-06 21:57:13 +0000

    Unifies Node ps1 plans and makes them able to be built multiple times
    Signed-off-by: Nell Shamrell <nellshamrell@gmail.com>

--- node6/plan.ps1
+++ node6/plan.ps1
@@ -1,26 +1,9 @@
+. "..\node\plan.ps1"
+
 $pkg_name="node6"
 $pkg_origin="core"
-$pkg_version="6.11.5"
+$pkg_version="6.14.3"
 $pkg_description="Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine."
-$pkg_upstream_url="https://nodejs.org/"
-$pkg_license=@("MIT")
-$pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_source="https://nodejs.org/dist/v$pkg_version/node-v$pkg_version-x64.msi"
-$pkg_shasum="c4aed94e82dbf246a1c9e0705c3054f0c0f3d9c4d8d025d877e0ef1f7b6cde4c"
-$pkg_build_deps=@("core/lessmsi")
-$pkg_bin_dirs=@("bin")
+$pkg_shasum="f67a3f3e24c25859c429fbd576d6d89301c74b5fff70533f4bcc97351df6dc02"
 
-function Invoke-Unpack {
-  write-host
-  lessmsi x (Resolve-Path "$HAB_CACHE_SRC_PATH/$pkg_filename").Path
-  mkdir "$HAB_CACHE_SRC_PATH/$pkg_dirname"
-  Move-Item "node-v$pkg_version-x64/SourceDir/nodejs" "$HAB_CACHE_SRC_PATH/$pkg_dirname"
-}
-
-function Invoke-Install {
-  Copy-Item "$HAB_CACHE_SRC_PATH/$pkg_dirname/nodejs/*" "$pkg_prefix/bin" -Recurse
-}
-
-function Invoke-Check() {
-  (& "$HAB_CACHE_SRC_PATH/$pkg_dirname/nodejs/node" --version) -eq "v$pkg_version"
-}
