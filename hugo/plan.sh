3a6aa9890754b8179aba0b7b3fb8bbdfe1d1f107
Graham Weldon <graham@grahamweldon.com>
2018-08-08 16:37:32 +0900

    Change to build hugo from source, to allow cacerts inclusion from patched go
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- hugo/plan.sh
+++ hugo/plan.sh
@@ -4,20 +4,33 @@ pkg_version="0.46"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("Apache-2.0")
 pkg_description="Hugo is one of the most popular open-source static site generators."
-pkg_source="https://github.com/gohugoio/hugo/releases/download/v${pkg_version}/hugo_${pkg_version}_Linux-64bit.tar.gz"
-pkg_shasum="96c431b1b76aed4833739966235098162b97ca9933e1ff2bcd09f7571842ea6b"
-pkg_build_deps=(core/go)
+pkg_build_deps=(
+  core/dep
+  rakops/mage
+)
 pkg_bin_dirs=(bin)
+pkg_source="https://github.com/gohugoio/hugo"
 pkg_upstream_url="https://gohugo.io"
+pkg_scaffolding="core/scaffolding-go"
 
-do_prepare() {
-  export GOPATH="${HAB_CACHE_SRC_PATH}"
+scaffolding_go_build_deps=(
+  github.com/magefile/mage
+)
+
+do_download() {
+  scaffolding_go_download
+  pushd "${scaffolding_go_pkg_path}" > /dev/null
+  git reset --hard "v${pkg_version}"
+  popd > /dev/null
 }
 
 do_build() {
-  return 0
+  pushd "${scaffolding_go_pkg_path}" > /dev/null
+  mage vendor
+  mage install
+  popd > /dev/null
 }
 
 do_install() {
-  cp "${HAB_CACHE_SRC_PATH}/hugo" "${pkg_prefix}/bin/"
+  cp "${GOPATH}/bin/hugo" "${pkg_prefix}/bin/hugo"
 }
