b461584d500f2029446e031265fc394afb44b76f
qubitrenegade <qubitrenegade@gmail.com>
2018-08-10 18:36:11 -0600

    Move core/python2 to pkg_deps from pkg_build_deps.  Fixes 1780
    Signed-off-by: qubitrenegade <qubitrenegade@gmail.com>

--- docutils/plan.sh
+++ docutils/plan.sh
@@ -10,11 +10,10 @@ pkg_license=(
 )
 pkg_source=https://downloads.sourceforge.net/project/${pkg_name}/${pkg_name}/${pkg_version}/${pkg_name}-${pkg_version}.tar.gz
 pkg_shasum=c7db717810ab6965f66c8cf0398a98c9d8df982da39b4cd7f162911eb89596fa
-pkg_deps=()
+pkg_deps=(core/python2)
 pkg_build_deps=(
   core/make
   core/gcc
-  core/python2
 )
 pkg_bin_dirs=(bin)
 pkg_lib_dirs=(lib)
