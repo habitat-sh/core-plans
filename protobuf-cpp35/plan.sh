6392cebc20f8f534f2af68465182b8d7b6a8a9ad
Nell Shamrell <nellshamrell@gmail.com>
2018-08-13 16:19:39 -0700

    adds in protobuf-cpp35 plan so that protobuf-c can build again
    Signed-off-by: Nell Shamrell <nellshamrell@gmail.com>

---
+++ protobuf-cpp35/plan.sh
@@ -0,0 +1,9 @@
+source ../protobuf-cpp/plan.sh
+
+pkg_name=protobuf-cpp35
+pkg_distname=protobuf
+pkg_version=3.5.0
+pkg_description="Google's language-neutral, platform-neutral, extensible mechanism for serializing structured data - pinned at version 3.5.0"
+pkg_dirname="protobuf-${pkg_version}"
+pkg_source=https://github.com/google/${pkg_distname}/releases/download/v${pkg_version}/${pkg_distname}-all-${pkg_version}.tar.gz
+pkg_shasum=5c3e0b369e767993969fdebcb3f44cbddd7fdd86644a926780daab46341d10a7
