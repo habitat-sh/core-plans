2c14208d5e000ae1384c9ae138206b41c6efde7b
Nell Shamrell <nellshamrell@gmail.com>
2018-08-16 11:06:40 -0700

    updates minio to the latest version
    Signed-off-by: Nell Shamrell <nellshamrell@gmail.com>

--- minio/plan.sh
+++ minio/plan.sh
@@ -1,10 +1,10 @@
 pkg_name=minio
 pkg_origin=core
-pkg_version="2018-05-11T00-29-24Z"
+pkg_version="2018-08-02T23-11-36Z"
 pkg_description="Minio is a high performance distributed object storage server, designed for large-scale private cloud infrastructure."
 pkg_upstream_url="https://minio.io"
 pkg_source="https://dl.minio.io/server/minio/release/linux-amd64/archive/minio.RELEASE.${pkg_version}"
-pkg_shasum="7a1c81a5d31f581e0013b76f9507e0021b174de99da74c83d3f4186e56684e8d"
+pkg_shasum="3eaa2e7788e5dcfd5dc99375a8f27ea94afdf028072db1c13c31c04c8c1713f4"
 pkg_license=('Apache-2.0')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_bin_dirs=(bin)
