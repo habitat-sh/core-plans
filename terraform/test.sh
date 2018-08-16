557ef2d5c4f3670ae03eaefadcfbd7f39da18b24
Graham Weldon <graham@grahamweldon.com>
2018-08-16 09:33:55 +0900

    Update terraform to 0.11.8, add testing
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

---
+++ terraform/test.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+SKIPBUILD=${SKIPBUILD:-0}
+
+hab pkg install --binlink core/bats
+source ./plan.sh
+
+if [ ${SKIPBUILD} -eq 0 ]; then
+  set -e
+  build
+  source results/last_build.env
+  hab pkg install --binlink --force "results/${pkg_artifact}"
+  set +e
+fi
+
+bats test.bats
