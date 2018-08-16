373c2ac28ab96724901f1e8e5d7bee35f3c8a2ae
Graham Weldon <graham@grahamweldon.com>
2018-08-16 09:47:33 +0900

    Quoting
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- terraform/test.sh
+++ terraform/test.sh
@@ -5,7 +5,7 @@ SKIPBUILD=${SKIPBUILD:-0}
 hab pkg install --binlink core/bats
 source ./plan.sh
 
-if [ ${SKIPBUILD} -eq 0 ]; then
+if [ "${SKIPBUILD}" -eq 0 ]; then
   set -e
   build
   source results/last_build.env
