2a6bf12bcfde0c09305053b1cb95eab4c8c16363
Graham Weldon <graham@grahamweldon.com>
2018-08-19 11:19:44 +0900

    Update minio
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- minio/plan.sh
+++ minio/plan.sh
@@ -1,10 +1,10 @@
 pkg_name=minio
 pkg_origin=core
-pkg_version="2018-08-02T23-11-36Z"
+pkg_version="2018-08-18T03-49-57Z"
 pkg_description="Minio is a high performance distributed object storage server, designed for large-scale private cloud infrastructure."
 pkg_upstream_url="https://minio.io"
 pkg_source="https://dl.minio.io/server/minio/release/linux-amd64/archive/minio.RELEASE.${pkg_version}"
-pkg_shasum="3eaa2e7788e5dcfd5dc99375a8f27ea94afdf028072db1c13c31c04c8c1713f4"
+pkg_shasum="8422d4ba558172b25ff35a9b612a6abeeb1bc64435dd64a10d9f88364f96512e"
 pkg_license=('Apache-2.0')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_bin_dirs=(bin)
