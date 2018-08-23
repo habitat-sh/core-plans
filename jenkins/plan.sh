20ddbd12d36630d45a2f3e67208b5c498563a67c
Graham Weldon <graham@grahamweldon.com>
2018-08-23 21:34:25 +0900

    Update jenkins to 2.121.3 - Security updates
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- jenkins/plan.sh
+++ jenkins/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=jenkins
 pkg_origin=core
-pkg_version=2.121.2
+pkg_version=2.121.3
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="The leading open source automation server, Jenkins provides hundreds of plugins to support building, deploying and automating any project."
 pkg_license=('MIT')
 pkg_upstream_url="https://jenkins.io/"
 pkg_source="http://mirrors.jenkins.io/war-stable/${pkg_version}/jenkins.war"
-pkg_shasum="da0f9d106e936246841a898471783fb4fbdbbacc8d42a156b7306a0855189602"
+pkg_shasum="50fbce11fa147d0ecd9ecf36cdae83ef795fb7d4776f33b5ea13bc15bf6e3c13"
 pkg_deps=(core/jre8 core/curl)
 pkg_exports=(
   [port]=jenkins.http.port
