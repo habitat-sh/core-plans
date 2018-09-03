5c5c2d2246a2b48a3c3d4859294cc2ca11d1080a
Yauhen Artsiukhou <jsirex@gmail.com>
2018-09-03 21:56:14 +0300

    Upgrade scaffolding chef
    Improvements:
    * Allowed customization of `chef/chef-client` and `chef/chef-dk` packages
    * All actions have been moved to functions
    * Improved detection of `policyfiles` directory
    * Used `$policyfile_lock` for the `chef install` by default
    * Fallback to `$policyfile_rb` for the `chef install` if no lock file
    * Only `$policyfile_lock` used when exporting
    * Added node attributes via `default.toml`
    * Added `named_run_list` setting
    * Added GEM_PATH. Set to `$pkg_prefix`. This allows bundled gems
    * Hardened permissions from `0755 -> 0750`
    
    Fixes:
    * Replaced typo `chown 0755` -> `chmod 0755`
    * Removed lock files deletion and magic detection of included
      policies: `include_policy` method allows different sources and it is
      hard to detect where to look for policies. This way is too buggy
    
    Signed-off-by: Yauhen Artsiukhou <jsirex@gmail.com>

--- scaffolding-chef/README.md
+++ scaffolding-chef/README.md
@@ -1,10 +1,10 @@
 # Chef Scaffolding for Habitat
 
-This will take a Chef policy file, and build it for use as a habitat service.
+This will take a Chef Policy file and will build it for use as a habitat service.
 
-```
-scaffold_policy_name="base"
-pkg_name=chef-base
+```bash
+scaffold_policy_name=base
+pkg_name=chef-policy-base
 pkg_origin=adam
 pkg_version="0.1.0"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
@@ -12,4 +12,165 @@ pkg_license=("Apache-2.0")
 pkg_description="The Chef $scaffold_policy_name Policy"
 pkg_upstream_url="http://chef.io"
 pkg_scaffolding="core/scaffolding-chef"
+```
+
+Scaffolding expects `policyfiles` directory somewhere in projects directory tree. For example:
+
+```
+.
+├── habitat
+│   └── plan.sh
+├── policyfiles
+│   ├── base.lock.json
+│   ├── base.rb
+│   ├── test-policy.lock.json
+│   └── test-policy.rb
+└── test-cookbook
+    ├── metadata.rb
+    └── recipes
+        └── default.rb
+```
+
+## Variables
+
+* `scaffold_policy_name` - **Required**. Specify policy to use
+* `scaffold_policy_group` - *Optional*. Set policy group in compile time. Default: `local`
+* `scaffold_chef_client` - *Optional*. Override `Chef Client` package. Default: `chef/chef-client`
+* `scaffold_chef_dk` - *Optional*. Override `Chef DK` package. Default `chef/chef-dk`
+
+## Bundled Gems
+
+This scaffolding allows you to bundle gems with a policy. Just install them in `$pkg_prefix` in `do_install` phase.
+`$pkg_prefix` will be added to `GEM_PATH` environment variable automatically. Don't forget to call `do_default_install`:
+
+```bash
+scaffold_policy_name=test-policy
+scaffold_policy_group=local
+scaffold_chef_client=chef/chef-client
+scaffold_chef_dk=chef/chef-dk
+
+pkg_name=policy-test
+pkg_origin=adam
+pkg_version="0.1.0"
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_license=("Apache-2.0")
+pkg_description="The Chef $scaffold_policy_name Policy"
+pkg_upstream_url="http://chef.io"
+pkg_scaffolding="core/scaffolding-chef"
+
+pkg_build_deps=(
+  core/git
+  core/make
+  core/gcc
+  core/gcc-libs
+  core/curl
+)
+
+do_build_config() {
+  do_default_build_config
+
+  cat << EOF >> $pkg_prefix/config/client-config.rb
+
+# Don't attempt reach any sources
+clear_gem_sources true
+rubygems_url []
+EOF
+  cat <<EOF >> $pkg_prefix/config/bootstrap-config.rb
+
+# Don't attempt reach any sources
+clear_gem_sources true
+rubygems_url []
+EOF
+}
+
+do_install() {
+  do_default_install
+  do_install_gems
+}
+
+do_install_gems() {
+  # You're free to install any gems, dealing with native extensions, etc
+  gem install mongo -v 2.6 -i $pkg_prefix
+  gem install docker-api -v 1.34.0 -i $pkg_prefix
+  gem install toml -i $pkg_prefix
+}
+
+```
+
+## Offline Deployment
+
+It is easily possible to create self-sufficient deployment archives with habitat and policies:
+
+1. Implement application package with `habitat`
+1. Write `Chef Cookbook` to manage your application
+1. Write `Chef Policy` file
+1. Add application packages into dependencies
+
+```bash
+scaffold_policy_name=myapp
+scaffold_policy_group=prod
+# scaffold_chef_client=chef/chef-client
+# scaffold_chef_dk=chef/chef-dk
+
+pkg_name=policy-myapp
+pkg_origin=adam
+pkg_version="1.1.1"
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_license=("Apache-2.0")
+pkg_description="The Chef $scaffold_policy_name Policy"
+pkg_upstream_url="http://chef.io"
+pkg_scaffolding="core/scaffolding-chef"
+
+pkg_deps=(
+  adam/myapp-database
+  adam/myapp-web
+  adam/myapp-loadbalancer
+)
+
+pkg_build_deps=(
+  core/git
+  core/make
+  core/gcc
+  core/gcc-libs
+  core/curl
+)
+
+do_build_config() {
+  do_default_build_config
+
+  cat << EOF >> $pkg_prefix/config/client-config.rb
+
+# Don't attempt reach any sources
+clear_gem_sources true
+rubygems_url []
+EOF
+  cat <<EOF >> $pkg_prefix/config/bootstrap-config.rb
+
+# Don't attempt reach any sources
+clear_gem_sources true
+rubygems_url []
+EOF
+}
+
+do_install() {
+  do_default_install
+  do_install_gems
+}
+
+do_install_gems() {
+  # Required for the habitat cookbook
+  gem install toml -i $pkg_prefix
+}
+```
+
+Then export policy to a tar and install to a server:
+
+```bash
+# Locally
+hab pkg export tar adam/policy-myapp
+
+# Upload tar to a server and install with:
+tar xf adam-policy-myapp-*.tar.gz -C /
+cd $(/hab/bin/hab pkg path adam/policy-myapp)
+/hab/bin/hab pkg exec adam/policy-myapp chef-client -z -c config/bootstrap-config.rb
 ```
--- scaffolding-chef/lib/scaffolding.sh
+++ scaffolding-chef/lib/scaffolding.sh
@@ -3,26 +3,34 @@
 #
 
 if [ -z "${scaffold_policy_name+x}" ]; then
-  echo "You must set \$scaffold_policy_name to a valid policy name. For example:"
-  echo
-  echo "\$scaffold_policy_name=base"
-  echo
-  echo "Will build a base.rb policyfile"
-  exit 1
+  warn "You must set \$scaffold_policy_name to a valid policy name. For example:"
+  warn ""
+  warn "\$scaffold_policy_name=base"
+  warn ""
+  warn "Will build policy either from base.lock.json or base.rb"
+  exit_with "scaffold_policy_name is unset. Exiting" 1
 fi
 
 scaffolding_load() {
-  parent_deps="${pkg_deps[*]}"
-  pkg_deps=("chef/chef-client" "core/cacerts")
-  for i in $parent_deps; do
-    pkg_deps+=($i)
-  done
-  parent_build_deps="${pkg_build_deps[*]}"
-  pkg_build_deps=("chef/chef-dk" "core/git")
-  for i in $parent_build_deps; do
-    pkg_build_deps+=($i)
-  done
-  pkg_svc_user="root"
+  : "${scaffold_policy_group:=local}"
+  : "${scaffold_chef_client:=chef/chef-client}"
+  : "${scaffold_chef_dk:=chef/chef-dk}"
+
+  pkg_deps=(
+    ${pkg_deps[@]}
+    $scaffold_chef_client
+    core/cacerts
+    core/busybox-static
+  )
+
+  pkg_build_deps=(
+    ${pkg_build_deps[@]}
+    $scaffold_chef_dk
+    core/git
+  )
+
+  pkg_svc_user=root
+  pkg_svc_group=root
   pkg_svc_run="set_just_so_you_will_render"
 }
 
@@ -38,99 +46,197 @@ do_default_unpack() {
   return 0
 }
 
-do_default_build_service() {
-  ## Create hooks
-  mkdir -p "$pkg_prefix/hooks"
-  chown 0644 "$pkg_prefix/hooks"
+do_default_build_config() {
+  scaffolding_build_default_toml
 
-  # Run hook
-  cat << EOF >> "$pkg_prefix/hooks/run"
-#!/bin/sh
+  scaffolding_build_config
+  scaffolding_build_config_bootstrap
+  scaffolding_build_config_client
+  scaffolding_build_config_node
+
+  scaffolding_build_hooks
+  scaffolding_build_hook_run
+}
+
+do_default_build_service() {
+  return 0
+}
+
+do_default_build() {
+  scaffolding_detect_policy
+  scaffolding_build_policy
+}
+
+do_default_install() {
+  scaffolding_install_policy
+}
+
+do_default_strip() {
+  return 0
+}
+
+scaffolding_detect_policy() {
+  local search_path="$PLAN_CONTEXT"
+
+  while [ "$search_path" != "/" ]; do
+    build_line "Looking for policy files in $search_path/policyfiles"
+    test -d "$search_path/policyfiles" && break
+    search_path=$(dirname "$search_path")
+  done
+
+  policyfiles="$search_path/policyfiles"
+  policyfile_rb=$policyfiles/$scaffold_policy_name.rb
+  policyfile_lock=$policyfiles/$scaffold_policy_name.lock.json
+
+  if [ ! -d "$policyfiles" ]; then
+    exit_with "Unable to find policyfiles directory in all parent folders"
+  fi
+
+  if [ -f "$policyfile_lock" ]; then
+    build_line "Detected $policyfile_lock"
+  elif [ -f "$policyfile_rb" ]; then
+    warn "Detected only source $policyfile_rb"
+  else
+    exit_with "Neither $policyfile_lock nor $policyfile_rb was found"
+  fi
+}
+
+scaffolding_build_policy() {
+  if [ -f "$policyfile_lock" ]; then
+    build_line "Using lock from: $policyfile_lock"
+    chef install "$policyfile_lock"
+  elif [ -f "$policyfile_rb" ]; then
+    warn "Using source from: $policyfile_rb"
+    chef install "$policyfile_rb"
+  else
+    exit_with "Policy: $scaffold_policy_name is not found"
+  fi
+}
+
+scaffolding_build_default_toml() {
+  cat << EOF > "$pkg_prefix/default.toml"
+env_path_prefix = "/sbin:/usr/sbin:/usr/local/sbin:/usr/local/bin:/usr/bin:/bin"
+
+interval = 1800
+splay = 180
+log_level = "warn"
+
+named_run_list = ""
+
+ssl_verify_mode = ":verify_none"
+
+[data_collector]
+enable = false
+token = "set_to_your_token"
+server_url = "set_to_your_url"
+
+# Chefs' Node attributes
+[node]
+policy_name = "$scaffold_policy_name"
+policy_group = "$scaffold_policy_group"
+EOF
+
+  chmod 0640 "$pkg_prefix/default.toml"
+}
+
+scaffolding_build_config() {
+  if [ ! -d "$pkg_prefix/config" ]; then
+    mkdir -p "$pkg_prefix/config"
+    chmod 0750 "$pkg_prefix/config"
+  fi
+}
+
+# This config can be used as is without habitat supervisor running
+scaffolding_build_config_bootstrap() {
+  cat << EOF > "$pkg_prefix/config/bootstrap-config.rb"
+policy_name '$scaffold_policy_name'
+policy_group '$scaffold_policy_group'
+
+use_policyfile true
+policy_document_native_api true
+
+cache_path "$pkg_svc_data_path/cache"
+node_path "$pkg_svc_data_path/nodes"
+role_path "$pkg_svc_data_path/roles"
+
+ssl_verify_mode :verify_none
+
+ENV['PATH'] = "/sbin:/usr/sbin:/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:#{ENV['PATH']}"
+ENV['GEM_PATH'] = "$pkg_prefix:#{ENV['GEM_PATH']}"
+EOF
+  chmod 0640 "$pkg_prefix/config/bootstrap-config.rb"
+}
+
+scaffolding_build_config_client() {
+  cat << EOF > "$pkg_prefix/config/client-config.rb"
+policy_name '$scaffold_policy_name'
+policy_group '$scaffold_policy_group'
+
+{{#if cfg.named_run_list ~}}
+named_run_list "{{cfg.named_run_list}}"
+{{/if ~}}
+
+use_policyfile true
+policy_document_native_api true
+
+cache_path "$pkg_svc_data_path/cache"
+node_path "$pkg_svc_data_path/nodes"
+role_path "$pkg_svc_data_path/roles"
+
+json_attribs "{{pkg.svc_config_path}}/node.json"
+ssl_verify_mode {{cfg.ssl_verify_mode}}
+
+{{#if cfg.data_collector.enable ~}}
+data_collector.token "{{cfg.data_collector.token}}"
+data_collector.server_url "{{cfg.data_collector.server_url}}"
+{{/if ~}}
+
+ENV['PATH'] = "{{cfg.env_path_prefix}}:#{ENV['PATH']}"
+ENV['GEM_PATH'] = "$pkg_prefix:#{ENV['GEM_PATH']}"
+EOF
+  chmod 0640 "$pkg_prefix/config/client-config.rb"
+}
+
+scaffolding_build_config_node() {
+  cat <<EOF > "$pkg_prefix/config/node.json"
+{{toJson cfg.node}}
+EOF
+
+  chmod 0750 "$pkg_prefix/config/node.json"
+}
+
+scaffolding_build_hooks() {
+  if [ ! -d "$pkg_prefix/hooks" ]; then
+    mkdir -p "$pkg_prefix/hooks"
+    chmod 0750 "$pkg_prefix/hooks"
+  fi
+}
+
+scaffolding_build_hook_run() {
+  cat << EOF > "$pkg_prefix/hooks/run"
+#!{{pkgPathFor "core/busybox-static"}}/bin/sh
 
 export SSL_CERT_FILE="{{pkgPathFor "core/cacerts"}}/ssl/cert.pem"
 
 cd {{pkg.path}}
 
 exec 2>&1
+
 while true; do
-SPLAY_DURATION=\$({{pkgPathFor "core/coreutils"}}/bin/shuf -i 0-{{cfg.splay}} -n 1)
+SPLAY_DURATION=\$({{pkgPathFor "core/busybox-static"}}/bin/shuf -i 0-{{cfg.splay}} -n 1)
 sleep \$SPLAY_DURATION
-chef-client -z -l {{cfg.log_level}} -c $pkg_svc_config_path/client-config.rb --once --no-fork --run-lock-timeout {{cfg.interval}}
+chef-client -z -l {{cfg.log_level}} -c {{pkg.svc_config_path}}/client-config.rb --once --no-fork --run-lock-timeout {{cfg.interval}}
 sleep {{cfg.interval}}
 done
 EOF
-  chown 0755 "$pkg_prefix/hooks/run"
+
+  chmod 0750 "$pkg_prefix/hooks/run"
 }
 
-do_default_build() {
-  if [ -d "$PLAN_CONTEXT/../policyfiles" ]; then
-    _policyfile_path="$PLAN_CONTEXT/../policyfiles"
+scaffolding_install_policy() {
+  if [ -f "$policyfile_lock" ]; then
+    chef export "$policyfile_lock" "$pkg_prefix"
   else
-    if [ -d "$PLAN_CONTEXT/../../policyfiles" ]; then
-      _policyfile_path="$PLAN_CONTEXT/../../policyfiles"
-    else
-      if [ -d "$PLAN_CONTEXT/../../../policyfiles" ]; then
-        _policyfile_path="$PLAN_CONTEXT/../../../policyfiles"
-      else
-        echo "Cannot detect a policyfiles directory!"
-        exit 1
-      fi
-    fi
+    exit_with "$policyfile_lock was not found"
   fi
-  rm -f "$_policyfile_path"/*.lock.json
-  policyfile="$_policyfile_path/$scaffold_policy_name.rb"
-  for x in $(grep include_policy "$policyfile" | awk -F "," '{print $1}' | awk -F '"' '{print $2}' | tr -d " "); do
-    chef install "$_policyfile_path/$x.rb"
-  done
-  chef install "$policyfile"
-}
-
-do_default_install() {
-  chef export "$_policyfile_path/$scaffold_policy_name.lock.json" "$pkg_prefix"
-
-  mkdir -p "$pkg_prefix/config"
-  chown 0755 "$pkg_prefix/config"
-  cat << EOF >> "$pkg_prefix/.chef/config.rb"
-cache_path "$pkg_svc_data_path/cache"
-node_path "$pkg_svc_data_path/nodes"
-role_path "$pkg_svc_data_path/roles"
-
-ssl_verify_mode {{cfg.ssl_verify_mode}}
-chef_zero.enabled true
-EOF
-
-  cp "$pkg_prefix/.chef/config.rb" "$pkg_prefix/config/bootstrap-config.rb"
-  cat << EOF >> "$pkg_prefix/config/bootstrap-config.rb"
-ENV['PATH'] = "/sbin:/usr/sbin:/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:#{ENV['PATH']}"
-EOF
-
-  cp "$pkg_prefix/.chef/config.rb" "$pkg_prefix/config/client-config.rb"
-  cat << EOF >> "$pkg_prefix/config/client-config.rb"
-ENV['PATH'] = "{{cfg.env_path_prefix}}:#{ENV['PATH']}"
-
-{{#if cfg.data_collector.enable ~}}
-data_collector.token "{{cfg.data_collector.token}}"
-data_collector.server_url "{{cfg.data_collector.server_url}}"
-{{/if ~}}
-EOF
-  chown 0644 "$pkg_prefix/config/client-config.rb"
-
-  ## Create config
-  cat << EOF >> "$pkg_prefix/default.toml"
-interval = 1800
-splay = 180
-log_level = "warn"
-env_path_prefix = "/sbin:/usr/sbin:/usr/local/sbin:/usr/local/bin:/usr/bin:/bin"
-ssl_verify_mode = ":verify_peer"
-
-[data_collector]
-enable = false
-token = "set_to_your_token"
-server_url = "set_to_your_url"
-EOF
-  chown 0644 "$pkg_prefix/default.toml"
-}
-
-do_default_strip() {
-  return 0
 }
