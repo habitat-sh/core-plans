c15366b97073a340a6c7223cc8f5125cee33a4d4
Nell Shamrell <nellshamrell@gmail.com>
2018-09-06 15:19:48 -0700

    fixes mage build
    Signed-off-by: Nell Shamrell <nellshamrell@gmail.com>

--- mage/plan.sh
+++ mage/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=mage
 pkg_origin=core
-pkg_version="2.1"
+pkg_version="v1.2.4"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("Apache-2.0")
 pkg_description="Mage is a make/rake-like build tool using Go. You write plain-old go functions, and Mage automatically uses them as Makefile-like runnable targets."
