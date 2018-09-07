a88f5943674aaf37d5747d984af8c5ab35725957
Graham Weldon <graham@grahamweldon.com>
2018-09-07 09:05:33 +0900

    Upgrade vault to 0.11.1
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- vault/plan.sh
+++ vault/plan.sh
@@ -1,12 +1,12 @@
 pkg_origin=core
 pkg_name=vault
-pkg_version=0.10.4
+pkg_version=0.11.1
 pkg_description="A tool for managing secrets."
 pkg_maintainer='The Habitat Maintainers <humans@habitat.sh>'
 pkg_license=("MPL-2.0")
 pkg_upstream_url=https://www.vaultproject.io/
 pkg_source="https://releases.hashicorp.com/vault/${pkg_version}/vault_${pkg_version}_linux_amd64.zip"
-pkg_shasum=b3c8a62e8f2f1b2d9d7ddca3d8bda620ab0091bb038da0d013c26f7b6b18f13a
+pkg_shasum=eb8d2461d0ca249c1f91005f878795998bdeafccfde0b9bae82343541ce65996
 pkg_filename="${pkg_name}-${pkg_version}_linux_amd64.zip"
 pkg_deps=()
 pkg_build_deps=(core/unzip)
