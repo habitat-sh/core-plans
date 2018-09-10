c82e8f072a8548b14bc4caf9e7a38a3e67f08379
Ben Dang <me@bdang.it>
2018-09-09 22:11:22 -0700

    Bump googletest 1.8.1 and deprecate googlemock
    Signed-off-by: Ben Dang <me@bdang.it>

--- googlemock/README.md
+++ googlemock/README.md
@@ -1,17 +1,10 @@
-Google Mock
-===========
+Google Mock (deprecated)
+========================
 
-Maintainer: [Ben Dang](me@bdang.it)
+Google Mock has been folded into [Google Test](../googletest/README.md) which means if you need any
+of the Google Mock libraries, all you need to include your plan is `core/googletest`.
 
-Google Mock are part of the Google Test unit test framework for C/C++.  This package contains
-pre-built static libraries for you to include into your project.  More info on usage can be
-followed [here](https://github.com/google/googletest/tree/master/googlemock).
+## Maintainers:
 
-To help with locating the static libraries, a custom `pkg_config` has been included.  When you
-build your app with Habitat, your `CXX_FLAGS` will have the correct include folder and your
-`LD_LIBRARY_PATH` will have the path to the static library.  All you get to focus on is writing
-awesome tests!
-
-## Usage
-
-Just include `core/googlemock` preferably in `pkg_build_deps`.
+* The Habitat Maintainers: <humans@habitat.sh>
+* [Ben Dang](me@bdang.it)
--- googletest/plan.sh
+++ googletest/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=googletest
 pkg_origin=core
-pkg_version="1.8.0"
+pkg_version="1.8.1"
 pkg_description="$(cat << EOF
 Google C++ Testing Framework helps you write better C++ tests.
 EOF
@@ -8,7 +8,7 @@ EOF
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('bsd-3-clause')
 pkg_source="https://github.com/google/${pkg_name}/archive/release-${pkg_version}.tar.gz"
-pkg_shasum="58a6f4277ca2bc8565222b3bbd58a177609e9c488e8a72649359ba51450db7d8"
+pkg_shasum="9bf1fe5182a604b4135edc1a425ae356c9ad15e9b23f9f12a02e80184c3a249c"
 pkg_upstream_url="https://github.com/google/googletest"
 pkg_dirname="${pkg_name}-release-${pkg_version}"
 
@@ -20,52 +20,45 @@ pkg_build_deps=(
   core/cmake
   core/gcc
   core/make
+  core/python2
 )
 
 pkg_include_dirs=(include)
-pkg_lib_dirs=(lib)
-pkg_pconfig_dirs=(lib/pkgconfig)
+pkg_lib_dirs=(lib64)
+pkg_pconfig_dirs=(lib64/pkgconfig)
 
-BUILDDIR="build"
+do_setup_environment() {
+  set_buildtime_env BUILD_DIR "build"
+}
 
 do_prepare() {
-  export GTEST_TARGET="${pkg_name}"
-  build_line "Plan Setting GTEST_TARGET=${GTEST_TARGET}"
+  mkdir -p "${BUILD_DIR}"
 
-  mkdir "${BUILDDIR}" || true
-  mkdir "${BUILDDIR}/${GTEST_TARGET}" || true
+  # patch version as it is suppose to be 1.8.1
+  sed -e "s/GOOGLETEST_VERSION 1.9.0/GOOGLETEST_VERSION ${pkg_version}/" -i CMakeLists.txt
 }
 
 do_build() {
-  cd "${BUILDDIR}/${GTEST_TARGET}" || exit
+  pushd "${BUILD_DIR}" || exit 1
   cmake -Dgtest_build_samples="${DO_CHECK}" \
-      -Dgmock_build_samples="${DO_CHECK}" \
       -Dgtest_build_tests="${DO_CHECK}" \
       -Dgmock_build_tests="${DO_CHECK}" \
       -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-      "../../${GTEST_TARGET}"
+      ..
   make
+  popd || exit 1
 }
 
 do_check() {
-  cd "${BUILDDIR}/${GTEST_TARGET}" || exit
+  pushd "${BUILD_DIR}" || exit 1
   CTEST_OUTPUT_ON_FAILURE=1 make test
+  popd || exit 1
 }
 
 do_install() {
-  cd "${BUILDDIR}/${GTEST_TARGET}" || exit
+  pushd "${BUILD_DIR}" || exit 1
   make install
-  cd ../.. || exit
+  popd || exit 1
 
-  install -Dm644 googletest/m4/gtest.m4 "${pkg_prefix}/share/aclocal"
-  install -Dm644 googletest/LICENSE "${pkg_prefix}/share/licenses/license.txt"
-
-  mkdir -p "${pkg_prefix}/lib/pkgconfig"
-  cat <<EOF > "${pkg_prefix}/lib/pkgconfig/libgtest.pc"
-Name: libgtest
-Description: ${pkg_description}
-Version: ${pkg_version}
-Cflags: -I${pkg_prefix}/include/gtest
-Libs: -L${pkg_prefix}/lib -lgtest -lgtest_main
-EOF
+  install -Dm644 LICENSE "${pkg_prefix}/share/licenses/license.txt"
 }
