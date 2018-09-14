6b10f38359307b6e6875859d38e7b9bdc75a47cc
Graham Weldon <graham@grahamweldon.com>
2018-09-14 10:56:04 +0900

    Added SASL support to memcached, and added tests
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- memcached/plan.sh
+++ memcached/plan.sh
@@ -7,8 +7,16 @@ pkg_upstream_url="https://memcached.org/"
 pkg_license=('BSD')
 pkg_source=http://www.memcached.org/files/${pkg_name}-${pkg_version}.tar.gz
 pkg_shasum=494c060dbd96d546c74ab85a3cc3984d009b4423767ac33e05dd2340c01f1c4b
-pkg_deps=(core/glibc core/libevent)
-pkg_build_deps=(core/git core/gcc core/make)
+pkg_deps=(
+  core/glibc
+  core/libevent
+  core/libsasl2
+)
+pkg_build_deps=(
+  core/git
+  core/gcc
+  core/make
+)
 pkg_bin_dirs=(bin)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
@@ -17,3 +25,10 @@ pkg_exports=(
   [port]=port
 )
 pkg_exposes=(port)
+
+do_build() {
+  ./configure \
+    --prefix="${pkg_prefix}" \
+    --enable-sasl
+  make
+}
