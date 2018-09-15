40741f1a56f3c26122ffd28e82d5a2b82b0943a6
Graham Weldon <graham@grahamweldon.com>
2018-09-15 18:10:05 +0900

    Upgrade packer to 1.3.1, add simple test
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- packer/plan.sh
+++ packer/plan.sh
@@ -1,10 +1,10 @@
 pkg_name=packer
 pkg_origin=core
-pkg_version=1.3.0
+pkg_version=1.3.1
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('MPL2')
 pkg_source="https://releases.hashicorp.com/${pkg_name}/${pkg_version}/${pkg_name}_${pkg_version}_linux_amd64.zip"
-pkg_shasum="0512af351124e63f8079458391e7f7b316e4b3ac4dc40e6f18058fd924f1b24a"
+pkg_shasum="254cf648a638f7ebd37dc1b334abe940da30b30ac3465b6e0a9ad59829932fa3"
 pkg_description="Packer is a tool for creating machine and container images for multiple platforms from a single source configuration."
 pkg_upstream_url=https://packer.io
 pkg_build_deps=(core/unzip)
