86695f39651a4122ea1c2c12b075527f16afb9c4
Graham Weldon <graham@grahamweldon.com>
2018-09-18 10:40:11 +0900

    Upgrade dotnet-core to 2.1.4, add tests
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- dotnet-core/plan.sh
+++ dotnet-core/plan.sh
@@ -1,14 +1,14 @@
 pkg_name=dotnet-core
 pkg_origin=core
-pkg_version=2.0.6
+pkg_version=2.1.4
 pkg_license=('MIT')
 pkg_upstream_url=https://www.microsoft.com/net/core
 pkg_description=".NET Core is a blazing fast, lightweight and modular platform
   for creating web applications and services that run on Windows,
   Linux and Mac."
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_source="https://download.microsoft.com/download/8/D/A/8DA04DA7-565B-4372-BBCE-D44C7809A467/dotnet-runtime-${pkg_version}-linux-x64.tar.gz"
-pkg_shasum=0eeb894d6e17ac0fc036997ba11ddf6665da5161e2e898788220e1945fa9bf11
+pkg_source="https://download.microsoft.com/download/A/7/8/A78F1D25-8D5C-4411-B544-C7D527296D5E/dotnet-runtime-2.1.4-linux-x64.tar.gz"
+pkg_shasum=55e6d5c93ab5bb492c82b8d0d57d0d67ac720733f10c18605d8b93d26866adb2
 pkg_filename="dotnet-debian-x64.${pkg_version}.tar.gz"
 pkg_deps=(
   core/curl
@@ -29,17 +29,17 @@ pkg_bin_dirs=(bin)
 
 do_unpack() {
   # Extract into $pkg_dirname instead of straight into $HAB_CACHE_SRC_PATH.
-  mkdir -p "$HAB_CACHE_SRC_PATH/$pkg_dirname"
-  tar xf "$HAB_CACHE_SRC_PATH/$pkg_filename" \
-    -C "$HAB_CACHE_SRC_PATH/$pkg_dirname" \
+  mkdir -p "${HAB_CACHE_SRC_PATH}/${pkg_dirname}"
+  tar xf "${HAB_CACHE_SRC_PATH}/${pkg_filename}" \
+    -C "${HAB_CACHE_SRC_PATH}/${pkg_dirname}" \
     --no-same-owner
 }
 
 do_prepare() {
   find . -type f -name 'dotnet' \
-    -exec patchelf --interpreter "$(pkg_path_for glibc)/lib/ld-linux-x86-64.so.2" --set-rpath "$LD_RUN_PATH" {} \;
+    -exec patchelf --interpreter "$(pkg_path_for glibc)/lib/ld-linux-x86-64.so.2" --set-rpath "${LD_RUN_PATH}" {} \;
   find . -type f -name '*.so*' \
-    -exec patchelf --set-rpath "$LD_RUN_PATH" {} \;
+    -exec patchelf --set-rpath "${LD_RUN_PATH}" {} \;
 }
 
 do_build() {
@@ -47,8 +47,11 @@ do_build() {
 }
 
 do_install() {
-  cp -a . "$pkg_prefix/bin"
-  chmod o+r -R "$pkg_prefix/bin"
+  cp -a . "${pkg_prefix}/bin"
+  chmod o+r -R "${pkg_prefix}/bin"
+  # Move text files out of bin directory
+  mv "${pkg_prefix}/bin/LICENSE.txt" "${pkg_prefix}/LICENSE.txt"
+  mv "${pkg_prefix}/bin/ThirdPartyNotices.txt" "${pkg_prefix}/ThirdPartyNotices.txt"
 }
 
 do_strip() {
