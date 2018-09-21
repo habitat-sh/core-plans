ac25a5e4716182305736a10cc9ecaed78ad6512c
Graham Weldon <graham@grahamweldon.com>
2018-09-21 14:35:14 +0900

    Update nodejs to 10.11.0
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- node10/plan.sh
+++ node10/plan.sh
@@ -2,11 +2,11 @@ source "../node/plan.sh"
 
 pkg_name=node10
 pkg_origin=core
-pkg_version=10.10.0
+pkg_version=10.11.0
 pkg_description="Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine."
 pkg_license=('MIT')
 pkg_upstream_url=https://nodejs.org/
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="https://nodejs.org/dist/v${pkg_version}/node-v${pkg_version}.tar.gz"
-pkg_shasum=8cfcb0d6f859645934a9318d44872e9ebcf30dde6f567f453e6380912b2acff5
+pkg_shasum=f721552552fb11ef99aba290fc6e696a8647adc98d643db6651e81ed07c4037e
 pkg_dirname="node-v${pkg_version}"
