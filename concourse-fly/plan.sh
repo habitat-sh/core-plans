f58335e19f0ee6acdea23ca19fd02036a036e39c
W. Duncan Fraser <wdf@alasconnect.com>
2018-09-24 15:10:28 -0500

    Rolled concourse and fly forward to 4.2.1 Converted deps to multiline arrays
    Signed-off-by: W. Duncan Fraser <wdf@alasconnect.com>

--- concourse-fly/plan.sh
+++ concourse-fly/plan.sh
@@ -1,13 +1,18 @@
 pkg_name=concourse-fly
 pkg_origin=core
-pkg_version="3.13.0"
+pkg_version="4.2.1"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_description="Concourse CLI for interacting with the ATC API"
 pkg_license=('Apache-2.0')
+pkg_description="Concourse CLI for interacting with the ATC API"
 pkg_upstream_url="https://concourse.ci"
-
 pkg_source="https://github.com/concourse/concourse.git"
-pkg_build_deps=("core/cacerts" "core/gnupg" "core/go" "core/git")
+
+pkg_build_deps=(
+  core/cacerts
+  core/gnupg
+  core/go
+  core/git
+)
 pkg_bin_dirs=(bin)
 
 do_download() {
--- concourse/plan.sh
+++ concourse/plan.sh
@@ -1,16 +1,21 @@
 pkg_name=concourse
 pkg_origin=core
-pkg_version="3.13.0"
+pkg_version="4.2.1"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('Apache-2.0')
-pkg_source="https://github.com/${pkg_name}/${pkg_name}/releases/download/v${pkg_version}/${pkg_name}_linux_amd64"
-pkg_filename="${pkg_name}_linux_amd64"
-pkg_shasum="cd232c5dd7a47616f5c8037a90c7b945edae8c6588bf5df833dd632af08a1d4b"
-pkg_deps=(core/glibc)
-pkg_build_deps=(core/patchelf)
-pkg_bin_dirs=(bin)
 pkg_description="CI that scales with your project"
 pkg_upstream_url="https://concourse.ci"
+pkg_source="https://github.com/${pkg_name}/${pkg_name}/releases/download/v${pkg_version}/${pkg_name}_linux_amd64"
+pkg_filename="${pkg_name}_linux_amd64"
+pkg_shasum="e3cada9e536af9596cfb812dc4af37cbc9ff7e4ed6f3d3ee91a6b62a6356d02a"
+
+pkg_deps=(
+  core/glibc
+)
+pkg_build_deps=(
+  core/patchelf
+)
+pkg_bin_dirs=(bin)
 
 do_unpack() {
   pushd "${HAB_CACHE_SRC_PATH}"
