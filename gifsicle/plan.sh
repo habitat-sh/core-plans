9c3931fd4448d828a652c483d4f7c4bfb3a113a6
Graham Weldon <graham@grahamweldon.com>
2018-09-30 10:00:16 +0900

    Upgrade gifsicle to 1.91, add tests
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- gifsicle/plan.sh
+++ gifsicle/plan.sh
@@ -1,11 +1,11 @@
 pkg_name=gifsicle
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_origin=core
-pkg_version=1.88
+pkg_version=1.91
 pkg_license=('GPLv2')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source=https://www.lcdf.org/gifsicle/gifsicle-${pkg_version}.tar.gz
-pkg_shasum=4585d2e683d7f68eb8fcb15504732d71d7ede48ab5963e61915201f9e68305be
+pkg_shasum=0a4ee602aa244cdcdd86a250a6b39c94d8343cf526b8fae862d8a0efc337a800
 pkg_bin_dirs=(bin)
 pkg_deps=(core/zlib core/glibc)
 pkg_build_deps=(core/zlib core/coreutils core/diffutils core/patch core/make core/gcc core/sed core/glibc)
