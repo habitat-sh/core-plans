65edf4a3a1a079ad2000ed2bd8e2271b74e6729a
Graham Weldon <graham@grahamweldon.com>
2018-10-05 13:23:20 +0900

    Update nghttp2 library to 1.34.0
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- nghttp2/plan.sh
+++ nghttp2/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=nghttp2
 pkg_origin=core
-pkg_version=1.33.0
+pkg_version=1.34.0
 pkg_description="nghttp2 is an open source HTTP/2 C Library."
 pkg_upstream_url=https://nghttp2.org/
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('MIT')
 pkg_source="https://github.com/${pkg_name}/${pkg_name}/releases/download/v${pkg_version}/${pkg_name}-${pkg_version}.tar.gz"
-pkg_shasum=42fff7f290100c02234ac3b0095852e4392e6bfd95ebed900ca8bd630850d88c
+pkg_shasum=8889399ddd38aa0405f6e84f1c050a292286089441686b8a9c5e937de4f5b61d
 pkg_build_deps=(
   core/make
   core/gcc
