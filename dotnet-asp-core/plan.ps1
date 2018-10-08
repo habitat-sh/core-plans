72393e4a131b2c392fdc3208a9a3b674625dcc43
Christopher P. Maher <chris@mahercode.io>
2018-10-08 11:31:43 -0700

    Updated Windows Plan

--- dotnet-asp-core/plan.ps1
+++ dotnet-asp-core/plan.ps1
@@ -1,15 +1,13 @@
-$pkg_name="dotnet-core"
+$pkg_name="dotnet-asp-core"
 $pkg_origin="core"
-$pkg_version="2.0.6"
+$pkg_version="2.1.5"
 $pkg_license=('MIT')
-$pkg_upstream_url="https://www.microsoft.com/net/core"
-$pkg_description=".NET Core is a blazing fast, lightweight and modular platform
-  for creating web applications and services that run on Windows,
-  Linux and Mac."
+$pkg_upstream_url="https://docs.microsoft.com/en-us/aspnet/core"
+$pkg_description="ASP.NET Core is a cross-platform, high-performance, open-source framework for building modern, cloud-based, Internet-connected applications."
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-$pkg_source="https://download.microsoft.com/download/8/D/A/8DA04DA7-565B-4372-BBCE-D44C7809A467/dotnet-runtime-$pkg_version-win-x64.zip"
-$pkg_shasum="d10abca0c869e72fcb45fe75940b1f28aed5dbbb1d5b048737d71d732a7b6670"
-$pkg_filename="dotnet-win-x64.$pkg_version.zip"
+$pkg_source="https://download.visualstudio.microsoft.com/download/pr/6abfd5c4-f9e2-41fb-9363-fd60e3f9132f/1a5d3c82408f5e27b0e83be8c7f1ae42/aspnetcore-runtime-$pkg_version-win-x64.zip"
+$pkg_shasum="5b15819d68fe5a3ea1b181e1e3c33cce9884b80182256492eb4c10836b7b9dce"
+$pkg_filename="asp-dotnet-win-x64.$pkg_version.zip"
 $pkg_bin_dirs=@("bin")
 
 function Invoke-Install {
