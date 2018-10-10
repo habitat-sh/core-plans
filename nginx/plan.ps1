f9ffa8862f83f1928b2cc5738855544cf1b6226f
Graham Weldon <graham@grahamweldon.com>
2018-10-10 10:14:21 +0900

    Update windows version to 1.15.5
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- nginx/plan.ps1
+++ nginx/plan.ps1
@@ -1,12 +1,12 @@
 $pkg_name="nginx"
 $pkg_origin="core"
-$pkg_version="1.15.4"
+$pkg_version="1.15.5"
 $pkg_description="NGINX web server."
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_license=('BSD-2-Clause')
 $pkg_source="https://nginx.org/download/nginx-$pkg_version.zip"
 $pkg_upstream_url="https://nginx.org/"
-$pkg_shasum="16e450dc417155135642450de3eb86318610faa8d280fa7b02ce68263dcc0b15"
+$pkg_shasum="bc5582e8459ce9a56919f6b3a7db2ba666a5223f63e0f3dae58b3a82766172ef"
 $pkg_bin_dirs=@('bin')
 $pkg_exports=@{port="http.listen.port"}
 $pkg_exposes=@('port')
