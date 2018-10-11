61d5705ebd155483927750735c4fd920afe9a172
Graham Weldon <graham@grahamweldon.com>
2018-10-11 11:58:17 +0900

    Update node/node10 to 10.12.0 on Linux and Windows
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- node/plan.ps1
+++ node/plan.ps1
@@ -1,12 +1,12 @@
 $pkg_name="node"
 $pkg_origin="core"
-$pkg_version="10.8.0"
+$pkg_version="10.12.0"
 $pkg_description="Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine."
 $pkg_upstream_url="https://nodejs.org/"
 $pkg_license=@("MIT")
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_source="https://nodejs.org/dist/v$pkg_version/node-v$pkg_version-x64.msi"
-$pkg_shasum="9d03d6bc78d7375fa549005c9b12cf5da4b01ee52b60834107f5f603d82a68f2"
+$pkg_shasum="21fcc88b9af133b0ecdf03a0cdf965ad5f2a547759096fb3b050be4ca871de8f"
 $pkg_build_deps=@("core/lessmsi")
 $pkg_bin_dirs=@("bin")
 
