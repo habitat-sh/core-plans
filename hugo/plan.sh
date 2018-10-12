fa4d4e6597488c0605ecda48e06f0f62f5b26859
Graham Weldon <graham@grahamweldon.com>
2018-10-12 23:47:06 +0900

    Update hugo to 0.49.2, add tests
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- hugo/plan.sh
+++ hugo/plan.sh
@@ -1,39 +1,35 @@
 pkg_name=hugo
 pkg_origin=core
-pkg_version="0.46"
+pkg_version="0.49.2"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("Apache-2.0")
 pkg_description="Hugo is one of the most popular open-source static site generators."
 pkg_build_deps=(
-  core/dep
-  core/mage
+  core/go
+  core/git
+  core/gcc
 )
 pkg_deps=(
-  core/glibc
+  # core/glibc
 )
 pkg_bin_dirs=(bin)
-pkg_source="https://github.com/gohugoio/hugo"
+pkg_repository="https://github.com/gohugoio/hugo.git"
 pkg_upstream_url="https://gohugo.io"
-pkg_scaffolding="core/scaffolding-go"
-
-scaffolding_go_build_deps=(
-  github.com/magefile/mage
-)
-
-do_download() {
-  scaffolding_go_download
-  pushd "${scaffolding_go_pkg_path}" > /dev/null
-  git reset --hard "v${pkg_version}"
-  popd > /dev/null
-}
 
 do_build() {
-  pushd "${scaffolding_go_pkg_path}" > /dev/null
-  mage vendor
-  mage install
+  local hugo_dir="${HAB_CACHE_SRC_PATH}/hugo-${pkg_version}"
+  git clone \
+    --depth 1 \
+    --branch "v${pkg_version}" \
+    --config advice.detachedHead=false \
+    "${pkg_repository}" \
+    "${hugo_dir}"
+  pushd "${hugo_dir}" > /dev/null
+  go install --tags extended
+  go build -o hugo main.go
   popd > /dev/null
 }
 
 do_install() {
-  cp "${GOPATH}/bin/hugo" "${pkg_prefix}/bin/hugo"
+  cp "${HAB_CACHE_SRC_PATH}/hugo-${pkg_version}/hugo" "${pkg_prefix}/bin/"
 }
