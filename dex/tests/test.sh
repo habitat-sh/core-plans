0f8cfbcbe270ea0ac205880f4a5c11e88c05803f
Stephan Renatus <srenatus@chef.io>
2018-10-12 11:37:55 +0200

    [dex] add tests
    These are modelled after the consul ones, and also snatched their helper
    script.
    
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

---
+++ dex/tests/test.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+TESTDIR="$(dirname "${0}")"
+PLANDIR="$(dirname "${TESTDIR}")"
+SKIPBUILD=${SKIPBUILD:-0}
+
+source "${TESTDIR}/helpers.bash"
+
+hab pkg install --binlink core/bats
+
+hab pkg install core/busybox-static
+hab pkg binlink core/busybox-static nc
+hab pkg install -b core/jq-static core/curl
+
+# Wait for supervisor to start
+echo "Waiting for supervisor to start"
+wait_listen tcp 9632 30
+
+source "${PLANDIR}/plan.sh"
+# Unload the service if its already loaded.
+hab svc unload "${HAB_ORIGIN}/${pkg_name}"
+
+if [ "${SKIPBUILD}" -eq 0 ]; then
+  set -e
+  pushd "${PLANDIR}" > /dev/null
+  build
+  popd > /dev/null
+  set +e
+fi
+
+source results/last_build.env
+hab pkg install --binlink --force "results/${pkg_artifact}"
+hab svc load "${pkg_ident}"
+
+# Wait for 5 seconds on first check, to ensure service is up.
+echo "Waiting for dex to start (5s)"
+wait_listen tcp 5556 5
+
+bats "${TESTDIR}/test.bats"
