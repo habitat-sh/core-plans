3b62b230ee876fe5166ffb02f6c0bba11d6cfa16
Graham Weldon <graham@grahamweldon.com>
2018-10-18 09:08:42 +0900

    Fallback to plan build if there are no tests
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- bin/travis-plan-test.sh
+++ bin/travis-plan-test.sh
@@ -22,14 +22,19 @@ while read -r line; do
   fi
 
   if [ ! -f "${line}/tests/test.sh" ]; then
-    echo "Skipping ${line}: (no test.sh file)"
-    continue
-  fi
-
-  hab studio run "${line}/tests/test.sh"
-  result=$?
-  if [ "${result}" -eq 1 ]; then
-    echo "[FAIL] Exiting studio testing after test failure for ${line}"
-    exit 1
+    echo "Performing a default build as there are no tests:"
+    hab studio build "${line}"
+    result=$?
+    if [ "${result}" -eq 1 ]; then
+      echo "[FAIL] Exiting studio testing after build failure for ${line}"
+      exit 1
+    fi
+  else
+    hab studio run "${line}/tests/test.sh"
+    result=$?
+    if [ "${result}" -eq 1 ]; then
+      echo "[FAIL] Exiting studio testing after build/test failure for ${line}"
+      exit 1
+    fi
   fi
 done < <(git show --pretty=format: --name-only "$TRAVIS_COMMIT_RANGE"|grep -v "^$"|sort|uniq|grep "\/" | awk -F'/' '{print $1}'|sort|uniq)
