acc2024630753207ddd149837569412dce5e125c
Graham Weldon <graham@grahamweldon.com>
2018-10-30 09:48:42 +0900

    Update packer to 1.3.2
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- packer/plan.sh
+++ packer/plan.sh
@@ -1,10 +1,10 @@
 pkg_name=packer
 pkg_origin=core
-pkg_version=1.3.1
+pkg_version=1.3.2
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('MPL2')
 pkg_source="https://releases.hashicorp.com/${pkg_name}/${pkg_version}/${pkg_name}_${pkg_version}_linux_amd64.zip"
-pkg_shasum="254cf648a638f7ebd37dc1b334abe940da30b30ac3465b6e0a9ad59829932fa3"
+pkg_shasum="5e51808299135fee7a2e664b09f401b5712b5ef18bd4bad5bc50f4dcd8b149a1"
 pkg_description="Packer is a tool for creating machine and container images for multiple platforms from a single source configuration."
 pkg_upstream_url=https://packer.io
 pkg_build_deps=(core/unzip)
