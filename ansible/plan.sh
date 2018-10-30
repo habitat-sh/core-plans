c40bccc6fd4c72b01f677782d79eb7d32e3f59da
Graham Weldon <graham@grahamweldon.com>
2018-10-30 09:33:54 +0900

    Update ansible to 2.7.1
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- ansible/plan.sh
+++ ansible/plan.sh
@@ -1,11 +1,11 @@
 pkg_name=ansible
 pkg_origin=core
-pkg_version="2.6.6"
+pkg_version="2.7.1"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("GPL-3.0")
 pkg_source="https://github.com/${pkg_name}/${pkg_name}/archive/v${pkg_version}.tar.gz"
 pkg_filename="${pkg_name}-${pkg_version}.tar.gz"
-pkg_shasum="dbfa7e263d88caf222c070a25b3fb7e5cf2f558470f5a953e712c4b88c1f9d71"
+pkg_shasum="354f3fcf5fa837d48a8cd9bff7c74db0c85a2031d608109481c467a7d57cc3b9"
 pkg_deps=(
   core/libffi
   core/python2
