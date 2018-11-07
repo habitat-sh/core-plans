523d66fe2e4a7a2cb5eacf8bfa3ea80a8f63824b
Graham Weldon <graham@grahamweldon.com>
2018-11-07 11:08:32 +0900

    Update logstash to 6.4.3, add tests
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

---
+++ logstash/tests/test.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+TESTDIR="$(dirname "${0}")"
+PLANDIR="$(dirname "${TESTDIR}")"
+SKIPBUILD=${SKIPBUILD:-0}
+
+hab pkg install --binlink core/bats
+
+hab pkg install --binlink core/jre8
+
+hab pkg install core/busybox-static
+hab pkg binlink core/busybox-static ps
+hab pkg binlink core/busybox-static wc
+
+source "${PLANDIR}/plan.sh"
+
+if [ "${SKIPBUILD}" -eq 0 ]; then
+  # Unload the service if its already loaded.
+  hab svc unload "${HAB_ORIGIN}/${pkg_name}"
+
+  set -e
+  pushd "${PLANDIR}" > /dev/null
+  build
+  source results/last_build.env
+  hab pkg install --binlink --force "results/${pkg_artifact}"
+  hab svc load "${pkg_ident}"
+  popd > /dev/null
+  set +e
+
+  # Give some time for the service to start up
+  sleep 5
+fi
+
+bats "${TESTDIR}/test.bats"
