2620e271b7cec0d637e7f730aaffc7f4761bdef5
Scott Macfarlane <smacfarlane@chef.io>
2018-11-09 11:40:39 -0800

    Address shellcheck and EOL whitespace
    Signed-off-by: Scott Macfarlane <smacfarlane@chef.io>

--- php/test/test.sh
+++ php/test/test.sh
@@ -1,10 +1,10 @@
-#!/bin/bash 
+#!/bin/bash
 
-set -euo pipefail 
+set -euo pipefail
 
-if [[ -z "${1:-}" ]]; then 
+if [[ -z "${1:-}" ]]; then
 	echo "Usage: test.sh <fully qualified package ident>"
-	echo 
+	echo
 	echo "ex: test.sh core/php/7.2.8/20181108151533"
 	exit 1
 fi
@@ -12,10 +12,9 @@ fi
 pkg_ident=${1}
 
 # This depends on the package being present on the depot OR
-# in your local cache. If you're testing locally this is a 
-# reasonably safe assumption to make, as long as the cache 
-# isn't cleaned aggressively 
-hab pkg install $pkg_ident 
+# in your local cache. If you're testing locally this is a
+# reasonably safe assumption to make, as long as the cache
+# isn't cleaned aggressively
+hab pkg install "$pkg_ident"
 
-hab pkg exec $pkg_ident php "$(dirname $0)"/libzip_support.php 
-
+hab pkg exec "$pkg_ident" php "$(dirname "$0")"/libzip_support.php
