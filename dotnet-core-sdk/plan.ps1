e2242038fd84b2a6e4bbe09e6968d9d2f4086236
Christopher P. Maher <chris@mahercode.io>
2018-11-30 13:58:40 -0800

    Updating Linux and Windows plans to 2.1.500
    Signed-off-by: Christopher P. Maher <chris@mahercode.io>

--- dotnet-core-sdk/plan.ps1
+++ dotnet-core-sdk/plan.ps1
@@ -1,14 +1,14 @@
 $pkg_name="dotnet-core-sdk"
 $pkg_origin="core"
-$pkg_version="2.1.403"
+$pkg_version="2.1.500"
 $pkg_license=('MIT')
 $pkg_upstream_url="https://www.microsoft.com/net/core"
 $pkg_description=".NET Core is a blazing fast, lightweight and modular platform
   for creating web applications and services that run on Windows,
   Linux and Mac."
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-$pkg_source="https://download.visualstudio.microsoft.com/download/pr/28820b2a-0aec-4c24-a271-a14bcb3e2686/5e0ad8ae32f1497e8d0cace2447b9e01/dotnet-sdk-$pkg_version-win-x64.zip"
-$pkg_shasum="6837e66804c2a782212e68546d19711718c93ee28a62807ab6d381a55583ab26"
+$pkg_source="https://download.visualstudio.microsoft.com/download/pr/2a508a9d-91e8-4126-904c-f7a515f8a33b/24ff5fe2610ce1ce76370ed053b14094/dotnet-sdk-$pkg_version-win-x64.zip"
+$pkg_shasum="113575b3126791ee4d77e0fb36d9407bd62270101c8351f8da0937b660cb7cd8"
 $pkg_bin_dirs=@("bin")
 
 function Invoke-Install {
