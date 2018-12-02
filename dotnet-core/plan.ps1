cc107ff47f6faf8a2d4fee15e82456851706b6e0
Christopher P. Maher <chris@mahercode.io>
2018-12-02 10:33:41 -0800

    Updating dotnet-core to 2.1.6
    Signed-off-by: Christopher P. Maher <chris@mahercode.io>

--- dotnet-core/plan.ps1
+++ dotnet-core/plan.ps1
@@ -1,14 +1,14 @@
 $pkg_name="dotnet-core"
 $pkg_origin="core"
-$pkg_version="2.1.5"
+$pkg_version="2.1.6"
 $pkg_license=('MIT')
 $pkg_upstream_url="https://www.microsoft.com/net/core"
 $pkg_description=".NET Core is a blazing fast, lightweight and modular platform
   for creating web applications and services that run on Windows,
   Linux and Mac."
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-$pkg_source="https://download.visualstudio.microsoft.com/download/pr/ce443d89-75f1-4122-aaa8-c094a9017b4a/255b06ace4207a8ee923758160ed01c3/dotnet-runtime-${pkg_version}-win-x64.zip"
-$pkg_shasum="bfee610814720072bf67d3cc5738345a83ebb517add66f4fdaea280f417e91ed"
+$pkg_source="https://download.visualstudio.microsoft.com/download/pr/3f6b6def-4e9a-4405-b21f-89f77d1605c4/52be50baa0e9bfa118fe6de80be89ab6/dotnet-runtime-${pkg_version}-win-x64.zip"
+$pkg_shasum="f528261c7532847025d19dafd11d543cefc4860209feb0d7ea5356518a288548"
 $pkg_filename="dotnet-win-x64.$pkg_version.zip"
 $pkg_bin_dirs=@("bin")
 
