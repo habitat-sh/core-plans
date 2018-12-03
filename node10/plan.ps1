03687a3bcbf923972a9a865306548ec4bb30b245
Graham Weldon <graham@grahamweldon.com>
2018-12-03 16:22:47 +0900

    Update Node to 10.14.1 (LTS)
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- node10/plan.ps1
+++ node10/plan.ps1
@@ -2,7 +2,7 @@
 
 $pkg_name="node10"
 $pkg_origin="core"
-$pkg_version="10.11.0"
+$pkg_version="10.14.1"
 $pkg_description="Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine."
 $pkg_source="https://nodejs.org/dist/v$pkg_version/node-v$pkg_version-x64.msi"
-$pkg_shasum="46cc9fe075db65a081607a8c803d098deccbb0f1ab3b3dafb9651ec9c0ac853f"
+$pkg_shasum="3b471bba5b19ef58b65460b1f0b71d27bceeaa9218809f75dedc98a6f7a426be"
--- node10/plan.sh
+++ node10/plan.sh
@@ -2,11 +2,11 @@ source "../node/plan.sh"
 
 pkg_name=node10
 pkg_origin=core
-pkg_version=10.11.0
+pkg_version=10.14.1
 pkg_description="Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine."
 pkg_license=('MIT')
 pkg_upstream_url=https://nodejs.org/
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="https://nodejs.org/dist/v${pkg_version}/node-v${pkg_version}.tar.gz"
-pkg_shasum=f721552552fb11ef99aba290fc6e696a8647adc98d643db6651e81ed07c4037e
+pkg_shasum=b97b355f3774adbeb4ffce52e275029e767ba9f317f9eb573175410b6255919f
 pkg_dirname="node-v${pkg_version}"
