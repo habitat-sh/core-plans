cb2c8dd1bba5fbeab9b313a46786d18ab4ce05c2
Ryan Cragun <ryan@chef.io>
2018-12-03 16:24:21 -0700

    [minio] bump version to 2018-11-30T03-56-59Z
    Signed-off-by: Ryan Cragun <ryan@chef.io>

--- minio/plan.sh
+++ minio/plan.sh
@@ -1,10 +1,10 @@
 pkg_name=minio
 pkg_origin=core
-pkg_version="2018-10-05T01-03-03Z"
+pkg_version="2018-11-30T03-56-59Z"
 pkg_description="Minio is a high performance distributed object storage server, designed for large-scale private cloud infrastructure."
 pkg_upstream_url="https://minio.io"
 pkg_source="https://dl.minio.io/server/minio/release/linux-amd64/archive/minio.RELEASE.${pkg_version}"
-pkg_shasum="dd5a89b17bdba7bfb508d6734e7879101204052f01eeb5cb8845eb76b011351e"
+pkg_shasum="cbb86818efa9f281195334e37cebc8aa014a41cd20173216617c2267b0d71ec3"
 pkg_license=('Apache-2.0')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_bin_dirs=(bin)
