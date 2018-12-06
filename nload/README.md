1f8a5d9109d06c4a960705ebc0ec0438e552694b
Josh Hudson <jhudson@chef.io>
2018-12-05 21:20:11 -0700

    Add plan for nload
    Signed-off-by: Josh Hudson <jhudson@chef.io>

---
+++ nload/README.md
@@ -0,0 +1,19 @@
+# nload
+
+Real time network traffic monitor for the text console
+
+## Maintainers
+
+The Habitat Maintainers: <humans@habitat.sh>
+
+## Type of Package
+
+Binary package
+
+## Usage
+
+This package provides `nload` the GNU cli tool for network load monitoring.
+
+The `nload` executable should be symlinked to `/bin/nload`.
+
+See https://github.com/rolandriegel/nload/ for more details.
---
+++ nload/plan.sh
@@ -0,0 +1,16 @@
+pkg_name=nload
+pkg_description="Real time network traffic monitor for the text console"
+pkg_origin=core
+pkg_version="0.7.4"
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_license=("GPL-2.0-or-later")
+pkg_source="https://github.com/rolandriegel/${pkg_name}/archive/v${pkg_version}.tar.gz"
+pkg_shasum="a73b3a75356776860fc4c40daebce04c5022f73d39704a12fb0aeb88a751216a"
+pkg_upstream_url="https://github.com/rolandriegel/nload"
+pkg_build_deps=(core/make core/gcc core/automake core/autoconf)
+pkg_deps=(core/glibc core/gcc-libs core/ncurses)
+pkg_bin_dirs=(bin)
+
+do_prepare() {
+  ./run_autotools
+}
