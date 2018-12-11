7d93478b473aeef93299caa2d6efc43085f56077
Tim Smith <tsmith@chef.io>
2018-12-11 15:23:02 -0800

    Update Ruby from 2.5.1 to 2.5.3
    This resolves multiple CVEs and a large number of bugs.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- ruby/plan.sh
+++ ruby/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=ruby
 pkg_origin=core
-pkg_version=2.5.1
+pkg_version=2.5.3
 pkg_description="A dynamic, open source programming language with a focus on \
   simplicity and productivity. It has an elegant syntax that is natural to \
   read and easy to write."
@@ -8,7 +8,7 @@ pkg_license=("Ruby")
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source=https://cache.ruby-lang.org/pub/${pkg_name}/${pkg_name}-${pkg_version}.tar.gz
 pkg_upstream_url=https://www.ruby-lang.org/en/
-pkg_shasum=dac81822325b79c3ba9532b048c2123357d3310b2b40024202f360251d9829b1
+pkg_shasum=9828d03852c37c20fa333a0264f2490f07338576734d910ee3fd538c9520846c
 pkg_deps=(core/glibc core/ncurses core/zlib core/openssl core/libyaml core/libffi core/readline)
 pkg_build_deps=(core/coreutils core/diffutils core/patch core/make core/gcc core/sed)
 pkg_lib_dirs=(lib)
