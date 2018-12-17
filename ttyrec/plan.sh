4797bf9b758b614d6a0c040d9c1eabd29415e6e1
Scott Macfarlane <smacfarlane@chef.io>
2018-12-17 09:39:30 -0800

    Update ttyrec plan
    Signed-off-by: Scott Macfarlane <smacfarlane@chef.io>

--- ttyrec/plan.sh
+++ ttyrec/plan.sh
@@ -7,17 +7,12 @@ pkg_license=('bsd')
 pkg_upstream_url="http://0xcc.net/ttyrec/"
 pkg_source=http://0xcc.net/$pkg_name/${pkg_name}-${pkg_version}.tar.gz
 pkg_shasum=ef5e9bf276b65bb831f9c2554cd8784bd5b4ee65353808f82b7e2aef851587ec
-pkg_deps=(core/glibc/2.22)
-pkg_build_deps=(core/coreutils core/patch core/make core/gcc)
+pkg_deps=(core/glibc)
+pkg_build_deps=(core/coreutils core/make core/gcc)
 pkg_bin_dirs=(bin)
 
-do_prepare() {
-  # Apply third party patch, originally designed for RHEL5
-  patch -p1 -i "$PLAN_CONTEXT/ttyrec-1.0.8.RHEL5.patch"
-}
-
 do_build() {
-  make
+  make CFLAGS+="-DSVR4 -DHAVE_inotify -D_XOPEN_SOURCE=500"
 }
 
 do_install() {
