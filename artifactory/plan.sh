c8bc5d2d698ccb4f716d142878ef5a26ed6c253c
Meade Kincke <thedarkula2049@gmail.com>
2018-12-25 05:38:15 +0000

    Updated artifactory to latest version.
    Signed-off-by: Meade Kincke <thedarkula2049@gmail.com>

--- artifactory/plan.sh
+++ artifactory/plan.sh
@@ -1,13 +1,13 @@
 pkg_origin=core
 pkg_name=artifactory
-pkg_version=6.4.1
+pkg_version=6.6.0
 pkg_description="Artifactory is an advanced binary repository manager for use by build tools (like Maven and Gradle), dependency management tools (like Ivy and NuGet) and build servers (like Jenkins, Hudson, TeamCity and Bamboo).
 Repository managers serve two purposes: they act as highly configurable proxies between your organization and external repositories and they also provide build servers with a deployment destination for your internally generated artifacts."
 pkg_upstream_url=https://www.jfrog.com/artifactory/
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("JFrog Artifactory EULA")
 pkg_source="https://bintray.com/jfrog/${pkg_name}/download_file?file_path=jfrog-artifactory-oss-${pkg_version}.zip"
-pkg_shasum=edfc77564b8be1ee11807906f8a4fc09fb02ba163acff91e9eed9006c94d6b94
+pkg_shasum="5f226ac60edb974f16af0ecaccbb72b3223e46cf91af55be2ddb57f367bb4162"
 pkg_deps=(core/bash core/jre8)
 pkg_exports=(
   [port]=port
