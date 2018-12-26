df0475ce0c07b15ddb8d61c3a936ae72e219d0c2
Meade Kincke <thedarkula2049@gmail.com>
2018-12-26 08:16:34 +0000

    Updated cairo to latest version.
    Signed-off-by: Meade Kincke <thedarkula2049@gmail.com>

--- cairo/plan.sh
+++ cairo/plan.sh
@@ -1,13 +1,13 @@
 pkg_name=cairo
 pkg_origin=core
-pkg_version="1.14.10"
+pkg_version="1.16.0"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=(
   "LGPL-2.1"
   "MPL-1.1"
 )
 pkg_source="https://www.cairographics.org/releases/${pkg_name}-${pkg_version}.tar.xz"
-pkg_shasum="7e87878658f2c9951a14fc64114d4958c0e65ac47530b8ac3078b2ce41b66a09"
+pkg_shasum="5e7b29b3f113ef870d1e3ecf8adf21f923396401604bda16d44be45e66052331"
 pkg_description="Cairo is a 2D graphics library with support for multiple output devices."
 pkg_upstream_url="https://www.cairographics.org"
 pkg_deps=(
