a5e97ea03cdde8c896d5fde0a8b2b6a98845ea89
Meade Kincke <thedarkula2049@gmail.com>
2018-12-26 08:21:16 +0000

    Updated camlp4 to latest version.
    Signed-off-by: Meade Kincke <thedarkula2049@gmail.com>

--- camlp4/plan.sh
+++ camlp4/plan.sh
@@ -1,13 +1,13 @@
 pkg_name=camlp4
 pkg_origin=core
-pkg_version="4.04"
+pkg_version="4.07"
 pkg_description="Camlp4 is a software system for writing extensible parsers for programming languages. It provides a set of OCaml libraries that are used to define grammars as well as loadable syntax extensions of such grammars."
 pkg_upstream_url="https://github.com/ocaml/camlp4"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('LGPL-2.0')
 pkg_source="https://github.com/ocaml/camlp4/archive/${pkg_version}+1.tar.gz"
 pkg_dirname="${pkg_name}-${pkg_version}-1"
-pkg_shasum="6044f24a44053684d1260f19387e59359f59b0605cdbf7295e1de42783e48ff1"
+pkg_shasum="ecdb8963063f41b387412317685f79823a26b3f53744f0472058991876877090"
 pkg_deps=(
   core/coreutils
   core/glibc
