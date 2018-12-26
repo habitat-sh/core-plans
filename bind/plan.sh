9d4a07d5eeb7e4f4855b9190eb6171cc76410720
Meade Kincke <thedarkula2049@gmail.com>
2018-12-26 06:13:28 +0000

    Updated bind to latest version.
    Signed-off-by: Meade Kincke <thedarkula2049@gmail.com>

--- bind/plan.sh
+++ bind/plan.sh
@@ -1,24 +1,26 @@
 pkg_name=bind
 pkg_origin=core
-pkg_version=9.11.2
+pkg_version=9.13.5
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="Versatile, Classic, Complete Name Server Software"
 pkg_upstream_url="https://www.isc.org/downloads/bind/"
 pkg_license=("MPL-2.0")
-pkg_source="https://ftp.isc.org/isc/bind9/9.11.2/bind-${pkg_version}.tar.gz"
-pkg_shasum="7f46ad8620f7c3b0ac375d7a5211b15677708fda84ce25d7aeb7222fe2e3c77a"
+pkg_source="https://ftp.isc.org/isc/bind9/${pkg_version}/bind-${pkg_version}.tar.gz"
+pkg_shasum="bbde0b81c66a7c7f5b074c8f0e714ed8aa235e4b930e28953cab0ae3cae94e4b"
+pkg_build_deps=(
+  core/diffutils
+  core/file
+  core/gcc
+  core/libcap
+  core/make
+  core/perl
+  core/python2
+)
 pkg_deps=(
   core/glibc
   core/libxml2
   core/openssl
   core/zlib
-)
-pkg_build_deps=(
-  core/diffutils
-  core/file
-  core/gcc
-  core/make
-  core/perl
 )
 pkg_bin_dirs=(
   bin
@@ -33,10 +35,13 @@ do_prepare() {
     ln -sv "$(pkg_path_for file)/bin/file" /usr/bin/file
     _clean_file=true
   fi
+
+  pip install ply
 }
 
 do_build() {
-  ./configure --prefix="${pkg_prefix}" \
+  ./configure \
+    --prefix="${pkg_prefix}" \
     --with-libxml2="$(pkg_path_for "core/libxml2")" \
     --with-openssl="$(pkg_path_for "core/openssl")"
   make
