8e7723018ae5ab4bd4ca7d7c50d208d087873520
Graham Weldon <graham@grahamweldon.com>
2018-12-27 13:27:07 +0900

    Update Node and Node11 to 11.6.0
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- node/plan.ps1
+++ node/plan.ps1
@@ -1,12 +1,12 @@
 $pkg_name="node"
 $pkg_origin="core"
-$pkg_version="11.0.0"
+$pkg_version="11.6.0"
 $pkg_description="Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine."
 $pkg_upstream_url="https://nodejs.org/"
 $pkg_license=@("MIT")
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_source="https://nodejs.org/dist/v$pkg_version/node-v$pkg_version-x64.msi"
-$pkg_shasum="a4be82fad7610131a68507aad93db4bb5809025af499b667e8201c93ee85aea4"
+$pkg_shasum="b5a896741c8cc1f5ccf353d24577c5b78b11af9e52cb9969a70d27d311e3873c"
 $pkg_build_deps=@("core/lessmsi")
 $pkg_bin_dirs=@("bin")
 
