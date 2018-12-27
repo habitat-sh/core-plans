c5f07cf2e455ebf50d2061a857a87db61d6ee341
Graham Weldon <graham@grahamweldon.com>
2018-12-27 14:59:49 +0900

    Update jenkins to 1.150.1, add extra timeout for service load
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- jenkins/plan.sh
+++ jenkins/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=jenkins
 pkg_origin=core
-pkg_version=2.138.3
+pkg_version=2.150.1
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="The leading open source automation server, Jenkins provides hundreds of plugins to support building, deploying and automating any project."
 pkg_license=('MIT')
 pkg_upstream_url="https://jenkins.io/"
 pkg_source="http://mirrors.jenkins.io/war-stable/${pkg_version}/jenkins.war"
-pkg_shasum="953e4dda2d3065284c0016b3e8279e097f830c128b1f712d84780ff2b0751e7d"
+pkg_shasum="7a38586d5a3a1a83498809a83715728bb2f01b58a7dd3a88366f076efdaf6669"
 pkg_deps=(core/jre8 core/curl)
 pkg_exports=(
   [port]=jenkins.http.port
