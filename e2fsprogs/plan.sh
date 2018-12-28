050809ba29184b833bd0b3207bb7179496846414
Meade Kincke <thedarkula2049@gmail.com>
2018-12-28 21:39:51 +0000

    Updated e2fsprogs to latest version.
    Signed-off-by: Meade Kincke <thedarkula2049@gmail.com>

--- e2fsprogs/plan.sh
+++ e2fsprogs/plan.sh
@@ -1,19 +1,20 @@
 pkg_name=e2fsprogs
 pkg_origin=core
-pkg_version="1.44.1"
+pkg_version="1.44.5"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="Ext2/3/4 filesystem userspace utilities"
 pkg_license=('GPL-2.0')
 pkg_upstream_url="http://e2fsprogs.sourceforge.net/"
 pkg_source="https://git.kernel.org/pub/scm/fs/ext2/e2fsprogs.git/snapshot/e2fsprogs-${pkg_version}.tar.gz"
-pkg_shasum="1531c91198520d6868d1106b6b508c5fc780533952f63c2dfd2afec282aa7097"
-pkg_deps=(
-  core/glibc
-)
+pkg_shasum="da54ead3366d2f9f5951c6250c8b6d5d7305216dd5edc11a089370c58387abe7"
+pkg_deps=(core/glibc)
 pkg_build_deps=(
   core/make
   core/gcc
 )
 pkg_lib_dirs=(lib)
 pkg_include_dirs=(include)
-pkg_bin_dirs=(bin sbin)
+pkg_bin_dirs=(
+  bin
+  sbin
+)
