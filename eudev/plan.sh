5353c0b8d8df736f9fc72820fff0ab3b44fd460f
Meade Kincke <thedarkula2049@gmail.com>
2018-12-29 02:18:07 +0000

    Updated eudev to latest version.
    Signed-off-by: Meade Kincke <thedarkula2049@gmail.com>

--- eudev/plan.sh
+++ eudev/plan.sh
@@ -1,15 +1,25 @@
 pkg_name=eudev
 pkg_origin=core
-pkg_version="3.2.1"
+pkg_version="3.2.7"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('GPL-2.0')
 pkg_source="http://dev.gentoo.org/~blueness/${pkg_name}/${pkg_name}-${pkg_version}.tar.gz"
-pkg_shasum="57e3d9e51cfefbdad431848ea0ad8ae1cde04928da42474f44bd200b12f5fe19"
-pkg_deps=(core/glibc core/gperf)
-pkg_build_deps=(core/make core/gcc core/sed)
+pkg_shasum="3004614bd253c1f98558460215027aaf60d7592c70be27fd384ec01db87bf062"
+pkg_deps=(
+  core/glibc
+  core/gperf
+)
+pkg_build_deps=(
+  core/make
+  core/gcc
+  core/sed
+)
 pkg_lib_dirs=(lib)
 pkg_include_dirs=(include)
-pkg_bin_dirs=(bin sbin)
+pkg_bin_dirs=(
+  bin
+  sbin
+)
 pkg_pconfig_dirs=(lib/pkgconfig)
 pkg_description="eudev is a device manager for the Linux kernel"
 pkg_upstream_url="https://wiki.gentoo.org/wiki/Project:Eudev"
