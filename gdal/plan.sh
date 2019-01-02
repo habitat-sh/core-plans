2ecea5a96734f28bd7e6d59a8af7c5ea5f2e4a92
Meade Kincke <thedarkula2049@gmail.com>
2019-01-02 08:17:10 +0000

    Updated gdal to latest version and added testing.
    Signed-off-by: Meade Kincke <thedarkula2049@gmail.com>

--- gdal/plan.sh
+++ gdal/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=gdal
 pkg_origin=core
-pkg_version=2.2.1
+pkg_version=2.4.0
 pkg_description="GDAL is a translator library for raster and vector geospatial data formats"
 pkg_upstream_url=http://www.gdal.org/
 pkg_license=('MIT')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source=http://download.osgeo.org/gdal/${pkg_version}/gdal-${pkg_version}.tar.gz
-pkg_shasum=61837706abfa3e493f3550236efc2c14bd6b24650232f9107db50a944abf8b2f
+pkg_shasum=a568cf3dc7bb203ae12a48e1eb2a42302cded499ef6eccaf9e8f09187d8ce75a
 pkg_build_deps=(
   core/gcc
   core/make
@@ -14,8 +14,8 @@ pkg_build_deps=(
   core/patchelf
 )
 pkg_deps=(
-  core/glibc
   core/gcc-libs
+  core/glibc
 )
 pkg_bin_dirs=(bin)
 pkg_include_dirs=(include)
