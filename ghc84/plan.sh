305b6d0b2fa0a2bdebee67f809d844de8b1f39ad
Meade Kincke <thedarkula2049@gmail.com>
2019-01-03 08:43:20 +0000

    Updated ghc and ghc84 to latest version and added testing.
    Signed-off-by: Meade Kincke <thedarkula2049@gmail.com>

--- ghc84/plan.sh
+++ ghc84/plan.sh
@@ -1,15 +1,15 @@
 # shellcheck disable=SC2148,SC1091
-source ../ghc/plan.sh
+source "$(dirname "${BASH_SOURCE[0]}")/../ghc/plan.sh"
 
 pkg_name=ghc84
 pkg_origin=core
-pkg_version=8.4.3
+pkg_version=8.4.4
 pkg_license=('BSD-3-Clause')
 pkg_upstream_url="https://www.haskell.org/ghc/"
 pkg_description="The Glasgow Haskell Compiler"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="http://downloads.haskell.org/~ghc/${pkg_version}/ghc-${pkg_version}-src.tar.xz"
-pkg_shasum="ae47afda985830de8811243255aa3744dfb9207cb980af74393298b2b62160d6"
+pkg_shasum="11117735a58e507c481c09f3f39ae5a314e9fbf49fc3109528f99ea7959004b2"
 pkg_dirname="ghc-${pkg_version}"
 
 pkg_include_dirs=(lib/ghc-${pkg_version}/include)
