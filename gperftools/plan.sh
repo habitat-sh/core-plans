a3f6b807a06d49b4dd2dc690943dec2cfba283d1
Meade Kincke <thedarkula2049@gmail.com>
2019-01-03 19:45:23 +0000

    Updated gperftools to latest version.
    Signed-off-by: Meade Kincke <thedarkula2049@gmail.com>

--- gperftools/plan.sh
+++ gperftools/plan.sh
@@ -1,14 +1,26 @@
 pkg_name=gperftools
 pkg_origin=core
-pkg_version=2.6.90
+pkg_version=2.7
 pkg_description="Google Performance Tools"
 pkg_upstream_url=https://github.com/gperftools/gperftools
 pkg_license=('BSDv3')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="https://github.com/gperftools/gperftools/releases/download/${pkg_name}-${pkg_version}/${pkg_name}-${pkg_version}.tar.gz"
-pkg_shasum=2f8ec5129701e6c68c75009db5303761b65b51867c63e49ec19b946930cfef1f
-pkg_deps=(core/glibc core/gcc-libs core/graphviz core/coreutils core/grep core/perl core/binutils)
-pkg_build_deps=(core/gcc core/make core/automake)
+pkg_shasum=1ee8c8699a0eff6b6a203e59b43330536b22bbcbe6448f54c7091e5efb0763c9
+pkg_build_deps=(
+  core/automake
+  core/gcc
+  core/make
+)
+pkg_deps=(
+  core/binutils
+  core/coreutils
+  core/gcc-libs
+  core/glibc
+  core/grep
+  core/graphviz
+  core/perl
+)
 pkg_bin_dirs=(bin)
 pkg_lib_dirs=(lib)
 pkg_include_dirs=(include)
@@ -27,6 +39,10 @@ do_build() {
       -i "src/pprof"
 }
 
+do_install() {
+  do_default_install
+  fix_interpreter "$pkg_prefix/bin/pprof" core/coreutils bin/env
+}
 do_check() {
   make check
 }
