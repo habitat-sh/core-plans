5c5da57c8dede7847bd27f94b4ef06c11e90093b
mwrock <matt@mattwrock.com>
2019-01-25 15:27:30 -0800

    add 2008r2/win7 compatibility to aspnet4
    Signed-off-by: mwrock <matt@mattwrock.com>

--- iis-aspnet4/plan.ps1
+++ iis-aspnet4/plan.ps1
@@ -4,3 +4,4 @@ $pkg_version="0.1.0"
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_license=@("Apache-2.0")
 $pkg_description="Enables ASP.Net 4.5 and dependent features"
+$pkg_deps=@("core/iis-webserverrole")
