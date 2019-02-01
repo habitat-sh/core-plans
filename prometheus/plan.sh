ca541ed3dbb2c2d75b031f90dce6d77b1578666e
Graham Weldon <graham@grahamweldon.com>
2019-02-01 11:03:33 +0900

    Update prometheus to 2.7.1, fix program output from STDERR to STDOUT for supervisor logging, bypass cgo user requirements for build
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- prometheus/plan.sh
+++ prometheus/plan.sh
@@ -2,12 +2,12 @@ pkg_name=prometheus
 pkg_description="Prometheus monitoring"
 pkg_upstream_url=http://prometheus.io
 pkg_origin=core
-pkg_version=2.4.3
+pkg_version=2.7.1
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('Apache-2.0')
 pkg_bin_dirs=(bin)
 pkg_source="https://github.com/prometheus/prometheus/archive/v${pkg_version}.tar.gz"
-pkg_shasum=4f84697f746a81df45e434a27b4b6b2c23c08768aa0f68b7bcf88f4ee6d1d8d6
+pkg_shasum=bfbeb434342a03b5849e2ec7a0cbe573067299cf59ccf59db0cacd8db8800bb0
 prom_pkg_dir="$HAB_CACHE_SRC_PATH/${pkg_name}-${pkg_version}"
 prom_build_dir="${prom_pkg_dir}/src/${pkg_source}"
 pkg_build_deps=(
@@ -24,7 +24,6 @@ pkg_binds_optional=(
   [targets]="metric-http-port"
 )
 
-
 do_setup_environment() {
   export GOPATH="$HAB_CACHE_SRC_PATH/$pkg_dirname"
 }
@@ -38,7 +37,7 @@ do_unpack() {
 
 do_build() {
   pushd "$prom_pkg_dir/src/github.com/prometheus/prometheus" || exit 1
-  PREFIX="$pkg_prefix/bin" make build
+  USER="root" PREFIX="$pkg_prefix/bin" make build
   popd || exit 1
 }
 
