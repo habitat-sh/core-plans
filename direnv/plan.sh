cf449cfa82e63840b7bd2c249b1c32997ecfc0cb
Graham Weldon <graham@grahamweldon.com>
2019-02-01 09:19:40 +0900

    Update direnv to 1.19.1, add tests
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- direnv/plan.sh
+++ direnv/plan.sh
@@ -1,11 +1,11 @@
 pkg_name=direnv
 pkg_origin=core
-pkg_version="2.18.2"
+pkg_version="2.19.1"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('MIT')
 pkg_source="https://github.com/${pkg_name}/${pkg_name}/releases/download/v${pkg_version}/${pkg_name}.linux-amd64"
 pkg_filename="${pkg_name}.linux-amd64"
-pkg_shasum="a53e3c23160e881bfb977b282bacddd3731d0f2a972ce5667a567cbd6e825ed1"
+pkg_shasum="3341afc4a2255a680e446230f9db5cee66f2f208f3c43cb251ba908957fe7636"
 pkg_bin_dirs=(bin)
 pkg_description="direnv is an environment switcher for the shell."
 pkg_upstream_url="https://direnv.net/"
