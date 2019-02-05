e396103b91a17b6889a31f90a8913d8755fa511a
Meade Kincke <thedarkula2049@gmail.com>
2019-02-05 20:48:46 +0000

    Added necessary fields to gradle4.
    Signed-off-by: Meade Kincke <thedarkula2049@gmail.com>

--- gradle4/plan.sh
+++ gradle4/plan.sh
@@ -5,3 +5,7 @@ pkg_origin=core
 pkg_version=4.10.3
 pkg_source=https://services.gradle.org/distributions/${pkg_name}-${pkg_version}-bin.zip
 pkg_shasum=8626cbf206b4e201ade7b87779090690447054bc93f052954c78480fa6ed186e
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_description="A powerful build system for the JVM"
+pkg_upstream_url=http://gradle.org
+pkg_license=('Apache-2.0')
