f3eed86719c01f1a977f47bdd5e22050734c9987
Graham Weldon <graham@grahamweldon.com>
2019-02-06 11:26:47 +0900

    Fix dirname for package extraction
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- gradle4/plan.sh
+++ gradle4/plan.sh
@@ -5,6 +5,7 @@ pkg_distname=gradle
 pkg_origin=core
 pkg_version=4.10.3
 pkg_source=https://services.gradle.org/distributions/${pkg_distname}-${pkg_version}-bin.zip
+pkg_dirname="${pkg_distname}-${pkg_version}"
 pkg_shasum=8626cbf206b4e201ade7b87779090690447054bc93f052954c78480fa6ed186e
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="A powerful build system for the JVM"
--- gradle5/plan.sh
+++ gradle5/plan.sh
@@ -5,6 +5,7 @@ pkg_distname=gradle
 pkg_origin=core
 pkg_version=5.1
 pkg_source=https://services.gradle.org/distributions/${pkg_distname}-${pkg_version}-bin.zip
+pkg_dirname="${pkg_distname}-${pkg_version}"
 pkg_shasum=7506638a380092a0406364c79d6c87d03d23017fc25a5770379d1ce23c3fcd4d
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="A powerful build system for the JVM"
