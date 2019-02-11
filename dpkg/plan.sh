7c93170e483e21594e7ccac10c70bb3f6ac214c2
W. Duncan Fraser <duncan@wduncanfraser.com>
2019-02-10 20:16:58 -0600

    Fixed missing runtime deps for dpkg Cleaned up plan
    Signed-off-by: W. Duncan Fraser <duncan@wduncanfraser.com>

--- dpkg/plan.sh
+++ dpkg/plan.sh
@@ -3,13 +3,14 @@ pkg_origin=core
 pkg_version=1.19.4
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('GPL-2.0')
-pkg_upstream_url=https://wiki.debian.org/dpkg
+pkg_upstream_url="https://wiki.debian.org/dpkg"
 pkg_description="dpkg is a package manager for Debian-based systems"
-pkg_source=http://http.debian.net/debian/pool/main/d/${pkg_name}/${pkg_name}_${pkg_version}.tar.xz
-pkg_shasum=c15234e98655689586bff2d517a6fdc6135d139c54d52ae9cfa6a90007fee0ae
+pkg_source="http://http.debian.net/debian/pool/main/d/${pkg_name}/${pkg_name}_${pkg_version}.tar.xz"
+pkg_shasum="c15234e98655689586bff2d517a6fdc6135d139c54d52ae9cfa6a90007fee0ae"
 pkg_deps=(
   core/bzip2
   core/glibc
+  core/ncurses
   core/tar
   core/zlib
   core/xz
@@ -23,7 +24,6 @@ pkg_build_deps=(
   core/libtool
   core/patch
   core/make
-  core/ncurses
   core/perl
   core/pkg-config
   core/xz
@@ -32,14 +32,9 @@ pkg_build_deps=(
 pkg_bin_dirs=(bin sbin)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
-pkg_description="Debian Package Manager"
 
 do_build() {
-  export prefix=$pkg_prefix
-  do_default_build
-}
+  ./configure --prefix="${pkg_prefix}"
 
-do_install() {
-  export prefix=$pkg_prefix
-  do_default_install
+  make
 }
