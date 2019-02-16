6cc051547886516f180c2f9931f0a157a8de162a
Steven Danna <steve@chef.io>
2019-02-16 15:34:45 +0000

    [iproute2] Fix missing iptables dependency in m_xt.so
    Using the check-bins.sh script in #2305
    
    Before:
    
    ```
    > bin/check-bins.sh /hab/pkgs/core/iproute2/4.16.0/20190216142156/
    MISSING DEPENDENCIES
    /hab/pkgs/core/iproute2/4.16.0/20190216142156/lib/tc/m_xt.so:
          libxtables.so.11
    > echo $?
    1
    ```
    
    After:
    
    ```
    > bin/check-bins.sh /hab/pkgs/core/iproute2/4.16.0/20190216150620
    > echo $?
    0
    ```
    
    Fixes #2218
    
    Signed-off-by: Steven Danna <steve@chef.io>

--- iproute2/plan.sh
+++ iproute2/plan.sh
@@ -13,12 +13,14 @@ pkg_build_deps=(
   core/bison
   core/flex
   core/gcc
-  core/iptables
   core/m4
   core/make
   core/pkg-config
 )
-pkg_deps=(core/glibc)
+pkg_deps=(
+    core/glibc
+    core/iptables
+)
 
 do_build() {
   SBINDIR="$pkg_prefix/sbin"
