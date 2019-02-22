57f86469ba5ccd024a02b772b9b71b7699e9f7de
Meade Kincke <thedarkula2049@gmail.com>
2019-02-22 19:39:26 +0000

    Updated bdwgc to 8.0.2, split version 7 and 8, updated bdwgc7 to 7.6.10.
    Signed-off-by: Meade Kincke <thedarkula2049@gmail.com>

---
+++ bdwgc7/plan.sh
@@ -0,0 +1,12 @@
+source "$(dirname "${BASH_SOURCE[0]}")/../bdwgc/plan.sh"
+
+pkg_name=bdwgc
+pkg_origin=core
+pkg_version=7.6.10
+pkg_description="A garbage collector for C and C++"
+pkg_upstream_url="http://www.hboehm.info/gc/"
+pkg_license=('X11 style license')
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_source="http://www.hboehm.info/gc/gc_source/gc-${pkg_version}.tar.gz"
+pkg_dirname="gc-${pkg_version}"
+pkg_shasum="4fc766749a974700c576bbfb71b4a73b2ed746082e2fc8388bfb0b54b636af14"
