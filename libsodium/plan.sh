2a1c11e5316d9cbc952ba4003d5e588b2a435c6a
Tim Smith <tsmith@chef.io>
2019-02-26 15:40:10 -0800

    Update libsodium plans to 1.0.17
    I have no ability to test the windows plan, but here's the linux plan:
    
       libsodium: hab-plan-build cleanup
       libsodium:
       libsodium: Source Path: /hab/cache/src/libsodium-1.0.17
       libsodium: Installed Path: /hab/pkgs/tas50/libsodium/1.0.17/20190226233714
       libsodium: Artifact: /src/libsodium/results/tas50-libsodium-1.0.17-20190226233714-x86_64-linux.hart
       libsodium: Build Report: /src/libsodium/results/last_build.env
       libsodium: SHA256 Checksum: ede0f268f650fa6fb438d257dbc74c4cb7fd3e6f05ec1cca9faf5b115a6f0792
       libsodium: Blake2b Checksum: 579b9a23cc0f453e71ec9c0814d6cda5a498c6828b1cbb509f5a1e606728d8e0
       libsodium:
       libsodium: I love it when a plan.sh comes together.
       libsodium:
       libsodium: Build time: 1m42s
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- libsodium/plan.sh
+++ libsodium/plan.sh
@@ -1,7 +1,6 @@
 pkg_name=libsodium
-_distname="$pkg_name"
 pkg_origin=core
-pkg_version=1.0.16
+pkg_version=1.0.17
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="\
 Sodium is a new, easy-to-use software library for encryption, decryption, \
@@ -11,9 +10,9 @@ API to improve usability even further.\
 "
 pkg_upstream_url="https://github.com/jedisct1/libsodium"
 pkg_license=('ISC')
-pkg_source="https://download.libsodium.org/libsodium/releases/${_distname}-${pkg_version}.tar.gz"
-pkg_shasum="eeadc7e1e1bcef09680fb4837d448fbdf57224978f865ac1c16745868fbd0533"
-pkg_dirname="${_distname}-${pkg_version}"
+pkg_source="https://download.libsodium.org/libsodium/releases/libsodium-${pkg_version}.tar.gz"
+pkg_shasum="0cc3dae33e642cc187b5ceb467e0ad0e1b51dcba577de1190e9ffa17766ac2b1"
+pkg_dirname="libsodium-${pkg_version}"
 pkg_deps=(
   core/glibc
 )
