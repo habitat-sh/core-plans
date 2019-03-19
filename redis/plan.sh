ded275d14ff005e8d7c6ab07f5a7dd68f1286266
Graham Weldon <graham@grahamweldon.com>
2019-03-19 10:50:20 +0900

    Update redis to latest redis4
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- redis/plan.sh
+++ redis/plan.sh
@@ -1,13 +1,13 @@
 pkg_name=redis
 pkg_dist_name=redis
 pkg_origin=core
-pkg_version="4.0.10"
+pkg_version="4.0.14"
 pkg_description="Persistent key-value database, with built-in net interface"
 pkg_upstream_url="http://redis.io/"
 pkg_license=("BSD-3-Clause")
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="http://download.redis.io/releases/${pkg_dist_name}-${pkg_version}.tar.gz"
-pkg_shasum="1db67435a704f8d18aec9b9637b373c34aa233d65b6e174bdac4c1b161f38ca4"
+pkg_shasum="1e1e18420a86cfb285933123b04a82e1ebda20bfb0a289472745a087587e93a7"
 pkg_bin_dirs=(bin)
 pkg_build_deps=(
   core/make
