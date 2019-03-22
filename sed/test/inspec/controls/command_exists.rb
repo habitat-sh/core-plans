060ade7f057eeaeffcfed21c99e3724f91f65163
James Stocks <jstocks@chef.io>
2019-03-22 14:16:41 +0000

    Improve sed tests by adding control blocks.
    Signed-off-by: James Stocks <jstocks@chef.io>

--- sed/test/inspec/controls/command_exists.rb
+++ sed/test/inspec/controls/command_exists.rb
@@ -1,6 +1,8 @@
 pkg_ident = `grep pkg_ident results/last_build.env | cut -d \= -f2`.strip
-command_path = "/hab/pkgs/#{pkg_ident}/bin/sed"
+executable_path = "/hab/pkgs/#{pkg_ident}/bin/sed"
 
-describe file(command_path) do
-  it { should exist }
+control 'The sed exe exists in the last built package' do
+  describe file(executable_path) do
+    it { should exist }
+  end
 end
--- sed/test/inspec/controls/command_works.rb
+++ sed/test/inspec/controls/command_works.rb
@@ -1,9 +1,17 @@
 pkg_ident = `grep pkg_ident results/last_build.env | cut -d \= -f2`.strip
-command_path = "/hab/pkgs/#{pkg_ident}/bin/sed"
-command_test = "#{command_path} --help"
 
-describe bash(command_test) do
-  its('stdout') { should match /Usage: #{command_path} \[OPTION\]/ }
-  its('stderr') { should eq '' }
-  its('exit_status') { should eq 0 }
+control 'sed works and is the last built pkg_ident' do
+  describe command('hab pkg exec core/sed sed --help') do
+    its('stdout') { should match /Usage: .+#{pkg_ident}\/bin\/sed \[OPTION\]/ }
+    its('stderr') { should eq '' }
+    its('exit_status') { should eq 0 }
+  end
+end
+
+control 'Basic sed usage' do
+  describe command('echo \'catcat\' | hab pkg exec core/sed sed \'s/cat/dog/g\'') do
+    its('stdout') { should match /^dogdog$/ }
+    its('stderr') { should eq '' }
+    its('exit_status') { should eq 0 }
+  end
 end
