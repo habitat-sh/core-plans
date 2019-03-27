0f925f5c8af24a62f168ea5586d711872b17a9a6
Graham Weldon <graham@grahamweldon.com>
2019-03-27 11:11:19 +0900

    Update Node10 to 10.15.2 (required for Kibana)
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- node10/plan.ps1
+++ node10/plan.ps1
@@ -2,7 +2,7 @@
 
 $pkg_name="node10"
 $pkg_origin="core"
-$pkg_version="10.15.0"
+$pkg_version="10.15.2"
 $pkg_description="Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine."
 $pkg_source="https://nodejs.org/dist/v$pkg_version/node-v$pkg_version-x64.msi"
-$pkg_shasum="1cf90ea486607a1e4e3191e4880d4e6a256168d800fe33a6e46680149b88e3ed"
+$pkg_shasum="12a802653c0737a4ba882a06511ad1fb58cfe038bf55b082c7fe6243bdf03cf5"
