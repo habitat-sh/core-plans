0546d2c7782fd8618ac22c985b5b61585c53b740
Tom Duffield <tom@chef.io>
2019-03-29 10:11:17 -0500

    [ruby25] Update Ruby25 to 2.5.5
    Signed-off-by: Tom Duffield <tom@chef.io>

--- ruby25/plan.sh
+++ ruby25/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=ruby25
 pkg_origin=core
-pkg_version=2.5.3
+pkg_version=2.5.5
 pkg_description="A dynamic, open source programming language with a focus on \
   simplicity and productivity. It has an elegant syntax that is natural to \
   read and easy to write."
@@ -8,7 +8,7 @@ pkg_license=("Ruby")
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source=https://cache.ruby-lang.org/pub/ruby/ruby-${pkg_version}.tar.gz
 pkg_upstream_url=https://www.ruby-lang.org/en/
-pkg_shasum=9828d03852c37c20fa333a0264f2490f07338576734d910ee3fd538c9520846c
+pkg_shasum=28a945fdf340e6ba04fc890b98648342e3cccfd6d223a48f3810572f11b2514c
 pkg_deps=(core/glibc core/ncurses core/zlib core/openssl core/libyaml core/libffi core/readline)
 pkg_build_deps=(core/coreutils core/diffutils core/patch core/make core/gcc core/sed)
 pkg_lib_dirs=(lib)
