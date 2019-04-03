a1032a4717c89cd887ffbca6414fa826428f36ca
Graham Weldon <graham@grahamweldon.com>
2019-04-03 12:04:37 +0900

    Update telegraf to 1.10.2, add minimal service test
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- telegraf/tests/test.sh
+++ telegraf/tests/test.sh
@@ -6,14 +6,17 @@ SKIPBUILD=${SKIPBUILD:-0}
 
 hab pkg install --binlink core/bats
 
-source "${PLANDIR}/plan.sh"
-
 if [ "${SKIPBUILD}" -eq 0 ]; then
+  source "${PLANDIR}/plan.sh"
+  # Unload the service if its already loaded.
+  hab svc unload "${HAB_ORIGIN}/${pkg_name}"
+
   set -e
   pushd "${PLANDIR}" > /dev/null
   build
   source results/last_build.env
   hab pkg install --binlink --force "results/${pkg_artifact}"
+  hab svc load "${pkg_ident}"
   popd > /dev/null
   set +e
 fi
