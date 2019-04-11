b141b033138c1fb2547ad1c4f3dc6b4898a74af8
James Stocks <jstocks@chef.io>
2019-04-11 15:10:21 +0000

    [dotnet-472-dev-pack] Fix download URL
    Signed-off-by: James Stocks <jstocks@chef.io>

--- dotnet-472-dev-pack/plan.ps1
+++ dotnet-472-dev-pack/plan.ps1
@@ -7,5 +7,5 @@ $pkg_description=".NET 4.7.2 Targeting Pack and the .NET 4.7.2 SDK."
 $pkg_upstream_url="https://www.microsoft.com/net/download/all"
 $pkg_license=@("Microsoft Software License")
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-$pkg_source="https://download.microsoft.com/download/5/F/E/5FE505D0-E753-4F1A-B8D6-D9E73C0C28C7/NDP472-DevPack-ENU.exe"
-$pkg_shasum="15916f064a0ad061463ace80d9405f2d80d29655a13516f3676d421d0337d756"
+$pkg_source="https://download.microsoft.com/download/3/B/F/3BFB9C35-405D-45DF-BDAF-0EB57D047888/NDP472-DevPack-ENU.exe"
+$pkg_shasum="2f4ee2852b95c37a806e2deec567751dd59b0dd27049641bbd4e1c0e22adfe46"
