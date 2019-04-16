044b38abd98bd6bf1349dd8a37a8428bff6af0a5
Scott Macfarlane <macfarlane.scott@gmail.com>
2019-04-16 14:10:30 -0700

    Correct download urls
    Signed-off-by: Scott Macfarlane <macfarlane.scott@gmail.com>

--- ffmpeg3/plan.sh
+++ ffmpeg3/plan.sh
@@ -3,10 +3,11 @@ source "$(dirname "${BASH_SOURCE[0]}")/../ffmpeg/plan.sh"
 pkg_name=ffmpeg3
 pkg_origin=core
 pkg_version=3.4.6
-pkg_source=https://ffmpeg.org/releases/${pkg_name}-${pkg_version}.tar.gz
+pkg_source=https://ffmpeg.org/releases/ffmpeg-${pkg_version}.tar.gz
 pkg_shasum=982cf1c409fab413648561c8d78167c7a75fe47b429dc7b5d5e956b19245040d
 pkg_description="A complete, cross-platform solution to record, convert and \
 stream audio and video."
 pkg_upstream_url=https://ffmpeg.org/
 pkg_license=('LGPL-2.1+' 'GPL-2.0+')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_dirname="ffmpeg-$pkg_version"
--- ffmpeg4/plan.sh
+++ ffmpeg4/plan.sh
@@ -3,10 +3,11 @@ source "$(dirname "${BASH_SOURCE[0]}")/../ffmpeg/plan.sh"
 pkg_name=ffmpeg4
 pkg_origin=core
 pkg_version=4.1.3
-pkg_source=https://ffmpeg.org/releases/${pkg_name}-${pkg_version}.tar.gz
+pkg_source=https://ffmpeg.org/releases/ffmpeg-${pkg_version}.tar.gz
 pkg_shasum=2f5b24f30e41963ce80f0ab7c78a1b91e86fb3fbb4a7661147c572c587177eee
 pkg_description="A complete, cross-platform solution to record, convert and \
 stream audio and video."
 pkg_upstream_url=https://ffmpeg.org/
 pkg_license=('LGPL-2.1+' 'GPL-2.0+')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_dirname="ffmpeg-$pkg_version"
