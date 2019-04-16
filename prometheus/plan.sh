c2cf524363d0aca2d50ca3f7138f428742ed1c98
Graham Weldon <graham@grahamweldon.com>
2019-04-16 11:10:31 +0900

    Update prometheus to 2.9.0
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- prometheus/plan.sh
+++ prometheus/plan.sh
@@ -2,13 +2,13 @@ pkg_name=prometheus
 pkg_description="Prometheus monitoring"
 pkg_upstream_url=http://prometheus.io
 pkg_origin=core
-pkg_version=2.7.1
+pkg_version=2.9.0
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('Apache-2.0')
 pkg_bin_dirs=(bin)
 pkg_source="https://github.com/prometheus/prometheus/archive/v${pkg_version}.tar.gz"
-pkg_shasum=fe239e23379ce6d50c5577f515d7f576f1c6f9396ea47fd87c08fbf8b5e163fb
-prom_pkg_dir="$HAB_CACHE_SRC_PATH/${pkg_name}-${pkg_version}"
+pkg_shasum=d42656f45cd1b6399b2983223ef718bfc475cc518de11081acce9bea22db623c
+prom_pkg_dir="${HAB_CACHE_SRC_PATH}/${pkg_name}-${pkg_version}"
 prom_build_dir="${prom_pkg_dir}/src/${pkg_source}"
 pkg_build_deps=(
   core/go
@@ -25,24 +25,24 @@ pkg_binds_optional=(
 )
 
 do_setup_environment() {
-  export GOPATH="$HAB_CACHE_SRC_PATH/$pkg_dirname"
+  export GOPATH="${HAB_CACHE_SRC_PATH}/${pkg_dirname}"
 }
 
 do_unpack() {
-  mkdir -p "$prom_pkg_dir/src/github.com/prometheus/prometheus"
-  pushd "$prom_pkg_dir/src/github.com/prometheus/prometheus" || exit 1
-  tar xf "$HAB_CACHE_SRC_PATH/$pkg_filename" --strip 1 --no-same-owner
+  mkdir -p "${prom_pkg_dir}/src/github.com/prometheus/prometheus"
+  pushd "${prom_pkg_dir}/src/github.com/prometheus/prometheus" || exit 1
+  tar xf "${HAB_CACHE_SRC_PATH}/${pkg_filename}" --strip 1 --no-same-owner
   popd || exit 1
 }
 
 do_build() {
-  pushd "$prom_pkg_dir/src/github.com/prometheus/prometheus" || exit 1
-  USER="root" PREFIX="$pkg_prefix/bin" make build
+  pushd "${prom_pkg_dir}/src/github.com/prometheus/prometheus" || exit 1
+  USER="root" PREFIX="${pkg_prefix}/bin" make build
   popd || exit 1
 }
 
 do_check() {
-  pushd "$prom_pkg_dir/src/github.com/prometheus/prometheus" || exit 1
+  pushd "${prom_pkg_dir}/src/github.com/prometheus/prometheus" || exit 1
   make test
   popd || exit 1
 }
--- prometheus2/plan.sh
+++ prometheus2/plan.sh
@@ -2,11 +2,11 @@ source "$(dirname "${BASH_SOURCE[0]}")/../prometheus/plan.sh"
 
 pkg_name=prometheus2
 pkg_origin=core
-pkg_version=2.7.1
+pkg_version=2.9.0
 pkg_description="Prometheus monitoring"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('Apache-2.0')
 pkg_source="https://github.com/prometheus/prometheus/archive/v${pkg_version}.tar.gz"
-pkg_shasum=fe239e23379ce6d50c5577f515d7f576f1c6f9396ea47fd87c08fbf8b5e163fb
-prom_pkg_dir="$HAB_CACHE_SRC_PATH/${pkg_name}-${pkg_version}"
+pkg_shasum=d42656f45cd1b6399b2983223ef718bfc475cc518de11081acce9bea22db623c
+prom_pkg_dir="${HAB_CACHE_SRC_PATH}/${pkg_name}-${pkg_version}"
 prom_build_dir="${prom_pkg_dir}/src/${pkg_source}"
