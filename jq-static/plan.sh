f2bab552acf30b1243641d31f412bea85dd3ecb3
Scott Macfarlane <macfarlane.scott@gmail.com>
2019-04-18 11:09:07 -0700

    Update jq-static metadata
    Signed-off-by: Scott Macfarlane <macfarlane.scott@gmail.com>

--- jq-static/plan.sh
+++ jq-static/plan.sh
@@ -2,9 +2,11 @@ pkg_name=jq-static
 pkg_distname=jq
 pkg_origin=core
 pkg_version=1.5
-pkg_license=('mit')
+pkg_license=('MIT')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_source=https://github.com/stedolan/$pkg_distname/releases/download/${pkg_distname}-${pkg_version}/jq-linux64
+pkg_source="https://github.com/stedolan/$pkg_distname/releases/download/${pkg_distname}-${pkg_version}/jq-linux64"
+pkg_upstream_url="https://stedolan.github.io/jq/"
+pkg_description="jq is a lightweight and flexible command-line JSON processor."
 pkg_shasum=c6b3a7d7d3e7b70c6f51b706a3b90bd01833846c54d32ca32f0027f00226ff6d
 pkg_deps=()
 pkg_build_deps=(core/coreutils core/wget)
@@ -19,5 +21,5 @@ do_build() {
 }
 
 do_install() {
-  install -D $HAB_CACHE_SRC_PATH/$pkg_filename $pkg_prefix/bin/jq
+  install -D "$HAB_CACHE_SRC_PATH"/"$pkg_filename" "$pkg_prefix"/bin/jq
 }
