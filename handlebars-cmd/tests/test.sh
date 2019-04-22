ab4b9b707df03583355ad7423b9595802373eac2
Scott Macfarlane <smacfarlane@chef.io>
2019-04-22 15:15:10 -0700

    Add minimal test for handlebars-cmd
    Signed-off-by: Scott Macfarlane <smacfarlane@chef.io>

---
+++ handlebars-cmd/tests/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if [ -z "${1:-}" ]; then
+	echo "Usage: handlebars-cmd/tests/test.sh FULLY_QUALIFIED_PACKAGE_IDENTIFIER"
+	exit 1
+fi
+
+pkg_ident="$1"
+
+hab pkg install core/bats
+
+tests_path="$(dirname "$0")"
+
+export pkg_ident
+hab pkg exec core/bats bats "$tests_path"/bats/test.bats
