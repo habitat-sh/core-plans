c62c40fb6caf053dafb26850376608a38d939960
Graham Weldon <graham@grahamweldon.com>
2019-04-25 09:05:20 +0900

    Update Caddy to 1.0.0, skip version check due to upstream bug
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- caddy/plan.sh
+++ caddy/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=caddy
 pkg_origin=core
-pkg_version="0.11.5"
+pkg_version=1.0.0
 pkg_maintainer='The Habitat Maintainers <humans@habitat.sh>'
 pkg_license=("Apache-2.0")
 pkg_source="https://github.com/mholt/caddy/releases/download/v${pkg_version}/caddy_v${pkg_version}_linux_amd64.tar.gz"
-pkg_shasum=d0ca02afdfd895b8e41051518e3f527185ac64ae6c55ca4e8de677b3d92b678b
+pkg_shasum=e720c3a6593c878fc7ecc00baf4348683c0e8e35ab67d2db4d364881be96896e
 pkg_description="Fast, cross-platform HTTP/2 web server with automatic HTTPS"
-pkg_upstream_url="https://caddyserver.com"
+pkg_upstream_url=https://caddyserver.com
 pkg_svc_run="caddy -conf ${pkg_svc_config_path}/Caddyfile"
 pkg_exposes=(port)
 pkg_exports=(
