b7ad1811b044b0d28036f429151e36ea1c31ddc0
Will Fisher <wfisher@chef.io>
2019-04-30 08:38:46 -0700

    Fix missing runtime deps
    The freetype-config will still throw some messages about missing
    `PKG_CONFIG_PATH` because it's using the pkg-config and expecting
    it to exist when it's normally set as part of a
    build
    
    Signed-off-by: Will Fisher <wfisher@chef.io>

--- freetype/plan.sh
+++ freetype/plan.sh
@@ -9,20 +9,22 @@ pkg_source=http://download.savannah.gnu.org/releases/freetype/${pkg_name}-${pkg_
 pkg_filename=${pkg_name}-${pkg_version}.tar.gz
 pkg_shasum=db8d87ea720ea9d5edc5388fc7a0497bb11ba9fe972245e0f7f4c7e8b1e1e84d
 pkg_build_deps=(
-  core/coreutils
   core/diffutils
   core/gcc
   core/make
-  core/pkg-config
 )
 pkg_deps=(
+  core/coreutils
   core/bzip2
   core/glibc
   core/libpng
   core/zlib
   core/bash
+  core/sed
+  core/pkg-config
 )
 pkg_include_dirs=(include)
+pkg_pconfig_dirs=(lib/pkgconfig)
 pkg_lib_dirs=(lib)
 pkg_bin_dirs=(bin)
 
@@ -30,7 +32,7 @@ do_install() {
   do_default_install
 
   build_line "Remove space from freetype-config interperter"
-  sed -i 's@ /bin/sh@/bin/sh@' "$CACHE_PATH/builds/unix/freetype-config"
+  sed -i 's@#! /bin/sh@#!/bin/sh@' "$CACHE_PATH/builds/unix/freetype-config"
 
   build_line "Copy freetype-config to bin"
   install "$CACHE_PATH/builds/unix/freetype-config" "$pkg_prefix/bin/"
