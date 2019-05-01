e88202c9c37841428d0835822e9913df39f7b34e
Tom Duffield <tom@chef.io>
2019-05-01 16:07:19 -0500

    [aws-cli] Add plan.ps1 to aws-cli
    Signed-off-by: Tom Duffield <tom@chef.io>

---
+++ aws-cli/plan.ps1
@@ -0,0 +1,33 @@
+$pkg_name="aws-cli"
+$pkg_origin="core"
+$pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+$pkg_license=('Apache-2.0')
+$pkg_description="The AWS Command Line Interface (CLI) is a unified tool to \
+  manage your AWS services. With just one tool to download and configure, you \
+  can control multiple AWS services from the command line and automate them \
+  through scripts."
+$pkg_upstream_url="https://aws.amazon.com/cli/"
+$pkg_deps=@(
+  "core/python"
+)
+$pkg_bin_dirs=@("Scripts")
+
+function pkg_version {
+  python -m pip search --disable-pip-version-check awscli | %{ if( $_ -match "^awscli \((.+)\)") { $matches[1]; } }
+}
+
+function Invoke-Before {
+  Set-PkgVersion
+}
+
+function Invoke-Prepare {
+  python -m pip install virtualenv
+  python -m virtualenv "$pkg_prefix"
+  ."$pkg_prefix/Scripts/activate"
+}
+
+function Invoke-Install {
+  python -m pip install "awscli==$pkg_version"
+  # Write out versions of all pip packages to package
+  python -m pip freeze > "$pkg_prefix/requirements.txt"
+}
