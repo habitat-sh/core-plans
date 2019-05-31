a2b03164e3aeac82c8a7c10c4fe5fb8643537130
Stephan Renatus <srenatus@chef.io>
2019-05-31 09:37:20 +0200

    [opa] update tests
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

--- opa/tests/test.sh
+++ opa/tests/test.sh
@@ -1,39 +1,42 @@
 #!/bin/sh
+#/ Usage: test.sh <pkg_ident>
+#/
+#/ Example: test.sh core/opa/0.11.0/20190531065119
+#/
+
+set -euo pipefail
+
+if [[ -z "${1:-}" ]]; then
+  grep '^#/' < "${0}" | cut -c4-
+  exit 1
+fi
 
 TESTDIR="$(dirname "${0}")"
-PLANDIR="$(dirname "${TESTDIR}")"
-SKIPBUILD=${SKIPBUILD:-0}
-
 source "${TESTDIR}/helpers.bash"
 
-hab pkg install --binlink core/bats
+export TEST_PKG_IDENT="${1}"
+hab pkg install "${TEST_PKG_IDENT}"
 
 hab pkg install core/busybox-static
 hab pkg binlink core/busybox-static nc
-hab pkg install -b core/jq-static core/curl
+hab pkg install core/bats core/curl core/jq-static --binlink
 
-# Wait for supervisor to start
-echo "Waiting for supervisor to start"
+hab sup term
+while hab sup status 2>/dev/null; do
+  echo "Waiting for supervisor to terminate"
+  sleep 1
+done
+
+hab sup run &
+
+echo "Waiting for supervisor to start (30s)"
 wait_listen tcp 9632 30
-
-source "${PLANDIR}/plan.sh"
-# Unload the service if its already loaded.
-hab svc unload "${HAB_ORIGIN}/${pkg_name}"
-
-if [ "${SKIPBUILD}" -eq 0 ]; then
-  set -e
-  pushd "${PLANDIR}" > /dev/null
-  build
-  popd > /dev/null
-  set +e
-fi
-
-source "${PLANDIR}/results/last_build.env"
-hab pkg install --binlink --force "${PLANDIR}/results/${pkg_artifact}"
-hab svc load "${pkg_ident}"
+hab svc load "${TEST_PKG_IDENT}"
 
 # Wait for 5 seconds on first check, to ensure service is up.
 echo "Waiting for opa to start (5s)"
 wait_listen tcp 8181 5
 
 bats "${TESTDIR}/test.bats"
+
+hab svc unload "${TEST_PKG_IDENT}" || true
