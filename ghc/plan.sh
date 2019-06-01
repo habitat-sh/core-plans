95e5763cefd48ba9e6d3249539fefbd9ad3d74e9
W. Duncan Fraser <duncan@wduncanfraser.com>
2019-05-31 23:08:47 -0500

    Updated GHC to 8.6.5
    Signed-off-by: W. Duncan Fraser <duncan@wduncanfraser.com>

--- ghc/plan.sh
+++ ghc/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=ghc
 pkg_origin=core
-pkg_version=8.6.3
+pkg_version=8.6.5
 pkg_license=('BSD-3-Clause')
 pkg_upstream_url="https://www.haskell.org/ghc/"
 pkg_description="The Glasgow Haskell Compiler"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="http://downloads.haskell.org/~ghc/${pkg_version}/ghc-${pkg_version}-src.tar.xz"
-pkg_shasum="9f9e37b7971935d88ba80426c36af14b1e0b3ec1d9c860f44a4391771bc07f23"
+pkg_shasum="4d4aa1e96f4001b934ac6193ab09af5d6172f41f5a5d39d8e43393b9aafee361"
 
 pkg_bin_dirs=(bin)
 pkg_lib_dirs=(lib)
