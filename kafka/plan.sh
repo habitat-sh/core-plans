6ebcf6095b892200159afedc511034325e5f76aa
Graham Weldon <graham@grahamweldon.com>
2019-06-13 11:12:11 +0900

    Update Kafka to 2.2.1, add testing
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- kafka/plan.sh
+++ kafka/plan.sh
@@ -1,9 +1,10 @@
 pkg_name=kafka
 pkg_origin=core
-pkg_version=0.10.2.2
-pkg_source="http://archive.apache.org/dist/${pkg_name}/${pkg_version}/${pkg_name}_2.11-${pkg_version}.tgz"
-pkg_shasum="60f587ed8d1ee6e8e8057f13da6eee472f95c8d2ea691f6aab74edb842dc9950"
-pkg_dirname="${pkg_name}_2.11-${pkg_version}"
+pkg_version=2.2.1
+scala_version=2.12
+pkg_source="http://archive.apache.org/dist/${pkg_name}/${pkg_version}/${pkg_name}_${scala_version}-${pkg_version}.tgz"
+pkg_shasum=1b8f4f94514fd1f35dc2aba0114b56cf69afa0027184f2a2964625fc1bb46deb
+pkg_dirname="${pkg_name}_${scala_version}-${pkg_version}"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="A distributed streaming platform"
 pkg_upstream_url="https://kafka.apache.org/"
@@ -12,7 +13,7 @@ pkg_bin_dirs=(bin)
 pkg_deps=(
   core/bash-static
   core/coreutils
-  core/jre8
+  core/openjdk11
 )
 pkg_binds=(
   [zookeeper]="port"
---
+++ kafka/tests/test.bats
@@ -0,0 +1,11 @@
+TEST_PKG_VERSION="$(echo "${TEST_PKG_IDENT}" | cut -d/ -f3)"
+
+@test "Service is running" {
+  echo -e "$(hab svc status)"
+  [ "$(hab svc status | grep "kafka\.default" | awk '{print $4}' | grep up)" ]
+}
+
+@test "Listening on port 9092" {
+  result="$(netstat -peanut | grep java | grep LISTEN | awk '{print $4}' | awk -F':' '{print $2}' | grep 9092)"
+  [ "${result}" -eq 9092 ]
+}
