7c0f5bd021c860f8ba82633b9c5996bdab09a5b4
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2019-06-13 15:42:24 +0100

    Add tests for core/git: * for windows and linux plans * with amendments according to comments in issue #2678
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- git/tests/test.bats
+++ git/tests/test.bats
@@ -1,6 +1,5 @@
-source "${BATS_TEST_DIRNAME}/../plan.sh"
-
-@test "Version matches" {
-  result="$(git --version | awk '{print $3}')"
-  [ "$result" = "${pkg_version}" ]
+@test "Version matches plan" {
+  TEST_PKG_VERSION="$(echo "${TEST_PKG_IDENT}" | cut -d/ -f3)"
+  actual_version="$(hab pkg exec "${TEST_PKG_IDENT}" git --version | awk '{print $3}')"
+  diff <(echo "$actual_version") <( echo "${TEST_PKG_VERSION}")
 }
---
+++ git/tests/test.pester.ps1
@@ -0,0 +1,14 @@
+param (
+    [Parameter()]
+    [string]$PackageIdentifier = $(throw "Fully qualified package identifier must be given as a parameter.")
+)
+
+$PackageVersion = $PackageIdentifier.split('/')[2]
+Describe "core/git" {
+    Context "git" {
+        $OutputVariable = (hab pkg exec $PackageIdentifier git --version | Out-String).split(' ')[2]
+        It "returns --version that matches the plan" {
+            "$OutputVariable" | Should -Match "$PackageVersion"
+        }
+    }
+}
