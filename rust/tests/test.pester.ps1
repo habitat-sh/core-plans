d07085992cc3e812213aff32c25de69db6730a7e
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2019-06-13 17:07:57 +0100

    Add tests to core/rust * for windows and linux plans * with amendments according to comments in issue #2678
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

---
+++ rust/tests/test.pester.ps1
@@ -0,0 +1,14 @@
+param (
+    [Parameter()]
+    [string]$PackageIdentifier = $(throw "Fully qualified package identifier must be given as a parameter.")
+)
+
+$PackageVersion = $PackageIdentifier.split('/')[2]
+Describe "core/rust" {
+    Context "rustc" {
+        It "returns --version that matches the plan" {
+            $OutputVariable = (hab pkg exec $PackageIdentifier rustc --version | Out-String).split(' ')[1]
+            "$OutputVariable" | Should -BeExactly "$PackageVersion"
+        }
+    }
+}
