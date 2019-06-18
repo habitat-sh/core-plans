039f669e6f46f5b95a46f2c4686cc8835c4ac463
echohack <echohack@users.noreply.github.com>
2019-06-18 12:52:39 -0700

    update libpipeline to 1.5.1, fix minor formatting
    Signed-off-by: echohack <echohack@users.noreply.github.com>

--- libpipeline/README.md
+++ libpipeline/README.md
@@ -10,6 +10,10 @@ libpipeline is a C library for manipulating pipelines of subprocesses in a flexi
 
 Binary package
 
+## Changelog
+
+The changelog for this software is in a somewhat non-standard location: http://git.savannah.nongnu.org/cgit/libpipeline.git/tree/NEWS
+
 ## Usage
 
 *TODO: Add instructions for usage*
--- libpipeline/plan.sh
+++ libpipeline/plan.sh
@@ -1,15 +1,19 @@
 pkg_origin=core
 pkg_name=libpipeline
-pkg_version=1.4.1
+pkg_version=1.5.1
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_license=('GPL-3.0')
+pkg_license=('GPL-3.0-or-later')
 pkg_description="libpipeline is a C library for manipulating pipelines of subprocesses in a flexible and convenient way."
 pkg_upstream_url=http://libpipeline.nongnu.org/
 pkg_source="http://download.savannah.gnu.org/releases/libpipeline/libpipeline-${pkg_version}.tar.gz"
-pkg_shasum=da46d7b20163aadb9db2faae483f734e9096a7550c84b94029abeab62dd1b9ee
+pkg_shasum=d633706b7d845f08b42bc66ddbe845d57e726bf89298e2cee29f09577e2f902f
 pkg_deps=(core/glibc)
-pkg_build_deps=(core/gcc   core/coreutils
-                core/make  core/diffutils)
+pkg_build_deps=(
+  core/gcc
+  core/coreutils
+  core/make
+  core/diffutils
+)
 pkg_lib_dirs=(lib)
 pkg_include_dirs=(include)
 
