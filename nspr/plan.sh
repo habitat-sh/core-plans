f766de8c8a4fa8b527d800fb65b97b75db3bef6c
Kristian Vlaardingerbroek <kvlaardingerbroek@schubergphilis.com>
2019-06-18 14:06:38 +0200

    [nspr] Update to 4.21
    Signed-off-by: Kristian Vlaardingerbroek <kvlaardingerbroek@schubergphilis.com>

--- nspr/plan.sh
+++ nspr/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=nspr
 pkg_origin=core
-pkg_version=4.12
+pkg_version=4.21
 pkg_license=("MPL-2.0")
 pkg_description="Netscape Portable Runtime"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_upstream_url=https://developer.mozilla.org/en-US/docs/Mozilla/Projects/NSPR
-pkg_source=https://ftp.mozilla.org/pub/mozilla.org/nspr/releases/v${pkg_version}/src/${pkg_name}-${pkg_version}.tar.gz
-pkg_shasum=e0b10a1e569153668ff8bdea6c7e491b389fab69c2f18285a1ebf7c2ea4269de
+pkg_source=https://ftp.mozilla.org/pub/nspr/releases/v${pkg_version}/src/${pkg_name}-${pkg_version}.tar.gz
+pkg_shasum=15ea32c7b100217b6e3193bc03e77f485d9bf7504051443ba9ce86d1c17c6b5a
 pkg_deps=(core/glibc)
 pkg_build_deps=(core/gcc core/make)
 pkg_bin_dirs=(bin)
