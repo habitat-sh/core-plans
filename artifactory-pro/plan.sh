9edefc84033ebfef8e16c18e099b9f592d0fd2f9
echohack <echohack@users.noreply.github.com>
2019-06-19 09:57:52 -0700

    update to 6.10.4
    Signed-off-by: echohack <echohack@users.noreply.github.com>

--- artifactory-pro/plan.sh
+++ artifactory-pro/plan.sh
@@ -1,13 +1,13 @@
 pkg_origin=core
 pkg_name=artifactory-pro
-pkg_version=6.10.3
+pkg_version=6.10.4
 pkg_description="Artifactory is an advanced binary repository manager for use by build tools (like Maven and Gradle), dependency management tools (like Ivy and NuGet) and build servers (like Jenkins, Hudson, TeamCity and Bamboo).
 Repository managers serve two purposes: they act as highly configurable proxies between your organization and external repositories and they also provide build servers with a deployment destination for your internally generated artifacts."
 pkg_upstream_url=https://www.jfrog.com/artifactory/
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("JFrog Artifactory EULA")
 pkg_source=https://dl.bintray.com/jfrog/${pkg_name}/org/artifactory/pro/jfrog-${pkg_name}/${pkg_version}/jfrog-${pkg_name}-${pkg_version}.zip
-pkg_shasum=392610eeedd13a3eea8472bb3577776bd237f6637673da1df9732a4424cb7f89
+pkg_shasum=307053165f2e96d74182e1a75105b46f57ed49b625e53b43c84da6f6baabccbf
 pkg_deps=(core/bash core/openjdk11)
 pkg_exports=(
   [port]=port
