b5e205d44bb7ca4f5ac84470bfa0f77cc4ef9a07
Jeff Brimager <JBrimager@gmail.com>
2019-06-19 21:31:44 -0500

    removed env variable setting. was causing dependency and path issues with packages downstream
    Signed-off-by: Jeff Brimager <JBrimager@gmail.com>

--- corretto8/plan.ps1
+++ corretto8/plan.ps1
@@ -3,16 +3,18 @@ $pkg_origin="core"
 $pkg_version="8.212.04.2"
 $pkg_description="Corretto is a build of the Open Java Development Kit (OpenJDK) with long-term support from Amazon"
 $pkg_upstream_url="https://aws.amazon.com/corretto/"
-$pkg_license=@("GPLv2+CE")
+$pkg_license=@("GPL-2.0-only")
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_source="https://d3pxv6yz143wms.cloudfront.net/${pkg_version}/amazon-corretto-${pkg_version}-windows-x64-jdk.zip"
 $pkg_shasum="3c867c861f7aaf04f7d87dbc51e4075e0c45066e8a4b0ab2ac8cece8aa575c7e"
 $pkg_dirname="amazon-corretto-$pkg_version"
-
-function Invoke-SetupEnvironment {
-    Set-RuntimeEnv JAVA_HOME "$pkg_prefix"
- }
+$pkg_bin_dirs=@("bin")
+$pkg_lib_dis=@("lib")
 
 function Invoke-Build {
     Copy-Item "$HAB_CACHE_SRC_PATH/$pkg_dirname/*" "$pkg_prefix" -Recurse -Force
+}
+
+function Invoke-Check {
+  (& "$HAB_CACHE_SRC_PATH/$pkg_dirname/bin/java.exe" --version).StartsWith("openjdk version $pkg_version")
 }
