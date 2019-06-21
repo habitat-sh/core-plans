2bd284b7af67cba2e618c88f6e25109b499daa27
Ian Henry <ihenry@chef.io>
2019-06-21 10:33:19 -0400

    Corrects build deps in make, mpfr, ncurses, perl, procps-ng, sed, texinfo, zlib
    Signed-off-by: Ian Henry <ihenry@chef.io>

--- ncurses/plan.sh
+++ ncurses/plan.sh
@@ -13,7 +13,6 @@ pkg_source="http://ftp.gnu.org/gnu/${pkg_name}/${pkg_name}-${pkg_version}.tar.gz
 pkg_shasum="aa057eeeb4a14d470101eff4597d5833dcef5965331be3528c08d99cebaa0d17"
 pkg_deps=(
   core/glibc
-  core/gcc-libs
 )
 pkg_build_deps=(
   core/coreutils
