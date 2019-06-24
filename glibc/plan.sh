7f8bb38b5fd82c4872e703cd9c0b43c942e32fca
Scott Macfarlane <smacfarlane@chef.io>
2019-06-24 11:25:23 -0700

    Remove deprecated option from TZ generation
    Signed-off-by: Scott Macfarlane <smacfarlane@chef.io>

--- glibc/plan.sh
+++ glibc/plan.sh
@@ -306,9 +306,9 @@ EOF
       mkdir -p "$ZONEINFO"/{posix,right}
       for tz in etcetera southamerica northamerica europe africa antarctica \
           asia australasia backward pacificnew systemv; do
-        zic -L /dev/null -d "$ZONEINFO" -y "sh yearistype.sh" ${tz}
-        zic -L /dev/null -d "$ZONEINFO/posix" -y "sh yearistype.sh" ${tz}
-        zic -L leapseconds -d "$ZONEINFO/right" -y "sh yearistype.sh" ${tz}
+        zic -L /dev/null -d "$ZONEINFO" ${tz}
+        zic -L /dev/null -d "$ZONEINFO/posix" ${tz}
+        zic -L leapseconds -d "$ZONEINFO/right" ${tz}
       done
       cp -v zone.tab zone1970.tab iso3166.tab "$ZONEINFO"
       zic -d "$ZONEINFO" -p America/New_York
