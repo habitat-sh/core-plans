451638f0171df6d62d2f6aa7a6c7283b95159ddc
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2019-06-27 19:01:40 +0100

    Uplift openssl version 1.0.2s and * add tests to core/openssl for windows and linux plans * update windows tests to parse the openssl version and to perform a successful encoding
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

---
+++ openssl/tests/test.pester.ps1
@@ -0,0 +1,20 @@
+param (
+    [Parameter()]
+    [string]$PackageIdentifier = $(throw "Fully qualified package identifier must be given as a parameter.")
+)
+
+$PackageVersion = $PackageIdentifier.split('/')[2]
+Describe "core/openssl" {
+    Context "openssl" {
+        $OutputVariable = (hab pkg exec $PackageIdentifier openssl version | Out-String).Trim()
+        $OutputVariable -match "(?<=^OpenSSL )([^ ]*)"
+        It "version matches $PackageVersion" {
+            $matches[0] | Should -BeExactly "$PackageVersion"
+        }
+
+        $OutputVariable = ("a_byte_character" | hab pkg exec $PackageIdentifier openssl enc -base64| Out-String).Trim()
+        It "returns correct encoded string" {
+            "$OutputVariable" | Should -BeExactly "YV9ieXRlX2NoYXJhY3Rlcg0K"
+        }
+    }
+}
