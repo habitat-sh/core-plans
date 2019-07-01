60694a95c9b10c5bb54bde790213a02877a0a30d
Scott Macfarlane <smacfarlane@chef.io>
2019-07-01 11:56:06 -0700

    Address Feedback and add tests
    Signed-off-by: Scott Macfarlane <smacfarlane@chef.io>

--- glibc/tests/test.sh
+++ glibc/tests/test.sh
@@ -10,5 +10,6 @@ TEST_PKG_IDENT="$1"
 hab pkg install core/bats --binlink
 hab pkg install "$TEST_PKG_IDENT"
 
+
 export TEST_PKG_IDENT
-bats "${TESTDIR}/bats/test.bats"
+bats "${TESTDIR}/bats/test.bats" --tap
