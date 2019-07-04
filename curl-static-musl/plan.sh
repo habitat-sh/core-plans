544e76b8a0ec36564aced4705eee28c0b5486348
Graham Weldon <graham@grahamweldon.com>
2019-07-04 09:11:31 +0900

    Update curl-static-musl to 7.65.1
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- curl-static-musl/plan.sh
+++ curl-static-musl/plan.sh
@@ -3,14 +3,14 @@ source "$(dirname "${BASH_SOURCE[0]}")/../curl/plan.sh"
 pkg_name=curl-static-musl
 pkg_distname=curl
 pkg_origin=core
-pkg_version=7.63.0
+pkg_version=7.65.1
 pkg_description="curl is an open source command line tool and library for
   transferring data with URL syntax."
 pkg_upstream_url=https://curl.haxx.se/
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('curl')
-pkg_source=https://curl.haxx.se/download/${pkg_distname}-${pkg_version}.tar.gz
-pkg_dirname=${pkg_distname}-${pkg_version}
+pkg_source="https://curl.haxx.se/download/${pkg_distname}-${pkg_version}.tar.gz"
+pkg_dirname="${pkg_distname}-${pkg_version}"
 pkg_deps=(
   core/cacerts
 )
@@ -28,27 +28,28 @@ pkg_lib_dirs=()
 
 do_prepare() {
   export CC=musl-gcc
-  build_line "Setting CC=$CC"
+  build_line "Setting CC=${CC}"
 }
 
 do_build() {
-  LDFLAGS="-static" PKG_CONFIG="pkg-config --static" ./configure --prefix="$pkg_prefix" \
-              --with-ca-bundle="$(pkg_path_for cacerts)/ssl/certs/cacert.pem" \
-              --with-ssl="$(pkg_path_for openssl-musl)" \
-              --with-zlib="$(pkg_path_for zlib-musl)" \
-              --disable-manual \
-              --disable-ldap \
-              --disable-ldaps \
-              --disable-rtsp \
-              --enable-proxy \
-              --enable-optimize \
-              --disable-dependency-tracking \
-              --enable-ipv6 \
-              --without-libidn \
-              --without-gnutls \
-              --without-librtmp \
-              --enable-static \
-              --disable-shared
+  LDFLAGS="-static" PKG_CONFIG="pkg-config --static" ./configure \
+    --prefix="${pkg_prefix}" \
+    --with-ca-bundle="$(pkg_path_for cacerts)/ssl/certs/cacert.pem" \
+    --with-ssl="$(pkg_path_for openssl-musl)" \
+    --with-zlib="$(pkg_path_for zlib-musl)" \
+    --disable-manual \
+    --disable-ldap \
+    --disable-ldaps \
+    --disable-rtsp \
+    --enable-proxy \
+    --enable-optimize \
+    --disable-dependency-tracking \
+    --enable-ipv6 \
+    --without-libidn \
+    --without-gnutls \
+    --without-librtmp \
+    --enable-static \
+    --disable-shared
 
   make curl_LDFLAGS=-all-static
 }
