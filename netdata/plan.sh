b1fd82c0a0e87da6ae886c7cd6197299f1039b47
Graham Weldon <graham@grahamweldon.com>
2019-07-05 14:17:11 +0900

    Update Netdata repo
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- netdata/plan.sh
+++ netdata/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=netdata
 pkg_origin=core
-pkg_version="1.15.0"
+pkg_version=1.15.0
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("GPL-3.0-or-later")
 pkg_description="netdata is a system for distributed real-time performance and health monitoring."
-pkg_upstream_url="https://github.com/firehol/netdata"
-pkg_source="https://github.com/firehol/${pkg_name}/archive/v${pkg_version}.tar.gz"
-pkg_shasum="60cf4a15c25c828e25ae1218a485c21ef23423f7b6a9a1148ce9eea9c9726a39"
+pkg_upstream_url="https://github.com/netdata/netdata"
+pkg_source="https://github.com/netdata/${pkg_name}/archive/v${pkg_version}.tar.gz"
+pkg_shasum=60cf4a15c25c828e25ae1218a485c21ef23423f7b6a9a1148ce9eea9c9726a39
 pkg_build_deps=(
   core/autoconf
   core/autogen
@@ -23,8 +23,6 @@ pkg_deps=(
   core/python
   core/util-linux
   core/zlib
-
-  # for fix_interpreter
   core/coreutils
 )
 pkg_bin_dirs=(sbin)
