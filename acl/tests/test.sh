a0b67f9514edc466912021dc024f9e80e6468bba
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2019-07-08 18:32:30 +0100

    Add tests for core/acl and * remove the '[[' from the test verification
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

---
+++ acl/tests/test.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -euo pipefail
+
+#/ Usage: test.sh <pkg_ident>
+#/
+#/ Example: test.sh core/opa/0.11.0/20190531065119
+#/
+
+TESTDIR="$(dirname "${0}")"
+
+if [[ -z "${1:-}" ]]; then
+  grep '^#/' < "${0}" | cut -c4-
+  exit 1
+fi
+
+TEST_PKG_IDENT="$1"
+
+hab pkg install core/bats --binlink
+hab pkg install "$TEST_PKG_IDENT"
+
+export TEST_PKG_IDENT
+bats "${TESTDIR}/test.bats"
