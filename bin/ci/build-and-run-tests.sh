f1cd844f16685c815b5d7184e37bd110456f42c9
David Echo <echohack@users.noreply.github.com>
2019-07-08 14:08:03 -0700

    remove double build and merge DO_CHECK
    Signed-off-by: David Echo <echohack@users.noreply.github.com>

--- bin/ci/build-and-run-tests.sh
+++ bin/ci/build-and-run-tests.sh
@@ -41,13 +41,14 @@ echo "--- :key: Generating fake origin key"
 # we won't have access to any valid signing keys.
 hab origin key generate "$HAB_ORIGIN"
 
+echo "--- :construction: Starting build for $plan"
 # We want to ensure that we build from the project root. This
 # creates a subshell so that the cd will only affect that process
 project_root="$(git rev-parse --show-toplevel)"
 (
   cd "$project_root"
   echo "--- :construction: Building $plan"
-  hab pkg build "$plan"
+  env DO_CHECK=true hab pkg build "$plan"
   source results/last_build.env
   echo "--- :mag: Testing $pkg_ident"
   if [ ! -f "$plan/tests/test.sh" ]; then
