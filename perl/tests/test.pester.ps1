c60bea6c266f3edff0ac26bd7c81512c1a5f51f4
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2019-07-09 13:46:43 +0100

    Add tests to core/perl * for windows and linux * fixing the windows stdout issue according to #2745
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

---
+++ perl/tests/test.pester.ps1
@@ -0,0 +1,15 @@
+param (
+    [Parameter()]
+    [string]$PackageIdentifier = $(throw "Fully qualified package identifier must be given as a parameter.")
+)
+
+$PackageVersion = $PackageIdentifier.split('/')[2]
+Describe "core/perl" {
+    Context "perl" {
+        $OutputVariable = (hab pkg exec $PackageIdentifier perl.exe --version | Out-String)
+        $OutputVariable -match "(?<=\(v)(.*?)(?=\) built for MSWin32)"
+        It "returns --version that matches the plan"  {
+            $matches[1] | Should -BeExactly "$PackageVersion"
+        }
+    }
+}
