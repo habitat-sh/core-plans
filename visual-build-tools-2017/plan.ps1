d7938833e59c7bf2a2cbd987306e9f0d443c7111
mwrock <matt@mattwrock.com>
2019-07-18 14:56:07 -0700

    add nuget build tools to vs 2017
    Signed-off-by: mwrock <matt@mattwrock.com>

--- visual-build-tools-2017/plan.ps1
+++ visual-build-tools-2017/plan.ps1
@@ -23,7 +23,7 @@ $pkg_include_dirs=@(
 )
 
 function Invoke-Unpack {
-  Start-Process "$HAB_CACHE_SRC_PATH/$pkg_filename" -Wait -ArgumentList "--quiet --layout $HAB_CACHE_SRC_PATH/$pkg_dirname --lang en-US --add Microsoft.VisualStudio.Workload.MSBuildTools --add Microsoft.VisualStudio.Workload.VCTools --add Microsoft.VisualStudio.Component.VC.ATLMFC"
+  Start-Process "$HAB_CACHE_SRC_PATH/$pkg_filename" -Wait -ArgumentList "--quiet --layout $HAB_CACHE_SRC_PATH/$pkg_dirname --lang en-US --add Microsoft.VisualStudio.Workload.MSBuildTools --add Microsoft.VisualStudio.Workload.VCTools --add Microsoft.VisualStudio.Component.VC.ATLMFC --add Microsoft.VisualStudio.Component.NuGet.BuildTools"
   Push-Location "$HAB_CACHE_SRC_PATH/$pkg_dirname"
   try {
     Get-ChildItem "$HAB_CACHE_SRC_PATH/$pkg_dirname" -Include *.vsix -Exclude @('*x86*', '*.arm.*') -Recurse | % {
