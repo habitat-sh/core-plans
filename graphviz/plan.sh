48daafb1afe1342e33e45bf8d57e2490132b8455
Scott Macfarlane <smacfarlane@chef.io>
2019-07-23 09:55:11 -0700

    Update download location of graphviz
    Signed-off-by: Scott Macfarlane <smacfarlane@chef.io>

--- graphviz/plan.sh
+++ graphviz/plan.sh
@@ -5,9 +5,9 @@ pkg_license=("EPL-1.0")
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="Graphviz - Graph Visualization Software"
 pkg_upstream_url="https://graphviz.gitlab.io/"
-pkg_dirname="${pkg_name}-stable_release_${pkg_version}"
-pkg_source="https://gitlab.com/graphviz/graphviz/-/archive/stable_release_${pkg_version}/${pkg_dirname}.tar.bz2"
-pkg_shasum="92f654c95b412920777ceddf90e6564ffa871a77e4f6155ab437a2d3a2129e2b"
+pkg_dirname="${pkg_name}-${pkg_version}"
+pkg_source="https://graphviz.gitlab.io/pub/graphviz/stable/SOURCES/graphviz.tar.gz"
+pkg_shasum="ca5218fade0204d59947126c38439f432853543b0818d9d728c589dfe7f3a421"
 pkg_deps=(
   core/glibc
   core/libtool
@@ -22,16 +22,17 @@ pkg_build_deps=(
     core/gcc
     core/make
     core/pkg-config
+    core/git
 )
 pkg_bin_dirs=(bin)
 pkg_lib_dirs=(lib)
 pkg_include_dirs=(include)
 
 do_prepare() {
-  # TODO: if we don't copy this aclocal will fail. need to figure out how to fix this
-  cp "$(pkg_path_for core/pkg-config)/share/aclocal/pkg.m4" "$(pkg_path_for core/automake)/share/aclocal/"
+  ACLOCAL_PATH="$ACLOCAL_PATH:$(pkg_path_for core/pkg-config)/share/aclocal"
+  ACLOCAL_PATH="$ACLOCAL_PATH:$(pkg_path_for core/automake)/share/aclocal"
+  export ACLOCAL_PATH
 
-  libtoolize
   ./autogen.sh
 }
 
