8c93eea30869b17cc16f8e190bed3732e2a0d6df
Scott Macfarlane <smacfarlane@chef.io>
2019-07-23 10:14:43 -0700

    Change python to python-minimal
    Signed-off-by: Scott Macfarlane <smacfarlane@chef.io>

--- glibc/plan.sh
+++ glibc/plan.sh
@@ -27,7 +27,7 @@ pkg_build_deps=(
   core/sed
   core/perl
   core/m4
-  core/python
+  core/python-minimal
 )
 pkg_bin_dirs=(bin)
 pkg_include_dirs=(include)
@@ -300,17 +300,17 @@ ethers: files
 rpc: files
 EOF
 
-    
-    # Install timezone data. 
+
+    # Install timezone data.
     # We set --sysconfdir=$pkg_prefix/etc during our build, so we need to
     # embed timezone data in this package.
-    # 
+    #
     # zic /dev/null creates posix timezones without leapseconds
     # zic leapseconds creates right timezones with leapseconds
-    # zic -d "$ZONEINFO" creates  posixrules file. We use New York because POSIX 
+    # zic -d "$ZONEINFO" creates  posixrules file. We use New York because POSIX
     #   requires the daylight savings time rules to be in accordance with US rules.
 
-    extract_src tzdatak
+    extract_src tzdata
     pushd ./tzdata > /dev/null
       ZONEINFO="$pkg_prefix/share/zoneinfo"
       mkdir -p "$ZONEINFO"/{posix,right}
