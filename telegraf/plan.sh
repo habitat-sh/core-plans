45bf9746f53973b210e7181aff23d74e2556f940
Graham Weldon <graham@grahamweldon.com>
2019-07-24 11:26:36 +0900

    Update telegraf to 1.11.3
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- telegraf/plan.sh
+++ telegraf/plan.sh
@@ -1,11 +1,11 @@
 pkg_name=telegraf
 pkg_origin=core
-pkg_version="1.10.3"
+pkg_version=1.11.3
 pkg_license=('MIT')
 pkg_description="telegraf - client for InfluxDB"
 pkg_upstream_url="https://github.com/influxdata/telegraf/"
 pkg_source="https://dl.influxdata.com/${pkg_name}/releases/${pkg_name}-${pkg_version}-static_linux_amd64.tar.gz"
-pkg_shasum="fe5552e24a26e1de698207ff9c89e4a716cd3f65904bef6bcb60bd34743d9c83"
+pkg_shasum=565a1b5cc9cbfd797f245a25addcecf791b3381f6b137994dd8e56850b1c3d1a
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_svc_run="telegraf --config ${pkg_svc_config_path}/telegraf.conf"
 pkg_build_deps=(
