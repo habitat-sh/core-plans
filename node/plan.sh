44a7677bdb15d43f6f1899121cdcb82ca038cd54
Graham Weldon <graham@grahamweldon.com>
2019-07-24 10:50:43 +0900

    Update node to 12.7.0
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- node/plan.sh
+++ node/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=node
 pkg_origin=core
-pkg_version=12.6.0
+pkg_version=12.7.0
 pkg_description="Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine."
 pkg_upstream_url=https://nodejs.org/
 pkg_license=('MIT')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="https://nodejs.org/dist/v${pkg_version}/node-v${pkg_version}.tar.gz"
-pkg_shasum=8b47a34a5507ee24abb91f26f8799bf3af66172b0ffd45981a7fcf1dde463bd4
+pkg_shasum=d5e63fd4ee88d539a69b9e71631d03014bd8e98596e741515e3d7aa930f4630a
 pkg_deps=(
   core/glibc
   core/gcc-libs
--- node12/plan.ps1
+++ node12/plan.ps1
@@ -2,7 +2,7 @@
 
 $pkg_name="node12"
 $pkg_origin="core"
-$pkg_version="12.6.0"
+$pkg_version="12.7.0"
 $pkg_description="Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine."
 $pkg_source="https://nodejs.org/dist/v$pkg_version/node-v$pkg_version-x64.msi"
-$pkg_shasum="2aa6856cde3d36220d34c70000c6305802e95f2fda1c1d62bb639c10371b55a6"
+$pkg_shasum="7f13b75f785d919dbb046c66f60f1be5ae07400ec222a52fce058849561f00c0"
