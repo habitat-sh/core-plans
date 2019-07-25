be2689fbb76e1ac07786fdc63df9d2dbda4d2e53
Graham Weldon <graham@grahamweldon.com>
2019-07-25 17:07:40 +0900

    Update sccache to 0.2.9, add tests, fix build, add features
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- sccache/plan.sh
+++ sccache/plan.sh
@@ -1,22 +1,34 @@
 pkg_name=sccache
 pkg_origin=core
-pkg_version=0.2.4
+pkg_version=0.2.9
 pkg_license=('Apache-2.0')
-pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_source=https://github.com/mozilla/sccache/archive/${pkg_version}.tar.gz
-pkg_shasum="82d8d553a0cf2752e71266523f9aac30c1b5a12fa6b1faea8c7c6e1ec9827371"
-pkg_bin_dirs=(bin)
 pkg_upstream_url="https://github.com/mozilla/sccache"
 pkg_description="sccache is ccache with cloud storage"
-pkg_deps=(core/glibc core/gcc-libs core/openssl)
-pkg_build_deps=(core/rust core/gcc core/pkg-config core/openssl core/make)
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_source="https://github.com/mozilla/sccache/archive/${pkg_version}.tar.gz"
+pkg_shasum=a24cf714dad8f3f1a50a7ae32665451e36487e3c76f5d92d57f5e4ef7176c0c3
+pkg_bin_dirs=(bin)
+pkg_deps=(
+  core/glibc
+  core/gcc-libs
+  core/openssl
+)
+pkg_build_deps=(
+  core/rust
+  core/gcc
+  core/pkg-config
+  core/openssl
+  core/make
+)
 pkg_svc_user="root"
 pkg_svc_group="root"
 
 do_build() {
-  cargo build --release
+  cargo build \
+    --features="all" \
+    --release
 }
 
 do_install() {
-  cargo install --root "${pkg_prefix}"
+  install -v -D "${CACHE_PATH}/target/release/sccache" "${pkg_prefix}/bin/sccache"
 }
