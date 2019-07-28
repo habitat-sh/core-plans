d925f1ec551cc229ccdbb034a34a433c9cc35c30
Graham Weldon <graham@grahamweldon.com>
2019-07-29 07:58:32 +0900

    Update libimagequant to 2.12.5
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- libimagequant/plan.sh
+++ libimagequant/plan.sh
@@ -1,16 +1,19 @@
 pkg_name=libimagequant
 pkg_origin=core
-pkg_version="2.12.2"
+pkg_version=2.12.5
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("GPL-3.0-or-later")
-pkg_source="https://github.com/ImageOptim/${pkg_name}/archive/${pkg_version}.tar.gz"
-pkg_shasum="23ccecb4898ec17474914cfd2fbc4684425f7fd249117f2f1e3f3ba0bf8159e6"
-pkg_deps=(core/coreutils)
-pkg_build_deps=(core/make core/gcc)
-pkg_lib_dirs=(lib)
-pkg_include_dirs=(include)
 pkg_description="Small, portable C library for high-quality conversion of RGBA images to 8-bit indexed-color (palette) images."
 pkg_upstream_url="https://pngquant.org/lib"
+pkg_source="https://github.com/ImageOptim/${pkg_name}/archive/${pkg_version}.tar.gz"
+pkg_shasum=9dc07f3bf6efaf03241fd514e62108be484a373871e2e02c117e6efb49d26293
+pkg_deps=(core/coreutils)
+pkg_build_deps=(
+  core/make
+  core/gcc
+)
+pkg_lib_dirs=(lib)
+pkg_include_dirs=(include)
 
 do_build() {
   fix_interpreter "configure" core/coreutils bin/env
