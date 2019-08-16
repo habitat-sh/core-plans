134cdda1747b1868395931e0b22b4f7d189cedf9
Josh Black <raskchanky@gmail.com>
2019-08-16 10:01:53 -0700

    Bump rust to 1.37.0
    Signed-off-by: Josh Black <raskchanky@gmail.com>

--- rust/plan.ps1
+++ rust/plan.ps1
@@ -1,12 +1,12 @@
 ﻿$pkg_name="rust"
 $pkg_origin="core"
-$pkg_version="1.36.0"
+$pkg_version="1.37.0"
 $pkg_description="Safe, concurrent, practical language"
 $pkg_upstream_url="https://www.rust-lang.org/"
 $pkg_license=@("Apache-2.0", "MIT")
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_source="https://static.rust-lang.org/dist/rust-$pkg_version-x86_64-pc-windows-msvc.msi"
-$pkg_shasum="e86148196e71c9c826101d37b66393dfaacef824559d92a9f4e48f030a7aa1e4"
+$pkg_shasum="1e7d5617b7d49d7dbe406e6b4e820d5ad41e021911d69131227ac554aae9c27f"
 $pkg_deps=@("core/visual-cpp-redist-2015", "core/visual-cpp-build-tools-2015")
 $pkg_build_deps=@("core/lessmsi")
 $pkg_bin_dirs=@("bin")
