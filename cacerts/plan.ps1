e28ccc7761a8e831884f8776fd5c10d795206daa
Daniel Bright <danielcbright@gmail.com>
2019-08-29 15:05:33 -0400

    Updated pkg_version to use the YYYY.DD.MM format now that we're specifying a package name to download. Also changed the Windows plan to create a SymbolicLink instead of just copying the file twice, this now matches how it's done in the Linux plan.
    Signed-off-by: Daniel Bright <danielcbright@gmail.com>

--- cacerts/plan.ps1
+++ cacerts/plan.ps1
@@ -1,6 +1,10 @@
 $pkg_name="cacerts"
 $pkg_origin="core"
-$pkg_version="_set_from_downloaded_cacerts_file_"
+$pkg_description="\
+The Mozilla CA certificate store in PEM format (around 250KB uncompressed).
+"
+$pkg_upstream_url="https://curl.haxx.se/docs/caextract.html"
+$pkg_version="2019.28.08"
 $pkg_license=@('MPL-2.0')
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_source="https://curl.haxx.se/ca/cacert-2019-08-28.pem"
@@ -8,32 +12,12 @@ $pkg_shasum="38b6230aa4bee062cd34ee0ff6da173250899642b1937fc130896290b6bd91e3"
 function Invoke-Build { }
 
 function Invoke-Unpack {
-  Set-PackageVersion
   mkdir "$HAB_CACHE_SRC_PATH/$pkg_dirname" -Force
   Copy-Item "$HAB_CACHE_SRC_PATH/$pkg_filename" "$HAB_CACHE_SRC_PATH/$pkg_dirname" -Force
 }
 
 function Invoke-Install {
   mkdir "$pkg_prefix/ssl/certs"
-  Copy-Item "$pkg_filename" "$pkg_prefix/ssl/certs" -Force
-  Copy-Item "$pkg_filename" "$pkg_prefix/ssl/cert.pem" -Force
-}
-
-function Set-PackageVersion {
-  # Extract the build date of the certificates file
-  $datePrefix = "## Certificate data from Mozilla as of: "
-  $dateLine = Get-Content "$HAB_CACHE_SRC_PATH/$pkg_filename" | ? {
-    $_.StartsWith($datePrefix)
-  }
-  $buildDate = $dateLine.Substring($datePrefix.Length)
-
-  # Update the `$pkg_version` value with the build date
-  $script:pkg_version=[datetime]::ParseExact($buildDate, "ddd MMM dd HH:mm:ss yyyy GMT", $null).ToString("yyyy.MM.dd")
-  Write-BuildLine "Version updated to $pkg_version from CA Certs file"
-
-  # Several metadata values get their defaults from the value of `$pkg_version`
-  # so we must update these as well
-  $script:pkg_dirname="$pkg_name-$pkg_version"
-  $script:pkg_prefix="$HAB_PKG_PATH/$pkg_origin/$pkg_name/$pkg_version/$pkg_release"
-  $script:pkg_artifact="$HAB_CACHE_ARTIFACT_PATH/$pkg_origin-$pkg_name-$pkg_version-$pkg_release-$pkg_target.$_artifact_ext"
+  Copy-Item "$pkg_filename" "$pkg_prefix/ssl/certs/cacert.pem" -Force
+  New-Item -ItemType SymbolicLink -Path "$pkg_prefix/ssl/" -Name "cert.pem" -Value "$pkg_prefix/ssl/certs/cacert.pem" -Force
 }
