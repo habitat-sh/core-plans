b375f4555b18bac825ecb525f3047c524d0ee8f8
Daniel Bright <danielcbright@gmail.com>
2019-08-29 15:05:33 -0400

    Updated pkg_version to use the YYYY.DD.MM format now that we're specifying a package name to download. Also changed the Windows plan to create a SymbolicLink instead of just copying the file twice, this now matches how it's done in the Linux plan.
    Signed-off-by: Daniel Bright <danielcbright@gmail.com>

--- cacerts/plan.sh
+++ cacerts/plan.sh
@@ -4,6 +4,7 @@ pkg_description="\
 The Mozilla CA certificate store in PEM format (around 250KB uncompressed).
 "
 pkg_upstream_url="https://curl.haxx.se/docs/caextract.html"
+pkg_version="2019.28.08"
 pkg_license=('MPL-2.0')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="https://curl.haxx.se/ca/cacert-2019-08-28.pem"
@@ -11,21 +12,8 @@ pkg_shasum="38b6230aa4bee062cd34ee0ff6da173250899642b1937fc130896290b6bd91e3"
 pkg_deps=()
 pkg_build_deps=()
 
-pkg_version() {
-  local build_date
-  # Extract the build date of the certificates file
-  # shellcheck disable=SC2002
-  build_date="$(cat "$HAB_CACHE_SRC_PATH/$pkg_filename" \
-    | grep 'Certificate data from Mozilla' \
-    | sed 's/^## Certificate data from Mozilla as of: //')"
-
-  # Update the `$pkg_version` value with the build date
-  date --date="$build_date" "+%Y.%m.%d"
-}
-
 do_download() {
   do_default_download
-  update_pkg_version
 }
 
 do_unpack() {
