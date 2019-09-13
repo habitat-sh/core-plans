3dfea8a5c134cde70d62fee9cea04e7cbdfd8512
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2019-09-13 16:01:47 +0100

    Refactor tests to use ./bin/ci/test_helpers.sh
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- opa/tests/test.sh
+++ opa/tests/test.sh
@@ -6,6 +6,8 @@
 
 set -euo pipefail
 
+source "$(dirname "${0}")/../../bin/ci/test_helpers.sh"
+
 if [[ -z "${1:-}" ]]; then
   grep '^#/' < "${0}" | cut -c4-
   exit 1
@@ -20,16 +22,8 @@ hab pkg binlink core/busybox-static nc
 hab pkg install core/bats core/curl core/jq-static --binlink
 hab pkg install "${TEST_PKG_IDENT}"
 
-hab sup term
-while hab sup status 2>/dev/null; do
-  echo "Waiting for supervisor to terminate"
-  sleep 1
-done
-hab sup run &
-
-echo "Waiting for supervisor to start (30s)"
-wait_listen tcp 9632 30
-hab svc load "${TEST_PKG_IDENT}"
+ci_ensure_supervisor_running
+ci_load_service "${TEST_PKG_IDENT}"
 
 # Wait for 5 seconds on first check, to ensure service is up.
 echo "Waiting for opa to start (5s)"
