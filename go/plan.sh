41f90e1bd0d723f2882db1110eddc8315c536c32
Blake Johnson <bstier@chef.io>
2019-10-01 12:47:42 -0700

    Update go to 1.13.1
    Per https://groups.google.com/forum/m/#!topic/golang-announce/cszieYyuL9Q
    
    Signed-off-by: Blake Johnson <bstier@chef.io>

--- go/plan.sh
+++ go/plan.sh
@@ -1,13 +1,13 @@
 pkg_name=go
 pkg_origin=core
-pkg_version=1.13
+pkg_version=1.13.1
 pkg_description="Go is an open source programming language that makes it easy to
   build simple, reliable, and efficient software."
 pkg_upstream_url=https://golang.org/
 pkg_license=('BSD')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="https://storage.googleapis.com/golang/go${pkg_version}.src.tar.gz"
-pkg_shasum=3fc0b8b6101d42efd7da1da3029c0a13f22079c0c37ef9730209d8ec665bf122
+pkg_shasum=81f154e69544b9fa92b1475ff5f11e64270260d46e7e36c34aafc8bc96209358
 pkg_dirname=go
 pkg_deps=(
   core/glibc
