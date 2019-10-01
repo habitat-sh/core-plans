305fd950e16f9620518c43b2e802cb2eb982ebbc
Graham Weldon <graham@grahamweldon.com>
2019-10-01 10:26:46 +0900

    Update protobuf to 3.9.2, add tests
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- protobuf/plan.ps1
+++ protobuf/plan.ps1
@@ -1,12 +1,12 @@
 $pkg_name="protobuf"
 $pkg_origin="core"
-$pkg_version="3.9.1"
+$pkg_version="3.9.2"
 $pkg_file_name=$pkg_name + ($pkg_version).Replace(".", "")
 $pkg_description="Protocol buffers are a language-neutral, platform-neutral extensible mechanism for serializing structured data."
 $pkg_upstream_url="https://developers.google.com/protocol-buffers/"
 $pkg_license=("BSD")
 $pkg_source="https://github.com/google/${pkg_name}/releases/download/v${pkg_version}/${pkg_name}-all-${pkg_version}.zip"
-$pkg_shasum="aed089110977f7cabda19d550b4d503eb93fa0f73c7a470c4695f27b63029544"
+$pkg_shasum="c18c03b2beb14c8813e1ca4601bfef07755eccaf202c3d6a1187186fc30bb8a1"
 $pkg_deps=@(
     "core/zlib"
 )
