5f37dce8e0fee2784c4af5747e410dfe90f0adc5
Graham Weldon <graham@grahamweldon.com>
2019-10-02 09:00:55 +0900

    Update packer to 1.4.4
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- packer/plan.ps1
+++ packer/plan.ps1
@@ -1,11 +1,11 @@
 $pkg_name="packer"
 $pkg_origin="core"
-$pkg_version="1.4.2"
+$pkg_version="1.4.4"
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_license=@('MPL2')
 $pkg_bin_dirs=@("bin")
 $pkg_source="https://releases.hashicorp.com/packer/${pkg_version}/packer_${pkg_version}_windows_amd64.zip"
-$pkg_shasum="0db7527e81672d51fc436081eff0e49e8873baee0564e427c5dc73a3f44fa840"
+$pkg_shasum="310a98e6ac7d4ee4f306a5f8385628a2e79a2cb8f7d2e894379e3a97ff5ed35b"
 
 function Invoke-Install {
   Copy-Item "$HAB_CACHE_SRC_PATH/$pkg_name-$pkg_version/$pkg_name.exe" $pkg_prefix\bin
--- packer/plan.sh
+++ packer/plan.sh
@@ -1,10 +1,10 @@
 pkg_name=packer
 pkg_origin=core
-pkg_version=1.4.2
+pkg_version=1.4.4
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('MPL2')
 pkg_source="https://releases.hashicorp.com/${pkg_name}/${pkg_version}/${pkg_name}_${pkg_version}_linux_amd64.zip"
-pkg_shasum=2fcbd1662ac76dc4dec381bdc7b5e6316d5b9d48e0774a32fe6ef9ec19f47213
+pkg_shasum=b4dc37877a0fd00fc72ebda98977c2133be9ba6b26bcdd13b1b14a369e508948
 pkg_description="Packer is a tool for creating machine and container images for multiple platforms from a single source configuration."
 pkg_upstream_url=https://packer.io
 pkg_build_deps=(core/unzip)
