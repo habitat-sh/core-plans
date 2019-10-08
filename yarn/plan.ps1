2cba10f5e3c02c9161e40212e98751a1d696f9d8
Graham Weldon <graham@grahamweldon.com>
2019-10-09 08:58:53 +0900

    Update yarn to 1.19.1
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- yarn/plan.ps1
+++ yarn/plan.ps1
@@ -1,13 +1,13 @@
 $pkg_name="yarn"
 $pkg_origin="core"
-$pkg_version="1.18.0"
+$pkg_version="1.19.1"
 $pkg_description="Yarn is a package manager for your code. It allows you to use and share code with other developers from around the world. Yarn does this quickly, securely, and reliably so you don’t ever have to worry."
 $pkg_maintainer="The Habitat Maintainers humans@habitat.sh"
 $pkg_upstream_url="https://yarnpkg.com/"
 $pkg_license=@("BSD-2-Clause")
 $pkg_filename="$pkg_name-$pkg_version.msi"
 $pkg_source="https://yarnpkg.com/downloads/${pkg_version}/${pkg_filename}"
-$pkg_shasum="51096d02da48535c501cb9c99f363d665dd6c1b6eeb2da1915dc63448878d211"
+$pkg_shasum="28013aa7e4e36b75bc9b09ca1d1e5aa9c10ce6c7b9ca2f271fc7c5f81e813775"
 $pkg_bin_dirs=@("bin")
 $pkg_deps=@("core/node")
 $pkg_build_deps=@("core/lessmsi")
--- yarn/plan.sh
+++ yarn/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=yarn
 pkg_origin=core
-pkg_version=1.18.0
+pkg_version=1.19.1
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="Yarn is a package manager for your code. It allows you to use and share code with other developers from around the world. Yarn does this quickly, securely, and reliably so you don’t ever have to worry."
 pkg_upstream_url=https://yarnpkg.com/
 pkg_license=('BSD-2-Clause')
 pkg_source="https://yarnpkg.com/downloads/${pkg_version}/yarn-v${pkg_version}.tar.gz"
-pkg_shasum=2f8d93c217ecca06eb720794c8d1484a67d37bdb58ab761108b5c651d59d3fc6
+pkg_shasum=34293da6266f2aae9690d59c2d764056053ff7eebc56b80b8df05010c3da9343
 pkg_bin_dirs=(bin)
 pkg_build_deps=()
 pkg_deps=(
