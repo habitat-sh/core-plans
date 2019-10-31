57917747a450cad94cc39abb2f03ce49ef9bd6e1
Graham Weldon <graham@grahamweldon.com>
2019-11-01 08:05:32 +0900

    Update terraform to 0.12.13
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- terraform/plan.sh
+++ terraform/plan.sh
@@ -1,13 +1,13 @@
 pkg_name=terraform
 pkg_origin=core
-pkg_version=0.12.12
+pkg_version=0.12.13
 pkg_license=('MPL-2.0')
 pkg_description="Terraform is a tool for building, changing, and combining infrastructure safely and efficiently"
 pkg_upstream_url="http://www.terraform.io/"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="https://releases.hashicorp.com/${pkg_name}/${pkg_version}/${pkg_name}_${pkg_version}_linux_amd64.zip"
 pkg_filename="${pkg_name}_${pkg_version}_linux_amd64.zip"
-pkg_shasum=67bc7a49c0946ad48b14cc6e95482fdd3e7e9f7dc6811f4ce6ff531fc565bd3a
+pkg_shasum=63f765a3f83987b67b046a9c31acff1ec9ee618990d0eab4db34eca6c0d861ec
 pkg_build_deps=(core/unzip)
 pkg_deps=()
 pkg_bin_dirs=(bin)
