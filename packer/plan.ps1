0e738f55ec973c2ef1f4935f8b19fd7fb914199a
Graham Weldon <graham@grahamweldon.com>
2019-11-05 11:02:54 +0900

    Update packer to 1.4.5
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- packer/plan.ps1
+++ packer/plan.ps1
@@ -1,11 +1,11 @@
 $pkg_name="packer"
 $pkg_origin="core"
-$pkg_version="1.4.4"
+$pkg_version="1.4.5"
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_license=@('MPL2')
 $pkg_bin_dirs=@("bin")
 $pkg_source="https://releases.hashicorp.com/packer/${pkg_version}/packer_${pkg_version}_windows_amd64.zip"
-$pkg_shasum="310a98e6ac7d4ee4f306a5f8385628a2e79a2cb8f7d2e894379e3a97ff5ed35b"
+$pkg_shasum="5da1b38beaad735a8b7390865d33e5f60d830fa93e5485f593aae2254dcc4ad8"
 
 function Invoke-Install {
   Copy-Item "$HAB_CACHE_SRC_PATH/$pkg_name-$pkg_version/$pkg_name.exe" $pkg_prefix\bin
