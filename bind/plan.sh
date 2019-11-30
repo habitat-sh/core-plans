3ba26072757bc2627bc0a7d480ebc8491e5a3d0e
Tim Smith <tsmith84@gmail.com>
2019-11-29 22:14:14 -0800

    Update bind to 9.15.6
    I'm not sure how this compiled before since 9.15 requires libuv, but
    I've added that since it fails early if the lib is missing.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- bind/plan.sh
+++ bind/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=bind
 pkg_origin=core
-pkg_version=9.15.4
+pkg_version=9.15.6
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="Versatile, Classic, Complete Name Server Software"
 pkg_upstream_url="https://www.isc.org/downloads/bind/"
 pkg_license=("MPL-2.0")
 pkg_source="https://ftp.isc.org/isc/bind9/${pkg_version}/bind-${pkg_version}.tar.gz"
-pkg_shasum=679288d6c42dc862afc69afbad53caf64c9565328623e710204e9b4b140eecb8
+pkg_shasum=0b0ebe2b4da531cf1f80be9e205213324968da4ac8ee844dbf0dfce65440c04e
 pkg_deps=(
   core/glibc
   core/libxml2
@@ -15,6 +15,7 @@ pkg_deps=(
   core/libcap
   core/busybox-static
   core/python
+  core/libuv
 )
 pkg_build_deps=(
   core/diffutils
