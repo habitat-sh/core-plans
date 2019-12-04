e405bdea2f0d55026111fa4aa12c104e99d84eff
mwrock <matt@mattwrock.com>
2019-12-03 18:02:34 -0800

    bump .net core to v3.1
    Signed-off-by: mwrock <matt@mattwrock.com>

--- dotnet-core-sdk/plan.ps1
+++ dotnet-core-sdk/plan.ps1
@@ -1,14 +1,14 @@
 $pkg_name="dotnet-core-sdk"
 $pkg_origin="core"
-$pkg_version="2.2.401"
+$pkg_version="3.1.100"
 $pkg_license=('MIT')
 $pkg_upstream_url="https://www.microsoft.com/net/core"
 $pkg_description=".NET Core is a blazing fast, lightweight and modular platform
   for creating web applications and services that run on Windows,
   Linux and Mac."
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-$pkg_source="https://download.visualstudio.microsoft.com/download/pr/4de548ef-9b51-4b1f-ae3a-60ebd6a6f2b5/01fce24fe286e7475fdbecc60f1daee5/dotnet-sdk-$pkg_version-win-x64.zip"
-$pkg_shasum="adff0b27798ce6233ee8952ee83b02d9f197214fab77c9ee311b1ce904f52bb7"
+$pkg_source="https://download.visualstudio.microsoft.com/download/pr/28a2c4ff-6154-473b-bd51-c62c76171551/ea47eab2219f323596c039b3b679c3d6/dotnet-sdk-$pkg_version-win-x64.zip"
+$pkg_shasum="abcd034b230365d9454459e271e118a851969d82516b1529ee0bfea07f7aae52"
 $pkg_bin_dirs=@("bin")
 
 function Invoke-Install {
--- dotnet-core-sdk/plan.sh
+++ dotnet-core-sdk/plan.sh
@@ -1,14 +1,14 @@
 pkg_name=dotnet-core-sdk
 pkg_origin=core
-pkg_version=2.2.401
+pkg_version=3.1.100
 pkg_license=('MIT')
 pkg_upstream_url=https://www.microsoft.com/net/core
 pkg_description=".NET Core is a blazing fast, lightweight and modular platform
   for creating web applications and services that run on Windows,
   Linux and Mac."
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_source="https://download.visualstudio.microsoft.com/download/pr/228832ea-805f-45ab-8c88-fa36165701b9/16ce29a06031eeb09058dee94d6f5330/dotnet-sdk-${pkg_version}-linux-x64.tar.gz"
-pkg_shasum=32cb42407b0171742310cefb187fd63f1380841af97f2ee914ec47c3ebed9422
+pkg_source="https://download.visualstudio.microsoft.com/download/pr/d731f991-8e68-4c7c-8ea0-fad5605b077a/49497b5420eecbd905158d86d738af64/dotnet-sdk-${pkg_version}-linux-x64.tar.gz"
+pkg_shasum=3687b2a150cd5fef6d60a4693b4166994f32499c507cd04f346b6dda38ecdc46
 pkg_filename="dotnet-dev-debian-x64.${pkg_version}.tar.gz"
 pkg_deps=(
   core/coreutils
