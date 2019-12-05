b751e5e620db664c463f541a63b5266758ae68c9
Daniel Bright <danielcbright@gmail.com>
2019-12-05 09:39:44 -0500

    updating version schema for proper  YYYY.DD.MM format
    Signed-off-by: Daniel Bright <danielcbright@gmail.com>

--- cacerts/plan.ps1
+++ cacerts/plan.ps1
@@ -4,11 +4,11 @@ $pkg_description="\
 The Mozilla CA certificate store in PEM format (around 250KB uncompressed).
 "
 $pkg_upstream_url="https://curl.haxx.se/docs/caextract.html"
-$pkg_version="2019.28.08"
+$pkg_version="2019.11.27"
 $pkg_license=@('MPL-2.0')
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-$pkg_source="https://curl.haxx.se/ca/cacert-2019-08-28.pem"
-$pkg_shasum="38b6230aa4bee062cd34ee0ff6da173250899642b1937fc130896290b6bd91e3"
+$pkg_source="https://curl.haxx.se/ca/cacert-2019-11-27.pem"
+$pkg_shasum="0d98a1a961aab523c9dc547e315e1d79e887dea575426ff03567e455fc0b66b4"
 function Invoke-Build { }
 
 function Invoke-Unpack {
