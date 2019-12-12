a2e57441f746aa1b57ec1ba8f87caea9afb31860
michael sorens <msorens@chef.io>
2019-12-12 13:44:22 -0800

    Update node to 12.13.1
    At the time of this commit,
    12.13.x brings node into the "active LTS" phase,
    so suitable for production use.
    Reference: https://nodejs.org/en/about/releases/
    
    Signed-off-by: michael sorens <msorens@chef.io>

--- node/plan.sh
+++ node/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=node
 pkg_origin=core
-pkg_version=12.9.0
+pkg_version=12.13.1
 pkg_description="Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine."
 pkg_upstream_url=https://nodejs.org/
 pkg_license=('MIT')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="https://nodejs.org/dist/v${pkg_version}/node-v${pkg_version}.tar.gz"
-pkg_shasum=88cb425086a87323288c8389e974e8c1001b81fe11c529e64592e8feb2d12f93
+pkg_shasum=4ee710087687c8de142329d95085f5cba66e454a2c9ea7ec11e1f4b476d6d1ac
 pkg_deps=(
   core/glibc
   core/gcc-libs
