7dfa919c0da31cbed7a54681b4a6d3b8f93faec7
Salim Afiune <afiune@chef.io>
2019-12-20 16:49:04 +0100

    [go][win] upgrade go to 1.13.5
    Signed-off-by: Salim Afiune <afiune@chef.io>

--- go/plan.ps1
+++ go/plan.ps1
@@ -1,12 +1,12 @@
 $pkg_name="go"
 $pkg_origin="core"
-$pkg_version="1.13.1"
+$pkg_version="1.13.5"
 $pkg_description="Go is an open source programming language that makes it easy to build simple, reliable, and efficient software."
 $pkg_upstream_url="https://golang.org/"
 $pkg_license="BSD"
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_source="https://dl.google.com/go/go$pkg_version.windows-amd64.msi"
-$pkg_shasum="ee6ff4f31766096e6ff00bdd855e007c473cc9080f3588d7be0c5ec0e0b85578"
+$pkg_shasum="eabcf66d6d8f44de21a96a18d957990c62f21d7fadcb308a25c6b58c79ac2b96"
 $pkg_build_deps=@("core/lessmsi")
 $pkg_dirname="go"
 $pkg_bin_dirs=@("bin")
