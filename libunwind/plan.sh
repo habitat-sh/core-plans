d23ad34a0d6bc639a42897086df4b57b13fdc4bf
Graham Weldon <graham@grahamweldon.com>
2020-01-16 09:54:59 +0900

    Update libunwind to 1.2.1
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- libunwind/plan.sh
+++ libunwind/plan.sh
@@ -1,14 +1,23 @@
 pkg_name=libunwind
 pkg_origin=core
-pkg_version=1.1
+pkg_version=1.2.1
 pkg_description="A C programming interface to determine the call-chain of a program."
 pkg_upstream_url="http://www.nongnu.org/libunwind/"
 pkg_license=('MIT')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="http://download.savannah.gnu.org/releases/libunwind/libunwind-${pkg_version}.tar.gz"
-pkg_shasum=9dfe0fcae2a866de9d3942c66995e4b460230446887dbdab302d41a8aee8d09a
-pkg_deps=(core/glibc core/gcc-libs)
-pkg_build_deps=(core/gcc core/make core/diffutils core/file core/coreutils)
+pkg_shasum=3f3ecb90e28cbe53fba7a4a27ccce7aad188d3210bb1964a923a731a27a75acb
+pkg_deps=(
+  core/glibc
+  core/gcc-libs
+)
+pkg_build_deps=(
+  core/gcc
+  core/make
+  core/diffutils
+  core/file
+  core/coreutils
+)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
 
