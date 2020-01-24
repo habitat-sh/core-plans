7a5bd3f8ad9724ac328be394ceaa6dc31b9aea6c
mwrock <matt@mattwrock.com>
2020-01-24 15:14:50 -0800

    fix psscriptanalyzer windows test
    Signed-off-by: mwrock <matt@mattwrock.com>

--- psscriptanalyzer/tests/test.pester.ps1
+++ psscriptanalyzer/tests/test.pester.ps1
@@ -4,10 +4,11 @@ param (
 )
 
 Describe "The PSScriptAnalyzer version" {
+    $origin = $PackageIdentifier.split("/")[0]
     $expectedVersion = $PackageIdentifier.split("/")[2]
 
     It "matches given ident version ${expectedVersion}" {
-        Import-Module "$(hab pkg path core/psscriptanalyzer)/module/PSScriptAnalyzer.psd1"
+        Import-Module "$(hab pkg path $origin/psscriptanalyzer)/module/PSScriptAnalyzer.psd1"
         (Get-Command Invoke-ScriptAnalyzer).Version.ToString() | Should -Be $expectedVersion
     }
 }
