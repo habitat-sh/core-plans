2936443d8ff9a9e7b471f90f54980bfa846a126f
Graham Weldon <graham@grahamweldon.com>
2020-01-29 08:41:42 +0900

    Update maven to 3.6.3
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- maven/plan.sh
+++ maven/plan.sh
@@ -1,12 +1,12 @@
 pkg_origin=core
 pkg_name=maven
-pkg_version=3.6.2
+pkg_version=3.6.3
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="A software project management and comprehension tool"
 pkg_upstream_url="https://maven.apache.org/"
 pkg_license=("Apache-2.0")
 pkg_source="http://apache.cs.utah.edu/maven/maven-3/${pkg_version}/source/apache-maven-${pkg_version}-src.tar.gz"
-pkg_shasum=7ed94655b4098729de292a13c96765bb90a407bb6feb64f201a9444aebde69d1
+pkg_shasum=7c1c990ba64dd4f88688120cc2ec93bf33dd500d2a62ae5cd57bd4b7f6335c07
 pkg_dirname="apache-${pkg_name}-${pkg_version}"
 pkg_deps=(
   core/coreutils
@@ -20,11 +20,11 @@ pkg_lib_dirs=(lib)
 do_prepare() {
   export JAVA_HOME
   JAVA_HOME="$(pkg_path_for corretto8)"
-  build_line "Setting JAVA_HOME=$JAVA_HOME"
+  build_line "Setting JAVA_HOME=${JAVA_HOME}"
 }
 
 do_build() {
-  mvn -DdistributionTargetDir="$pkg_prefix" install
+  mvn -DdistributionTargetDir="${pkg_prefix}" install
 }
 
 do_install() {
