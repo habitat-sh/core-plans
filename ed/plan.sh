c3bb98b3f7a441277908fd680f67e055a2bd31c2
Steven Danna <steve@chef.io>
2020-02-25 00:37:47 +0000

    [ed] Update to 1.16
        ed: hab-plan-build cleanup
        ed:
        ed: Source Path: /hab/cache/src/ed-1.16
        ed: Installed Path: /hab/pkgs/ssd/ed/1.16/20200225003545
        ed: Artifact: /src/results/ssd-ed-1.16-20200225003545-x86_64-linux.hart
        ed: Build Report: /src/results/last_build.env
        ed: SHA256 Checksum: 7b178f8eaea3d26b59bace1f3d12aca2656b31aff9b27bd377bc3f28ec485f7d
        ed: Blake2b Checksum: a726f6a587dfee62078b26157ef21a3c2233aafafbca804b6816f574bf7c204f
        ed:
        ed: I love it when a plan.sh comes together.
        ed:
        ed: Build time: 0m24s
        [3][default:/src:0]# # /hab/pkgs/ssd/ed/1.16/20200225003545/bin/ed --version
        GNU ed 1.16
        Copyright (C) 1994 Andrew L. Moore.
        Copyright (C) 2020 Antonio Diaz Diaz.
        License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
        This is free software: you are free to change and redistribute it.
        There is NO WARRANTY, to the extent permitted by law.
    
    Changes:
    
    * The substitute command no longer complains about an 'Infinite
      substitution loop' with a substitution like 's/^/#/g'. (Reported by
      Bjoern Wibben).
    * The length limit of the prompt string has been removed. (Reported by
      Tim Chase).
    * It has been documented in the manual that extended regular
      expression operators may be unavailable depending on the particular
      regex implementation installed in the system running ed. (Reported
      by Brian Zwahr).
    * Several fixes and improvements have been made to the manual.
    
    Signed-off-by: Steven Danna <steve@chef.io>

--- ed/plan.sh
+++ ed/plan.sh
@@ -1,12 +1,12 @@
-pkg_name=ed
+pkg_name="ed"
 pkg_origin=core
-pkg_version=1.15
+pkg_version=1.16
 pkg_description="The standard text editor."
 pkg_upstream_url="https://www.gnu.org/software/ed/"
 pkg_license=('GPL-3.0')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="https://ftp.gnu.org/gnu/ed/ed-${pkg_version}.tar.lz"
-pkg_shasum=ad4489c0ad7a108c514262da28e6c2a426946fb408a3977ef1ed34308bdfd174
+pkg_shasum=cfc07a14ab048a758473ce222e784fbf031485bcd54a76f74acfee1f390d8b2c
 pkg_deps=(core/glibc)
 pkg_build_deps=(core/gcc core/make core/lzip core/diffutils)
 pkg_bin_dirs=(bin)
