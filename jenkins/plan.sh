96c9887e62b3e8db8334688068ffe653d02e3961
Graham Weldon <graham@grahamweldon.com>
2020-03-18 09:27:18 +0900

    Update jenkins to 2.204.5
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- jenkins/plan.sh
+++ jenkins/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=jenkins
 pkg_origin=core
-pkg_version=2.204.1
+pkg_version=2.204.5
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="The leading open source automation server, Jenkins provides hundreds of plugins to support building, deploying and automating any project."
 pkg_license=('MIT')
 pkg_upstream_url="https://jenkins.io/"
 pkg_source="http://mirrors.jenkins.io/war-stable/${pkg_version}/jenkins.war"
-pkg_shasum=12b9ebbf9eb1cd1deab0d11512511bcd80a5d3a754dffab54dd6385d788d5284
+pkg_shasum=94c73fa5b72e0a4eb52c5c99c08351f85a51d138f3dbaff6f64e4406353f839c
 pkg_deps=(
   core/openjdk11
   core/curl
