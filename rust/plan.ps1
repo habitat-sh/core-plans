5351f3a39c462d51ea5c0e465b638f6ac45c16c9
David McNeil <mcneil.david2@gmail.com>
2020-03-20 13:00:27 -0400

    Update Rust to 1.42.0
    Signed-off-by: David McNeil <mcneil.david2@gmail.com>

--- rust/plan.ps1
+++ rust/plan.ps1
@@ -1,12 +1,12 @@
 $pkg_name="rust"
 $pkg_origin="core"
-$pkg_version="1.41.0"
+$pkg_version="1.42.0"
 $pkg_description="Safe, concurrent, practical language"
 $pkg_upstream_url="https://www.rust-lang.org/"
 $pkg_license=@("Apache-2.0", "MIT")
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_source="https://static.rust-lang.org/dist/rust-$pkg_version-x86_64-pc-windows-msvc.msi"
-$pkg_shasum="ad9a80a19de737244f8dfda61506f520ca9a2f8329986fb1f666034da099470f"
+$pkg_shasum="d14eb12467a2a022de56938164993cb58820c68d6becb312faaf92ca2a68ddec"
 $pkg_deps=@("core/visual-cpp-redist-2015", "core/visual-cpp-build-tools-2015")
 $pkg_build_deps=@("core/lessmsi")
 $pkg_bin_dirs=@("bin")
