2f4348d3f13a8ba9ffa7e671af08cf43bb228eb6
Steven Marshall <SMarshall@chef.io>
2020-04-09 22:59:32 +0100

    Update libgpg-error to 1.37
    Signed-off-by: Steven Marshall <SMarshall@chef.io>

--- libgpg-error/plan.sh
+++ libgpg-error/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=libgpg-error
 pkg_origin=core
-pkg_version=1.36
+pkg_version=1.37
 pkg_license=('GPL-2.0-or-later')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="ftp://ftp.gnupg.org/gcrypt/${pkg_name}/${pkg_name}-${pkg_version}.tar.bz2"
 pkg_upstream_url="https://www.gnupg.org/software/libgpg-error/index.html"
 pkg_description="Libgpg-error is a small library that originally defined common error values for all GnuPG components."
-pkg_shasum=babd98437208c163175c29453f8681094bcaf92968a15cafb1a276076b33c97c
+pkg_shasum=b32d6ff72a73cf79797f7f2d039e95e9c6f92f0c1450215410840ab62aea9763
 pkg_deps=(core/glibc)
 pkg_build_deps=(
   core/gcc
