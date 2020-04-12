2ec59ce1c46b32c636ca853995c86359100c148f
Caius Durling <dev@caius.name>
2020-04-12 10:52:45 +0100

    [ruby27] Upgrade to latest point release
    https://www.ruby-lang.org/en/news/2020/03/31/ruby-2-7-1-released/
    
    Signed-off-by: Caius Durling <dev@caius.name>

--- ruby27/plan.sh
+++ ruby27/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=ruby27
 pkg_origin=core
-pkg_version=2.7.0
+pkg_version=2.7.1
 pkg_description="A dynamic, open source programming language with a focus on \
   simplicity and productivity. It has an elegant syntax that is natural to \
   read and easy to write."
@@ -8,7 +8,7 @@ pkg_license=("Ruby")
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source=https://cache.ruby-lang.org/pub/ruby/2.7/ruby-${pkg_version}.tar.gz
 pkg_upstream_url=https://www.ruby-lang.org/en/
-pkg_shasum=8c99aa93b5e2f1bc8437d1bbbefd27b13e7694025331f77245d0c068ef1f8cbe
+pkg_shasum=d418483bdd0000576c1370571121a6eb24582116db0b7bb2005e90e250eae418
 pkg_deps=(core/glibc core/ncurses core/zlib core/openssl core/libyaml core/libffi core/readline core/nss-myhostname)
 pkg_build_deps=(core/coreutils core/diffutils core/patch core/make core/gcc core/sed)
 pkg_lib_dirs=(lib)
