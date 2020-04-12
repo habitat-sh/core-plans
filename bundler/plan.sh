d4b8297778aadb648ef773dd0058ab21c4df4c92
Caius Durling <dev@caius.name>
2020-04-12 11:10:53 +0100

    [bundler] Upgrade to release 2.1.4
    Major version upgrade from bundler 1.7 to bundler 2.1
    
    Signed-off-by: Caius Durling <dev@caius.name>

--- bundler/plan.sh
+++ bundler/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=bundler
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_version=1.17.3
+pkg_version=2.1.4
 pkg_origin=core
 pkg_license=('bundler')
 pkg_description="The Ruby language dependency manager"
