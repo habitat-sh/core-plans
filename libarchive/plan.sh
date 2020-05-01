7b11711107f59a5441e3cfc00ed2b362761c512d
Graham Weldon <graham@grahamweldon.com>
2020-05-01 11:26:54 +0900

    Update libarchive to 3.4.2
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- libarchive/plan.sh
+++ libarchive/plan.sh
@@ -1,13 +1,13 @@
 pkg_name=libarchive
-_distname=$pkg_name
+_distname="${pkg_name}"
 pkg_origin=core
-pkg_version=3.4.0
+pkg_version=3.4.2
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="Multi-format archive and compression library"
-pkg_upstream_url="https://www.libarchive.org"
+pkg_upstream_url=https://www.libarchive.org
 pkg_license=('BSD')
 pkg_source="http://www.libarchive.org/downloads/${_distname}-${pkg_version}.tar.gz"
-pkg_shasum="8643d50ed40c759f5412a3af4e353cffbce4fdf3b5cf321cb72cacf06b2d825e"
+pkg_shasum=8643d50ed40c759f5412a3af4e353cffbce4fdf3b5cf321cb72cacf06b2d825e
 pkg_dirname="${_distname}-${pkg_version}"
 pkg_deps=(
   core/glibc
@@ -30,7 +30,7 @@ do_build() {
   # R_X86_64_64 symbols all the way down. By default it will make a 32S symbol table
   CFLAGS="$CFLAGS -fPIC" \
     ./configure \
-    --prefix="$pkg_prefix" \
+    --prefix="${pkg_prefix}" \
     --without-xml2 \
     --without-lzo2
   make
