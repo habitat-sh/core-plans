39cfc4641236d962d076e5306c421fa8a281bfe6
Graham Weldon <graham@grahamweldon.com>
2020-05-01 09:53:58 +0900

    Update nginx to 1.18.0
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- nginx/plan.ps1
+++ nginx/plan.ps1
@@ -1,12 +1,12 @@
 $pkg_name="nginx"
 $pkg_origin="core"
-$pkg_version="1.17.8"
+$pkg_version="1.18.0"
 $pkg_description="NGINX web server."
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_license=('BSD-2-Clause')
 $pkg_source="https://nginx.org/download/nginx-$pkg_version.zip"
 $pkg_upstream_url="https://nginx.org/"
-$pkg_shasum="de82e682a147da48e0cd8c4c2a905d69ddb8836cdbbe04d630e69c7a410fdfe1"
+$pkg_shasum="a505a776eaf869b09f15e0e588c18784f036720de8518346668f20f2866a71d5"
 $pkg_bin_dirs=@('bin')
 $pkg_exports=@{port ="http.listen.port"}
 $pkg_exposes=@('port')
