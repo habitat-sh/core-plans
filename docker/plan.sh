421f5e2f6be97fd8f9aa81a4967844b63714c1fe
Dekus <dekusdenial@hotmail.com>
2020-05-04 22:25:05 -0400

    Fix missing binaries and update docker@19.03.8
    Docker has renamed some of its binaries so `docker*` won't work anymore
    
    Signed-off-by: dekusdenial <dekusdenial@hotmail.com>

--- docker/plan.sh
+++ docker/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=docker
 pkg_description="The Docker Engine"
 pkg_origin=core
-pkg_version=19.03.3
+pkg_version=19.03.8
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('Apache-2')
 pkg_source="https://download.docker.com/linux/static/stable/x86_64/${pkg_name}-${pkg_version}.tgz"
 pkg_upstream_url=https://docs.docker.com/engine/installation/binaries/
-pkg_shasum=c3c8833e227b61fe6ce0bc5c17f97fa547035bef4ef17cf6601f30b0f20f4ce5
+pkg_shasum=7f4115dc6a3c19c917f8b9664d7b51c904def1c984e082c4600097433323cf6f
 pkg_dirname=docker
 pkg_bin_dirs=(bin)
 
@@ -15,7 +15,7 @@ do_build() {
 }
 
 do_install() {
-  for bin in docker*; do
+  for bin in *; do
     install -v -D "${bin}" "${pkg_prefix}/bin/${bin}"
   done
 }
