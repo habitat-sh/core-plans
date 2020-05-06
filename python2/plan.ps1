ad42ef1675f9562423ce72621b3052785ec495f2
MindNumbing <SMarshall@chef.io>
2020-05-06 13:47:12 +0100

    Updating python2 windows to 2.7.18
    Signed-off-by: MindNumbing <SMarshall@chef.io>

--- python2/plan.ps1
+++ python2/plan.ps1
@@ -1,12 +1,12 @@
 $pkg_name="python2"
-$pkg_version="2.7.16"
+$pkg_version="2.7.18"
 $pkg_origin="core"
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_license=@('Python-2.0')
 $pkg_description="Python is a programming language that lets you work quickly and integrate systems more effectively."
 $pkg_upstream_url="https://www.python.org"
 $pkg_source="https://www.python.org/ftp/python/$pkg_version/python-$pkg_version.amd64.msi"
-$pkg_shasum="7c0f45993019152d46041a7db4b947b919558fdb7a8f67bcd0535bc98d42b603"
+$pkg_shasum="b74a3afa1e0bf2a6fc566a7b70d15c9bfabba3756fb077797d16fffa27800c05"
 $pkg_build_deps=@("core/lessmsi")
 $pkg_bin_dirs=@("bin", "bin/Scripts")
 
