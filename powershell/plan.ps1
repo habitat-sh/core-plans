5ada841331e9ca87e79b5abb31ed1b2ace238f5e
mwrock <matt@mattwrock.com>
2020-05-22 14:47:51 -0700

    [powershell] bump to 7.0.1
    Signed-off-by: mwrock <matt@mattwrock.com>

--- powershell/plan.ps1
+++ powershell/plan.ps1
@@ -1,12 +1,12 @@
 $pkg_name="powershell"
 $pkg_origin="core"
-$pkg_version="7.0.0"
+$pkg_version="7.0.1"
 $pkg_license=@("MIT")
 $pkg_upstream_url="https://msdn.microsoft.com/powershell"
 $pkg_description="PowerShell is a cross-platform (Windows, Linux, and macOS) automation and configuration tool/framework that works well with your existing tools and is optimized for dealing with structured data (e.g. JSON, CSV, XML, etc.), REST APIs, and object models. It includes a command-line shell, an associated scripting language and a framework for processing cmdlets."
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_source="https://github.com/PowerShell/PowerShell/releases/download/v$pkg_version/PowerShell-$pkg_version-win-x64.zip"
-$pkg_shasum="cda2ca2227fbec2c753aa760859667b02007d14646a831bd908b8f749cbcc687"
+$pkg_shasum="0ce5e2c20c773e54f931adc60c77f599ca90c25b14f53fdbded19572847b45c7"
 $pkg_filename="powershell-$pkg_version-win-x64.zip"
 $pkg_bin_dirs=@("bin")
 
--- powershell/plan.sh
+++ powershell/plan.sh
@@ -1,12 +1,12 @@
 pkg_name="powershell"
 pkg_origin="core"
-pkg_version="7.0.0"
+pkg_version="7.0.1"
 pkg_license=("MIT")
 pkg_upstream_url="https://msdn.microsoft.com/powershell"
 pkg_description="PowerShell is a cross-platform (Windows, Linux, and macOS) automation and configuration tool/framework that works well with your existing tools and is optimized for dealing with structured data (e.g. JSON, CSV, XML, etc.), REST APIs, and object models. It includes a command-line shell, an associated scripting language and a framework for processing cmdlets."
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="https://github.com/PowerShell/PowerShell/releases/download/v$pkg_version/PowerShell-$pkg_version-linux-x64.tar.gz"
-pkg_shasum="3e80a662a5defb283185961330c4a44d5d5179f9eb2a2ee74ac7e1d3caf16b2c"
+pkg_shasum="7843e7b4dbad39fcfb9433e715564fe186baa8bf6f3a4fcdc981dc3160f4bf77"
 pkg_filename="powershell-$pkg_version-linux-x64.tar.gz"
 pkg_bin_dirs=("bin")
 pkg_deps=("core/icu" "core/openssl" "core/glibc" "core/gcc-libs")
