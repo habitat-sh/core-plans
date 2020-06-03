c360940b8b64cfd30f57b423131bb72087461fc0
Yauhen Artsiukhou <jsirex@gmail.com>
2020-06-03 15:03:04 +0300

    Update minio version to 2020-06-01T17-28-03Z
    Signed-off-by: Yauhen Artsiukhou <jsirex@gmail.com>

--- minio/plan.sh
+++ minio/plan.sh
@@ -1,10 +1,10 @@
 pkg_name=minio
 pkg_origin=core
-pkg_version="2019-07-31T18-57-56Z"
+pkg_version="2020-06-01T17-28-03Z"
 pkg_description="Minio is a high performance distributed object storage server, designed for large-scale private cloud infrastructure."
 pkg_upstream_url="https://minio.io"
 pkg_source="https://dl.minio.io/server/minio/release/linux-amd64/archive/minio.RELEASE.${pkg_version}"
-pkg_shasum=78fc08a095bd1985a96ebc727fd3855840dd2c79d6f3bd2b542a940d7b183a42
+pkg_shasum=540a434b605b67913da2d83db6057cac98f303bbbe8f829ab5c6d5b58157c6c7
 pkg_license=('Apache-2.0')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_bin_dirs=(bin)
