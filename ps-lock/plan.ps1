2f6935423223ef2278f25aed2118566a41a2e5ef
mwrock <matt@mattwrock.com>
2020-06-04 14:14:35 -0700

    [ps-lock] use -IsPath when setting PSModulePath so it works in a local studio
    Signed-off-by: mwrock <matt@mattwrock.com>

--- ps-lock/plan.ps1
+++ ps-lock/plan.ps1
@@ -1,12 +1,12 @@
 $pkg_name="ps-lock"
 $pkg_origin="core"
-$pkg_version="0.1.0"
+$pkg_version="0.1.1"
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_license=@("Apache-2.0")
 $pkg_description="Provides synchronization of scripts run in hooks that declare the same lock"
 
 function Invoke-SetupEnvironment {
-    Push-RuntimeEnv "PSModulePath" "\hab\pkgs\$pkg_origin\$pkg_name\$pkg_version\$pkg_release\Modules"
+    Push-RuntimeEnv -IsPath "PSModulePath" "\hab\pkgs\$pkg_origin\$pkg_name\$pkg_version\$pkg_release\Modules"
 }
 
 function Invoke-Install {
