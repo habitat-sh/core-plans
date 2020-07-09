4d6c10a8f569dfc6dfe0e0f445a3feea4455ed1a
Graham Weldon <graham@grahamweldon.com>
2020-07-09 15:36:46 +0900

    Cleanup and standardise
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- asciinema/plan.sh
+++ asciinema/plan.sh
@@ -1,20 +1,15 @@
 pkg_name=asciinema
 pkg_origin=core
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_license=('GPL-3.0-or-later')
+pkg_license=("GPL-3.0-or-later")
 pkg_description="Terminal session recorder"
 pkg_upstream_url=https://github.com/asciinema/asciinema
-pkg_build_deps=(
-  core/gawk
-)
-pkg_deps=(
-  core/python
-)
+pkg_build_deps=(core/gawk)
+pkg_deps=(core/python)
 pkg_bin_dirs=(bin)
-
 pkg_version() {
   export LC_ALL=en_US LANG=en_US
-  pip search --disable-pip-version-check $pkg_name | grep "^$pkg_name (" | awk -F'[()]' '{print $2}'
+  pip search --disable-pip-version-check "${pkg_name}" | grep "^${pkg_name} (" | awk -F'[()]' '{print $2}'
 }
 
 do_before() {
@@ -22,9 +17,9 @@ do_before() {
 }
 
 do_prepare() {
-  python -m venv "$pkg_prefix"
+  python -m venv "${pkg_prefix}"
   # shellcheck source=/dev/null
-  source "$pkg_prefix/bin/activate"
+  source "${pkg_prefix}/bin/activate"
 }
 
 do_build() {
@@ -32,9 +27,9 @@ do_build() {
 }
 
 do_install() {
-  pip install "$pkg_name==$pkg_version"
+  pip install "${pkg_name}==${pkg_version}"
   # Write out versions of all pip packages to package
-  pip freeze > "$pkg_prefix/requirements.txt"
+  pip freeze > "${pkg_prefix}/requirements.txt"
 }
 
 do_strip() {
