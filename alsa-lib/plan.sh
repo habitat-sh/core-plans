a61a2cade63d76cf1cb5bb5e03c6348523692dd9
Graham Weldon <graham@grahamweldon.com>
2020-07-09 11:35:04 +0900

    Cleanup and standardise
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- alsa-lib/plan.sh
+++ alsa-lib/plan.sh
@@ -2,11 +2,11 @@ pkg_name=alsa-lib
 pkg_origin=core
 pkg_version=1.1.9
 pkg_description="The Advanced Linux Sound Architecture (ALSA) provides audio and MIDI functionality to the Linux operating system."
-pkg_upstream_url="http://alsa-project.org/"
+pkg_upstream_url=https://alsa-project.org/
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_license=('LGPL-2.1-or-later')
+pkg_license=("LGPL-2.1-or-later")
 pkg_source="ftp://ftp.alsa-project.org/pub/lib/${pkg_name}-${pkg_version}.tar.bz2"
-pkg_shasum="488373aef5396682f3a411a6d064ae0ad196b9c96269d0bb912fbdeec94b994b"
+pkg_shasum=488373aef5396682f3a411a6d064ae0ad196b9c96269d0bb912fbdeec94b994b
 pkg_deps=(
   core/glibc
 )
@@ -34,7 +34,7 @@ do_check() {
 }
 
 do_end() {
-  if [[ -n "$_clean_file" ]]; then
+  if [[ -n "${_clean_file}" ]]; then
     rm -fv /usr/bin/file
   fi
 }
