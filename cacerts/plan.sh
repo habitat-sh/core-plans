2b15b9a3df584e9dd40249c5c84e9bd4f1333d9e
Daniel Bright <danielcbright@gmail.com>
2020-07-21 19:38:45 -0400

    updating to latest version as this PR has been here a while
    Signed-off-by: Daniel Bright <danielcbright@gmail.com>

--- cacerts/plan.sh
+++ cacerts/plan.sh
@@ -4,11 +4,11 @@ pkg_description="\
 The Mozilla CA certificate store in PEM format (around 250KB uncompressed).
 "
 pkg_upstream_url="https://curl.haxx.se/docs/caextract.html"
-pkg_version="2019.11.27"
+pkg_version="2020.06.24"
 pkg_license=('MPL-2.0')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_source="https://curl.haxx.se/ca/cacert-2019-11-27.pem"
-pkg_shasum="0d98a1a961aab523c9dc547e315e1d79e887dea575426ff03567e455fc0b66b4"
+pkg_source="https://curl.haxx.se/ca/cacert-2020-06-24.pem"
+pkg_shasum="726889705b00f736200ed7999f7a50021b8735d53228d679c4e6665aa3b44987"
 pkg_deps=()
 pkg_build_deps=()
 
