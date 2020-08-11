35bd77f96ad2024b05a87043d8321ca8b3032378
Ryan Cragun <ryan@chef.io>
2020-08-11 16:37:41 -0600

    [go] update to 1.15
    Signed-off-by: Ryan Cragun <ryan@chef.io>

--- go/plan.ps1
+++ go/plan.ps1
@@ -1,12 +1,12 @@
 $pkg_name="go"
 $pkg_origin="core"
-$pkg_version="1.14.5"
+$pkg_version="1.15"
 $pkg_description="Go is an open source programming language that makes it easy to build simple, reliable, and efficient software."
 $pkg_upstream_url="https://golang.org/"
 $pkg_license=@("BSD-3-Clause")
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_source="https://dl.google.com/go/go$pkg_version.windows-amd64.msi"
-$pkg_shasum="f18964960f921e786a9321361af95fd12537881b24c619fbf9381430005ed7cc"
+$pkg_shasum="02944771ca3cffe7bcda85c6ec16eb6dac94e4b1c877cceb610ecab92d4e0a32"
 $pkg_build_deps=@("core/lessmsi")
 $pkg_dirname="go"
 $pkg_bin_dirs=@("bin")
