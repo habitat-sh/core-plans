d00ac1bce29926f47e8cc96399a5f61ed24d8d47
Graham Weldon <graham@grahamweldon.com>
2020-09-02 13:40:33 +0900

    Update HAProxy to 2.2.2
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- haproxy/plan.sh
+++ haproxy/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=haproxy
 pkg_origin=core
 pkg_description="The Reliable, High Performance TCP/HTTP Load Balancer"
-pkg_version=2.1.4
+pkg_version=2.2.2
 pkg_maintainer='The Habitat Maintainers <humans@habitat.sh>'
 pkg_license=('GPL-2.0' 'LGPL-2.1')
-pkg_source="https://www.haproxy.org/download/2.1/src/haproxy-${pkg_version}.tar.gz"
+pkg_source="https://www.haproxy.org/download/2.2/src/haproxy-${pkg_version}.tar.gz"
 pkg_upstream_url="https://www.haproxy.org/"
-pkg_shasum=51030ff696d7067162b4d24d354044293aecfbb36d7acc2f840c8d928bfe91cd
+pkg_shasum=391c705a46c6208a63a67ea842c6600146ca24618531570c89c7915b0c6a54d6
 pkg_svc_run='haproxy -f config/haproxy.conf -db'
 pkg_svc_user=root
 pkg_svc_group=root
