8af62fe391c0dfaa1d53018a3fced2388f3bcd1c
Stephan Renatus <srenatus@chef.io>
2020-09-25 13:28:47 +0200

    [opa] bump 0.23.1 -> 0.23.2
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

--- opa/plan.sh
+++ opa/plan.sh
@@ -1,7 +1,7 @@
 pkg_name=opa
 pkg_description="Open Policy Agent (OPA) is a lightweight general-purpose policy engine that can be co-located with your service."
 pkg_origin=core
-pkg_version="0.23.1"
+pkg_version="0.23.2"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("Apache-2.0")
 pkg_source="https://github.com/open-policy-agent/opa/archive/v${pkg_version}.tar.gz"
@@ -16,7 +16,7 @@ pkg_build_deps=(
     core/git
     core/go
 )
-pkg_shasum="db84bcf9040623a5f7977e7b4d2fa7f15ac61efdbd2e6c13cafe1bf67bdac07a"
+pkg_shasum="0f6ea7ccab5f3adbac77d4f84a400b172497b021cdea4d06d9666f0260fa3fb7"
 
 do_prepare() {
   sed -e "s#\#\!/usr/bin/env bash#\#\!$(pkg_path_for bash)/bin/bash#" -i build/*.sh
