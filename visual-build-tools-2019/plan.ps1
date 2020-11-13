2190c02d348e4a56a68d6c5e46238d98a024e0a0
mwrock <matt@mattwrock.com>
2020-11-13 13:29:54 -0800

    update plan with latest 16.8 tooling
    Signed-off-by: mwrock <matt@mattwrock.com>

--- visual-build-tools-2019/plan.ps1
+++ visual-build-tools-2019/plan.ps1
@@ -1,6 +1,6 @@
 $pkg_name="visual-build-tools-2019"
 $pkg_origin="core"
-$pkg_version="16.7.3"
+$pkg_version="16.8.1"
 $pkg_description="Standalone compiler, libraries and scripts"
 $pkg_upstream_url="https://visualstudio.microsoft.com/downloads/#build-tools-for-visual-studio-2019"
 $pkg_license=@("Microsoft Software License")
@@ -10,24 +10,24 @@ $pkg_shasum="d3ffaa4ec21a06d29b773e544c0c613df65b270ad30c036b623188a8b12dd745"
 $pkg_build_deps=@("core/7zip")
 
 $pkg_bin_dirs=@(
-    "Contents\VC\Tools\MSVC\14.27.29110\bin\HostX64\x64",
-    "Contents\VC\Redist\MSVC\14.27.29016\x64\Microsoft.VC142.CRT",
+    "Contents\VC\Tools\MSVC\14.28.29333\bin\HostX64\x64",
+    "Contents\VC\Redist\MSVC\14.28.29325\x64\Microsoft.VC142.CRT",
     "Contents\MSBuild\Current\Bin\amd64"
 )
 $pkg_lib_dirs=@(
-    "Contents\VC\Tools\MSVC\14.27.29110\atlmfc\lib\x64",
-    "Contents\VC\Tools\MSVC\14.27.29110\lib\x64"
+    "Contents\VC\Tools\MSVC\14.28.29333\atlmfc\lib\x64",
+    "Contents\VC\Tools\MSVC\14.28.29333\lib\x64"
 )
 $pkg_include_dirs=@(
-    "Contents\VC\Tools\MSVC\14.27.29110\atlmfc\include",
-    "Contents\VC\Tools\MSVC\14.27.29110\include"
+    "Contents\VC\Tools\MSVC\14.28.29333\atlmfc\include",
+    "Contents\VC\Tools\MSVC\14.28.29333\include"
 )
 
 function Invoke-SetupEnvironment {
     Set-RuntimeEnv "DisableRegistryUse" "true"
     Set-RuntimeEnv "UseEnv" "true"
-    Set-RuntimeEnv "VCToolsVersion" "14.27.29016"
-    Set-RuntimeEnv -IsPath "VCToolsInstallDir_160" "$pkg_prefix\Contents\VC\Redist\MSVC\14.27.29016"
+    Set-RuntimeEnv "VCToolsVersion" "14.28.29325"
+    Set-RuntimeEnv -IsPath "VCToolsInstallDir_160" "$pkg_prefix\Contents\VC\Redist\MSVC\14.28.29325"
 }
 
 function Invoke-Unpack {
