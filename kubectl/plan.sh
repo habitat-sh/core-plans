0db183606605cde8ae51f773475b86d218945875
Chris Alfano <chris@jarv.us>
2021-02-17 22:00:06 +0000

    [kubectl] update to 1.18.16
    Signed-off-by: Chris Alfano <chris@jarv.us>

--- kubectl/plan.sh
+++ kubectl/plan.sh
@@ -4,9 +4,9 @@ pkg_description="kubectl CLI tool"
 pkg_upstream_url=https://github.com/kubernetes/kubernetes
 pkg_license=('Apache-2.0')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_version=1.18.10
+pkg_version=1.18.16
 pkg_source=https://github.com/kubernetes/kubernetes/archive/v${pkg_version}.tar.gz
-pkg_shasum=d6a23211590873c03f700826135d79fcf3056b644f4f1baa6c49c34d3c7d7667
+pkg_shasum=8c5b0376e330322715ede38b5d8b319cb0115669381431dfdfe4015693bccfd8
 pkg_dirname="kubernetes-${pkg_version}"
 
 pkg_bin_dirs=(bin)
