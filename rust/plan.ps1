44dded3ca8f0a4757779e4bd4db30bbc6d76dd11
Matt Wrock <matt@mattwrock.com>
2021-03-03 18:40:32 -0800

    [rust] bump rust to 1.50.0
    Signed-off-by: Matt Wrock <matt@mattwrock.com>

--- rust/plan.ps1
+++ rust/plan.ps1
@@ -1,12 +1,12 @@
 $pkg_name="rust"
 $pkg_origin="core"
-$pkg_version="1.47.0"
+$pkg_version="1.50.0"
 $pkg_description="Safe, concurrent, practical language"
 $pkg_upstream_url="https://www.rust-lang.org/"
 $pkg_license=@("Apache-2.0", "MIT")
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_source="https://static.rust-lang.org/dist/rust-$pkg_version-x86_64-pc-windows-msvc.msi"
-$pkg_shasum="b586d496c5febb5ecbc944751567aed06b1e7b3704c15ac93708c831950550d4"
+$pkg_shasum="c38eb865683b9fcdd12ba0cb9a3dba746694b0e00083739080381cc909aee5fe"
 $pkg_deps=@("core/visual-cpp-redist-2015", "core/visual-cpp-build-tools-2015")
 $pkg_build_deps=@("core/lessmsi")
 $pkg_bin_dirs=@("bin")
