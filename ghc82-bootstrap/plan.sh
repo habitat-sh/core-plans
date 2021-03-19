e1170857fe4d2c0a48ac347e8821b50fb1e4ae04
Lucas Giancola <lucas.giancola@live.com>
2021-03-19 11:02:44 -0400

    $(git log -1 --pretty=%B) Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- ghc82-bootstrap/plan.sh
+++ ghc82-bootstrap/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=ghc82-bootstrap
 pkg_origin=core
-pkg_version=8.2.1
+pkg_version=8.2.2
 pkg_license=('BSD-3-Clause')
 pkg_upstream_url="https://www.haskell.org/ghc/"
 pkg_description="The Glasgow Haskell Compiler - Binary Bootstrap"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="http://downloads.haskell.org/~ghc/${pkg_version}/ghc-${pkg_version}-x86_64-deb8-linux.tar.xz"
-pkg_shasum="543b81bf610240bd0398111d6c6607a9094dc2d159b564057d46c8a3d1aaa130"
+pkg_shasum="bb8ec3634aa132d09faa270bbd604b82dfa61f04855655af6f9d14a9eedc05fc"
 pkg_dirname="ghc-${pkg_version}"
 
 pkg_bin_dirs=(bin)
