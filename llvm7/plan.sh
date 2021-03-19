0ff94cc354b65fd8035e04943d816eea609ddbd4
Lucas Giancola <lucas.giancola@live.com>
2021-03-19 13:02:54 -0400

    Bumped 'llvm7' version 7.0.1 -> 7.1.0 | Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- llvm7/plan.sh
+++ llvm7/plan.sh
@@ -3,11 +3,11 @@ source "$(dirname "${BASH_SOURCE[0]}")/../llvm/plan.sh"
 
 pkg_name=llvm7
 pkg_origin=core
-pkg_version=7.0.1
+pkg_version=7.1.0
 pkg_license=('NCSA')
 pkg_description="Next-gen compiler infrastructure"
 pkg_upstream_url="http://llvm.org/"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_filename="llvm-${pkg_version}.src.tar.xz"
-pkg_source="http://llvm.org/releases/${pkg_version}/llvm-${pkg_version}.src.tar.xz"
-pkg_shasum="a38dfc4db47102ec79dcc2aa61e93722c5f6f06f0a961073bd84b78fb949419b"
+pkg_source="https://github.com/llvm/llvm-project/releases/download/llvmorg-${pkg_version}/llvm-${pkg_version}.src.tar.xz"
+pkg_shasum="512a9a8e0f7a2cf09dd26c8789582623d84bfa707275cbeecdb26c603d9c314e"
