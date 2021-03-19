895bc63e96c6f716d40b1da98d0d52003bdb50f8
Lucas Giancola <lucas.giancola@live.com>
2021-03-19 12:46:59 -0400

    Bumped 'imagemagick' version 7.0.9-9 -> 7.0.11-3 | Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- imagemagick/plan.sh
+++ imagemagick/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=imagemagick
 pkg_origin=core
-pkg_version=7.0.9-9
+pkg_version=7.0.11-3
 pkg_description="A software suite to create, edit, compose, or convert bitmap images."
 pkg_upstream_url="http://imagemagick.org/"
 pkg_license=('Apache-2.0')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_source="http://www.imagemagick.org/download/releases/ImageMagick-${pkg_version}.tar.xz"
-pkg_shasum=257c9e11480aef95ea98d13495e3beb360d48c26fa8bd3da2d21c61907111d81
+pkg_source="https://github.com/ImageMagick/ImageMagick/archive/refs/tags/${pkg_version}.tar.gz"
+pkg_shasum=2912cfffc03bd20bc32623b3bf4b68cee3d037d705eabbdd3463c36c8e4834a8
 pkg_deps=(
   core/glibc
   core/zlib
