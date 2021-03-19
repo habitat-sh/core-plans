296e15ca6237353b9af55540cefce955415d8ad5
Lucas Giancola <lucas.giancola@live.com>
2021-03-19 13:05:49 -0400

    Bumped 'libtasn1' version "4.13" -> "4.16.0" | Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- libtasn1/plan.sh
+++ libtasn1/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=libtasn1
 pkg_origin=core
-pkg_version="4.13"
+pkg_version="4.16.0"
 pkg_description="ASN.1 implementation"
 pkg_upstream_url="https://www.gnu.org/software/libtasn1/"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('LGPL-2.1-or-later')
 pkg_source="https://ftp.gnu.org/gnu/${pkg_name}/${pkg_name}-${pkg_version}.tar.gz"
-pkg_shasum="7e528e8c317ddd156230c4e31d082cd13e7ddeb7a54824be82632209550c8cca"
+pkg_shasum="0e0fb0903839117cb6e3b56e68222771bebf22ad7fc2295a0ed7d576e8d4329d"
 pkg_deps=(
   core/glibc
 )
