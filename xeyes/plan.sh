b233f6faac79d0b6d8ae0515a3131a015d08b9cf
Lucas Giancola <lucas.giancola@live.com>
2021-03-19 13:31:06 -0400

    Bumped 'xeyes' version 1.1.1 -> 1.1.2 | Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- xeyes/plan.sh
+++ xeyes/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=xeyes
 pkg_origin=core
-pkg_version=1.1.1
+pkg_version=1.1.2
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="xeyes"
 pkg_upstream_url="https://www.x.org/"
 pkg_license=('MIT')
 pkg_source="https://www.x.org/releases/individual/app/${pkg_name}-${pkg_version}.tar.bz2"
-pkg_shasum="975e98680cd59e1f9439016386609546ed08c284d0f05a95276f96aca6e8a521"
+pkg_shasum="4a675b34854da362bd8dff4f21ff92e0c19798b128ea0af24b7fc7c5ac2feea3"
 pkg_deps=(core/glibc
           core/xlib
           core/libxcb
