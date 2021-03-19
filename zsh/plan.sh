82de8c723bbfb046868f479c652f9b3153de030c
Lucas Giancola <lucas.giancola@live.com>
2021-03-19 14:23:11 -0400

    Bumped 'zsh' version 5.6.2 -> 5.8 | Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- zsh/plan.sh
+++ zsh/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=zsh
 pkg_origin=core
-pkg_version=5.6.2
+pkg_version=5.8
 pkg_description="Zsh is a shell designed for interactive use, although it is also a powerful scripting language."
 pkg_upstream_url="http://www.zsh.org"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('zsh')
 pkg_source="https://downloads.sourceforge.net/project/zsh/zsh/${pkg_version}/zsh-${pkg_version}.tar.xz"
-pkg_shasum=a50bd66c0557e8eca3b8fa24e85d0de533e775d7a22df042da90488623752e9e
+pkg_shasum=dcc4b54cc5565670a65581760261c163d720991f0d06486da61f8d839b52de27
 pkg_deps=(
   core/coreutils
   core/gdbm
