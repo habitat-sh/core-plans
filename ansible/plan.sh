e063719e045d6181c35e429c0f61839ae5ec486b
Lucas Giancola <lucas.giancola@live.com>
2021-03-19 13:29:08 -0400

    Bumped 'ansible' version 2.9.7 -> 2.11.0b2 | Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- ansible/plan.sh
+++ ansible/plan.sh
@@ -1,13 +1,13 @@
 pkg_name=ansible
 pkg_origin=core
-pkg_version=2.9.7
+pkg_version=2.11.0b2
 pkg_description="Ansible is a radically simple IT automation platform that makes your applications and systems easier to deploy."
 pkg_upstream_url="https://www.ansible.com/"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("GPL-3.0-only")
 pkg_source="https://github.com/${pkg_name}/${pkg_name}/archive/v${pkg_version}.tar.gz"
 pkg_filename="${pkg_name}-${pkg_version}.tar.gz"
-pkg_shasum=7b65a59b8f651186c1bbc6600970c18cfb7ab229962a2225c0b8fd1a95a770ab
+pkg_shasum=806996cf653f21e28086fffe66e2a0c1ae44dc0e723e4997255addc007632ad6
 pkg_deps=(
   core/libffi
   core/python2
