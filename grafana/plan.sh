d5beb89c5788294b1ee5dfa1c124ba8e90fdfdae
Lucas Giancola <lucas.giancola@live.com>
2021-03-19 14:16:09 -0400

    Bumped 'grafana' version 7.0.1 -> 7.4.5 | Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- grafana/plan.sh
+++ grafana/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=grafana
 pkg_origin=core
-pkg_version=7.0.1
+pkg_version=7.4.5
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("Apache-2.0")
 pkg_upstream_url=https://grafana.com/
 pkg_source="https://dl.grafana.com/oss/release/${pkg_name}-${pkg_version}.linux-amd64.tar.gz"
 pkg_filename="${pkg_name}-${pkg_version}.linux-amd64.tar.gz"
-pkg_shasum=eb2ad26dd832515cfac9c4a76096a1a656eee9794c874669f552810280ef046f
+pkg_shasum=9fff20effd682f3212e1a8ee21f46cbe535ab7a2f819cb4c36969dc1f9e832f5
 pkg_deps=(
   core/glibc
   core/bash
