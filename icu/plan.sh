41862aeeb4ea0691359a64e2f843fca1e2a2a229
Lucas Giancola <lucas.giancola@live.com>
2021-03-19 13:13:37 -0400

    Bumped 'icu' version 57.1 -> 57.2 | Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- icu/plan.sh
+++ icu/plan.sh
@@ -1,6 +1,6 @@
 pkg_origin=core
 pkg_name=icu
-pkg_version=57.1
+pkg_version=57.2
 pkg_description="$(cat << EOF
   ICU is a mature, widely used set of C/C++ and Java libraries providing
   Unicode and Globalization support for software applications. ICU is widely
@@ -12,7 +12,7 @@ pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("Unicode-TOU")
 # shellcheck disable=SC2059
 pkg_source="https://github.com/unicode-org/icu/releases/download/release-${pkg_version//./-}/icu4c-${pkg_version//./_}-src.tgz"
-pkg_shasum=ff8c67cb65949b1e7808f2359f2b80f722697048e90e7cfc382ec1fe229e9581
+pkg_shasum=d67a16e4b97173bab0e58a8b4a010c2a149fce20e0735d9c351570262a7c8172
 pkg_upstream_url="http://site.icu-project.org/"
 pkg_deps=(core/glibc core/gcc-libs)
 pkg_build_deps=(core/gcc core/make)
