e8c5b89f12e6ba2a1fb417b80f89a7dd30d1190c
Lucas Giancola <lucas.giancola@live.com>
2021-03-19 10:47:12 -0400

    Bumped 'opa' version "0.23.1" -> "0.27.1" | Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- opa/plan.sh
+++ opa/plan.sh
@@ -1,7 +1,7 @@
 pkg_name=opa
 pkg_description="Open Policy Agent (OPA) is a lightweight general-purpose policy engine that can be co-located with your service."
 pkg_origin=core
-pkg_version="0.23.1"
+pkg_version="0.27.1"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("Apache-2.0")
 pkg_source="https://github.com/open-policy-agent/opa/archive/v${pkg_version}.tar.gz"
@@ -16,7 +16,7 @@ pkg_build_deps=(
     core/git
     core/go
 )
-pkg_shasum="db84bcf9040623a5f7977e7b4d2fa7f15ac61efdbd2e6c13cafe1bf67bdac07a"
+pkg_shasum="daaa9f81fc12a70a980fa09a2b944d1059bbfa3ba052948cf71f5880027a9ad9"
 
 do_prepare() {
   sed -e "s#\#\!/usr/bin/env bash#\#\!$(pkg_path_for bash)/bin/bash#" -i build/*.sh
