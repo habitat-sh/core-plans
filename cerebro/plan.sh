027cb8a24ab5ae907f08b25c6440a9e9a1e454d2
Lucas Giancola <lucas.giancola@live.com>
2021-03-19 13:47:36 -0400

    Bumped 'cerebro' version 0.9.0 -> 0.9.3 | Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- cerebro/plan.sh
+++ cerebro/plan.sh
@@ -1,13 +1,13 @@
 pkg_name=cerebro
 pkg_origin=core
-pkg_version=0.9.0
+pkg_version=0.9.3
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="Cerebro: Elasticsearch Administration"
 pkg_upstream_url="https://github.com/lmenezes/cerebro"
 pkg_license=("Apache-2.0")
 pkg_filename="${pkg_name}-${pkg_version}.tgz"
 pkg_source="https://github.com/lmenezes/cerebro/releases/download/v${pkg_version}/${pkg_filename}"
-pkg_shasum=44f7d1b7c990571b483771ed8056eccbff37b8381aa3d148497e8a12161b4b91
+pkg_shasum=0c96d4bfd701b012eefb2464c11b25421c5da3f31ea9667da8fa31f8c8b36d35
 pkg_deps=(
 	core/coreutils
 	core/openjdk11
