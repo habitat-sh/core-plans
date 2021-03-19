26e221472ba89c5dadd0a81fafe3a49ad0e8afe2
Lucas Giancola <lucas.giancola@live.com>
2021-03-19 14:01:30 -0400

    Bumped 'libfastjson' version 0.99.8 -> 0.99.9 | Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- libfastjson/plan.sh
+++ libfastjson/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=libfastjson
 pkg_origin=core
-pkg_version=0.99.8
+pkg_version=0.99.9
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("MIT")
 pkg_description="A fast JSON library for C."
 pkg_upstream_url="https://github.com/rsyslog/libfastjson"
 pkg_source="https://github.com/rsyslog/libfastjson/archive/v${pkg_version}.tar.gz"
-pkg_shasum=7e49057b26a5a9e3c6623e024f95f9fd9a14b571b9150aeb89d6d475fc3633e3
+pkg_shasum=6e6e3702589ad229cb53547af0fecdd43ea9ac883ff09992c14b7ce2d2b27830
 pkg_deps=(core/glibc)
 pkg_build_deps=(
   core/make
