37f2408d6decc200b4575945d3e880f5ce7017da
rybons <bonnerrk@gmail.com>
2021-03-22 15:06:34 -0400

    Bumped 'relx' version 3.29.0 -> 3.33.0 | Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- relx/plan.sh
+++ relx/plan.sh
@@ -1,12 +1,12 @@
 pkg_origin=core
 pkg_name=relx
-pkg_version=3.29.0
+pkg_version=3.33.0
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="A release assembler for Erlang."
 pkg_license=(Apache-2.0)
 pkg_source=https://github.com/erlware/${pkg_name}/archive/v${pkg_version}.tar.gz
 pkg_upstream_url="https://github.com/erlware/relx"
-pkg_shasum=d64eca52e17ba3c6ffde695900054eb5ad8e679fd1f941981d85711664b0305f
+pkg_shasum=0f43b6cd1e3941634e75eb7b6241e1d3b18ccad4c2f8652475f3a388c24cfd87
 pkg_deps=(core/erlang core/coreutils)
 pkg_build_deps=(core/rebar3)
 pkg_bin_dirs=(bin)
