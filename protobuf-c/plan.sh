08f7452f85b4dbfd28098b4a93722130f93fc36c
rybons <bonnerrk@gmail.com>
2021-03-22 15:17:26 -0400

    Bumped 'protobuf-c' version 1.3.2 -> 1.3.3 | Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- protobuf-c/plan.sh
+++ protobuf-c/plan.sh
@@ -1,12 +1,12 @@
 pkg_origin=core
 pkg_name=protobuf-c
-pkg_version=1.3.2
+pkg_version=1.3.3
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('BSD-2-Clause')
 pkg_source="https://github.com/protobuf-c/protobuf-c/releases/download/v${pkg_version}/protobuf-c-${pkg_version}.tar.gz"
 pkg_upstream_url=https://github.com/protobuf-c/protobuf-c
 pkg_description="Protocol Buffers implementation in C"
-pkg_shasum=53f251f14c597bdb087aecf0b63630f434d73f5a10fc1ac545073597535b9e74
+pkg_shasum=03325278aca4c1781ed7c48c0e94320d3e57e8dcda54873a0bdaa463a091a1c1
 pkg_deps=(
   core/gcc-libs
   core/protobuf-cpp
