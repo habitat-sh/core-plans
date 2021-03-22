bc5af425e18576a48fbee079db1583a9bd326647
rybons <bonnerrk@gmail.com>
2021-03-22 15:01:23 -0400

    Bumped 'wrk' version 4.0.2 -> 4.1.0 | Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- wrk/plan.sh
+++ wrk/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=wrk
 pkg_origin=core
-pkg_version=4.0.2
+pkg_version=4.1.0
 pkg_license=(Apache-2.0)
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="Modern HTTP benchmarking tool"
 pkg_upstream_url=https://github.com/wg/${pkg_name}
 pkg_source=https://github.com/wg/${pkg_name}/archive/${pkg_version}.tar.gz
-pkg_shasum=a4a6ad6727733023771163e7250189a9a23e6253b5e5025191baa6092d5a26fb
+pkg_shasum=5c064c292b50f715206e1cd05cce97c2e605fd15348e70e43067bb2ac7c93f06
 pkg_deps=(core/openssl core/gcc core/zlib)
 pkg_build_deps=(core/make)
 pkg_bin_dirs=(bin)
