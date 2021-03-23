5e9a9964bb1cdafdd0c7abd90eedabb02baeb5dc
seajamied <jdegnan@chef.io>
2021-03-23 13:50:01 -0700

    Add node 14
    Signed-off-by: seajamied <jdegnan@chef.io>

--- node/plan.ps1
+++ node/plan.ps1
@@ -1,12 +1,12 @@
 $pkg_name="node"
 $pkg_origin="core"
-$pkg_version="12.22.1"
+$pkg_version="14.16.0"
 $pkg_description="Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine."
 $pkg_upstream_url="https://nodejs.org/"
 $pkg_license=@("MIT")
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_source="https://nodejs.org/dist/v$pkg_version/node-v$pkg_version-x64.msi"
-$pkg_shasum="02f53b3530a0310b1076db801770803527c63f724b56c22d6a0625c12a03c982"
+$pkg_shasum="d9243c9d02f5e4801b8b3ab848f45ce0da2882b5fff448191548ca49af434066"
 $pkg_build_deps=@("core/lessmsi")
 $pkg_bin_dirs=@("bin")
 
