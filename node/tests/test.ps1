9ed37bfc7e67289ceb9d9a8a81e9f9950df3501a
Matt Wrock <matt@mattwrock.com>
2021-03-24 10:22:18 -0700

    fix node pester tests
    Signed-off-by: Matt Wrock <matt@mattwrock.com>

--- node/tests/test.ps1
+++ node/tests/test.ps1
@@ -3,10 +3,8 @@ param (
     [string]$PackageIdentifier = $(throw "Usage: test.ps1 [test_pgk_ident] e.g. test.ps1 core/7zip/16.04/20190513101258")
 )
 
-if (-Not (Get-Module -ListAvailable -Name Pester)) {
-    hab pkg install core/pester
-    Import-Module "$(hab pkg path core/pester)\module\pester.psd1"
-}
+hab pkg install core/pester
+Import-Module "$(hab pkg path core/pester)\module\pester.psd1" -Force
 
 hab pkg install $PackageIdentifier
 
