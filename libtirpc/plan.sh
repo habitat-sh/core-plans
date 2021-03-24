041f09420ee2ad65c531d43957b6ab25d00b58d2
Lucas Giancola <lucas.giancola@live.com>
2021-03-24 11:48:15 -0400

    Bumped 'libtirpc' version "1.0.3" -> "1.3.1" | Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- libtirpc/plan.sh
+++ libtirpc/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=libtirpc
 pkg_origin=core
-pkg_version="1.0.3"
+pkg_version="1.3.1"
 pkg_description="Libtirpc is a port of Suns Transport-Independent RPC library to Linux."
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("BSD-3-Clause")
 pkg_upstream_url="http://libtirpc.sourceforge.net/"
 pkg_source="https://netix.dl.sourceforge.net/project/libtirpc/libtirpc/${pkg_version}/libtirpc-${pkg_version}.tar.bz2"
-pkg_shasum="86c3a78fc1bddefa96111dd233124c703b22a78884203c55c3e06b3be6a0fd5e"
+pkg_shasum="245895caf066bec5e3d4375942c8cb4366adad184c29c618d97f724ea309ee17"
 pkg_deps=(
   core/glibc
   core/krb5
