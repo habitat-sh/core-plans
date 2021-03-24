d91a41a6ae24cf9dc4d3ec0cf6ea74133162494a
Lucas Giancola <lucas.giancola@live.com>
2021-03-24 10:48:01 -0400

    Bumped 'ffmpeg3' version 3.4.6 -> 3.4.8 | Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- ffmpeg3/plan.sh
+++ ffmpeg3/plan.sh
@@ -2,9 +2,9 @@ source "$(dirname "${BASH_SOURCE[0]}")/../ffmpeg/plan.sh"
 
 pkg_name=ffmpeg3
 pkg_origin=core
-pkg_version=3.4.6
+pkg_version=3.4.8
 pkg_source=https://ffmpeg.org/releases/ffmpeg-${pkg_version}.tar.gz
-pkg_shasum=982cf1c409fab413648561c8d78167c7a75fe47b429dc7b5d5e956b19245040d
+pkg_shasum=839af372d8e52b1998aa67a433615e7607519107367af89a2714fa56f53d3d70
 pkg_description="A complete, cross-platform solution to record, convert and \
 stream audio and video."
 pkg_upstream_url=https://ffmpeg.org/
