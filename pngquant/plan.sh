7a8ff8f04c950d94b305de82b956c48b8337d07c
Lucas Giancola <lucas.giancola@live.com>
2021-03-24 11:02:19 -0400

    Bumped 'pngquant' version 2.12.5 -> 2.14.1 | Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- pngquant/plan.sh
+++ pngquant/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=pngquant
 pkg_origin=core
-pkg_version=2.12.5
+pkg_version=2.14.1
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("GPL-3.0-only")
 pkg_description="Lossy PNG compressor"
 pkg_upstream_url="https://pngquant.org"
 pkg_source="https://github.com/kornelski/${pkg_name}/archive/${pkg_version}.tar.gz"
-pkg_shasum=9d2c5197b21c42931623fb3e6064b91c133bfb52c84428ee1bf9b84712c9b83c
+pkg_shasum=e695e8e8d6ce2265c7eb5f23f7ac38aeeae822671dda20723ee41379a7b8c184
 pkg_deps=(
   core/coreutils
   core/libpng
