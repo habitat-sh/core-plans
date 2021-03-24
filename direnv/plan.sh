177590d9064f13c8a8f575737f683aef794226c4
Lucas Giancola <lucas.giancola@live.com>
2021-03-24 13:59:32 -0400

    Bumped 'direnv' version "2.19.1" -> "2.28.0" | Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- direnv/plan.sh
+++ direnv/plan.sh
@@ -1,11 +1,11 @@
 pkg_name=direnv
 pkg_origin=core
-pkg_version="2.19.1"
+pkg_version="2.28.0"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('MIT')
 pkg_source="https://github.com/${pkg_name}/${pkg_name}/releases/download/v${pkg_version}/${pkg_name}.linux-amd64"
 pkg_filename="${pkg_name}.linux-amd64"
-pkg_shasum="3341afc4a2255a680e446230f9db5cee66f2f208f3c43cb251ba908957fe7636"
+pkg_shasum="6131c0c0eb6a744abad96c176bb311a20c625a5a18b1e02459b1845652f241b7"
 pkg_bin_dirs=(bin)
 pkg_description="direnv is an environment switcher for the shell."
 pkg_upstream_url="https://direnv.net/"
