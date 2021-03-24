e1703577046596af9b36c7d321bdc6bef89c5d36
Lucas Giancola <lucas.giancola@live.com>
2021-03-24 11:06:02 -0400

    Bumped 'fd' version 7.3.0 -> 7.5.0 | Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- fd/plan.sh
+++ fd/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=fd
 pkg_origin=core
-pkg_version=7.3.0
+pkg_version=7.5.0
 pkg_license=('MIT' 'Apache-2.0')
 pkg_bin_dirs=(bin)
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_upstream_url="https://github.com/sharkdp/fd"
 pkg_source="https://github.com/sharkdp/fd/archive/v${pkg_version}.tar.gz"
-pkg_shasum="fbd48cc83c90a0ab09fc3bbe865708a3a528876a99f8304a17d07af7fb378170"
+pkg_shasum="0bf6599a206501e823a5d2aeff147c5f3cc8386087c776bca17d6dc72ece0084"
 pkg_filename="${pkg_name}-${pkg_version}.tar.gz"
 pkg_description="fd is a simple, fast and user-friendly alternative to find"
 pkg_deps=(
