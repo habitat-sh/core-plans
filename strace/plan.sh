31abf0baa9c7d516b94c8e0bb3295c61b5ddc4fe
rybons <bonnerrk@gmail.com>
2021-03-25 17:02:02 -0400

    Bumped 'strace' version 5.7 -> 5.11
    Signed-off-by: rybons <bonnerrk@gmail.com>

--- strace/plan.sh
+++ strace/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=strace
 pkg_origin=core
-pkg_version=5.7
+pkg_version=5.11
 pkg_license=("LGPL-2.1-or-later")
 pkg_description="strace is a system call tracer for Linux"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_upstream_url=https://strace.io/
 pkg_source="https://github.com/strace/strace/releases/download/v${pkg_version}/${pkg_name}-${pkg_version}.tar.xz"
-pkg_shasum=b284b59f9bcd95b9728cea5bd5c0edc5ebe360af73dc76fbf6334f11c777ccd8
+pkg_shasum=ffe340b10c145a0f85734271e9cce56457d23f21a7ea5931ab32f8cf4e793879
 pkg_deps=(
   core/glibc
   core/libunwind
