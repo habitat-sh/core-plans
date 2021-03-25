61514cb3c40ade58a1c4b0c6458590a0b88fe141
rybons <bonnerrk@gmail.com>
2021-03-25 11:48:43 -0400

    Bumped 'rkt' version 1.29.0 -> 1.30.0
    Signed-off-by: rybons <bonnerrk@gmail.com>

--- rkt/plan.sh
+++ rkt/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=rkt
 pkg_description="rkt - the pod native container engine"
 pkg_origin=core
-pkg_version=1.29.0
+pkg_version=1.30.0
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('Apache-2')
 pkg_source=https://github.com/rkt/rkt/releases/download/v${pkg_version}/rkt-v${pkg_version}.tar.gz
 pkg_upstream_url=https://github.com/rkt/rkt/releases
-pkg_shasum=4fdd3d46aa58a9e7a7d1c36a7cb390ea96fd297b694295eb79b8c185dbc73e7e
+pkg_shasum=ad72e3bba601b237296c58b70e650d39f34b38e7fa842374fb37e30ae0991cd9
 pkg_dirname=rkt-v${pkg_version}
 pkg_deps=(core/gnupg core/glibc)
 pkg_build_deps=(core/patchelf)
