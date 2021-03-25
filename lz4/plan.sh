f986fd18036b10f21d810a81e59e04c792be2e27
rybons <bonnerrk@gmail.com>
2021-03-25 11:14:33 -0400

    Bumped 'lz4' version 1.9.2 -> 1.9.3
    Signed-off-by: rybons <bonnerrk@gmail.com>

--- lz4/plan.sh
+++ lz4/plan.sh
@@ -1,12 +1,12 @@
 pkg_origin=core
 pkg_name=lz4
-pkg_version=1.9.2
+pkg_version=1.9.3
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('BSD-2 Clause' 'GPL-2.0')
 pkg_upstream_url=http://lz4.github.io/lz4/
 pkg_description="Extremely Fast Compression algorithm http://www.lz4.org"
 pkg_source="https://github.com/${pkg_name}/${pkg_name}/archive/v${pkg_version}.tar.gz"
-pkg_shasum=658ba6191fa44c92280d4aa2c271b0f4fbc0e34d249578dd05e50e76d0e5efcc
+pkg_shasum=030644df4611007ff7dc962d981f390361e6c97a34e5cbc393ddfbe019ffe2c1
 pkg_deps=(core/glibc)
 pkg_build_deps=(
   core/gcc
