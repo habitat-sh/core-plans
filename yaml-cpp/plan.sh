f4bd7cc454508a49e381a869a97dba6d5e62a3b1
rybons <bonnerrk@gmail.com>
2021-03-25 09:31:44 -0400

    Bumped 'yaml-cpp' version 0.6.2 -> 0.6.3
    Signed-off-by: rybons <bonnerrk@gmail.com>

--- yaml-cpp/plan.sh
+++ yaml-cpp/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=yaml-cpp
 pkg_origin=core
-pkg_version=0.6.2
+pkg_version=0.6.3
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('MIT')
 pkg_description="A YAML parser and emitter in C++"
 pkg_upstream_url="https://github.com/jbeder/yaml-cpp"
-pkg_source="https://github.com/jbeder/yaml-cpp/archive/yaml-cpp-0.6.2.tar.gz"
-pkg_shasum="e4d8560e163c3d875fd5d9e5542b5fd5bec810febdcba61481fe5fc4e6b1fd05"
+pkg_source="https://github.com/jbeder/yaml-cpp/archive/yaml-cpp-${pkg_version}.tar.gz"
+pkg_shasum="7e4325fb4a539e0fdc25da54d14c35d9ad5c525f3ad59308582f4d7fbf720680"
 pkg_dirname="yaml-cpp-yaml-cpp-${pkg_version}"
 pkg_deps=(
   core/gcc-libs
