b9e81172b613d149ec151e0881b65afd28a58d9b
rybons <bonnerrk@gmail.com>
2021-03-25 10:18:47 -0400

    Bumped 'ripgrep' version "11.0.1" -> "11.0.2"
    Signed-off-by: rybons <bonnerrk@gmail.com>

--- ripgrep/plan.sh
+++ ripgrep/plan.sh
@@ -1,11 +1,11 @@
 pkg_name=ripgrep
 pkg_origin=core
-pkg_version="11.0.1"
+pkg_version="11.0.2"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('MIT' 'Unlicense')
 pkg_source="https://github.com/BurntSushi/${pkg_name}/releases/download/${pkg_version}/${pkg_name}-${pkg_version}-x86_64-unknown-linux-musl.tar.gz"
 pkg_dirname="${pkg_name}-${pkg_version}-x86_64-unknown-linux-musl"
-pkg_shasum="ce74cabac9b39b1ad55837ec01e2c670fa7e965772ac2647b209e31ead19008c"
+pkg_shasum="c5b2c5cd04d69da91b8af6772f0c9710c7fdbba88237149f5ddd672efc920879"
 pkg_bin_dirs=(bin)
 pkg_description="ripgrep combines the usability of The Silver Searcher with the raw speed of grep."
 pkg_upstream_url="https://github.com/BurntSushi/ripgrep"
