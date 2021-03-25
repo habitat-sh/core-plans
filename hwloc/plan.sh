7435e6a8f84de25e7f73669ef83c8ceb12f43d48
Lucas Giancola <lucas.giancola@live.com>
2021-03-25 11:52:10 -0400

    Bumped version 2.0.1 -> 2.4.1
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- hwloc/plan.sh
+++ hwloc/plan.sh
@@ -1,13 +1,13 @@
 pkg_name=hwloc
 pkg_origin=core
-pkg_major_minor=2.0
+pkg_major_minor=2.4
 pkg_version="${pkg_major_minor}.1"
 pkg_description="Portable Hardware Locality"
 pkg_upstream_url=https://www.open-mpi.org/software/hwloc
 pkg_license=('BSD-2-Clause')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="https://www.open-mpi.org/software/hwloc/v${pkg_major_minor}/downloads/hwloc-${pkg_version}.tar.gz"
-pkg_shasum=f1156df22fc2365a31a3dc5f752c53aad49e34a5e22d75ed231cd97eaa437f9d
+pkg_shasum=392421e69f26120c8ab95d151fe989f2b4b69dab3c7735741c4e0a6d7de5de63
 pkg_deps=(core/glibc core/gcc-libs)
 pkg_build_deps=(core/gcc core/make core/diffutils)
 pkg_bin_dirs=(bin sbin)
