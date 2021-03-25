86529ba7d60fcb632e29f09c80400a952a2fbc85
rybons <bonnerrk@gmail.com>
2021-03-25 16:56:52 -0400

    Bumped 'fping' version "4.2" -> "4.4"
    Signed-off-by: rybons <bonnerrk@gmail.com>

--- fping/plan.sh
+++ fping/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=fping
 pkg_origin=core
-pkg_version="4.2"
+pkg_version="4.4"
 pkg_description="$(cat << EOF
   fping is a program to send ICMP echo probes to network hosts, similar to
   ping, but much better performing when pinging multiple hosts. fping has a
@@ -11,7 +11,7 @@ EOF
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('custom')
 pkg_source="https://github.com/schweikert/${pkg_name}/archive/v${pkg_version}.tar.gz"
-pkg_shasum="49b0ac77fd67c1ed45c9587ffab0737a3bebcfa5968174329f418732dbf655d4"
+pkg_shasum="9f854b65a52dc7b1749d6743e35d0a6268179d1a724267339fc9a066b2b72d11"
 pkg_upstream_url="http://fping.org/"
 pkg_build_deps=(
   core/autoconf
