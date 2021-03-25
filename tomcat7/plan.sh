00cf46c1f6add02f335a13dc871be71a062dda9f
rybons <bonnerrk@gmail.com>
2021-03-25 11:47:09 -0400

    Bumped 'tomcat7' version 7.0.73 -> 7.0.108
    Signed-off-by: rybons <bonnerrk@gmail.com>

--- tomcat7/plan.sh
+++ tomcat7/plan.sh
@@ -1,13 +1,13 @@
 pkg_name=tomcat7
 pkg_description="An open source implementation of the Java Servlet, JavaServer Pages, Java Expression Language and Java WebSocket technologies."
 pkg_origin=core
-pkg_version=7.0.73
+pkg_version=7.0.108
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('Apache-2.0')
 pkg_description="The Apache Tomcat software is an open source implementation of the Java Servlet, JavaServer Pages, Java Expression Language and Java WebSocket technologies."
 pkg_upstream_url="http://tomcat.apache.org/"
 pkg_source=http://archive.apache.org/dist/tomcat/tomcat-7/v${pkg_version}/bin/apache-tomcat-${pkg_version}.tar.gz
-pkg_shasum=0e814d6ad7d5b90e29c79887137420d3bc413540f9faa60d98f11e6c8a8fea85
+pkg_shasum=6dbf37fe4362cf1bc68959ad83ffea8daddd49c89ea273238765778eb229aa6a
 pkg_deps=(core/coreutils)
 pkg_exports=(
   [port]=server.port
