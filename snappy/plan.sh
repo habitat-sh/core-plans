01687d4d53e6aaf5f3f608f7d360b1ad68e7bdb0
rybons <bonnerrk@gmail.com>
2021-03-25 13:24:13 -0400

    Bumped 'snappy' version 1.1.4 -> 1.1.8
    Signed-off-by: rybons <bonnerrk@gmail.com>

--- snappy/plan.sh
+++ snappy/plan.sh
@@ -1,10 +1,10 @@
 pkg_origin=core
 pkg_name=snappy
-pkg_version=1.1.4
+pkg_version=1.1.8
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('BSD-3-Clause')
-pkg_source=https://github.com/google/snappy/releases/download/${pkg_version}/snappy-${pkg_version}.tar.gz
-pkg_shasum=134bfe122fd25599bb807bb8130e7ba6d9bdb851e0b16efcb83ac4f5d0b70057
+pkg_source=https://github.com/google/snappy/archive/${pkg_version}.tar.gz
+pkg_shasum=16b677f07832a612b0836178db7f374e414f94657c138e6993cbfc5dcc58651f
 pkg_deps=(core/glibc)
 pkg_build_deps=(core/gcc core/make core/autoconf core/automake core/pkg-config core/libtool core/m4 core/sed core/pkg-config)
 pkg_bin_dirs=(bin)
