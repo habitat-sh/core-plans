4f1c4085a8d728ec35f35dc10fd2b6a5db84edfd
Lucas Giancola <lucas.giancola@live.com>
2021-03-25 15:47:00 -0400

    Bumped 'sccache' version 0.2.13 -> 0.2.15
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- sccache/plan.sh
+++ sccache/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=sccache
 pkg_origin=core
-pkg_version=0.2.13
+pkg_version=0.2.15
 pkg_license=('Apache-2.0')
 pkg_upstream_url="https://github.com/mozilla/sccache"
 pkg_description="sccache is ccache with cloud storage"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="https://github.com/mozilla/sccache/archive/${pkg_version}.tar.gz"
-pkg_shasum=81c973cf9a89e77f02a6b5710298531ba2e50d2555e8a931e505fbf570522e2a
+pkg_shasum=e3ddfa85233106c98b4382b1e4272c6843065418a4b89a237e8b1455857e68fe
 pkg_bin_dirs=(bin)
 pkg_deps=(
   core/glibc
