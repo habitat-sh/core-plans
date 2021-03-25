33de91a487500c92d126d66e7e58ae999e277279
rybons <bonnerrk@gmail.com>
2021-03-25 13:31:35 -0400

    Bumped 'libtalloc' version "2.1.14" -> "2.3.2"
    Signed-off-by: rybons <bonnerrk@gmail.com>

--- libtalloc/plan.sh
+++ libtalloc/plan.sh
@@ -1,13 +1,13 @@
 pkg_name=libtalloc
 pkg_origin=core
-pkg_version="2.1.14"
+pkg_version="2.3.2"
 pkg_upstream_url=https://talloc.samba.org
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="Talloc is a hierarchical, reference counted memory pool system with destructors. It is the core memory allocator used in Samba."
 pkg_license=("GPL-3.0")
 pkg_source="https://www.samba.org/ftp/talloc/talloc-${pkg_version}.tar.gz"
 pkg_dirname="talloc-${pkg_version}"
-pkg_shasum="b185602756a628bac507fa8af8b9df92ace69d27c0add5dab93190ad7c3367ce"
+pkg_shasum="27a03ef99e384d779124df755deb229cd1761f945eca6d200e8cfd9bf5297bd7"
 pkg_lib_dirs=(lib)
 pkg_include_dirs=(include)
 pkg_pconfig_dirs=(lib/pkgconfig)
