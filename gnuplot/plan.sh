06ac7fb19ec28a3e84154429a923c9ba1b185ed6
rybons <bonnerrk@gmail.com>
2021-03-25 17:01:07 -0400

    Bumped 'gnuplot' version "5.2.6" -> "5.4.1"
    Signed-off-by: rybons <bonnerrk@gmail.com>

--- gnuplot/plan.sh
+++ gnuplot/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=gnuplot
 pkg_origin=core
-pkg_version="5.2.6"
+pkg_version="5.4.1"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_upstream_url="http://www.gnuplot.info"
 pkg_description="Gnuplot is a portable command-line driven graphing
@@ -8,7 +8,7 @@ utility for Linux, OS/2, MS Windows, OSX, VMS, and many other
 platforms"
 pkg_license=('gnuplot')
 pkg_source="https://sourceforge.net/projects/$pkg_name/files/$pkg_name/$pkg_version/$pkg_name-$pkg_version.tar.gz"
-pkg_shasum="35dd8f013139e31b3028fac280ee12d4b1346d9bb5c501586d1b5a04ae7a94ee"
+pkg_shasum="6b690485567eaeb938c26936e5e0681cf70c856d273cc2c45fabf64d8bc6590e"
 pkg_deps=(
   core/bzip2
   core/cairo
