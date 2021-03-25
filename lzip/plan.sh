075f83065512441397a35a5b09eecd5fb3073721
rybons <bonnerrk@gmail.com>
2021-03-25 13:49:24 -0400

    Bumped 'lzip' version 1.18 -> 1.22
    Signed-off-by: rybons <bonnerrk@gmail.com>

--- lzip/plan.sh
+++ lzip/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=lzip
 pkg_origin=core
-pkg_version=1.18
+pkg_version=1.22
 pkg_description="A lossless data compressor with a user interface similar to the one of gzip or bzip2."
 pkg_upstream_url="http://www.nongnu.org/lzip/lzip.html"
 pkg_license=('GPL-2.0')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="http://download.savannah.gnu.org/releases/lzip/lzip-${pkg_version}.tar.gz"
-pkg_shasum=47f9882a104ab05532f467a7b8f4ddbb898fa2f1e8d9d468556d6c2d04db14dd
+pkg_shasum=e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
 pkg_deps=(core/glibc core/gcc-libs)
 pkg_build_deps=(core/gcc core/make core/diffutils)
 pkg_bin_dirs=(bin)
