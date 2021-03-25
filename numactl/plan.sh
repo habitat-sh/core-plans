6134889a84ebbc62e9c3c66794221365d49c6cfd
rybons <bonnerrk@gmail.com>
2021-03-25 11:28:45 -0400

    Bumped 'numactl' version 2.0.12 -> 2.0.14
    Signed-off-by: rybons <bonnerrk@gmail.com>

--- numactl/plan.sh
+++ numactl/plan.sh
@@ -1,10 +1,10 @@
 pkg_origin=core
 pkg_name=numactl
-pkg_version=2.0.12
+pkg_version=2.0.14
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('GPL-2.0-or-later' 'LGPL-2.1-or-later')
 pkg_source=https://github.com/numactl/numactl/archive/v${pkg_version}.tar.gz
-pkg_shasum=7c3e819c2bdeb883de68bafe88776a01356f7ef565e75ba866c4b49a087c6bdf
+pkg_shasum=bf2601f6ec439480313a9cc5bfca6385ffcaeffef9b7f60e88f005585f9671e6
 pkg_deps=(core/glibc)
 pkg_build_deps=(core/gcc core/make core/autoconf core/automake core/libtool)
 pkg_bin_dirs=(bin)
