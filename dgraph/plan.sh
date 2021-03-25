a0a0d63d76721e48636ed9f9039a3864be75a6a6
rybons <bonnerrk@gmail.com>
2021-03-25 16:07:54 -0400

    Bumped 'dgraph' version "1.0.16" -> "1.2.6"
    Signed-off-by: rybons <bonnerrk@gmail.com>

--- dgraph/plan.sh
+++ dgraph/plan.sh
@@ -1,10 +1,10 @@
 pkg_name=dgraph
 pkg_origin=core
-pkg_version="1.0.16"
+pkg_version="1.2.6"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('Apache-2.0')
 pkg_source="https://github.com/${pkg_name}-io/${pkg_name}/releases/download/v${pkg_version}/${pkg_name}-linux-amd64.tar.gz"
-pkg_shasum=c778603e747e98fccf212d823c396f72be28153b7832f3804447f39a4ea6a92f
+pkg_shasum=367257b3725913215f7220d80943dc82a18d9b1a2b60784def30d1a21464f1bb
 pkg_deps=(core/glibc)
 pkg_build_deps=(core/patchelf)
 pkg_bin_dirs=(bin)
