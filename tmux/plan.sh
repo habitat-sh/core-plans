441670598f4b2ad6b17c35cf412d27a6f58ac023
Lucas Giancola <lucas.giancola@live.com>
2021-03-25 16:44:24 -0400

    Bumped 'tmux' version  ->
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- tmux/plan.sh
+++ tmux/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=tmux
 pkg_origin=core
-pkg_version=3.0a
+pkg_version=3.1c
 pkg_description="A terminal multiplexer"
 pkg_upstream_url=https://tmux.github.io/
 pkg_license=('MIT')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="https://github.com/tmux/tmux/releases/download/${pkg_version}/tmux-${pkg_version}.tar.gz"
-pkg_shasum=4ad1df28b4afa969e59c08061b45082fdc49ff512f30fc8e43217d7b0e5f8db9
+pkg_shasum=5555de5a540d3469421fbe9c14e4086504b2dac945c39651cf227196ecf905c7
 pkg_deps=(
   core/glibc
   core/libevent
