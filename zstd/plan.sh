1335486cef1e4ddb3275142709b5e2a6defdb5f1
rybons <bonnerrk@gmail.com>
2021-03-25 16:23:22 -0400

    Bumped 'zstd' version 1.4.0 -> 1.4.9
    Signed-off-by: rybons <bonnerrk@gmail.com>

--- zstd/plan.sh
+++ zstd/plan.sh
@@ -1,13 +1,13 @@
 pkg_origin=core
 pkg_name=zstd
-pkg_version=1.4.0
+pkg_version=1.4.9
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('BSD-3-Clause')
 pkg_description="Zstandard is a real-time compression algorithm, providing high compression ratios. "\
 "It offers a very wide range of compression / speed trade-off, while being backed by a very fast decoder"
 pkg_upstream_url=http://facebook.github.io/zstd/
 pkg_source="https://github.com/facebook/zstd/archive/v${pkg_version}.tar.gz"
-pkg_shasum='63be339137d2b683c6d19a9e34f4fb684790e864fee13c7dd40e197a64c705c1'
+pkg_shasum='29ac74e19ea28659017361976240c4b5c5c24db3b89338731a6feb97c038d293'
 pkg_deps=(
   core/glibc
   core/grep
