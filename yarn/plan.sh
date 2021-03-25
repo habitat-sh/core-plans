6f051d734ebc6a41e720be33ac0afbf8f7f421e8
rybons <bonnerrk@gmail.com>
2021-03-25 13:40:37 -0400

    Bumped 'yarn' version 1.22.4 -> 1.22.10
    Signed-off-by: rybons <bonnerrk@gmail.com>

--- yarn/plan.sh
+++ yarn/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=yarn
 pkg_origin=core
-pkg_version=1.22.4
+pkg_version=1.22.10
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="Yarn is a package manager for your code. It allows you to use and share code with other developers from around the world. Yarn does this quickly, securely, and reliably so you don’t ever have to worry."
 pkg_upstream_url=https://yarnpkg.com/
 pkg_license=('BSD-2-Clause')
 pkg_source="https://github.com/yarnpkg/yarn/releases/download/v${pkg_version}/yarn-v${pkg_version}.tar.gz"
-pkg_shasum=bc5316aa110b2f564a71a3d6e235be55b98714660870c5b6b2d2d3f12587fb58
+pkg_shasum=7e433d4a77e2c79e6a7ae4866782608a8e8bcad3ec6783580577c59538381a6e
 pkg_bin_dirs=(bin)
 pkg_build_deps=()
 pkg_deps=(
