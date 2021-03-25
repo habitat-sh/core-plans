5e38737cb6db5716e8d023405b30844b5d8278d2
rybons <bonnerrk@gmail.com>
2021-03-25 10:32:32 -0400

    Bumped 'ocamlbuild' version "0.11.0" -> "0.14.0"
    Signed-off-by: rybons <bonnerrk@gmail.com>

--- ocamlbuild/plan.sh
+++ ocamlbuild/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=ocamlbuild
 pkg_origin=core
-pkg_version="0.11.0"
+pkg_version="0.14.0"
 pkg_description="OCamlbuild is a generic build tool, that has built-in rules for building OCaml library and programs."
 pkg_upstream_url="https://github.com/ocaml/ocamlbuild"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('LGPL')
 pkg_source="https://github.com/ocaml/ocamlbuild/archive/${pkg_version}.tar.gz"
-pkg_shasum="1717edc841c9b98072e410f1b0bc8b84444b4b35ed3b4949ce2bec17c60103ee"
+pkg_shasum="c02adf52ec18d3e634baa546d22888e7aabbd7424b7e82d30104e697b1c88236"
 pkg_deps=(
   core/glibc
   core/ocaml
