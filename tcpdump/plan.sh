a3683bd61411b009845015464f029e236807ae9d
rybons <bonnerrk@gmail.com>
2021-03-25 17:06:14 -0400

    Bumped 'tcpdump' version 4.9.3 -> 4.99.0
    Signed-off-by: rybons <bonnerrk@gmail.com>

--- tcpdump/plan.sh
+++ tcpdump/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=tcpdump
 pkg_origin=core
-pkg_version=4.9.3
+pkg_version=4.99.0
 pkg_description="A powerful command-line packet analyzer."
 pkg_upstream_url="http://www.tcpdump.org/"
 pkg_license=('BSD')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="http://www.tcpdump.org/release/tcpdump-${pkg_version}.tar.gz"
-pkg_shasum=2cd47cb3d460b6ff75f4a9940f594317ad456cfbf2bd2c8e5151e16559db6410
+pkg_shasum=8cf2f17a9528774a7b41060323be8b73f76024f7778f59c34efa65d49d80b842
 # core/coreutils isn't /really/ needed at runtime, but fix_interpreter
 # only works if the dep is listed in pkg_deps
 pkg_deps=(
