b2a51c9cf7c44493757a438f439aa5e9a1ea15ef
Lucas Giancola <lucas.giancola@live.com>
2021-03-25 12:41:16 -0400

    Bumped 'lsyncd' version 2.2.1 -> 2.2.3
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- lsyncd/plan.sh
+++ lsyncd/plan.sh
@@ -1,11 +1,11 @@
 pkg_name=lsyncd
 pkg_origin=core
-pkg_version=2.2.1
+pkg_version=2.2.3
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('GPL-2.0')
 pkg_source="https://github.com/axkibe/$pkg_name/archive/release-$pkg_version.tar.gz"
 pkg_dirname="$pkg_name-release-$pkg_version"
-pkg_shasum="f41969454a17f9441a9b1809bb251235631768393bf5d29ad8e8142670ae4735"
+pkg_shasum="4423d107041a62f23d7d0eda195f990161983d6f6a09e2f1f42998c38570c65c"
 pkg_deps=(core/glibc)
 pkg_build_deps=(
   core/cmake
