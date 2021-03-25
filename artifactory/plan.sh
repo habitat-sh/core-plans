5261b948df1120282c1b13cc89bad04343208efa
Lucas Giancola <lucas.giancola@live.com>
2021-03-25 12:16:21 -0400

    Bumped 'artifactory' version 6.11.3 -> 6.23.13
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- artifactory/plan.sh
+++ artifactory/plan.sh
@@ -1,13 +1,13 @@
 pkg_origin=core
 pkg_name=artifactory
-pkg_version=6.11.3
+pkg_version=6.23.13
 pkg_description="Artifactory is an advanced binary repository manager for use by build tools (like Maven and Gradle), dependency management tools (like Ivy and NuGet) and build servers (like Jenkins, Hudson, TeamCity and Bamboo).
 Repository managers serve two purposes: they act as highly configurable proxies between your organization and external repositories and they also provide build servers with a deployment destination for your internally generated artifacts."
 pkg_upstream_url=https://www.jfrog.com/artifactory/
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("JFrog Artifactory EULA")
 pkg_source="https://bintray.com/jfrog/${pkg_name}/download_file?file_path=jfrog-artifactory-oss-${pkg_version}.zip"
-pkg_shasum=fc2277fa4da9cfd83ca3af9ca94b2b03717e3df60573ab19f0281c9954117eda
+pkg_shasum=0889c11b768a4b1a6bfb6939280c24e5bd22b36bc3438481b0b420443eb5f379
 pkg_deps=(
   core/bash
   core/openjdk11
