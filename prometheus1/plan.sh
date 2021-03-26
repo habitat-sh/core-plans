b022530549c2e64896d4b0d5bb382a6c7b648c7c
Lucas Giancola <lucas.giancola@live.com>
2021-03-26 08:28:36 -0400

    Bumped 'prometheus1' version 1.6.1 -> 1.8.2
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- prometheus1/plan.sh
+++ prometheus1/plan.sh
@@ -4,12 +4,12 @@ pkg_name=prometheus1
 pkg_origin=core
 pkg_description="Prometheus monitoring"
 pkg_upstream_url=http://prometheus.io
-pkg_version=1.6.1
+pkg_version=1.8.2
 pkg_maintainer="Lamont Lucas <lamont@fastrobot.com>"
 pkg_license=('Apache-2.0')
 pkg_bin_dirs=(bin)
 # not used, since I actually git clone the repo and checkout the pkg_version branch
 pkg_source="https://github.com/prometheus/prometheus/archive/v${pkg_version}.tar.gz"
-pkg_shasum=ecc9ce94fce45994c23b76eb0c5acbb1b942513be601872c8cd74d0821450c5e
+pkg_shasum=eaea402797a685b838ce5e9f07e37e4be1801ba77b4f01bce9882a4f360fc214
 prom_pkg_dir="$HAB_CACHE_SRC_PATH/${pkg_name}-${pkg_version}"
 prom_build_dir="${prom_pkg_dir}/src/${pkg_source}"
