07c70cb673447227e1878bc98bd86f122271974c
Lucas Giancola <lucas.giancola@live.com>
2021-03-26 09:37:36 -0400

    Bumped 're2c' version 1.1.1 -> 1.3
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- re2c/plan.sh
+++ re2c/plan.sh
@@ -1,13 +1,13 @@
 pkg_name=re2c
 pkg_origin=core
-pkg_version=1.1.1
+pkg_version=1.3
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('PDDL-1.0')
 pkg_upstream_url=http://re2c.org/
 pkg_description="re2c is a lexer generator for C/C++."
 pkg_source=https://github.com/skvadrik/${pkg_name}/releases/download/${pkg_version}/${pkg_name}-${pkg_version}.tar.gz
 pkg_filename=${pkg_name}-${pkg_version}.tar.bz2
-pkg_shasum=856597337ea00b24ce91f549f79e6eece1b92ba5f8b63292cad66c14ac7451cf
+pkg_shasum=0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5
 pkg_deps=(core/gcc-libs)
 pkg_build_deps=(core/coreutils core/diffutils core/gcc core/make)
 pkg_bin_dirs=(bin)
