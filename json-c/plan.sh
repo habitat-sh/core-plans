2d3b6c07f79dac20fc502213d67ba27e026dea55
Lucas Giancola <lucas.giancola@live.com>
2021-03-26 10:01:25 -0400

    Bumped 'json-c' version 0.13.1 -> 0.15
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- json-c/plan.sh
+++ json-c/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=json-c
 pkg_origin=core
-pkg_version=0.13.1
+pkg_version=0.15
 pkg_description="A JSON implementation in C"
 pkg_upstream_url=https://github.com/json-c/json-c
 pkg_license=('MIT')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source=https://s3.amazonaws.com/json-c_releases/releases/${pkg_name}-${pkg_version}.tar.gz
-pkg_shasum=b87e608d4d3f7bfdd36ef78d56d53c74e66ab278d318b71e6002a369d36f4873
+pkg_shasum=b8d80a1ddb718b3ba7492916237bbf86609e9709fb007e7f7d4322f02341a4c6
 pkg_build_deps=(
   core/autoconf
   core/busybox-static
