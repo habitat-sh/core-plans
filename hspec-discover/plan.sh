1ce0b5105d97b3734c584516ded6cfa50a497470
Lucas Giancola <lucas.giancola@live.com>
2021-03-26 10:17:28 -0400

    Bumped 'hspec-discover' version 2.7.0 -> 2.7.9
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- hspec-discover/plan.sh
+++ hspec-discover/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=hspec-discover
 pkg_origin=core
-pkg_version=2.7.0
+pkg_version=2.7.9
 pkg_license=('MIT')
 pkg_upstream_url="https://hspec.github.io/"
 pkg_description="Automatically discover and run Hspec tests"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="https://hackage.haskell.org/package/${pkg_name}-${pkg_version}/${pkg_name}-${pkg_version}.tar.gz"
-pkg_shasum="1cb6d6cd494a191b74aa54465005929c73911bf8cd79bb8f7773f4611bf06bd8"
+pkg_shasum="9bd0fe319bb5581fa3f3c97f846b942ec799f51617740b052224be87328226ff"
 
 pkg_bin_dirs=(bin)
 pkg_lib_dirs=(lib)
