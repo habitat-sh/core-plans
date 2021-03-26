2d8ecf086909675218caea9d8212a1e697aad3ba
Lucas Giancola <lucas.giancola@live.com>
2021-03-26 10:05:27 -0400

    Bumped 'dgraph-ratel' version "1.0.6" -> "1.2.8"
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- dgraph-ratel/plan.sh
+++ dgraph-ratel/plan.sh
@@ -1,10 +1,10 @@
 pkg_name=dgraph-ratel
 pkg_origin=core
-pkg_version="1.0.6"
+pkg_version="1.2.8"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('Apache-2.0')
 pkg_source="https://github.com/dgraph-io/dgraph/releases/download/v${pkg_version}/dgraph-linux-amd64.tar.gz"
-pkg_shasum="616a3dc22973c48bbe57036dde5ea91055761f565a22bfbe20b6079fd16a9156"
+pkg_shasum="7c243931749a93451344fc6762895217d157fa85cdae6bb0ce6c9b0ba6ea2edc"
 pkg_deps=(core/glibc)
 pkg_build_deps=(core/patchelf)
 pkg_bin_dirs=(bin)
