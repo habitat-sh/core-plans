cb40a3289a96576ec415b3bd5345c72c6d451398
Lucas Giancola <lucas.giancola@live.com>
2021-03-26 09:41:23 -0400

    Bumped 'libpipeline' version 1.5.1 -> 1.5.3
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- libpipeline/plan.sh
+++ libpipeline/plan.sh
@@ -1,12 +1,12 @@
 pkg_origin=core
 pkg_name=libpipeline
-pkg_version=1.5.1
+pkg_version=1.5.3
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('GPL-3.0-or-later')
 pkg_description="libpipeline is a C library for manipulating pipelines of subprocesses in a flexible and convenient way."
 pkg_upstream_url=http://libpipeline.nongnu.org/
 pkg_source="http://download.savannah.gnu.org/releases/libpipeline/libpipeline-${pkg_version}.tar.gz"
-pkg_shasum=d633706b7d845f08b42bc66ddbe845d57e726bf89298e2cee29f09577e2f902f
+pkg_shasum=e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
 pkg_deps=(core/glibc)
 pkg_build_deps=(
   core/gcc
