cbb6ea79348ef2e43b233f814fb27b4aff019c87
Lucas Giancola <lucas.giancola@live.com>
2021-03-26 11:43:33 -0400

    Bumped 'lcms2' version 2.8 -> 2.12
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- lcms2/plan.sh
+++ lcms2/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=lcms2
 pkg_origin=core
-pkg_version=2.8
+pkg_version=2.12
 pkg_description="Small-footprint color management engine, version 2"
 pkg_upstream_url=http://www.littlecms.com
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('MIT')
 pkg_source=http://downloads.sourceforge.net/sourceforge/lcms/${pkg_name}-${pkg_version}.tar.gz
-pkg_shasum=66d02b229d2ea9474e62c2b6cd6720fde946155cd1d0d2bffdab829790a0fb22
+pkg_shasum=2257531baedea1a47570e42b92634398afaef5ad5341562b497100048453dd45
 pkg_deps=(
   core/glibc
   core/jbigkit
