67725bd00b23e8df50a6ef91e81f9df3d531c8b7
Lucas Giancola <lucas.giancola@live.com>
2021-03-26 10:02:28 -0400

    Bumped 'icu56' version 56.2 -> 68.2
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- icu56/plan.sh
+++ icu56/plan.sh
@@ -1,6 +1,6 @@
 pkg_origin=core
 pkg_name=icu56
-pkg_version=56.2
+pkg_version=68.2
 pkg_description="$(cat << EOF
   ICU is a mature, widely used set of C/C++ and Java libraries providing
   Unicode and Globalization support for software applications. ICU is widely
@@ -12,7 +12,7 @@ pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("Unicode-TOU")
 # shellcheck disable=SC2059
 pkg_source="https://github.com/unicode-org/icu/releases/download/release-${pkg_version//./-}/icu4c-${pkg_version//./_}-src.tgz"
-pkg_shasum=187faf495133f4cffa22d626896e7288f43d342e6af5eb8b214a1bf37bad51a6
+pkg_shasum=5568abc4f3be6de512c5972892743ae32a4a2214dd76632e53eccd80d655cd07
 pkg_upstream_url="http://site.icu-project.org/"
 pkg_deps=(core/glibc core/gcc-libs)
 pkg_build_deps=(core/gcc core/make)
