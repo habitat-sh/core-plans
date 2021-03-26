85376ec4ecf9d19de8cc7f3ccf16e955cc0deb39
Lucas Giancola <lucas.giancola@live.com>
2021-03-26 10:11:51 -0400

    Fixed the version to be the proper one for the atk
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- at-spi2-atk/plan.sh
+++ at-spi2-atk/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=at-spi2-atk
 pkg_origin=core
-pkg_version=2.40.0
+pkg_version=2.38.0
 pkg_description="Service Provider Interface for the Assistive Technologies available on the GNOME platform"
 pkg_upstream_url=https://wiki.linuxfoundation.org/accessibility/atk/at-spi/at-spi_on_d-bus
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("LGPL-2.0")
 pkg_source="https://download.gnome.org/sources/${pkg_name}/${pkg_version%.*}/${pkg_name}-${pkg_version}.tar.xz"
-pkg_shasum=7515bf959b73b956ceb967351c7e299cbb3668a53d35f9c770eb72e00d93ced6
+pkg_shasum=e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
 pkg_deps=(
   core/at-spi2-core
   core/atk
