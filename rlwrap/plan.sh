aef494393abea56a3dbeb3cef664da907654dae7
Lucas Giancola <lucas.giancola@live.com>
2021-03-26 09:55:10 -0400

    Bumped 'rlwrap' version 0.43 -> 0.45
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- rlwrap/plan.sh
+++ rlwrap/plan.sh
@@ -1,12 +1,12 @@
 pkg_origin=core
 pkg_name=rlwrap
-pkg_version=0.43
+pkg_version=0.45
 pkg_description="A readline wrapper"
 pkg_upstream_url=https://github.com/hanslub42/rlwrap
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("GPL-2.0")
 pkg_source=https://github.com/hanslub42/rlwrap/releases/download/v${pkg_version}/${pkg_name}-${pkg_version}.tar.gz
-pkg_shasum=8e86d0b7882d9b8a73d229897a90edc207b1ae7fa0899dca8ee01c31a93feb2f
+pkg_shasum=0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5
 pkg_deps=(
   core/ncurses
   core/readline
