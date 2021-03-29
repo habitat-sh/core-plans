f303304fe27fd8c4ba7025f5dbcb7a4d11f95147
rybons <bonnerrk@gmail.com>
2021-03-29 15:56:19 -0400

    Bumped 'ghc' version 8.8.1 -> 8.10.4
    Signed-off-by: rybons <bonnerrk@gmail.com>

--- ghc/plan.sh
+++ ghc/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=ghc
 pkg_origin=core
-pkg_version=8.8.1
+pkg_version=8.10.4
 pkg_license=('BSD-3-Clause')
 pkg_upstream_url="https://www.haskell.org/ghc/"
 pkg_description="The Glasgow Haskell Compiler"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="http://downloads.haskell.org/~ghc/${pkg_version}/ghc-${pkg_version}-src.tar.xz"
-pkg_shasum="908a83d9b814da74585de9d39687189e6260ec3848131f9d9236cab8a123721a"
+pkg_shasum="52af871b4e08550257d720c2944ac85727d0b948407cef1bebfe7508c224910e"
 
 pkg_bin_dirs=(bin)
 pkg_lib_dirs=(lib)
