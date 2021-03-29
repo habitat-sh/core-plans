cae66227f509f68755a5667adc0baef56efce658
rybons <bonnerrk@gmail.com>
2021-03-29 15:59:12 -0400

    Bumped 'htop' version 3.0.2 -> 3.0.5
    Signed-off-by: rybons <bonnerrk@gmail.com>

--- htop/plan.sh
+++ htop/plan.sh
@@ -1,12 +1,12 @@
 pkg_origin=core
 pkg_name=htop
-pkg_version=3.0.2
+pkg_version=3.0.5
 pkg_description="This is htop, an interactive process viewer for Unix systems. It is a text-mode application (for console or X terminals) and requires ncurses."
 pkg_upstream_url="https://bintray.com/htop/"
 pkg_license=("GPL-2.0")
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="https://bintray.com/htop/source/download_file?file_path=htop-${pkg_version}.tar.gz"
-pkg_shasum="6471d9505daca5c64073fc37dbab4d012ca4fc6a7040a925dad4a7553e3349c4"
+pkg_shasum="19535f8f01ac08be2df880c93c9cedfc50fa92320d48e3ef92a30b6edc4d1917"
 
 pkg_deps=(
   core/glibc
