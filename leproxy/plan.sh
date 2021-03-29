a1eb48aec8f3a873a5cae0b5d52969b894c5c10b
rybons <bonnerrk@gmail.com>
2021-03-29 12:48:20 -0400

    Bumped 'leproxy' version "20180113" -> "20191014"
    Signed-off-by: rybons <bonnerrk@gmail.com>

--- leproxy/plan.sh
+++ leproxy/plan.sh
@@ -1,10 +1,10 @@
 pkg_name=leproxy
 pkg_origin=core
-pkg_version="20180113"
+pkg_version="20191014"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("MIT")
 pkg_source="https://github.com/artyom/${pkg_name}/releases/download/${pkg_version}/leproxy-linux-amd64.tar.gz"
-pkg_shasum="c8a7ee698c8f4b4f828905d13978de30a79d6d394f992d8b1a170469c8e0d148"
+pkg_shasum="21f04ed32f51b90854d899b653b52ee05cd1319310a9e8bc52c48e6e28859ef7"
 pkg_bin_dirs=(bin)
 pkg_description="https reverse proxy with automatic Letsencrypt usage for multiple hostnames/backends"
 pkg_upstream_url="https://github.com/artyom/leproxy"
