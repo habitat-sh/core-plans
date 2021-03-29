10bbd44dcbc971fe2bfd735a6de2916a5a58398b
rybons <bonnerrk@gmail.com>
2021-03-29 15:46:53 -0400

    Bumped 'lynx' version 2.8.8 -> 2.8.9rel.1
    Signed-off-by: rybons <bonnerrk@gmail.com>

--- lynx/plan.sh
+++ lynx/plan.sh
@@ -1,12 +1,12 @@
 pkg_origin=core
 pkg_name=lynx
-pkg_version=2.8.8
+pkg_version=2.8.9rel.1
 pkg_description="Lynx is the text web browser."
 pkg_upstream_url=http://lynx.browser.org/
 pkg_license=("GPL-2.0")
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_source="http://invisible-mirror.net/archives/$pkg_name/tarballs/$pkg_name${pkg_version}rel.2.tar.gz"
-pkg_shasum=234c9dc77d4c4594ad6216d7df4d49eae3019a3880e602f39721b35b97fbc408
+pkg_source="http://invisible-mirror.net/archives/$pkg_name/tarballs/$pkg_name${pkg_version}.tar.gz"
+pkg_shasum=a46e4167b8f02c066d2fe2eafcc5603367be0e3fe2e59e9fc4eb016f306afc8e
 pkg_dirname="$pkg_name${pkg_version//./-}"
 pkg_deps=(
   core/glibc
