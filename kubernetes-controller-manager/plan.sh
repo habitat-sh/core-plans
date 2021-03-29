05417fa66abf289b767de3c4f5cdd575d8048eb6
rybons <bonnerrk@gmail.com>
2021-03-29 12:54:13 -0400

    Update kubernetes dependency version to latest
    Signed-off-by: rybons <bonnerrk@gmail.com>

--- kubernetes-controller-manager/plan.sh
+++ kubernetes-controller-manager/plan.sh
@@ -5,7 +5,7 @@ pkg_upstream_url=https://github.com/kubernetes/kubernetes
 pkg_license=('Apache-2.0')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_version=1.20.5
-pkg_deps=("core/kubernetes/1.11.2")
+pkg_deps=("core/kubernetes/1.20.5")
 
 do_build() {
   return 0
