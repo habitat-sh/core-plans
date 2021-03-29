e7848066a6c801d57b30c2763cda605a110f598e
rybons <bonnerrk@gmail.com>
2021-03-29 15:07:06 -0400

    Bumped 'oniguruma' version 6.9.4 -> 6.9.6
    Signed-off-by: rybons <bonnerrk@gmail.com>

--- oniguruma/plan.sh
+++ oniguruma/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=oniguruma
 pkg_origin=core
-pkg_version=6.9.4
+pkg_version=6.9.6
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="Oniguruma is a modern and flexible regular expressions library"
 pkg_upstream_url="https://github.com/kkos/oniguruma"
 pkg_license=("BSD-2-Clause")
 pkg_source="https://github.com/kkos/oniguruma/archive/v${pkg_version}.tar.gz"
-pkg_shasum=aea68e5843b627f5fe6d3d6b598845b7f3622910e0568408e7cc2fa6b3690b87
+pkg_shasum=bd0faeb887f748193282848d01ec2dad8943b5dfcb8dc03ed52dcc963549e819
 pkg_deps=(
   core/glibc
   core/coreutils
