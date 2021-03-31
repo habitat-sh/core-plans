9102f18788a0e68c3b13ae290231339347db444a
Lucas Giancola <lucas.giancola@live.com>
2021-03-31 15:30:01 -0400

    Fixed shasum
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- bash-completion/plan.sh
+++ bash-completion/plan.sh
@@ -6,7 +6,7 @@ pkg_upstream_url=https://github.com/scop/bash-completion
 pkg_description="Programmable completion functions for bash"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="https://github.com/scop/bash-completion/releases/download/${pkg_version}/bash-completion-${pkg_version}.tar.xz"
-pkg_shasum=f13188a0f324d774f375f0e4c4894e87b61aedcb914e1147c9b2b372c8970fc0
+pkg_shasum=73a8894bad94dee83ab468fa09f628daffd567e8bef1a24277f1e9a0daf911ac
 pkg_deps=(
   core/bash
 )
