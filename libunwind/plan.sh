f6f855cac130878a65ef8e28036953fb6096569d
Lucas Giancola <lucas.giancola@live.com>
2021-03-31 15:32:04 -0400

    Fixed shasum
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- libunwind/plan.sh
+++ libunwind/plan.sh
@@ -6,7 +6,7 @@ pkg_upstream_url="http://www.nongnu.org/libunwind/"
 pkg_license=('MIT')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="http://download.savannah.gnu.org/releases/libunwind/libunwind-${pkg_version}.tar.gz"
-pkg_shasum=e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
+pkg_shasum=df59c931bd4d7ebfd83ee481c943edf015138089b8e50abed8d9c57ba9338435
 pkg_deps=(
   core/glibc
   core/gcc-libs
