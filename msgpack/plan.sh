f1a07b5b0a3091e4660f058b87384874dfd74aed
Lucas Giancola <lucas.giancola@live.com>
2021-04-01 14:28:05 -0400

    Fixed SHASUM
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- msgpack/plan.sh
+++ msgpack/plan.sh
@@ -6,7 +6,7 @@ pkg_description="MessagePack implementation for C and C++"
 pkg_upstream_url="https://github.com/msgpack/msgpack-c"
 pkg_license=('BSL-1.0')
 pkg_source="https://github.com/msgpack/${pkg_name}-c/releases/download/cpp-$pkg_version/${pkg_name}-$pkg_version.tar.gz"
-pkg_shasum="515bfe56c8f25339dce40722cac06b759bf772b5cb7a8b9bedb0e97c33edece0"
+pkg_shasum=6e114d12a5ddb8cb11f669f83f32246e484a8addd0ce93f274996f1941c1f07b
 pkg_deps=(core/glibc)
 pkg_build_deps=(
   core/cmake
