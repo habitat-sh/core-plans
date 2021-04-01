09639d41a52dd5436373b668d5177eda53452c16
Lucas Giancola <lucas.giancola@live.com>
2021-04-01 10:43:29 -0400

    Fixed shasum
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- giflib/plan.sh
+++ giflib/plan.sh
@@ -7,7 +7,7 @@ pkg_description="GIFLIB is a package of portable tools and library routines for 
 Also commonly known as libgif."
 pkg_upstream_url=http://giflib.sourceforge.net/
 pkg_source=http://downloads.sourceforge.net/sourceforge/$pkg_name/${pkg_name}-${pkg_version}.tar.gz
-pkg_shasum=
+pkg_shasum=31da5562f44c5f15d63340a09a4fd62b48c45620cd302f77a6d9acf0077879bd
 pkg_deps=(core/glibc)
 pkg_build_deps=(core/diffutils core/gcc core/make)
 pkg_lib_dirs=(lib)
