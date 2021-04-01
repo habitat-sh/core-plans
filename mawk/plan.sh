8939123da1da9eb75b936320a7d11b319cd1d8c8
Lucas Giancola <lucas.giancola@live.com>
2021-04-01 14:38:37 -0400

    Fixed SHASUM
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- mawk/plan.sh
+++ mawk/plan.sh
@@ -6,7 +6,7 @@ pkg_description="An interpreter for the AWK Programming Language"
 pkg_upstream_url="http://invisible-island.net/mawk/mawk.html"
 pkg_license=('GPL-2.0')
 pkg_source=https://invisible-mirror.net/archives/${pkg_name}/${pkg_name}-${pkg_version}.tgz
-pkg_shasum=2a8c8b82721c98b2e9db181173bea1cbca6f0d623ec11874a6d8835f0245aed1
+pkg_shasum=7fd4cd1e1fae9290fe089171181bbc6291dfd9bca939ca804f0ddb851c8b8237
 pkg_build_deps=(
   core/diffutils
   core/gcc
