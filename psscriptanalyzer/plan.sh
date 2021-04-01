03b7a6a0cf7dd32d8b69d158cb6e8f857c51979a
Lucas Giancola <lucas.giancola@live.com>
2021-04-01 09:28:02 -0400

    Fixed shasum
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- psscriptanalyzer/plan.sh
+++ psscriptanalyzer/plan.sh
@@ -6,7 +6,7 @@ pkg_upstream_url="https://github.com/PowerShell/PSScriptAnalyzer"
 pkg_description="PSScriptAnalyzer is the ubiquitous linter for PowerShell"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="https://github.com/PowerShell/PSScriptAnalyzer/releases/download/$pkg_version/PSScriptAnalyzer.zip"
-pkg_shasum="3e7e24b33602244e8fc54c3dd0d9675abf1727c881943326bff19e7957052976"
+pkg_shasum=151a4e6df2bc5a66192ee5db4af4132e17159860861191646cffd38d06ce71a2
 pkg_deps=("core/powershell")
 
 do_build() {
