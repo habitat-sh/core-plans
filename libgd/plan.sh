1af6a5e05fb11feb7c1cb665e03cdaf4628cee4d
Lucas Giancola <lucas.giancola@live.com>
2021-04-05 10:10:44 -0400

    Fixed shasum
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- libgd/plan.sh
+++ libgd/plan.sh
@@ -76,7 +76,7 @@ for their prior contributions.
 EOF
 )")
 pkg_source="https://github.com/$pkg_name/$pkg_name/releases/download/gd-$pkg_version/$pkg_name-$pkg_version.tar.xz"
-pkg_shasum="18ed11e6c3d2126ff89b47c0d11cd9af482b97df502a6fe9f725224eb58020cf"
+pkg_shasum=478a047084e0d89b83616e4c2cf3c9438175fb0cc55d8c8967f06e0427f7d7fb
 pkg_deps=(
   core/fontconfig
   core/freetype
