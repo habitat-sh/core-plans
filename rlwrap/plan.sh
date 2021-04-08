87ff81b8568d86e4f6d88a267a1366b11d6cb1c2
rybons <bonnerrk@gmail.com>
2021-04-08 14:59:00 -0400

    Downgrade to 0.44 on account of 0.45 release having no configure script available.
    Signed-off-by: rybons <bonnerrk@gmail.com>

--- rlwrap/plan.sh
+++ rlwrap/plan.sh
@@ -1,12 +1,12 @@
 pkg_origin=core
 pkg_name=rlwrap
-pkg_version=0.45
+pkg_version=0.44
 pkg_description="A readline wrapper"
 pkg_upstream_url=https://github.com/hanslub42/rlwrap
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("GPL-2.0")
-pkg_source=https://github.com/hanslub42/rlwrap/releases/download/v${pkg_version}/${pkg_name}-${pkg_version}.tar.gz
-pkg_shasum=0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5
+pkg_source=https://github.com/hanslub42/rlwrap/releases/download/${pkg_version}/${pkg_name}-${pkg_version}.tar.gz
+pkg_shasum=cd7ff50cde66e443cbea0049b4abf1cca64a74948371fa4f1b5d9a5bbce1e13c
 pkg_deps=(
   core/ncurses
   core/readline
