3b1705e8d4c2bc7feb75d4936b3245eb2c8b0047
rybons <bonnerrk@gmail.com>
2021-04-08 13:41:26 -0400

    Remove all download and verification steps related to GPG validation as required keys are no longer available at source.
    Signed-off-by: rybons <bonnerrk@gmail.com>

--- etcd/plan.sh
+++ etcd/plan.sh
@@ -19,30 +19,6 @@ pkg_exposes=(client-port server-port)
 pkg_svc_user="root"
 # pkg_svc_group="$pkg_svc_user"
 
-do_download() {
-  do_default_download
-
-  download_file "https://coreos.com/dist/pubkeys/app-signing-pubkey.gpg" \
-	        "app-signing-pubkey.gpg" \
-                "92ff168f819d50bb1bd73cbdb204ccc6c5845afa1d58d6ec8072b4d8e38fdb59"
-}
-
-do_verify() {
-  do_default_verify
-
-  verify_file "app-signing-pubkey.gpg" \
-	      "92ff168f819d50bb1bd73cbdb204ccc6c5845afa1d58d6ec8072b4d8e38fdb59"
-
-  # GPG verification
-  build_line "Verifying ${pkg_name}-${pkg_version}-linux-amd64.tar.gz signature"
-  GNUPGHOME=$(mktemp -d -p "$HAB_CACHE_SRC_PATH")
-  gpg --import --keyid-format LONG "${HAB_CACHE_SRC_PATH}/app-signing-pubkey.gpg"
-  gpg --batch --verify \
-  "${HAB_CACHE_SRC_PATH}"/${pkg_name}-${pkg_version}-linux-amd64.tar.gz
-  rm -r "$GNUPGHOME"
-  build_line "Signature verified for ${pkg_name}-${pkg_version}-linux-amd64.tar.gz"
-}
-
 do_build() {
   return 0
 }
