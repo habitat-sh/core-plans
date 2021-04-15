96bc01022fc35816e60abe6dca5add43caad665f
Andrew Chan <andrew.chan@shuttleops.io>
2021-04-15 13:30:13 -0400

    bump version & checksum to 1.18.1 fix package source download link
    Signed-off-by: Andrew Chan <andrew.chan@shuttleops.io>

--- telegraf/plan.sh
+++ telegraf/plan.sh
@@ -1,11 +1,11 @@
 pkg_name=telegraf
 pkg_origin=core
-pkg_version=1.18.0
+pkg_version=1.18.1
 pkg_license=('MIT')
 pkg_description="telegraf - client for InfluxDB"
 pkg_upstream_url="https://github.com/influxdata/telegraf/"
-pkg_source="https://dl.influxdata.com/${pkg_name}/releases/${pkg_name}-${pkg_version}-static_linux_amd64.tar.gz"
-pkg_shasum=2f0ca60b9172db8f52cb4afd8adac3c31573492b1b7da1c4950f5a70bbba361d
+pkg_source="https://dl.influxdata.com/${pkg_name}/releases/${pkg_name}-${pkg_version}_static_linux_amd64.tar.gz"
+pkg_shasum=e1be5cbc3d8e48f5919ebf3044c25555c8210ab11a0803d2feeb94c0f4ddebba
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_svc_run="telegraf --config ${pkg_svc_config_path}/telegraf.conf"
 pkg_build_deps=(
