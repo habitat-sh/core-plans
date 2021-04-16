80d5d9acb00d70976366c7e44df6813b8103c7f6
Lucas Giancola <lucas.giancola@live.com>
2021-04-16 09:57:50 -0400

    Calls the proper shell file to setup environment
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- mongo-tools/plan.sh
+++ mongo-tools/plan.sh
@@ -8,13 +8,13 @@ pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source=https://github.com/mongodb/mongo-tools/archive/r${pkg_version}.tar.gz
 pkg_shasum=575023eedbd391a3528268123f5c1f18da4f5dffb34a67766afc26f723d9cb4f
 pkg_dirname=${pkg_name}-r${pkg_version}
-pkg_filename=${pkg_name}-${pkg_version}.tar.gz
+pkg_filename=${pkg_name}-r${pkg_version}.tar.gz
 pkg_deps=(core/glibc)
 pkg_build_deps=(core/go core/coreutils core/gcc core/make)
 pkg_bin_dirs=(bin)
 
 do_build() {
-  ./set_gopath.sh
+  . ./set_goenv.sh
   export GOPATH=/hab/cache/src/mongo-tools-r${pkg_version}/.gopath:/hab/cache/src/mongo-tools-r${pkg_version}/vendor
   for i in mongodump mongoexport mongofiles mongoimport mongooplog mongorestore mongostat mongotop; do
     go build -o bin/$i $i/main/$i.go
