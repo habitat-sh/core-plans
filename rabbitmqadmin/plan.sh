46ed54d3190d53dcefa787af3ee0345df6446abb
Andrew Chan <andrew.chan@shuttleops.io>
2021-04-19 12:06:17 -0400

    udpate pkg_version and shasum to latest available on git repo (had to lower version as previous one did not exist)
    Signed-off-by: Andrew Chan <andrew.chan@shuttleops.io>

--- rabbitmqadmin/plan.sh
+++ rabbitmqadmin/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=rabbitmqadmin
 pkg_origin=core
-pkg_version=3.8.14
+pkg_version=3.8.9
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('MPL')
 pkg_description="Open source multi-protocol messaging broker (Administration CLI)"
 pkg_upstream_url="https://www.rabbitmq.com"
 pkg_source=https://raw.githubusercontent.com/rabbitmq/rabbitmq-management/v${pkg_version}/bin/rabbitmqadmin
-pkg_shasum=81697d08d1ffb533369ad836af1f643854725c10269e74084ccce5dcdbf87163
+pkg_shasum=c18706fda8de9e547cc882c14ddefe61f32835f7ca219229f7078fca3d17f459
 pkg_deps=(
   core/python2
   core/coreutils
