07ff9400699f2259bc4a6f5e43f511b1ab0f6f0a
raviduddela <ravishankar.duddela@progress.com>
2021-04-22 12:46:49 +0000

    adding build deps
    Signed-off-by: raviduddela <ravishankar.duddela@progress.com>

--- ffmpeg4/plan.sh
+++ ffmpeg4/plan.sh
@@ -6,6 +6,7 @@ pkg_version=4.3.2
 pkg_source="https://ffmpeg.org/releases/ffmpeg-${pkg_version}.tar.gz"
 pkg_shasum=b4701d5d1220cfa2f140090a0ae349cd76dafe335e60227d1e3f8301998634c9
 pkg_description="A complete, cross-platform solution to record, convert and stream audio and video."
+pkg_deps=(core/gcc core/yasm) 
 pkg_upstream_url=https://ffmpeg.org/
 pkg_license=('LGPL-2.1-or-later' 'GPL-2.0-or-later')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
