8b2a38c5ca89e0d916c8bb4108f823aca8d92071
Matt Wrock <matt@mattwrock.com>
2021-04-26 15:36:23 -0700

    fixing lynx build
    Signed-off-by: Matt Wrock <matt@mattwrock.com>

--- lynx/plan.sh
+++ lynx/plan.sh
@@ -7,7 +7,7 @@ pkg_license=("GPL-2.0")
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="http://invisible-mirror.net/archives/$pkg_name/tarballs/$pkg_name${pkg_version}.tar.gz"
 pkg_shasum=a46e4167b8f02c066d2fe2eafcc5603367be0e3fe2e59e9fc4eb016f306afc8e
-pkg_dirname="$pkg_name${pkg_version//./-}"
+pkg_dirname="$pkg_name${pkg_version}"
 pkg_deps=(
   core/glibc
   core/ncurses
@@ -15,18 +15,9 @@ pkg_deps=(
 pkg_build_deps=(
   core/gcc
   core/make
-  core/patch
 )
 pkg_bin_dirs=(bin)
 
 do_check() {
   make test
-}
-
-do_build() {
-  return 0
-}
-
-do_install() {
-  return 0
 }
