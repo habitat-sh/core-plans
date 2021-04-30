a7253234bd663f59384e8dae8cfc41347b4ba2d5
raviduddela <ravishankar.duddela@progress.com>
2021-04-30 08:27:14 +0000

    changing plan.sh to override build options
    Signed-off-by: RaviDuddela <RaviShankar.Duddela@progress.com>

--- ffmpeg4/plan.sh
+++ ffmpeg4/plan.sh
@@ -6,8 +6,21 @@ pkg_version=4.3.2
 pkg_source="https://ffmpeg.org/releases/ffmpeg-${pkg_version}.tar.gz"
 pkg_shasum=b4701d5d1220cfa2f140090a0ae349cd76dafe335e60227d1e3f8301998634c9
 pkg_description="A complete, cross-platform solution to record, convert and stream audio and video."
-pkg_deps=(core/gcc core/yasm) 
+pkg_deps=(core/gcc core/yasm)
 pkg_upstream_url=https://ffmpeg.org/
 pkg_license=('LGPL-2.1-or-later' 'GPL-2.0-or-later')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_dirname="ffmpeg-${pkg_version}"
+
+do_build() {
+
+./configure --enable-avisynth --enable-gpl --prefix="$pkg_prefix"
+make
+
+}
+
+do_install() {
+
+make install
+
+}
