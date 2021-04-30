ff1a9948e5c1a1793afa4389df12304c3a4c60f6
raviduddela <ravishankar.duddela@progress.com>
2021-04-30 12:49:41 +0000

    restoring to default build and adding avisynth and other dependencies
    Signed-off-by: RaviDuddela <RaviShankar.Duddela@progress.com>

--- ffmpeg4/plan.sh
+++ ffmpeg4/plan.sh
@@ -6,21 +6,36 @@ pkg_version=4.3.2
 pkg_source="https://ffmpeg.org/releases/ffmpeg-${pkg_version}.tar.gz"
 pkg_shasum=b4701d5d1220cfa2f140090a0ae349cd76dafe335e60227d1e3f8301998634c9
 pkg_description="A complete, cross-platform solution to record, convert and stream audio and video."
-pkg_deps=(core/gcc core/yasm)
+pkg_build_deps=(
+  core/diffutils
+  core/expat
+  core/gcc
+  core/libpng
+  core/libtasn1
+  core/make
+  core/nettle
+  core/p11-kit
+  core/pkg-config
+  core/yasm
+)
+pkg_deps=(
+  core/bzip2
+  core/fontconfig
+  core/freetype
+  core/glibc
+  core/gnutls
+  core/gmp
+  core/libxext
+  core/libdrm
+  core/libwebp
+  core/libxcb
+  core/libxml2
+  core/openjpeg
+  core/xz
+  core/zlib
+  core/avisynthplus
+)
 pkg_upstream_url=https://ffmpeg.org/
 pkg_license=('LGPL-2.1-or-later' 'GPL-2.0-or-later')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_dirname="ffmpeg-${pkg_version}"
-
-do_build() {
-
-./configure --enable-avisynth --enable-gpl --prefix="$pkg_prefix"
-make
-
-}
-
-do_install() {
-
-make install
-
-}
