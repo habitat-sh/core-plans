13e3335f086e00f0eb3d222883cd067824698096
Siraj Rauff <sirajrauff@gmail.com>
2021-05-03 22:13:21 -0400

    Updating haproxy20 to 2.0.22 & adding deps
    Signed-off-by: Siraj Rauff <sirajrauff@gmail.com>

--- haproxy20/plan.sh
+++ haproxy20/plan.sh
@@ -4,13 +4,19 @@ pkg_name=haproxy20
 pkg_origin=core
 pkg_description="The Reliable, High Performance TCP/HTTP Load Balancer"
 pkg_distname=haproxy
-pkg_version=2.0.21
+pkg_version=2.0.22
 pkg_maintainer='The Habitat Maintainers <humans@habitat.sh>'
 pkg_license=('GPL-2.0' 'LGPL-2.1')
 pkg_source="https://www.haproxy.org/download/2.0/src/${pkg_distname}-${pkg_version}.tar.gz"
-pkg_shasum=9823fd0e33d77827538edc3ac86fb5d5c099e63350bdafc3beaa2632491a0132
+pkg_shasum=5e1dccf30a3ad782d1e3298cc4e4955b168e4aa06cad4e3ea1c0334cdc0f966d
 pkg_dirname="${pkg_distname}-${pkg_version}"
 pkg_upstream_url="https://www.haproxy.org/"
+pkg_deps=(
+  core/gcc-libs
+  core/zlib
+  core/pcre
+  core/openssl
+)
 pkg_build_deps=(
   core/coreutils
   core/gcc
