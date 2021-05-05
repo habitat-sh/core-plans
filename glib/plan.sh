29cc4ce561096ace336ec1bc7564d30034066ab4
Siraj Rauff <sirajrauff@gmail.com>
2021-05-04 20:00:21 -0400

    Updating glib to use Meson build

--- glib/plan.sh
+++ glib/plan.sh
@@ -17,6 +17,7 @@ pkg_deps=(
   core/coreutils
   core/elfutils
   core/glibc
+  core/gcc-libs
   core/libffi
   core/libiconv
   core/pcre
@@ -31,7 +32,7 @@ pkg_build_deps=(
   core/gcc
   core/gettext
   core/libxslt
-  core/make
+  core/meson
   core/perl
   core/pkg-config
 )
@@ -41,28 +42,18 @@ pkg_include_dirs=(include)
 pkg_pconfig_dirs=(lib/pkgconfig)
 pkg_interpreters=(core/coreutils)
 
-do_prepare() {
-  if [[ ! -r /usr/bin/file ]]; then
-    ln -sv "$(pkg_path_for file)/bin/file" /usr/bin/file
-    _clean_file=true
-  fi
+do_build() {
+  meson _build --prefix="$pkg_prefix" \
+    -Diconv=external \
+    -Dgtk_doc=false \
+    --buildtype release
+  ninja -C _build
 }
 
-do_build() {
-  ./configure \
-    --prefix="$pkg_prefix" \
-    --with-libiconv \
-    --disable-gtk-doc \
-    --disable-fam
-  make
+do_install() {
+  ninja -C _build install
 }
 
 do_after() {
   fix_interpreter "$pkg_prefix/bin/*" core/coreutils bin/env
-}
-
-do_end() {
-  if [[ -n "$_clean_file" ]]; then
-    rm -fv /usr/bin/file
-  fi
 }
