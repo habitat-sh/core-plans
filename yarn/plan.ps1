705a7af26fbfcaa03f3405dc0993fa33a75ea699
Lucas Giancola <lucas.giancola@live.com>
2021-05-07 11:10:17 -0400

    Bumped 'yarn' version "1.22.4" -> "1.22.5"
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- yarn/plan.ps1
+++ yarn/plan.ps1
@@ -1,13 +1,13 @@
 $pkg_name="yarn"
 $pkg_origin="core"
-$pkg_version="1.22.4"
+$pkg_version="1.22.5"
 $pkg_description="Yarn is a package manager for your code. It allows you to use and share code with other developers from around the world. Yarn does this quickly, securely, and reliably so you don't ever have to worry."
 $pkg_maintainer="The Habitat Maintainers humans@habitat.sh"
 $pkg_upstream_url="https://yarnpkg.com/"
 $pkg_license=@("BSD-2-Clause")
 $pkg_filename="$pkg_name-$pkg_version.msi"
 $pkg_source="https://github.com/yarnpkg/yarn/releases/download/v${pkg_version}/${pkg_filename}"
-$pkg_shasum="6243f09fef4aa4e873f74133f94dc7dc715463dc14f9f8fe99e2627d99357bfb"
+$pkg_shasum="5f0eccd341a920d3eaf1398b22c5b6a633f21087205be1aa00afcd38037e7eed"
 $pkg_bin_dirs=@("bin")
 $pkg_deps=@("core/node")
 $pkg_build_deps=@("core/lessmsi")
