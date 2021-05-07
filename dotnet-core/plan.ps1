24fac11de42a198baf0146295b36c86954438c0a
Lucas Giancola <lucas.giancola@live.com>
2021-05-07 10:48:16 -0400

    Bumped 'dotnet-core' version "3.1.0" -> "3.1.14"
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- dotnet-core/plan.ps1
+++ dotnet-core/plan.ps1
@@ -1,6 +1,6 @@
 $pkg_name="dotnet-core"
 $pkg_origin="core"
-$pkg_version="3.1.0"
+$pkg_version="3.1.14"
 $pkg_license=('MIT')
 $pkg_upstream_url="https://www.microsoft.com/net/core"
 $pkg_description=".NET Core is a blazing fast, lightweight and modular platform
@@ -8,7 +8,7 @@ $pkg_description=".NET Core is a blazing fast, lightweight and modular platform
   Linux and Mac."
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_source="https://download.visualstudio.microsoft.com/download/pr/31b707c9-0484-48b5-b248-7f22946f88b5/a998787f1b26a7f742c84cbec7f145d2/dotnet-runtime-$pkg_version-win-x86.zip"
-$pkg_shasum="1b3cbe6bca79865a2a9ad0f716cab0754a9a4e8f43cb367e64a365949c47d86a"
+$pkg_shasum="aaca27f71c35cea5585e3b4ef521d8ef9f7b505a26c8979564c4cb77886fbfa2"
 $pkg_filename="dotnet-win-x64.$pkg_version.zip"
 $pkg_bin_dirs=@("bin")
 
