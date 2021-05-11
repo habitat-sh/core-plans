f789b2a0d4f092bbdd2760484ae63b71a206d85c
Sangameshwar Mandakanalli <sangameshwar.mandakanalli@progress.com>
2021-05-11 05:59:25 +0000

    docker-credential-helper bump version 0.6.0 to 0.6.3
    Signed-off-by: Sangameshwar Mandakanalli <sangameshwar.mandakanalli@progress.com>

--- docker-credential-helper/plan.ps1
+++ docker-credential-helper/plan.ps1
@@ -1,12 +1,12 @@
 $pkg_name = "docker-credential-helper"
 $pkg_description = "Docker Credential Helper"
 $pkg_origin = "core"
-$pkg_version = "0.6.0"
+$pkg_version = "0.6.3"
 $pkg_maintainer = "The Habitat Maintainers <humans@habitat.sh>"
 $pkg_license = @("MIT")
 $pkg_source = "https://github.com/docker/docker-credential-helpers/releases/download/v$pkg_version/docker-credential-wincred-v$pkg_version-amd64.zip"
 $pkg_upstream_url = "https://github.com/docker/docker-credential-helpers"
-$pkg_shasum = "e310dab940dcbcc5840c3687f288e1fc64384db88b4a427665a033461d06d576"
+$pkg_shasum = "837fa8f21c0bc94a72938970913bfe3804090cade0e96d570f47002a6c2e21f1"
 $pkg_bin_dirs = @("bin")
 
 function Invoke-Unpack {
