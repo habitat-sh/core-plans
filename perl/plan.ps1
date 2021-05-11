9ad1d488bf22484fcff5220fe1871b08a1dbe13b
Lucas Giancola <lucas.giancola@live.com>
2021-05-11 15:53:11 -0400

    Bumped 'perl' version "5.30.0" -> "5.33.9"
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- perl/plan.ps1
+++ perl/plan.ps1
@@ -1,11 +1,11 @@
 $pkg_name="perl"
 $pkg_origin="core"
-$pkg_version="5.30.0"
+$pkg_version="5.33.9"
 $pkg_description="Perl 5 is a highly capable, feature-rich programming language with over 29 years of development."
 $pkg_upstream_url="http://www.perl.org/"
 $pkg_license=@("GPL-1.0-or-later", "Artistic-1.0-Perl")
 $pkg_source="https://github.com/Perl/perl5/archive/v$pkg_version.zip"
-$pkg_shasum="2b3747deadea0510ef9e567e6a8b692e7dd4ecad024f1a5a6108b971093a95fd"
+$pkg_shasum="ff7e0e1259dbfd8be1fbfa8c11f980cbdbad15960f289d43adb06f6fcf693db3"
 $pkg_deps=@("core/visual-cpp-redist-2015")
 $pkg_build_deps=@("core/visual-cpp-build-tools-2015", "core/dmake")
 $pkg_bin_dirs=@("bin")
