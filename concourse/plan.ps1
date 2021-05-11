5095213cde2fda24501f5374c2f3365376c14ed0
Lucas Giancola <lucas.giancola@live.com>
2021-05-11 15:50:19 -0400

    Bumped 'concourse' version "4.2.2" -> "4.2.3"
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- concourse/plan.ps1
+++ concourse/plan.ps1
@@ -1,13 +1,13 @@
 $pkg_name="concourse"
 $pkg_origin="core"
-$pkg_version="4.2.2"
+$pkg_version="4.2.3"
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_license=('Apache-2.0')
 $pkg_description="CI that scales with your project"
 $pkg_upstream_url="https://concourse.ci"
 $pkg_source="https://github.com/${pkg_name}/${pkg_name}/releases/download/v${pkg_version}/${pkg_name}_windows_amd64.exe"
 $pkg_filename="${pkg_name}.exe"
-$pkg_shasum="d1ae2b0c6f2b10b11d793527b55a48813c421a6aa73b75a8496f093878114097"
+$pkg_shasum="f9f541c66e0ba6cbdb087197353aed50c851ec4abbdffeead4aac66d66d3a574"
 
 $pkg_bin_dirs=@("bin")
 
