96831fbef2371cd7b35025168ecacb7cf1b783ba
Lucas Giancola <lucas.giancola@live.com>
2021-05-11 16:02:18 -0400

    Bumped 'node' version "12.14.1" -> "12.22.1"
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- node/plan.ps1
+++ node/plan.ps1
@@ -1,12 +1,12 @@
 $pkg_name="node"
 $pkg_origin="core"
-$pkg_version="12.14.1"
+$pkg_version="12.22.1"
 $pkg_description="Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine."
 $pkg_upstream_url="https://nodejs.org/"
 $pkg_license=@("MIT")
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_source="https://nodejs.org/dist/v$pkg_version/node-v$pkg_version-x64.msi"
-$pkg_shasum="630bc34155e7fdb65c02ed44a37cd27dcf3f76a061c220e7af8baacdb0c2bb9c"
+$pkg_shasum="02f53b3530a0310b1076db801770803527c63f724b56c22d6a0625c12a03c982"
 $pkg_build_deps=@("core/lessmsi")
 $pkg_bin_dirs=@("bin")
 
