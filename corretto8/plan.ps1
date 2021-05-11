470897cbf8df44f2bed346b1f220f7fc3674df78
Lucas Giancola <lucas.giancola@live.com>
2021-05-11 15:21:44 -0400

    Bumped 'corretto8' version "8.212.04.2" -> "8.292.10.1"
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- corretto8/plan.ps1
+++ corretto8/plan.ps1
@@ -1,12 +1,12 @@
 $pkg_name="corretto8"
 $pkg_origin="core"
-$pkg_version="8.212.04.2"
+$pkg_version="8.292.10.1"
 $pkg_description="Corretto is a build of the Open Java Development Kit (OpenJDK) with long-term support from Amazon"
 $pkg_upstream_url="https://aws.amazon.com/corretto/"
 $pkg_license=@("GPLv2+CE")
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-$pkg_source="https://d3pxv6yz143wms.cloudfront.net/${pkg_version}/amazon-corretto-${pkg_version}-windows-x64-jdk.zip"
-$pkg_shasum="3c867c861f7aaf04f7d87dbc51e4075e0c45066e8a4b0ab2ac8cece8aa575c7e"
+$pkg_source=""https://corretto.aws/downloads/resources/${pkg_version}/amazon-corretto-${pkg_version}-windows-x64-jdk.zip
+$pkg_shasum="73fb262463cc7cf1a443a357f7e1dfa86c748171fd93050d940a5ecf40a9321a"
 $pkg_dirname="amazon-corretto-$pkg_version"
 $pkg_bin_dirs=@("bin")
 $pkg_lib_dirs=@("lib")
