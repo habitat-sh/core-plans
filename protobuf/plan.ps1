13a24e9abd27d87097a0175d1942d165821b5a4f
Lucas Giancola <lucas.giancola@live.com>
2021-05-11 15:33:19 -0400

    Bumped 'protobuf' version "3.12.3" -> "3.16.0"
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- protobuf/plan.ps1
+++ protobuf/plan.ps1
@@ -1,12 +1,12 @@
 $pkg_name="protobuf"
 $pkg_origin="core"
-$pkg_version="3.12.3"
+$pkg_version="3.16.0"
 $pkg_file_name=$pkg_name + ($pkg_version).Replace(".", "")
 $pkg_description="Protocol buffers are a language-neutral, platform-neutral extensible mechanism for serializing structured data."
 $pkg_upstream_url="https://developers.google.com/protocol-buffers/"
 $pkg_license=("BSD")
 $pkg_source="https://github.com/google/${pkg_name}/releases/download/v${pkg_version}/${pkg_name}-all-${pkg_version}.zip"
-$pkg_shasum="808cf33bc8b490e6ff707a1f6658c4d1fea8a8e3eed4c0273cecbb95c04ca4ec"
+$pkg_shasum="4c3899cbc84d5e3341ef884fb72950c3e444ad2a2806aee1bc7f83b2dda80956"
 $pkg_deps=@(
     "core/zlib"
 )
