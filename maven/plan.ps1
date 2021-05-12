0cf71148ae093b0f3b2479fe9f6e6d59391bca94
Lucas Giancola <lucas.giancola@live.com>
2021-05-12 12:07:50 -0400

    Bumped 'maven' version "3.6.3" -> "3.8.1"
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- maven/plan.ps1
+++ maven/plan.ps1
@@ -1,12 +1,12 @@
 $pkg_name="maven"
 $pkg_origin="core"
-$pkg_version="3.6.3"
+$pkg_version="3.8.1"
 $pkg_description="Apache Maven is a software project management and comprehension tool"
 $pkg_upstream_url="http://maven.apache.org/"
 $pkg_license=@("Apache-2.0")
 $pkg_deps=@("core/corretto8")
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-$pkg_source="http://mirrors.gigenet.com/apache/${pkg_name}/maven-3/${pkg_version}/binaries/apache-maven-${pkg_version}-bin.zip"
+$pkg_source="https://httpd-mirror.sergal.org/apache/${pkg_name}/maven-3/${pkg_version}/binaries/apache-maven-${pkg_version}-bin.zip"
 $pkg_shasum="444522b0af3a85e966f25c50adfcd00a1a6fc5fce79f503bff096e02b9977c2e"
 $pkg_dirname="apache-$pkg_name-$pkg_version"
 $pkg_bin_dirs=@("bin")
