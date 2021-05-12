1583a52152c1948571f87096ef539c91e71bb0b8
Lucas Giancola <lucas.giancola@live.com>
2021-05-12 10:15:49 -0400

    Bumped 'psscriptanalyzer' version "1.18.3" -> "1.19.1"
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- psscriptanalyzer/plan.ps1
+++ psscriptanalyzer/plan.ps1
@@ -1,12 +1,12 @@
 $pkg_name="psscriptanalyzer"
 $pkg_origin="core"
-$pkg_version="1.18.3"
+$pkg_version="1.19.1"
 $pkg_license=@('MIT')
 $pkg_upstream_url="https://github.com/PowerShell/PSScriptAnalyzer"
 $pkg_description="PSScriptAnalyzer is the ubiquitous linter for PowerShell"
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_source="https://github.com/PowerShell/PSScriptAnalyzer/releases/download/$pkg_version/PSScriptAnalyzer.zip"
-$pkg_shasum="8fcad735102fe3eaa9e090ec2ac09cfb7b1b2808ba00df1b2ff4c9a7383fc384"
+$pkg_shasum=""6938b5fed7fdd9277f1194bac2c802464f95e2e0838df065489d98eafdbcd901
 
 function Invoke-Install {
     mkdir "$pkg_prefix/module"
