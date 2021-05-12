1602f577d33020c4379974c1872a87abe71b853e
Lucas Giancola <lucas.giancola@live.com>
2021-05-12 11:14:30 -0400

    Bumped 'nuget' version "4.6.2" -> "4.9.4"
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- nuget/plan.ps1
+++ nuget/plan.ps1
@@ -1,12 +1,12 @@
 $pkg_name="nuget"
 $pkg_origin="core"
-$pkg_version="4.6.2"
+$pkg_version="4.9.4"
 $pkg_license=('Apache-2.0')
 $pkg_upstream_url="https://dist.nuget.org/index.html"
 $pkg_description="NuGet is the package manager for the Microsoft development platform including .NET."
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_source="https://dist.nuget.org/win-x86-commandline/v${pkg_version}/nuget.exe"
-$pkg_shasum="2c562c1a18d720d4885546083ec8eaad6773a6b80befb02564088cc1e55b304e"
+$pkg_shasum="cb139d855d06d07e7da892e8558fe16dcaa65cb381175c506f5ed0a759eaf8f6"
 $pkg_bin_dirs=@("bin")
 
 function Invoke-Unpack { }
