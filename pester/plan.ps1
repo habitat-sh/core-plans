84c1db60997907061b46f227b7d42f5a25a493df
Lucas Giancola <lucas.giancola@live.com>
2021-05-13 10:46:51 -0400

    Fixed shasum issue
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- pester/plan.ps1
+++ pester/plan.ps1
@@ -6,7 +6,7 @@ $pkg_upstream_url="https://github.com/pester/Pester"
 $pkg_description="Pester is the ubiquitous test and mock framework for PowerShell"
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_source="https://github.com/pester/Pester/archive/$pkg_version.zip"
-$pkg_shasum="b7161234bf0b0e750038f3e00ec73a95fabb136d52502c4736c7eb610bebd82e"
+$pkg_shasum="5fac0acf0bfb37b0f179bfa104acb368bb8cb01c590b00ab041de6d7ac803c68"
 $pkg_bin_dirs=@("module/bin")
 
 function Invoke-Install {
