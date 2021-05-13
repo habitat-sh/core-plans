570417a2b887a44763f77caab2a9d6cb4fa974ce
Lucas Giancola <lucas.giancola@live.com>
2021-05-13 11:19:27 -0400

    Fixed syntax of shasum
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- powershell/plan.ps1
+++ powershell/plan.ps1
@@ -6,7 +6,7 @@ $pkg_upstream_url="https://msdn.microsoft.com/powershell"
 $pkg_description="PowerShell is a cross-platform (Windows, Linux, and macOS) automation and configuration tool/framework that works well with your existing tools and is optimized for dealing with structured data (e.g. JSON, CSV, XML, etc.), REST APIs, and object models. It includes a command-line shell, an associated scripting language and a framework for processing cmdlets."
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_source="https://github.com/PowerShell/PowerShell/releases/download/v$pkg_version/PowerShell-$pkg_version-win-x64.zip"
-$pkg_shasum=""0b25c1e76ac055de94bb707c8c6576161971d687b930ad33f7d5da21202151e2
+$pkg_shasum="0b25c1e76ac055de94bb707c8c6576161971d687b930ad33f7d5da21202151e2"
 $pkg_filename="powershell-$pkg_version-win-x64.zip"
 $pkg_bin_dirs=@("bin")
 
