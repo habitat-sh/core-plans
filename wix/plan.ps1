c23ef4bed0507eef7c759000c139e1c6646058ad
Lucas Giancola <lucas.giancola@live.com>
2021-05-13 10:35:13 -0400

    Revert "Fixed shasum issue"
    This reverts commit e40e11d3e0f4c36fa1e497e8a0920fb4c2eac7e2.

--- wix/plan.ps1
+++ wix/plan.ps1
@@ -6,7 +6,7 @@ $pkg_upstream_url="http://wixtoolset.org/"
 $pkg_description="The most powerful set of tools available to create your windows installation experience."
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_source="https://wix.codeplex.com/downloads/get/1587180"
-$pkg_shasum="2c1888d5d1dba377fc7fa14444cf556963747ff9a0a289a3599cf09da03b9e2e"
+$pkg_shasum="493145b3fac22bdf8c55142a9f96ef8136d56b38d78a2322f13f1ba11f9cf2f8"
 $pkg_bin_dirs=@("bin")
 
 function Invoke-Unpack {
