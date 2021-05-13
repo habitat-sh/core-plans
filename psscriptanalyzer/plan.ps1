c0f87be009eb28a2da63f5759a53a682ab50296b
Lucas Giancola <lucas.giancola@live.com>
2021-05-13 11:20:24 -0400

    Fixed syntax of shasum
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- psscriptanalyzer/plan.ps1
+++ psscriptanalyzer/plan.ps1
@@ -6,7 +6,7 @@ $pkg_upstream_url="https://github.com/PowerShell/PSScriptAnalyzer"
 $pkg_description="PSScriptAnalyzer is the ubiquitous linter for PowerShell"
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_source="https://github.com/PowerShell/PSScriptAnalyzer/releases/download/$pkg_version/PSScriptAnalyzer.zip"
-$pkg_shasum=""6938b5fed7fdd9277f1194bac2c802464f95e2e0838df065489d98eafdbcd901
+$pkg_shasum="6938b5fed7fdd9277f1194bac2c802464f95e2e0838df065489d98eafdbcd901"
 
 function Invoke-Install {
     mkdir "$pkg_prefix/module"
