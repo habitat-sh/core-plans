78d542efb394860e6896fd125e9fc53fd4d807b5
Lucas Giancola <lucas.giancola@live.com>
2021-05-13 11:22:27 -0400

    Fixed syntax of shasum
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- dotnet-5-runtime/plan.ps1
+++ dotnet-5-runtime/plan.ps1
@@ -6,7 +6,7 @@ $pkg_upstream_url="https://dotnet.microsoft.com/"
 $pkg_description=".NET is a free, cross-platform, open-source developer platform for building many different types of applications."
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_source="https://dotnetcli.azureedge.net/dotnet/Runtime/$pkg_version/dotnet-runtime-$pkg_version-win-x64.zip"
-$pkg_shasum=""62464751fb0744e85acca14f14304ed21f0ab11b38ba16e43072cb3fc6ca16f7
+$pkg_shasum="62464751fb0744e85acca14f14304ed21f0ab11b38ba16e43072cb3fc6ca16f7"
 $pkg_bin_dirs=@("bin")
 
 function Invoke-Install {
