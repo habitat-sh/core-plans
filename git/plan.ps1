5691b0b7ad5da2114681cde9dc7a5b9bf4fb44b4
Lucas Giancola <lucas.giancola@live.com>
2021-05-13 10:48:27 -0400

    Fixed shasum issue
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- git/plan.ps1
+++ git/plan.ps1
@@ -8,7 +8,7 @@ $pkg_upstream_url = "https://git-scm.com/"
 $pkg_maintainer = "The Habitat Maintainers <humans@habitat.sh>"
 $pkg_license = @("GPL-2.0")
 $pkg_source = "https://github.com/git-for-windows/git/releases/download/v$pkg_version.windows.1/Git-$pkg_version-64-bit.tar.bz2"
-$pkg_shasum = "ce3f1105e0460cfa0d2c69c5311dbdb84136e5d060aaf2e2247b249ae1fc4601"
+$pkg_shasum = "5fac0acf0bfb37b0f179bfa104acb368bb8cb01c590b00ab041de6d7ac803c68"
 $pkg_bin_dirs = @("bin")
 $pkg_build_deps = @("core/7zip")
 
