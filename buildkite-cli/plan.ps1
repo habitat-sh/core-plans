68110ade7cd2aecfdf170b530b5f3caa52b1af18
Lucas Giancola <lucas.giancola@live.com>
2021-05-13 11:03:20 -0400

    Fixed shasum issue
    Signed-off-by: Lucas Giancola <lucas.giancola@live.com>

--- buildkite-cli/plan.ps1
+++ buildkite-cli/plan.ps1
@@ -7,7 +7,7 @@ $pkg_description="A command line interface for Buildkite"
 $pkg_deps=@("core/git", "core/buildkite-agent")
 $pkg_source="https://github.com/buildkite/cli/releases/download/v${pkg_version}/bk-windows-amd64-${pkg_version}.exe"
 $pkg_filename="bk-windows-amd64-${pkg_version}.exe"
-$pkg_shasum="3e57243bc4b179e44478410d18466b14ab3aa29c38876065a690789a408e04bd"
+$pkg_shasum="24d8a06f75f1916890fa8acb92dee71e01b3943daf4e094f44cce48a8834f7f2"
 $pkg_upstream_url="https://buildkite.com"
 $pkg_bin_dirs=@("bin")
 
