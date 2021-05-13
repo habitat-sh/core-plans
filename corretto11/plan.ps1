cc9b74f82d1417b40350789ed4c8d8bce64ea8e9
Sangameshwar Mandakanalli <sangameshwar.mandakanalli@progress.com>
2021-05-13 05:49:02 +0000

    pkg_source updated properly
    Signed-off-by: Sangameshwar Mandakanalli <sangameshwar.mandakanalli@progress.com>

--- corretto11/plan.ps1
+++ corretto11/plan.ps1
@@ -7,5 +7,8 @@ $pkg_description="Corretto is a build of the Open Java Development Kit (OpenJDK)
 $pkg_upstream_url="https://aws.amazon.com/corretto/"
 $pkg_license=@("GPLv2+CE")
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-$pkg_source="https://d3pxv6yz143wms.cloudfront.net/${pkg_version}/amazon-corretto-${pkg_version}-windows-x64.zip"
+$pkg_source="https://corretto.aws/downloads/resources/${pkg_version}/amazon-corretto-${pkg_version}-windows-x64-jdk.zip"
 $pkg_shasum="2c51cddecaf5f6cf2e37ecbae168d4c759cf56cfbda80dab3f3c032ac5a38b1d"
+
+
+
