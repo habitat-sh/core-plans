46dab3d6e5d3875e78f1235536685fae23d4a10f
Sangameshwar Mandakanalli <sangameshwar.mandakanalli@progress.com>
2021-05-17 10:12:20 +0000

    pkg_source updated properly
    Signed-off-by: Sangameshwar Mandakanalli <sangameshwar.mandakanalli@progress.com>

--- dotnet-core/plan.ps1
+++ dotnet-core/plan.ps1
@@ -7,8 +7,8 @@ $pkg_description=".NET Core is a blazing fast, lightweight and modular platform
   for creating web applications and services that run on Windows,
   Linux and Mac."
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-$pkg_source="https://download.visualstudio.microsoft.com/download/pr/31b707c9-0484-48b5-b248-7f22946f88b5/a998787f1b26a7f742c84cbec7f145d2/dotnet-runtime-$pkg_version-win-x86.zip"
-$pkg_shasum="aaca27f71c35cea5585e3b4ef521d8ef9f7b505a26c8979564c4cb77886fbfa2"
+$pkg_source="https://download.visualstudio.microsoft.com/download/pr/d88fda36-5e76-447e-8ed4-c3bd4151663c/c2d1485326cdeaab2168ee1d0ced1e0a/dotnet-runtime-$pkg_version-win-x64.zip"
+$pkg_shasum="18abe372088ffe1e0b7479440254eff4901bfa262a35c4ed2971cdfe58159fc1"
 $pkg_filename="dotnet-win-x64.$pkg_version.zip"
 $pkg_bin_dirs=@("bin")
 
