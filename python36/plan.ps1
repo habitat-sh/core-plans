e6adcff5074a81df321210e82123b3ed5e72d549
Nimit Jyotiana <nimit.jyotiana@hotmail.com>
2021-05-18 05:02:49 +0000

    version bumped to python v3.6.8
    It is latest available version for Windows, though lastest product version is python v3.6.13
    
    Signed-off-by: Nimit Jyotiana <nimit.jyotiana@hotmail.com>

--- python36/plan.ps1
+++ python36/plan.ps1
@@ -1,7 +1,7 @@
 . ../python/plan.ps1
 
 $pkg_name="python36"
-$pkg_version="3.6.13"
+$pkg_version="3.6.8"
 $pkg_origin="core"
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_license=@('Python-2.0')
