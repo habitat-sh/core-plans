cf503002e86aee8000f327a937d0b70ca252f509
Matt Wrock <matt@mattwrock.com>
2021-05-26 16:45:16 -0700

    use nmake instead of dmake
    Signed-off-by: Matt Wrock <matt@mattwrock.com>

--- perl/plan.ps1
+++ perl/plan.ps1
@@ -7,7 +7,7 @@ $pkg_license=@("GPL-1.0-or-later", "Artistic-1.0-Perl")
 $pkg_source="https://github.com/Perl/perl5/archive/v$pkg_version.zip"
 $pkg_shasum="d897bf5a4db8dd038394bb65507710d83287c320130f65050e718a57e4d0d924"
 $pkg_deps=@("core/visual-cpp-redist-2015")
-$pkg_build_deps=@("core/visual-cpp-build-tools-2015", "core/dmake")
+$pkg_build_deps=@("core/visual-cpp-build-tools-2015")
 $pkg_bin_dirs=@("bin")
 $pkg_lib_dirs=@("lib")
 
@@ -19,16 +19,16 @@ function Invoke-Build {
     $Env:CCTYPE   = "MSVC140"
     $Env:INST_TOP = "$pkg_prefix"
     Set-Location perl5-$pkg_version\win32
-    dmake
-    if($LASTEXITCODE -ne 0) { Write-Error "dmake failed!" }
+    nmake
+    if($LASTEXITCODE -ne 0) { Write-Error "nmake failed!" }
 }
 
 function Invoke-Install {
     $Env:CCTYPE   = "MSVC140"
     $Env:INST_TOP = "$pkg_prefix"
     Set-Location perl5-$pkg_version\win32
-    dmake install
-    if($LASTEXITCODE -ne 0) { Write-Error "dmake failed!" }
+    nmake install
+    if($LASTEXITCODE -ne 0) { Write-Error "nmake failed!" }
 }
 
 function Invoke-Check {
