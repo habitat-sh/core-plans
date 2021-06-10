0777532ed7e4ae54fd0e68913f814b3e4c413d3b
Siraj Rauff <sirajrauff@gmail.com>
2021-06-10 11:28:22 -0400

    Fixing 404 for htop release tarball, compiling from source tarball
    Signed-off-by: Siraj Rauff <sirajrauff@gmail.com>

--- htop/plan.sh
+++ htop/plan.sh
@@ -2,21 +2,26 @@ pkg_origin=core
 pkg_name=htop
 pkg_version=3.0.5
 pkg_description="This is htop, an interactive process viewer for Unix systems. It is a text-mode application (for console or X terminals) and requires ncurses."
-pkg_upstream_url="https://bintray.com/htop/"
+pkg_upstream_url="https://htop.dev"
 pkg_license=("GPL-2.0")
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_source="https://bintray.com/htop/source/download_file?file_path=htop-${pkg_version}.tar.gz"
-pkg_shasum="19535f8f01ac08be2df880c93c9cedfc50fa92320d48e3ef92a30b6edc4d1917"
+pkg_source="https://github.com/htop-dev/htop/archive/refs/tags/${pkg_version}.tar.gz"
+pkg_shasum="4c2629bd50895bd24082ba2f81f8c972348aa2298cc6edc6a21a7fa18b73990c"
 
 pkg_deps=(
   core/glibc
   core/ncurses
 )
 pkg_build_deps=(
+  core/autoconf
+  core/automake
   core/gcc
-  core/make
 )
 pkg_bin_dirs=(bin)
+
+do_prepare() {
+  ./autogen.sh
+}
 
 do_check() {
   make test
