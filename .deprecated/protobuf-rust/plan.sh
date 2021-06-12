1e233de0971c570b870c3aa9e21e5f27014a94db
Siraj Rauff <sirajrauff@gmail.com>
2021-06-12 12:20:39 -0400

    Pinning glibc/gcc-libs in protobuf-rust due to build failure
    Signed-off-by: Siraj Rauff <sirajrauff@gmail.com>

--- .deprecated/protobuf-rust/plan.sh
+++ .deprecated/protobuf-rust/plan.sh
@@ -7,8 +7,13 @@ pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_upstream_url="https://github.com/stepancheg/rust-protobuf"
 pkg_description="Rust implementation of Google protocol buffers"
 pkg_deps=(
-  core/glibc
-  core/gcc-libs
+  # Due to an update with glibc moving libpthread.so.0 contents into libc, the build
+  # will fail on glibc 2.32 and newer with 'relocation error: [..]-script-build: symbol pthread_sigmask
+  #   version GLIBC_2.2.5 not defined in file libpthread.so.0 with link time reference'
+  # Pin this to glibc 2.29, and gcc-libs 9.1.0 (dependency conflicts) to prevent this until the underlying
+  # dep is updated (protobuf v1.7.5)
+  core/glibc/2.29
+  core/gcc-libs/9.1.0
 )
 pkg_build_deps=(
   core/rust
