e5693ae2ebbe87899e451a045cc0184ecc494bd9
Siraj Rauff <sirajrauff@gmail.com>
2021-06-17 17:17:40 -0400

    Revert "Bumped 'dpkg' version 1.19.7 -> 1.20.7.1"
    This reverts commit ac093ea513e373fa6b6d88f0a2e3adcb48f562f0.
    
    Package was only in testing, 1.19.7 is latest stable

--- dpkg/plan.sh
+++ dpkg/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=dpkg
 pkg_origin=core
-pkg_version=1.20.7.1
+pkg_version=1.19.7
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('GPL-2.0-or-later')
 pkg_upstream_url="https://wiki.debian.org/dpkg"
 pkg_description="dpkg is a package manager for Debian-based systems"
 pkg_source="http://http.debian.net/debian/pool/main/d/${pkg_name}/${pkg_name}_${pkg_version}.tar.xz"
-pkg_shasum="0aad2de687f797ef8ebdabc7bafd16dc1497f1ce23bd9146f9aa73f396a5636f"
+pkg_shasum="4c27fededf620c0aa522fff1a48577ba08144445341257502e7730f2b1a296e8"
 pkg_deps=(
   core/bzip2
   core/glibc
