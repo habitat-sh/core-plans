830fe5184803a52fcdd6d795d86620ead46ba3de
Siraj Rauff <sirajrauff@gmail.com>
2021-06-17 19:19:02 -0400

    Reverting pinning of deps, needed updated rust
    Signed-off-by: Siraj Rauff <sirajrauff@gmail.com>

--- sccache/plan.sh
+++ sccache/plan.sh
@@ -9,14 +9,9 @@ pkg_source="https://github.com/mozilla/sccache/archive/v${pkg_version}.tar.gz"
 pkg_shasum=7dbe71012f9b0b57d8475de6b36a9a3b4802e44a135e886f32c5ad1b0eb506e0
 pkg_bin_dirs=(bin)
 pkg_deps=(
-  # Due to an update with glibc moving libpthread.so.0 contents into libc, the build
-  # will fail on glibc 2.32 and newer with 'relocation error: [..]-script-build: symbol pthread_getattr_np
-  #   version GLIBC_2.2.5 not defined in file libpthread.so.0 with link time reference'
-  # Pin this to glibc 2.29, and gcc-libs 9.1.0 (dependency conflicts) to prevent this until the underlying
-  # dep is updated (log v0.4.11)
-  core/glibc/2.29
-  core/gcc-libs/9.1.0
-  core/openssl/1.0.2w/20210514035028
+  core/glibc
+  core/gcc-libs
+  core/openssl
 )
 pkg_build_deps=(
   core/rust
