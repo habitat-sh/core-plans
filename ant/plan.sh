4adcb6186f6bc3f1c02a9371fed1d239ae084751
Siraj Rauff <sirajrauff@gmail.com>
2021-07-21 10:37:03 -0400

    Updating to 1.10.11, using ant-provided build instructions
    Signed-off-by: Siraj Rauff <sirajrauff@gmail.com>

--- ant/plan.sh
+++ ant/plan.sh
@@ -1,12 +1,13 @@
 pkg_origin=core
 pkg_name=ant
-pkg_version=1.10.9
+pkg_version=1.10.11
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("Apache-2.0")
 pkg_description="Ant is a Java based build tool."
 pkg_upstream_url=https://ant.apache.org/
 pkg_source="https://github.com/apache/ant/archive/rel/${pkg_version}.tar.gz"
-pkg_shasum=0f676da0507ca750f7cf77cbe7750a44213dc611a94e78a47870e79417e09776
+pkg_dirname="${pkg_name}-rel-${pkg_version}"
+pkg_shasum=55a5afe0d40449667162b1cdc6478bc1d99370ddcbb63d708913921fa22b6232
 pkg_build_deps=(
   core/python2
 )
@@ -18,21 +19,25 @@ pkg_deps=(
 pkg_bin_dirs=(bin)
 pkg_lib_dirs=(lib)
 
+do_setup_environment() {
+  set_runtime_env ANT_HOME "${pkg_prefix}"
+  set_buildtime_env ANT_HOME "${pkg_prefix}"
+}
+
 do_prepare() {
   JAVA_HOME="$(pkg_path_for core/corretto8)"
   export JAVA_HOME
-  cd "${HAB_CACHE_SRC_PATH}/${pkg_name}-rel-${pkg_version}"
+  build_line "Setting JAVA_HOME=${JAVA_HOME}"
+
   sed -i 's|/usr/bin/python|/usr/bin/python2|' src/script/runant.py
 }
 
 do_build() {
-  cd "${HAB_CACHE_SRC_PATH}/${pkg_name}-rel-${pkg_version}"
-  ./bootstrap.sh
-  bootstrap/bin/ant -Ddest=optional -f fetch.xml
-  bootstrap/bin/ant dist
+  sh build.sh \
+    -Ddist.dir="${pkg_prefix}" \
+    dist
 }
 
 do_install() {
-  mv "${HAB_CACHE_SRC_PATH}/${pkg_name}-rel-${pkg_version}/apache-${pkg_name}-${pkg_version}/bin/"* "${pkg_prefix}/bin/"
-  mv "${HAB_CACHE_SRC_PATH}/${pkg_name}-rel-${pkg_version}/apache-${pkg_name}-${pkg_version}/lib/"* "${pkg_prefix}/lib/"
+  sh build.sh install
 }
