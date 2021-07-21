b01d0a2a254fb1fb518d0b773011eb94494011c5
Siraj Rauff <sirajrauff@gmail.com>
2021-07-21 02:28:08 -0400

    Updating to 5.4.2, removing redundant libiconv dep (glibc provides)
    Signed-off-by: Siraj Rauff <sirajrauff@gmail.com>

--- gnuplot/plan.sh
+++ gnuplot/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=gnuplot
 pkg_origin=core
-pkg_version="5.4.1"
+pkg_version="5.4.2"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_upstream_url="http://www.gnuplot.info"
 pkg_description="Gnuplot is a portable command-line driven graphing
@@ -8,7 +8,7 @@ utility for Linux, OS/2, MS Windows, OSX, VMS, and many other
 platforms"
 pkg_license=('gnuplot')
 pkg_source="https://sourceforge.net/projects/$pkg_name/files/$pkg_name/$pkg_version/$pkg_name-$pkg_version.tar.gz"
-pkg_shasum="6b690485567eaeb938c26936e5e0681cf70c856d273cc2c45fabf64d8bc6590e"
+pkg_shasum="e57c75e1318133951d32a83bcdc4aff17fed28722c4e71f2305cfc2ae1cae7ba"
 pkg_deps=(
   core/bzip2
   core/cairo
@@ -24,7 +24,6 @@ pkg_deps=(
   core/liberation-fonts-ttf
   core/libffi
   core/libice
-  core/libiconv
   core/libgd
   core/libjpeg-turbo
   core/libpng
