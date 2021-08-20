c33d1483da05cd00a26d85c810072338aeaabb04
Jesse Prieur <jesse.prieur@gmail.com>
2021-08-19 21:24:21 -0400

    Updating version to 1.6.1, along with new shasum; Also added entry .bldr.toml
    Signed-off-by: Jesse Prieur <jesse.prieur@gmail.com>

--- lessmsi/plan.ps1
+++ lessmsi/plan.ps1
@@ -1,12 +1,12 @@
 $pkg_name="lessmsi"
 $pkg_origin="core"
-$pkg_version="1.6.1"
+$pkg_version="1.8.1"
 $pkg_license=('MIT')
 $pkg_upstream_url="http://lessmsi.activescott.com/"
 $pkg_description="A tool to view and extract the contents of a Windows Installer (.msi) file."
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_source="https://github.com/activescott/lessmsi/releases/download/v$pkg_version/lessmsi-v${pkg_version}.zip"
-$pkg_shasum="540b8801e08ec39ba26a100c855898f455410cecbae4991afae7bb2b4df026c7"
+$pkg_shasum="1309c1ba220806ca54c4603e53463c4ab50a9d1dc74d9da7abfe0822f2778012"
 $pkg_bin_dirs=@("bin")
 
 function Invoke-Unpack {
