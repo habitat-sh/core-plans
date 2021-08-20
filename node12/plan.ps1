0cbb90b3b511f98224db7126c69b0e92ddcfd132
Jesse Prieur <jesse.prieur@gmail.com>
2021-08-20 12:11:09 -0400

    Fixing Invoke-Check
    Signed-off-by: Jesse Prieur <jesse.prieur@gmail.com>

--- node12/plan.ps1
+++ node12/plan.ps1
@@ -6,3 +6,7 @@ $pkg_version="12.22.1"
 $pkg_description="Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine."
 $pkg_source="https://nodejs.org/dist/v$pkg_version/node-v$pkg_version-x64.msi"
 $pkg_shasum="02f53b3530a0310b1076db801770803527c63f724b56c22d6a0625c12a03c982"
+
+function Invoke-Check() {
+    (& "$HAB_CACHE_SRC_PATH/$pkg_dirname/node-v$pkg_version-x64/SourceDir/nodejs/node" --version) -eq "v$pkg_version"
+}
