7a284281c6f361f7bad611a579d53ae7dddc55f6
Siraj Rauff <sirajrauff@gmail.com>
2021-08-30 19:53:35 -0400

    Adding default variables for CI
    Signed-off-by: Siraj Rauff <sirajrauff@gmail.com>

--- libressl-musl/plan.sh
+++ libressl-musl/plan.sh
@@ -1,8 +1,13 @@
 source ../libressl/plan.sh
 
 pkg_name=libressl-musl
+pkg_origin=core
 # 2.9.2 is broken for musl
 pkg_version=2.9.1
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_description="Version of the TLS/crypto stack forked from OpenSSL"
+pkg_license=('OpenSSL')
+pkg_upstream_url=http://www.libressl.org/
 pkg_shasum=39e4dd856694dc10d564201e4549c46d2431601a2b10f3422507e24ccc8f62f8
 pkg_deps+=(core/musl)
 
