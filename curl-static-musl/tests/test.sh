71769828f9bd4d667cd090cb1b97b48158c7c3cd
Siraj Rauff <sirajrauff@gmail.com>
2021-09-01 12:19:31 -0400

    Copy tests from deprecated cURL into curl-static-musl
    Signed-off-by: Siraj Rauff <sirajrauff@gmail.com>

--- curl-static-musl/tests/test.sh
+++ curl-static-musl/tests/test.sh
@@ -1,1 +1,18 @@
-source "$(dirname "${BASH_SOURCE[0]}")/../../curl/tests/test.sh"
+#!/bin/sh
+#/ Usage: test.sh <pkg_ident>
+#/
+#/ Example: test.sh core/curl-static-musl/7.65.1/20190703171140
+#/
+
+set -euo pipefail
+
+if [[ -z "${1:-}" ]]; then
+  grep '^#/' < "${0}" | cut -c4-
+	exit 1
+fi
+
+TEST_PKG_IDENT="${1}"
+export TEST_PKG_IDENT
+hab pkg install core/bats core/jq-static --binlink
+hab pkg install "${TEST_PKG_IDENT}"
+bats "$(dirname "${0}")/test.bats"
