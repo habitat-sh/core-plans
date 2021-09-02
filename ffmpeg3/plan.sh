68d7a097ddc1fadf17242e5ba32e35855afe9955
Siraj Rauff <sirajrauff@gmail.com>
2021-09-01 21:31:06 -0400

    Adding default variables back in
    Signed-off-by: Siraj Rauff <sirajrauff@gmail.com>

--- ffmpeg3/plan.sh
+++ ffmpeg3/plan.sh
@@ -1,8 +1,13 @@
 source "$(dirname "${BASH_SOURCE[0]}")/../ffmpeg/plan.sh"
 
 pkg_name=ffmpeg3
+pkg_origin=core
 pkg_distname=ffmpeg
 pkg_version=3.4.8
+pkg_description="A complete, cross-platform solution to record, convert and stream audio and video."
+pkg_upstream_url=https://ffmpeg.org/
+pkg_license=('LGPL-2.1-or-later' 'GPL-2.0-or-later')
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="https://ffmpeg.org/releases/${pkg_distname}-${pkg_version}.tar.gz"
 pkg_shasum=839af372d8e52b1998aa67a433615e7607519107367af89a2714fa56f53d3d70
 pkg_dirname="ffmpeg-$pkg_version"
