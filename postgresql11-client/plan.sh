0841a38d69c988b8ff578dd3669b15dde1f73140
Sangameshwar Mandakanalli <smandaka@progress.com>
2021-11-12 17:45:48 +0530

    postgresql11-client from 11.11 to 11.14 And postgresql96-client from 9.6.21 to 9.6.24
    Signed-off-by: Sangameshwar Mandakanalli <smandaka@progress.com>

--- postgresql11-client/plan.sh
+++ postgresql11-client/plan.sh
@@ -1,5 +1,5 @@
 pkg_name=postgresql11-client
-pkg_version=11.11
+pkg_version=11.14
 pkg_origin=core
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="PostgreSQL is a powerful, open source object-relational database system."
@@ -7,7 +7,7 @@ pkg_upstream_url="https://www.postgresql.org/"
 pkg_license=('PostgreSQL')
 pkg_dirname="postgresql-${pkg_version}"
 pkg_source="https://ftp.postgresql.org/pub/source/v${pkg_version}/${pkg_dirname}.tar.bz2"
-pkg_shasum="40607b7fa15b7d63f5075a7277daf7b3412486aa5db3aedffdb7768b9298186c"
+pkg_shasum="965c7f4be96fb64f9581852c58c4f05c3812d4ad823c0f3e2bdfe777c162f999"
 
 pkg_deps=(
   core/bash
--- postgresql96-client/plan.sh
+++ postgresql96-client/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=postgresql96-client
-pkg_version=9.6.21
+pkg_version=9.6.24
 pkg_origin=core
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="PostgreSQL is a powerful, open source object-relational database system."
 pkg_upstream_url="https://www.postgresql.org/"
 pkg_license=('PostgreSQL')
 pkg_source="https://ftp.postgresql.org/pub/source/v${pkg_version}/postgresql-${pkg_version}.tar.bz2"
-pkg_shasum="930feaef28885c97ec40c26ab6221903751eeb625de92b22602706d7d47d1634"
+pkg_shasum="aeb7a196be3ebed1a7476ef565f39722187c108dd47da7489be9c4fcae982ace"
 pkg_dirname="postgresql-${pkg_version}"
 
 pkg_deps=(
