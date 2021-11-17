1b6507d2c3df099257ceb3c73cb00da41656845d
RaviDuddela <RaviShankar.Duddela@progress.com>
2021-11-18 00:42:13 +0530

    updating gsl
    Signed-off-by: RaviDuddela <RaviShankar.Duddela@progress.com>

--- gsl/plan.sh
+++ gsl/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=gsl
 pkg_origin=core
-pkg_version=2.6
+pkg_version=2.7
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('GPLv3')
 pkg_description="GSL is a numerical library for C and C++"
 pkg_upstream_url="https://www.gnu.org/software/gsl/"
 pkg_source=http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.gz
-pkg_shasum=b782339fc7a38fe17689cb39966c4d821236c28018b6593ddb6fd59ee40786a8
+pkg_shasum=efbbf3785da0e53038be7907500628b466152dbc3c173a87de1b5eba2e23602b
 pkg_build_deps=(core/make core/gcc core/diffutils)
 pkg_deps=(core/glibc)
 pkg_include_dirs=(include)
