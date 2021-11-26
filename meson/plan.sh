c0c575cb96b7197083ddfdbed7558aefcf54377e
Phani Sajja <sajja.phani@gmail.com>
2021-11-26 12:43:34 +0530

    Bumping package version to 0.60.2
    Signed-off-by:  Phani Sajja <sajja.phani@gmail.com>

--- meson/plan.sh
+++ meson/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=meson
 pkg_origin=core
-pkg_version=0.60.1
+pkg_version=0.60.2
 pkg_description="The Meson Build System"
 pkg_upstream_url="http://mesonbuild.com/"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('Apache-2.0')
 pkg_source="https://github.com/mesonbuild/${pkg_name}/archive/${pkg_version}.tar.gz"
-pkg_shasum=b06f7d621b90e094be0ea2157fa435648e069f19182d8d9402aa039727652b0c
+pkg_shasum=fc7c2f315b5b63fee0414b0b94b5a7d0e9c71c8c9bb8487314eb5a9a33984b8d
 pkg_deps=(
   # https://github.com/mesonbuild/meson/issues/7999
   # Doesn't seem to work against 3.9
