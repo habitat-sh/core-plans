0870ad3fad737443948e615f17453ba421071aa5
RaviDuddela <RaviShankar.Duddela@progress.com>
2021-12-01 08:13:34 +0530

    upgrading flatbuffers and freetds
    Signed-off-by: RaviDuddela <RaviShankar.Duddela@progress.com>

--- freetds/plan.sh
+++ freetds/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=freetds
 pkg_origin=core
-pkg_version=1.2.18
+pkg_version=1.3.3
 pkg_license=('LGPL2')
 pkg_upstream_url="http://www.freetds.org"
 pkg_description="FreeTDS is a set of libraries for Unix and Linux that allows your programs to natively talk to Microsoft SQL Server and Sybase databases."
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="http://www.freetds.org/files/stable/${pkg_name}-${pkg_version}.tar.bz2"
-pkg_shasum="10d47e60986cfc51f8170fa9eaba43114eebdde0ba6e6b1c4813d86f021aaadd"
+pkg_shasum="dba02e7f0661ff42dc289fc41d7cde7e03089fd326f08ee28c872ac9ff4a1c84"
 
 pkg_bin_dirs=(bin)
 pkg_lib_dirs=(lib)
