2d484b129c0e499fe4e07667549151ee1b49efb8
Nimit <nimit.jyotiana@hotmail.com>
2021-12-03 15:55:21 +0530

    upgraded vault to v1.9.0
    Signed-off-by: Nimit <nimit.jyotiana@hotmail.com>

--- vault/plan.sh
+++ vault/plan.sh
@@ -1,12 +1,12 @@
 pkg_origin=core
 pkg_name=vault
-pkg_version=1.8.5
+pkg_version=1.9.0
 pkg_description="A tool for managing secrets."
 pkg_maintainer='The Habitat Maintainers <humans@habitat.sh>'
 pkg_license=("MPL-2.0")
 pkg_upstream_url=https://www.vaultproject.io/
 pkg_source="https://releases.hashicorp.com/vault/${pkg_version}/vault_${pkg_version}_linux_amd64.zip"
-pkg_shasum=97d7ac9145ffdf256ab91045a4857a640ecaa7c56bbd66b61fb70990620f3d33
+pkg_shasum=6ad8b09ca70d5c2822c871b7bd20ecdefb68e9da4c4b9aa6062000a9bff19f7a
 pkg_filename="${pkg_name}-${pkg_version}_linux_amd64.zip"
 pkg_deps=()
 pkg_build_deps=(core/unzip)
