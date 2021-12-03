8783fc9a65be772a3d2ad82905cbe0194bb22fb9
Nimit <nimit.jyotiana@hotmail.com>
2021-12-03 17:35:29 +0530

    upgraded nettle to v3.7.3
    Signed-off-by: Nimit <nimit.jyotiana@hotmail.com>

--- nettle/plan.sh
+++ nettle/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=nettle
 pkg_origin=core
-pkg_version=3.7.2
+pkg_version=3.7.3
 pkg_description="A low-level cryptographic library"
 pkg_upstream_url="https://www.lysator.liu.se/~nisse/nettle/"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('LGPL-3.0-only' 'GPL-2.0-only' 'GPL-3.0-only')
 pkg_source="https://ftp.gnu.org/gnu/${pkg_name}/${pkg_name}-${pkg_version}.tar.gz"
-pkg_shasum=8d2a604ef1cde4cd5fb77e422531ea25ad064679ff0adf956e78b3352e0ef162
+pkg_shasum=661f5eb03f048a3b924c3a8ad2515d4068e40f67e774e8a26827658007e3bcf0
 pkg_deps=(
   core/glibc
   core/gmp
