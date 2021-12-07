760982bd795d854ce4b4c277a93066c4e8a589c5
RaviDuddela <RaviShankar.Duddela@progress.com>
2021-12-07 13:12:20 +0530

    updating doxygen,lua,nss and rlwrap
    Signed-off-by: RaviDuddela <RaviShankar.Duddela@progress.com>

--- rlwrap/plan.sh
+++ rlwrap/plan.sh
@@ -1,12 +1,12 @@
 pkg_origin=core
 pkg_name=rlwrap
-pkg_version=0.44
+pkg_version=0.45.2
 pkg_description="A readline wrapper"
 pkg_upstream_url=https://github.com/hanslub42/rlwrap
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("GPL-2.0")
-pkg_source=https://github.com/hanslub42/rlwrap/releases/download/${pkg_version}/${pkg_name}-${pkg_version}.tar.gz
-pkg_shasum=cd7ff50cde66e443cbea0049b4abf1cca64a74948371fa4f1b5d9a5bbce1e13c
+pkg_source=https://github.com/hanslub42/rlwrap/releases/download/v${pkg_version}/${pkg_name}-${pkg_version}.tar.gz
+pkg_shasum=9f8870deb46e473d21b5db89d709b6497f4ef9fa06d44eebc5f821daa00c8eca
 pkg_deps=(
   core/ncurses
   core/readline
