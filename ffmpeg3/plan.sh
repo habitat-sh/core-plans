640041d9210417ca13d90ff5d4856bb9ae2fb99e
Sangameshwar Mandakanalli <smandaka@progress.com>
2021-12-09 15:22:05 +0530

    drush to 8.4.8, ffmgeg3 to 3.4.9 and ffmpeg4 to 4.4.1 bumped

--- ffmpeg3/plan.sh
+++ ffmpeg3/plan.sh
@@ -3,13 +3,13 @@ source "$(dirname "${BASH_SOURCE[0]}")/../ffmpeg/plan.sh"
 pkg_name=ffmpeg3
 pkg_origin=core
 pkg_distname=ffmpeg
-pkg_version=3.4.8
+pkg_version=3.4.9
 pkg_description="A complete, cross-platform solution to record, convert and stream audio and video."
 pkg_upstream_url=https://ffmpeg.org/
 pkg_license=('LGPL-2.1-or-later' 'GPL-2.0-or-later')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="https://ffmpeg.org/releases/${pkg_distname}-${pkg_version}.tar.gz"
-pkg_shasum=839af372d8e52b1998aa67a433615e7607519107367af89a2714fa56f53d3d70
+pkg_shasum=afb2c2f8b97f202d6a9f7633bcb38bdebc3c54c5bad91278c50c4292aaac730f
 pkg_dirname="ffmpeg-$pkg_version"
 
 pkg_deps=(
