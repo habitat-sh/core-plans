1a7a8ced095dec1d903d900af1add9280ba22d92
Sangameshwar Mandakanalli <smandaka@progress.com>
2021-12-10 13:37:27 +0530

    bdwgc7 to 7.6.14 and bdwgc8 to 8.2.0 bumped
    Signed-off-by: Sangameshwar Mandakanalli <smandaka@progress.com>

--- bdwgc8/plan.sh
+++ bdwgc8/plan.sh
@@ -2,11 +2,11 @@ source "$(dirname "${BASH_SOURCE[0]}")/../bdwgc/plan.sh"
 
 pkg_name=bdwgc8
 pkg_origin=core
-pkg_version=8.0.4
+pkg_version=8.2.0
 pkg_description="A garbage collector for C and C++"
 pkg_upstream_url="http://www.hboehm.info/gc/"
 pkg_license=('X11 style license')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="https://github.com/ivmai/bdwgc/releases/download/v${pkg_version}/gc-${pkg_version}.tar.gz"
 pkg_dirname="gc-${pkg_version}"
-pkg_shasum="436a0ddc67b1ac0b0405b61a9675bca9e075c8156f4debd1d06f3a56c7cd289d"
+pkg_shasum="2540f7356cb74f6c5b75326c6d38a066edd796361fd7d4ed26e494d9856fed8f"
