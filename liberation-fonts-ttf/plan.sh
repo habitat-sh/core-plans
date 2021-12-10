6a01d4c5d18b41403bd30cf396c1d185f72ed46a
RaviDuddela <RaviShankar.Duddela@progress.com>
2021-12-10 12:10:20 +0530

    updating imagemagick and liberation-fonts-ttf
    Signed-off-by: RaviDuddela <RaviShankar.Duddela@progress.com>

--- liberation-fonts-ttf/plan.sh
+++ liberation-fonts-ttf/plan.sh
@@ -1,10 +1,10 @@
 pkg_name=liberation-fonts-ttf
 pkg_origin=core
-pkg_version="2.00.1"
+pkg_version="2.1.5"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('OFL-1.1')
-pkg_source="https://releases.pagure.org/liberation-fonts/$pkg_name-$pkg_version.tar.gz"
-pkg_shasum="7890278a6cd17873c57d9cd785c2d230d9abdea837e96516019c5885dd271504"
+pkg_source="https://github.com/liberationfonts/liberation-fonts/files/7261482/$pkg_name-$pkg_version.tar.gz"
+pkg_shasum="7191c669bf38899f73a2094ed00f7b800553364f90e2637010a69c0e268f25d0"
 pkg_deps=(core/fontconfig)
 pkg_description="The Liberation Fonts are intended to be replacements for the three most commonly used fonts on Microsoft systems: Times New Roman, Arial, and Courier New."
 pkg_upstream_url="https://pagure.io/liberation-fonts"
