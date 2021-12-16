07b28cad8e2abfdc3de9d92263848dbee3fdc040
Sangameshwar Mandakanalli <smandaka@progress.com>
2021-12-16 17:40:48 +0530

    jenkins bumped to 2.319.1 and libepoxy to 1.5.9
    Signed-off-by: Sangameshwar Mandakanalli <smandaka@progress.com>

--- jenkins/plan.sh
+++ jenkins/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=jenkins
 pkg_origin=core
-pkg_version=2.277.1
+pkg_version=2.319.1
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="The leading open source automation server, Jenkins provides hundreds of plugins to support building, deploying and automating any project."
 pkg_license=('MIT')
 pkg_upstream_url="https://jenkins.io/"
 pkg_source="http://mirrors.jenkins.io/war-stable/${pkg_version}/jenkins.war"
-pkg_shasum=399741db1152ee7bbe8dc08e0972efa0b128a1b55a0f8b10c087fefeec66d151
+pkg_shasum=7e4b848a752eda740c2c7a60956bf05d9df42602c805bbaeac897179b630a562
 pkg_deps=(
   core/openjdk11
   core/curl
