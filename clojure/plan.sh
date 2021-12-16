f7c24883a03232ccee1776d9c4236037632df2e7
Nimit <nimit.jyotiana@hotmail.com>
2021-12-16 15:07:04 +0530

    upgraded clojure to v1.10.3
    Signed-off-by: Nimit <nimit.jyotiana@hotmail.com>

--- clojure/plan.sh
+++ clojure/plan.sh
@@ -1,19 +1,21 @@
 pkg_origin=core
 pkg_name=clojure
-pkg_version=1.10.1.763
+pkg_version=1.10.3
 pkg_description="The Clojure programming language"
 pkg_upstream_url=https://clojure.org/
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("EPL-1.0")
-pkg_source=https://download.${pkg_name}.org/install/${pkg_name}-tools-${pkg_version}.tar.gz
-pkg_shasum=2a3ec8a6c3639035c2bba10945ae9007ab0dc9136766b95d2161f354e62a4d10
+pkg_source=https://github.com/clojure/clojure/archive/refs/tags/clojure-${pkg_version}.tar.gz
+pkg_shasum=949f58fa5d7a189ddc9b41c8a307d8a7f0aef3acad8430551bcd17c3d802bff5
 pkg_deps=(
   core/bash
   core/coreutils
   core/corretto
   core/rlwrap
 )
-pkg_build_deps=(core/ruby)
+pkg_build_deps=(
+	core/maven
+)
 pkg_bin_dirs=(bin)
 pkg_interpreters=(bin/env)
 pkg_exports=(
@@ -21,12 +23,11 @@ pkg_exports=(
 )
 pkg_svc_user="root"
 
+pkg_dirname=clojure-clojure-${pkg_version}
 do_build() {
-  fix_interpreter "${HAB_CACHE_SRC_PATH}/${pkg_name}-tools/clj" core/coreutils bin/env
-  fix_interpreter "${HAB_CACHE_SRC_PATH}/${pkg_name}-tools/clojure" core/coreutils bin/env
+	mvn package -Dmaven.test.skip=true
 }
 
 do_install() {
-  cd "${HAB_CACHE_SRC_PATH}/${pkg_name}-tools" || exit
-  sh ./install.sh "${pkg_prefix}"
+	mvn install
 }
