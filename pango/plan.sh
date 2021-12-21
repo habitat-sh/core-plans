7aca6889fa33835a5ab3b5018a4243e4823bc034
Phani Sajja <psajja@progress.com>
2021-12-21 16:32:07 +0530

    Update plan to fix build errors

--- pango/plan.sh
+++ pango/plan.sh
@@ -4,50 +4,61 @@ pkg_version="1.40.13"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('LGPL')
 pkg_source="https://download.gnome.org/sources/${pkg_name}/${pkg_version%.*}/${pkg_name}-${pkg_version}.tar.xz"
+pkg_filename=${pkg_name}-${pkg_version}.tar.xz
 pkg_shasum="f84e98db1078772ff4935b40a1629ff82ef0dfdd08d2cbcc0130c8c437857196"
 pkg_upstream_url="http://www.pango.org"
 pkg_description="Pango is a library for laying out and rendering of text, with an emphasis on internationalization."
 pkg_deps=(
   core/bzip2
   core/cairo
-  core/expat
-  core/fontconfig
-  core/freetype
+  core/pixman
   core/gcc-libs
   core/glib
   core/glibc
-  core/harfbuzz
+  core/pcre
   core/libffi
-  core/libice
-  core/libiconv
+  core/zlib
+  core/fontconfig
+  core/freetype
   core/libpng
-  core/libsm
+  core/xproto
   core/libxau
   core/libxcb
   core/libxdmcp
-  core/libxext
-  core/pcre
-  core/pixman
-  core/xlib
-  core/zlib
+  core/expat
+  core/util-linux
+  core/harfbuzz
 )
 pkg_build_deps=(
-  core/coreutils
-  core/diffutils
   core/file
   core/gcc
-  core/make
-  core/perl
+  core/cmake
   core/pkg-config
+  core/cacerts
+  core/curl
 )
 pkg_bin_dirs=(bin)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
 pkg_pconfig_dirs=(lib/pkgconfig)
 
-do_prepare() {
-  if [ ! -e /usr/bin/file ]
-  then
-    ln -sv "$(pkg_path_for core/file)/bin/file" /usr/bin/file
+do_download() {
+  # Downloading from https://download.gnome.org/ with wget results in a 302.
+  # Override `do_download` to use `curl`.
+  pushd $HAB_CACHE_SRC_PATH > /dev/null
+  if [[ -f $pkg_filename ]]; then
+    build_line "Found previous file '${pkg_filename}', attempting to re-use"
+    if verify_file $pkg_filename $pkg_shasum; then
+      build_line "Using cached and verified '${pkg_filename}'"
+      return 0
+    else
+      build_line "Clearing previous '${pkg_filename}' and re-attempting download"
+      rm -fv $pkg_filename
+    fi
   fi
+
+  build_line "Downloading '${pkg_source}' to '${pkg_filename}' with curl"
+  curl -L -O $pkg_source --cacert $(pkg_path_for cacerts)/ssl/cert.pem
+  build_line "Downloaded '${pkg_filename}'";
+  popd > /dev/null
 }
