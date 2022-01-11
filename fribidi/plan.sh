95f1a700680363fbad7d11a1c713d77e66a8e92b
RaviDuddela <RaviShankar.Duddela@progress.com>
2022-01-11 12:43:06 +0530

    add upstream url for checks
    Signed-off-by: RaviDuddela <RaviShankar.Duddela@progress.com>

--- fribidi/plan.sh
+++ fribidi/plan.sh
@@ -3,6 +3,7 @@ pkg_origin=core
 pkg_version="1.0.11"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('LGPL-2.1 License')
+pkg_upstream_url="https://github.com/fribidi/fribidi/"
 pkg_source="https://github.com/fribidi/fribidi/releases/download/v${pkg_version}/${pkg_name}-${pkg_version}.tar.xz"
 pkg_shasum="30f93e9c63ee627d1a2cedcf59ac34d45bf30240982f99e44c6e015466b4e73d"
 pkg_description="A command-line interface to the libfribidi library and can be used to convert a logical string to visual output."
