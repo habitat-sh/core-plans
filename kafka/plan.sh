98140e4f0945314e431ef1e49ec4cb282d920495
RaviDuddela <RaviShankar.Duddela@progress.com>
2022-02-03 13:56:17 +0530

    fetch url fix
    Signed-off-by: RaviDuddela <RaviShankar.Duddela@progress.com>

--- kafka/plan.sh
+++ kafka/plan.sh
@@ -1,7 +1,7 @@
 pkg_name=kafka
 pkg_origin=core
 pkg_version=2.7.2
-pkg_source="https://downloads.apache.org/${pkg_name}/${pkg_version}/${pkg_name}_2.13-${pkg_version}.tgz"
+pkg_source="http://archive.apache.org/dist/${pkg_name}/${pkg_version}/${pkg_name}_2.13-${pkg_version}.tgz"
 pkg_shasum="81a95f5aba56f43af424176a15cbfb695c48a0a7e3c5282b52bb670080ad09ae"
 pkg_dirname="${pkg_name}_2.13-${pkg_version}"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
