2c4d6a05a8fe6a3c891d6a53f67e371eede11e3f
RaviDuddela <RaviShankar.Duddela@progress.com>
2022-02-03 12:54:12 +0530

    fetch url fix
    Signed-off-by: RaviDuddela <RaviShankar.Duddela@progress.com>

--- storm/plan.sh
+++ storm/plan.sh
@@ -5,7 +5,7 @@ pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="Open source distributed realtime computation system."
 pkg_upstream_url="http://storm.apache.org/index.html"
 pkg_license=("Apache-2.0")
-pkg_source="http://apache.mediamirrors.org/storm/apache-storm-${pkg_version}/apache-storm-${pkg_version}.tar.gz"
+pkg_source="https://dlcdn.apache.org/storm/apache-storm-${pkg_version}/apache-storm-${pkg_version}.tar.gz"
 pkg_shasum="f575642bfa0222f04759b8bc3adaf857d0305005bf85d4ec754988e531f2c622"
 pkg_deps=(
   core/corretto8
