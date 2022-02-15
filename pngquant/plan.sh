a802238883f70fa216ccf41ff58903abec1835b5
Phani Sajja <sajja.phani@gmail.com>
2022-02-15 16:47:52 +0530

    Bumping package version to 2.17.0
    Signed-off-by:  Phani Sajja <sajja.phani@gmail.com>

--- pngquant/plan.sh
+++ pngquant/plan.sh
@@ -6,7 +6,7 @@ pkg_license=("GPL-3.0-only")
 pkg_description="Lossy PNG compressor"
 pkg_upstream_url="https://pngquant.org"
 pkg_source="http://pngquant.org/pngquant-${pkg_version}-src.tar.gz"
-pkg_shasum=a27cf0e64db499ccb3ddae9b36036e881f78293e46ec27a9e7a86a3802fcda66
+pkg_shasum=4b911a11aa0c35d364b608c917d13002126185c8c314ba4aa706b62fd6a95a7a
 pkg_deps=(
   core/coreutils
   core/libpng
