601f6f759dd82727914ec4d1b53839613783a87e
Phani Sajja <sajja.phani@gmail.com>
2022-02-15 16:49:46 +0530

    Bumping package version to 1.9.3
    Signed-off-by:  Phani Sajja <sajja.phani@gmail.com>

--- vault/plan.sh
+++ vault/plan.sh
@@ -1,6 +1,6 @@
 pkg_origin=core
 pkg_name=vault
-pkg_version=1.7.0
+pkg_version=1.9.3
 pkg_description="A tool for managing secrets."
 pkg_maintainer='The Habitat Maintainers <humans@habitat.sh>'
 pkg_license=("MPL-2.0")
