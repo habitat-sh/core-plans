faa116b55f190e336c85e62628c74d9f88a094a8
Phani Sajja <sajja.phani@gmail.com>
2022-02-24 19:34:56 +0530

    Bumping package version to 6.9.7
    Signed-off-by:  Phani Sajja <sajja.phani@gmail.com>

--- oniguruma/plan.sh
+++ oniguruma/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=oniguruma
 pkg_origin=core
-pkg_version=6.9.6
+pkg_version=6.9.7
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="Oniguruma is a modern and flexible regular expressions library"
 pkg_upstream_url="https://github.com/kkos/oniguruma"
 pkg_license=("BSD-2-Clause")
 pkg_source="https://github.com/kkos/oniguruma/archive/v${pkg_version}.tar.gz"
-pkg_shasum=aec9f6902ad8b7bb53b2c55d04686ea75da89a06694836b0362cb206578dfe89
+pkg_shasum=efbcdc109de940ac88e56f614195886b9f4f8745a4f57e3c1f8b563ea9519d09
 pkg_deps=(
   core/glibc
   core/coreutils
