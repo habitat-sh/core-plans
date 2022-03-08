948ba3c9b19c14fc1cb0d92c766b6662d0d4b9aa
Graham Weldon <graham@grahamweldon.com>
2022-03-08 09:53:52 +0900

    Update filebeat to 8.0.1
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- filebeat/plan.sh
+++ filebeat/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=filebeat
 pkg_origin=core
-pkg_version=7.12.0
+pkg_version=8.0.1
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("Apache-2.0")
 pkg_deps=(core/glibc)
@@ -23,9 +23,12 @@ do_download() {
   GOPATH="$(dirname "${HAB_CACHE_SRC_PATH}")"
   export GOPATH
   rm -rf "${GOPATH}/src/github.com/elastic/beats"
-  git clone https://github.com/elastic/beats "${GOPATH}"/src/github.com/elastic/beats
+  git clone \
+    --depth 1 \
+    --branch "v${pkg_version}" \
+    "https://github.com/elastic/beats" \
+    "${GOPATH}/src/github.com/elastic/beats"
   pushd "${HAB_CACHE_SRC_PATH}/github.com/elastic/beats/filebeat" > /dev/null
-  git checkout "v${pkg_version}"
   popd > /dev/null
 }
 
--- filebeat/tests/test.sh
+++ filebeat/tests/test.sh
@@ -6,6 +6,8 @@
 
 set -euo pipefail
 
+source "$(dirname "${0}")/../../bin/ci/test_helpers.sh"
+
 if [[ -z "${1:-}" ]]; then
   grep '^#/' < "${0}" | cut -c4-
 	exit 1
@@ -19,13 +21,11 @@ hab pkg binlink core/busybox-static ps
 hab pkg binlink core/busybox-static wc
 hab pkg install "${TEST_PKG_IDENT}"
 
-hab sup term
-hab sup run &
-sleep 5
-hab svc load "${TEST_PKG_IDENT}"
+WAIT_SECONDS=10
 
-# Allow service start
-WAIT_SECONDS=5
+ci_ensure_supervisor_running
+ci_load_service "${TEST_PKG_IDENT}" "${WAIT_SECONDS}"
+
 echo "Waiting ${WAIT_SECONDS} seconds for service to start..."
 sleep "${WAIT_SECONDS}"
 
