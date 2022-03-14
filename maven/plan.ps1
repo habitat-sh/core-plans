729860db55f9aaf58f51968b62c2f25acd641d66
Sangameshwar Mandakanalli <sangameshwar.mandakanalli@progress.com>
2022-03-14 07:23:09 +0000

    3.8.4 zip missing, so bumped 3.8.5
    Signed-off-by: Sangameshwar Mandakanalli <sangameshwar.mandakanalli@progress.com>

--- maven/plan.ps1
+++ maven/plan.ps1
@@ -1,13 +1,13 @@
 $pkg_name="maven"
 $pkg_origin="core"
-$pkg_version="3.8.4"
+$pkg_version="3.8.5"
 $pkg_description="Apache Maven is a software project management and comprehension tool"
 $pkg_upstream_url="http://maven.apache.org/"
 $pkg_license=@("Apache-2.0")
 $pkg_deps=@("core/corretto8")
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_source="http://mirrors.gigenet.com/apache/${pkg_name}/maven-3/${pkg_version}/binaries/apache-maven-${pkg_version}-bin.zip"
-$pkg_shasum="ccd67d1ee4fd79339c9b6f95d1e5e1e0e0209a8c1b095d9291e009afa0a492a5"
+$pkg_shasum="d53e045bc5c02aad179fae2fbc565d953354880db6661a8fab31f3a718d7b62c"
 $pkg_dirname="apache-$pkg_name-$pkg_version"
 $pkg_bin_dirs=@("bin")
 $pkg_lib_dirs=@("lib")
