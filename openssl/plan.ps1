e8c870c98c437ce4b022775ce8c2f3ac653d8414
Sangameshwar Mandakanalli <sangameshwar.mandakanalli@progress.com>
2022-03-14 08:34:38 +0000

    node14,openssl and packer bumped for windows
    Signed-off-by: Sangameshwar Mandakanalli <sangameshwar.mandakanalli@progress.com>

--- openssl/plan.ps1
+++ openssl/plan.ps1
@@ -1,12 +1,12 @@
 $pkg_name="openssl"
 $pkg_origin="core"
-$pkg_version="1.1.1k"
+$pkg_version="1.1.1l"
 $_pkg_version_text=($pkg_version).Replace(".", "_")
 $pkg_description="OpenSSL is an open source project that provides a robust, commercial-grade, and full-featured toolkit for the Transport Layer Security (TLS) and Secure Sockets Layer (SSL) protocols. It is also a general-purpose cryptography library."
 $pkg_upstream_url="https://www.openssl.org"
 $pkg_license=@("OpenSSL")
 $pkg_source="https://github.com/openssl/openssl/archive/OpenSSL_$_pkg_version_text.zip"
-$pkg_shasum="255c038f5861616f67b527434475d226f5fe00522fbd21fafd3df32019edd202"
+$pkg_shasum="23d8908e82b63af754018256a4eb02f13965f10067969f6a63f497960c11dbeb"
 $pkg_deps=@("core/visual-cpp-redist-2015")
 $pkg_build_deps=@("core/visual-cpp-build-tools-2015", "core/perl", "core/nasm")
 $pkg_bin_dirs=@("bin")
