980af54024d185d0f6c47c847bfde09f89f56a41
Sangameshwar Mandakanalli <sangameshwar.mandakanalli@progress.com>
2022-03-15 08:34:37 +0000

    dotnet-5-sdk,dotnet-asp-core,dotnet-core,dotnet-core-sdk and dotnet-core-sdk-preview bumped for windows
    Signed-off-by: Sangameshwar Mandakanalli <sangameshwar.mandakanalli@progress.com>

--- dotnet-asp-core/plan.ps1
+++ dotnet-asp-core/plan.ps1
@@ -1,12 +1,12 @@
 $pkg_name="dotnet-asp-core"
 $pkg_origin="core"
-$pkg_version="3.1.14"
+$pkg_version="3.1.23"
 $pkg_license=('MIT')
 $pkg_upstream_url="https://docs.microsoft.com/en-us/aspnet/core"
 $pkg_description="ASP.NET Core is a cross-platform, high-performance, open-source framework for building modern, cloud-based, Internet-connected applications."
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-$pkg_source="https://download.visualstudio.microsoft.com/download/pr/e4c43b9d-847a-4f92-aff3-ccae21822e43/cea2ed10f2fa8247a0cec513f6cc5e47/aspnetcore-runtime-$pkg_version-win-x64.zip"
-$pkg_shasum="e5d1e22325edd023d974d7e54c29295b1afedc9ac3dcc66825750ade32505a0f"
+$pkg_source="https://download.visualstudio.microsoft.com/download/pr/60c1fe64-3c9d-4a13-bffa-65aff86d039c/28bd30b0f5adaf8c99c5df019cfe826e/aspnetcore-runtime-$pkg_version-win-x64.zip"
+$pkg_shasum="01033f57c3a278ac69d12b5f5bcad72d0516f8b97ca73dd415987be2ba56d026"
 $pkg_filename="asp-dotnet-win-x64.$pkg_version.zip"
 $pkg_bin_dirs=@("bin")
 
