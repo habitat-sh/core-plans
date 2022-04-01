827283eb66643fea6bf6b11da1ae6d37e54d7258
Sangameshwar Mandakanalli <sangameshwar.mandakanalli@progress.com>
2022-04-01 07:51:58 +0000

    docker-cred-helper and git bumped for windows
    Signed-off-by: Sangameshwar Mandakanalli <sangameshwar.mandakanalli@progress.com>

--- git/plan.ps1
+++ git/plan.ps1
@@ -1,6 +1,6 @@
 $pkg_name = "git"
 $pkg_origin = "core"
-$pkg_version = "2.31.1"
+$pkg_version = "2.35.1"
 $pkg_description = "Git is a free and open source distributed version control
   system designed to handle everything from small to very large projects with
   speed and efficiency."
@@ -8,7 +8,7 @@ $pkg_upstream_url = "https://git-scm.com/"
 $pkg_maintainer = "The Habitat Maintainers <humans@habitat.sh>"
 $pkg_license = @("GPL-2.0")
 $pkg_source = "https://github.com/git-for-windows/git/releases/download/v$pkg_version.windows.1/Git-$pkg_version-64-bit.tar.bz2"
-$pkg_shasum = "c70934e84fd610cf8f492bdfe858d4738b8af09f5b5dc734795d2b40d99bcb53"
+$pkg_shasum = "a8dafe3ed839494e8b71212c1027830aa8d6f2f04365f4e523888f793f8e3fd5"
 $pkg_bin_dirs = @("bin")
 $pkg_build_deps = @("core/7zip")
 
