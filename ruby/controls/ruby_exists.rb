45bb5594ec1d40d74c6b7b36779e36ed220640fe
Nimit <nimit.jyotiana@hotmail.com>
2022-07-08 03:09:16 +0530

    [ruby] moved from chef-base-plan org
    Signed-off-by: Nimit <nimit.jyotiana@hotmail.com>

---
+++ ruby/controls/ruby_exists.rb
@@ -0,0 +1,36 @@
+title 'Tests to confirm ruby exists'
+
+plan_origin = ENV['HAB_ORIGIN']
+plan_name = input('plan_name', value: 'ruby')
+
+control 'core-plans-ruby-exists' do
+  impact 1.0
+  title 'Ensure ruby exists'
+  desc '
+  Verify ruby by ensuring bin/ruby 
+  (1) exists and
+  (2) is executable'
+  
+  plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
+  describe plan_installation_directory do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should_not be_empty }
+  end
+
+  [
+    "bundle",
+    "erb",
+    "gem",
+    "irb",
+    "rake",
+    "rdoc",
+    "ri",
+    "ruby",
+  ].each do |binary_name|
+    command_full_path = File.join(plan_installation_directory.stdout.strip, "bin", binary_name)
+    describe file(command_full_path) do
+      it { should exist }
+      it { should be_executable }
+    end
+  end
+end
---
+++ ruby/habitat/plan.sh
@@ -0,0 +1,43 @@
+pkg_name=ruby
+pkg_origin=core
+pkg_major=3.0
+pkg_version=3.0.3
+pkg_description="A dynamic, open source programming language with a focus on \
+  simplicity and productivity. It has an elegant syntax that is natural to \
+  read and easy to write."
+pkg_license=("Ruby")
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_source=https://cache.ruby-lang.org/pub/${pkg_name}/${pkg_major}/${pkg_name}-${pkg_version}.tar.gz
+pkg_upstream_url=https://www.ruby-lang.org/en/
+pkg_shasum=3586861cb2df56970287f0fd83f274bd92058872d830d15570b36def7f1a92ac
+pkg_deps=(core/glibc core/ncurses core/zlib core/openssl core/libyaml core/libffi core/readline core/nss-myhostname)
+pkg_build_deps=(core/coreutils core/diffutils core/patch core/make core/gcc core/sed)
+pkg_lib_dirs=(lib)
+pkg_include_dirs=(include)
+pkg_bin_dirs=(bin)
+pkg_interpreters=(bin/ruby)
+
+do_prepare() {
+  export CFLAGS="${CFLAGS} -O3 -g -pipe"
+  build_line "Setting CFLAGS='$CFLAGS'"
+}
+
+do_build() {
+  ./configure \
+    --prefix="$pkg_prefix" \
+    --enable-shared \
+    --disable-install-doc \
+    --with-openssl-dir="$(pkg_path_for core/openssl)" \
+    --with-libyaml-dir="$(pkg_path_for core/libyaml)"
+
+  make
+}
+
+do_check() {
+  make test
+}
+
+do_install() {
+  do_default_install
+  gem install rb-readline --no-document
+}
