5f0e5dfb8b85ccc086512bcee06360f95160df39
RaviDuddela <rduddela@progress.com>
2022-07-07 16:45:00 +0530

    moving acl,alsa-lib,attr,autoconf to core
    Signed-off-by: RaviDuddela <rduddela@progress.com>

---
+++ alsa-lib/controls/alsa-lib_works.rb
@@ -0,0 +1,27 @@
+title 'Tests to confirm alsa-lib works as expected'
+
+plan_origin = ENV['HAB_ORIGIN']
+plan_name = input('plan_name', value: 'alsa-lib')
+
+control 'core-plans-alsa-lib-works' do
+  impact 1.0
+  title 'Ensure alsa-lib works as expected'
+  desc '
+  Verify alsa-lib by ensuring that
+  (1) its installation directory exists 
+  (2) --help returns the expected response
+  '
+  
+  plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
+  describe plan_installation_directory do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should_not be_empty }
+  end
+  
+  plan_pkg_version = plan_installation_directory.stdout.split("/")[5]
+  command_full_path = File.join(plan_installation_directory.stdout.strip, "bin", "aserver")
+  describe command("#{command_full_path} --help 2>&1") do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should match /Usage:\s+[^\s]*\s+\[OPTIONS\]\s+server/ }
+  end
+end
---
+++ attr/controls/attr_exists.rb
@@ -0,0 +1,23 @@
+title 'Tests to confirm attr exists'
+
+plan_origin = ENV['HAB_ORIGIN']
+plan_name = input('plan_name', value: 'attr')
+
+control 'core-plans-attr-exists' do
+  impact 1.0
+  title 'Ensure attr exists'
+  desc '
+  Verify attr by ensuring bin/getfattr exists'
+  
+  plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
+  describe plan_installation_directory do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should_not be_empty }
+  end
+
+  command_relative_path = input('command_relative_path', value: 'bin/getfattr')
+  command_full_path = File.join(plan_installation_directory.stdout.strip, command_relative_path)
+  describe file(command_full_path) do
+    it { should exist }
+  end
+end
