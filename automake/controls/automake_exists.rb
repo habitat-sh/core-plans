8bec4dbc705a58a5130382c9ff18c211758ffd74
RaviDuddela <rduddela@progress.com>
2022-07-10 19:33:16 +0530

    fixing precommit hooks issues
    Signed-off-by: RaviDuddela <rduddela@progress.com>

--- automake/controls/automake_exists.rb
+++ automake/controls/automake_exists.rb
@@ -1,27 +1,22 @@
 title 'Tests to confirm automake exists'
-
 plan_origin = ENV['HAB_ORIGIN']
 plan_name = input('plan_name', value: 'automake')
-
 control 'core-plans-automake-exists' do
   impact 1.0
   title 'Ensure automake exists'
   desc '
   Verify automake by ensuring bin/automake exists'
-  
   plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
   describe plan_installation_directory do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
     #its('stderr') { should be_empty }
   end
-
   automake_full_path = File.join(plan_installation_directory.stdout.strip, "bin/automake")
   describe file(automake_full_path) do
     it { should exist }
     it { should be_executable }
   end
-
   aclocal_full_path = File.join(plan_installation_directory.stdout.strip, "bin/aclocal")
   describe file(aclocal_full_path) do
     it { should exist }
