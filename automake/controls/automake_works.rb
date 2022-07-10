56ed98f1403edbc673749b312f05d3c0ec3cdbad
RaviDuddela <rduddela@progress.com>
2022-07-10 19:52:00 +0530

    fixing trail white spaces
    Signed-off-by: RaviDuddela <rduddela@progress.com>

--- automake/controls/automake_works.rb
+++ automake/controls/automake_works.rb
@@ -1,12 +1,12 @@
 title 'Tests to confirm automake works as expected'
-plan_origin = ENV['HAB_ORIGIN']
-plan_name = input('plan_name', value: 'automake')
+plan_origin=ENV['HAB_ORIGIN']
+plan_name=input('plan_name', value: 'automake')
 control 'core-plans-automake-works' do
   impact 1.0
   title 'Ensure automake works as expected'
   desc '
   Verify automake by ensuring that
-  (1) its installation directory exists 
+  (1) its installation directory exists
   (2) binaries return the expected version
   '
   plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
@@ -15,8 +15,8 @@ control 'core-plans-automake-works' do
     its('stdout') { should_not be_empty }
     #its('stderr') { should be_empty }
   end
-  automake_full_path = File.join(plan_installation_directory.stdout.strip, "bin/automake")
-  plan_pkg_version = plan_installation_directory.stdout.split("/")[5]
+  automake_full_path=File.join(plan_installation_directory.stdout.strip, "bin/automake")
+  plan_pkg_version=plan_installation_directory.stdout.split("/")[5]
   describe command("#{automake_full_path} --version") do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
