edb6d4258edc1b716f753988a647bf56d148e7d2
Nimit <nimit.jyotiana@hotmail.com>
2022-07-13 15:39:20 +0530

    [xxhash] moved from chef-base-plan org
    Signed-off-by: Nimit <nimit.jyotiana@hotmail.com>

---
+++ xxhash/README.md
@@ -0,0 +1,2 @@
+# base-plan-skeleton
+Template for all new Chef Base Plans to simplify creation of repositories.
---
+++ xxhash/plan.sh
@@ -0,0 +1,47 @@
+pkg_name=xxhash
+pkg_origin=core
+pkg_version="0.8.1"
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_license=('BSD 2-Clause License' 'GPL V2')
+pkg_deps=(core/glibc)
+pkg_build_deps=(core/make core/gcc core/git)
+pkg_upstream_url="https://github.com/Cyan4973/xxHash"
+pkg_bin_dirs=(bin)
+pkg_include_dirs=(include)
+pkg_lib_dirs=(lib)
+pkg_pconfig_dirs=(lib/pkgconfig)
+
+do_begin() {
+  do_default_begin
+}
+
+do_download() {
+    repo_path="${HAB_CACHED_SRC_PATH}/${pkg_dirname}"
+    rm -Rf "${repo_path}"
+    git clone "${pkg_upstream_url}" "${repo_path}"
+	( cd "${HAB_CACHED_SRC_PATH}/${pkg_dirname}" || exit
+    git checkout tags/"v$pkg_version"
+  )
+}
+
+do_clean() {
+  do_default_clean
+}
+
+do_build() {
+  cd "${HAB_CACHED_SRC_PATH}/${pkg_dirname}"
+  make
+}
+
+do_check() {
+  return 0
+}
+
+do_install() {
+  cd "${HAB_CACHED_SRC_PATH}/${pkg_dirname}"
+  make install
+}
+
+do_end() {
+  do_default_end
+}
