9ccc740bba6d00469122f8ba317f5f90b82bbc7c
Nimit <nimit.jyotiana@hotmail.com>
2022-07-14 07:34:24 +0000

    [tar] update test cases for tar
    Signed-off-by: Nimit <nimit.jyotiana@hotmail.com>

--- tar/tests/test.sh
+++ tar/tests/test.sh
@@ -1,18 +1,20 @@
-#!/bin/sh
+#!/bin/bash
+#/ Usage: test.sh <pkg_ident>
+#/
+#/ Example: test.sh core/curl/7.65.1/20190703171140
+#/
 
 set -euo pipefail
 
-TESTDIR="$(dirname "${0}")"
-
-if [ -z "${1:-}" ]; then
-  echo "Usage: $0 FULLY_QUALIFIED_PACKAGE_IDENT"
-  exit 1
+if [[ -z "${1:-}" ]]; then
+	grep '^#/' < "${0}" | cut -c4-
+	exit 1
 fi
 
-TEST_PKG_IDENT="$1"
+TEST_PKG_IDENT="${1}"
+export TEST_PKG_IDENT
 
 hab pkg install core/bats --binlink
-hab pkg install "$TEST_PKG_IDENT"
+hab pkg install "${TEST_PKG_IDENT}"
 
-export TEST_PKG_IDENT
-bats "${TESTDIR}/test.bats"
+bats "$(dirname "${0}")/test.bats"
