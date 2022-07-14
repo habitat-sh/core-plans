1f65b179754adfa625bf39e49c5f971e920a2688
Sangameshwar Mandakanalli <smandaka@progress.com>
2022-07-14 18:21:40 +0530

    Moving libbsd and libcap from Base-plan to Core-plan
    Signed-off-by: Sangameshwar Mandakanalli <smandaka@progress.com>

---
+++ libbsd/plan.sh
@@ -0,0 +1,72 @@
+pkg_name=libbsd
+pkg_origin=core
+pkg_version=0.11.3
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_description="\
+This library provides useful functions commonly found on BSD systems, and \
+lacking on others like GNU systems\
+"
+pkg_upstream_url="https://libbsd.freedesktop.org/wiki/"
+pkg_license=('custom')
+pkg_source="https://libbsd.freedesktop.org/releases/${pkg_name}-${pkg_version}.tar.xz"
+pkg_shasum="ff95cf8184151dacae4247832f8d4ea8800fa127dbd15033ecfe839f285b42a1"
+pkg_deps=(
+  core/glibc
+  core/libmd
+)
+pkg_build_deps=(
+  core/coreutils
+  core/diffutils
+  core/file
+  core/gcc
+  core/make
+  core/patch
+  core/pkg-config
+  core/sed
+)
+pkg_include_dirs=(include)
+pkg_lib_dirs=(lib)
+pkg_pconfig_dirs=(lib/pkgconfig)
+
+do_prepare() {
+  if [[ ! -r /usr/bin/file ]]; then
+    ln -sv "$(pkg_path_for "core/file")/bin/file" /usr/bin/file
+    _clean_file=true
+  fi
+}
+
+do_install() {
+  do_default_install
+
+  # Install license file from README
+  install -Dm644 COPYING "${pkg_prefix}/share/licenses/LICENSE"
+}
+
+do_check() {
+  make check
+}
+
+do_end() {
+  if [[ -n "$_clean_file" ]]; then
+    rm -fv /usr/bin/file
+  fi
+}
+
+# ----------------------------------------------------------------------------
+# **NOTICE:** What follows are implementation details required for building a
+# first-pass, "stage1" toolchain and environment. It is only used when running
+# in a "stage1" Studio and can be safely ignored by almost everyone. Having
+# said that, it performs a vital bootstrapping process and cannot be removed or
+# significantly altered. Thank you!
+# ----------------------------------------------------------------------------
+if [[ "$STUDIO_TYPE" = "stage1" ]]; then
+  pkg_build_deps=(
+    core/gcc
+    core/coreutils
+    core/sed
+    core/diffutils
+    core/make
+    core/patch
+    core/file
+  )
+fi
