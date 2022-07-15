590cc7b294457aafc8d6e3dc5aaf78c72a80ddc9
RaviDuddela <rduddela@progress.com>
2022-07-15 15:47:49 +0530

    trim whitespace
    Signed-off-by: RaviDuddela <rduddela@progress.com>

--- automake/controls/automake_works.rb
+++ automake/controls/automake_works.rb
@@ -1,8 +1,6 @@
 title 'Tests to confirm automake works as expected'
-
 plan_origin = ENV['HAB_ORIGIN']
 plan_name = input('plan_name', value: 'automake')
-
 control 'core-plans-automake-works' do
   impact 1.0
   title 'Ensure automake works as expected'
@@ -11,14 +9,12 @@ control 'core-plans-automake-works' do
   (1) its installation directory exists
   (2) binaries return the expected version
   '
-
   plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
   describe plan_installation_directory do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
     #its('stderr') { should be_empty }
   end
- 
   automake_full_path = File.join(plan_installation_directory.stdout.strip, "bin/automake")
   plan_pkg_version = plan_installation_directory.stdout.split("/")[5]
   describe command("#{automake_full_path} --version") do
@@ -27,7 +23,6 @@ control 'core-plans-automake-works' do
     its('stdout') { should match /automake \(GNU automake\) #{plan_pkg_version}/ }
     #its('stderr') { should be_empty }
   end
-
   aclocal_full_path = File.join(plan_installation_directory.stdout.strip, "bin/aclocal")
   describe command("#{aclocal_full_path} --version") do
     its('exit_status') { should eq 0 }
