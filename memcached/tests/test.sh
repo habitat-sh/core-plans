704f52d50f3a5129477abcbb190154e4d78b5e7a
Sangameshwar Mandakanalli <smandaka@progress.com>
2022-07-21 12:11:20 +0530

    moving memcached and minio from Base-plan to Core-plan
    Signed-off-by: Sangameshwar Mandakanalli <smandaka@progress.com>

---
+++ memcached/tests/test.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#/ Usage: test.sh <pkg_ident>
+#/
+#/ Example: test.sh core/php/7.2.8/20181108151533
+#/
+
+source "$(dirname "${0}")/../../bin/ci/test_helpers.sh"
+
+if [[ -z "${1:-}" ]]; then
+  grep '^#/' < "${0}" | cut -c4-
+	exit 1
+fi
+
+TEST_PKG_IDENT="${1}"
+export TEST_PKG_IDENT
+
+hab pkg install core/bats --binlink
+hab pkg install core/busybox-static
+hab pkg binlink core/busybox-static ps
+hab pkg binlink core/busybox-static netstat
+hab pkg binlink core/busybox-static wc
+hab pkg binlink core/busybox-static uniq
+
+hab pkg install "${TEST_PKG_IDENT}"
+
+ci_ensure_supervisor_running
+ci_load_service "${TEST_PKG_IDENT}" 10
+
+bats "$(dirname "${0}")/test.bats"
+hab svc unload "${TEST_PKG_IDENT}" || true
---
+++ minio/plan.sh
@@ -0,0 +1,28 @@
+pkg_name=minio
+pkg_origin=core
+pkg_version=2021-11-03T03-36-36Z
+pkg_description="Minio is a high performance distributed object storage server, designed for large-scale private cloud infrastructure."
+pkg_upstream_url="https://minio.io"
+pkg_source="https://dl.minio.io/server/minio/release/linux-amd64/archive/minio.RELEASE.${pkg_version}"
+pkg_shasum=17745acc86cc338f6c41fe27df143cc065c945b3bb24dec21b153b4ac9f0811a
+pkg_license=('Apache-2.0')
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_bin_dirs=(bin)
+
+do_unpack() {
+  return 0
+}
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  mkdir -p "${pkg_prefix}/bin"
+  cp "../minio.RELEASE.${pkg_version}" "${pkg_prefix}/bin/minio"
+  chmod +x "${pkg_prefix}/bin/minio"
+}
+
+do_strip() {
+  return 0
+}
