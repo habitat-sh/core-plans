c64c81778876e64969e4d8c4787e52262686bd10
Sangameshwar Mandakanalli <smandaka@progress.com>
2022-07-22 13:46:05 +0530

    moving pkg-config and postgresql from Base-plan to Core-plan
    Signed-off-by: Sangameshwar Mandakanalli <smandaka@progress.com>

---
+++ postgresql/controls/postgresql_exists.rb
@@ -0,0 +1,26 @@
+title 'Tests to confirm postgresql exists'
+
+plan_origin = ENV['HAB_ORIGIN']
+plan_name = input('plan_name', value: 'postgresql')
+
+control 'core-plans-postgresql-exists' do
+  impact 1.0
+  title 'Ensure postgresql exists'
+  desc '
+  Verify postgresql by ensuring bin/postgres 
+  (1) exists and
+  (2) is executable'
+  
+  plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
+  describe plan_installation_directory do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should_not be_empty }
+    its('stderr') { should be_empty }
+  end
+
+  command_full_path = File.join(plan_installation_directory.stdout.strip, "bin", "postgres")
+  describe file(command_full_path) do
+    it { should exist }
+    it { should be_executable }
+  end
+end
