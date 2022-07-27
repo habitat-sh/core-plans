f79745af8a646699b617334aeb7c0d865422bb6c
Sangameshwar Mandakanalli <smandaka@progress.com>
2022-07-27 11:58:11 +0530

    Fixing the Buildkite issue
    Signed-off-by: Sangameshwar Mandakanalli <smandaka@progress.com>

--- libxfixes/controls/libxfixes_library_exists.rb
+++ libxfixes/controls/libxfixes_library_exists.rb
@@ -7,12 +7,12 @@ control 'core-plans-libxfixes-library-exists' do
   impact 1.0
   title 'Ensure libxfixes library exists'
   desc '
-  Verify libxfixes library by ensuring that 
-  (1) its installation directory exists; 
-  (2) the library exists; 
+  Verify libxfixes library by ensuring that
+  (1) its installation directory exists;
+  (2) the library exists;
   (3) its pkgconfig metadata contains the expected version
   '
-  
+
   plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
   describe plan_installation_directory do
     its('exit_status') { should eq 0 }
