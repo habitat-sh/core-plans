2508e047212ed48a7f8b769b4c9047807f70f05f
Sangameshwar Mandakanalli <smandaka@progress.com>
2022-07-27 12:20:17 +0530

    Fixing the Buildkite issue
    Signed-off-by: Sangameshwar Mandakanalli <smandaka@progress.com>

--- libxtst/controls/libxtst_library_exists.rb
+++ libxtst/controls/libxtst_library_exists.rb
@@ -7,12 +7,12 @@ control 'core-plans-libxtst-library-exists' do
   impact 1.0
   title 'Ensure libxtst library exists'
   desc '
-  Verify libxtst library by ensuring that 
-  (1) its installation directory exists; 
-  (2) the library exists; 
+  Verify libxtst library by ensuring that
+  (1) its installation directory exists;
+  (2) the library exists;
   (3) its pkgconfig metadata contains the expected version
   '
-  
+
   plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
   describe plan_installation_directory do
     its('exit_status') { should eq 0 }
