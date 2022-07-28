2c4b1711803be8a7a0bb7780009da597f1e8ba1f
Nimit <nimit.jyotiana@hotmail.com>
2022-07-28 12:18:57 +0000

    [rsync] fix for whitespaces errors
    Signed-off-by: Nimit <nimit.jyotiana@hotmail.com>

--- rsync/controls/rsync_works.rb
+++ rsync/controls/rsync_works.rb
@@ -8,16 +8,16 @@ control 'core-plans-rsync-works' do
   title 'Ensure rsync works as expected'
   desc '
   Verify rsync by ensuring that
-  (1) its installation directory exists 
+  (1) its installation directory exists
   (2) it returns the expected version
   '
-  
+
   plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
   describe plan_installation_directory do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
   end
-  
+
   plan_pkg_version = plan_installation_directory.stdout.split("/")[5]
   ["rsync"].each do |binary_name|
     command_full_path = File.join(plan_installation_directory.stdout.strip, "bin", binary_name)
