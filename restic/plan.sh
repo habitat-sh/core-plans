192ba07005555b3b209f2c2a9b43980e4d15e16c
Chris Alfano <chris@jarv.us>
2023-03-21 12:01:26 -0400

    [restic] update to 0.15.1
    Signed-off-by: Chris Alfano <chris@jarv.us>

--- restic/plan.sh
+++ restic/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=restic
 pkg_origin=core
-pkg_version="0.12.1"
+pkg_version="0.15.1"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("BSD-2-Clause")
 pkg_source="https://github.com/restic/${pkg_name}/releases/download/v${pkg_version}/${pkg_name}-${pkg_version}.tar.gz"
-pkg_shasum="a9c88d5288ce04a6cc78afcda7590d3124966dab3daa9908de9b3e492e2925fb"
+pkg_shasum="fce382fdcdac0158a35daa640766d5e8a6e7b342ae2b0b84f2aacdff13990c52"
 pkg_build_deps=(
-  core/go
+  core/go19
 )
 pkg_bin_dirs=(bin)
 pkg_description="Fast, secure, efficient backup program"
