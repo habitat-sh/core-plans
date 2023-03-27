d592183b08ff09edac32675aaeab020d0a66a636
Jason Heath <jason.heath@progress.com>
2023-03-27 09:22:43 -0400

    Revving Rust to 1.68.1
    Signed-off-by: Jason Heath <jason.heath@progress.com>

--- rust/plan.ps1
+++ rust/plan.ps1
@@ -1,12 +1,12 @@
 $pkg_name="rust"
 $pkg_origin="core"
-$pkg_version="1.68.0"
+$pkg_version="1.68.1"
 $pkg_description="Safe, concurrent, practical language"
 $pkg_upstream_url="https://www.rust-lang.org/"
 $pkg_license=@("Apache-2.0", "MIT")
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_source="https://static.rust-lang.org/dist/rust-$pkg_version-x86_64-pc-windows-msvc.msi"
-$pkg_shasum = "68c24484c79cde83d3197ffdf03657d16590385536e45cefa63e60e274de0bb1"
+$pkg_shasum = "28fdfbed3d7ef913f3d09fc1c05fc6be55fb65551497fb455f7a007e00126f81"
 $pkg_deps=@("core/visual-cpp-redist-2015", "core/visual-cpp-build-tools-2015")
 $pkg_build_deps=@("core/lessmsi")
 $pkg_bin_dirs=@("bin")
