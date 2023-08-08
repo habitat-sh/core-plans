75364a76060614d80aeccebe19272aaa3fbf412b
Nimit <nimit.jyotiana@hotmail.com>
2023-08-08 08:26:57 +0000

    fix for trailing whitespaces
    Signed-off-by: Nimit <nimit.jyotiana@hotmail.com>

--- openssl/controls/openssl_exists.rb
+++ openssl/controls/openssl_exists.rb
@@ -8,7 +8,7 @@ control 'core-plans-openssl-exists' do
   title 'Ensure openssl exists'
   desc '
   Verify openssl by ensuring bin/openssl exists'
-  
+
   plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
   describe plan_installation_directory do
     its('exit_status') { should eq 0 }
