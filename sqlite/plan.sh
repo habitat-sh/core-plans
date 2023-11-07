97e5d6ffab972988adb5e905b5248e02f62fc1e1
Ravi Duddela <rduddela@progress.com>
2023-11-07 13:26:21 +0000

    upgrade sqlite,CHEF-8240
    Signed-off-by: Ravi Duddela <rduddela@progress.com>

--- sqlite/plan.sh
+++ sqlite/plan.sh
@@ -1,15 +1,15 @@
 pkg_name=sqlite
-pkg_version=3.38.5
-pkg_dist_version=3380500
+pkg_version=3.44.0
+pkg_dist_version=3440000
 pkg_origin=core
 pkg_license=('Public Domain')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="A software library that implements a self-contained, serverless, zero-configuration, transactional SQL database engine."
 pkg_upstream_url=https://www.sqlite.org/
-pkg_source="https://www.sqlite.org/2022/${pkg_name}-autoconf-${pkg_dist_version}.tar.gz"
+pkg_source="https://www.sqlite.org/2023/${pkg_name}-autoconf-${pkg_dist_version}.tar.gz"
 pkg_filename="${pkg_name}-autoconf-${pkg_dist_version}.tar.gz"
 pkg_dirname="${pkg_name}-autoconf-${pkg_dist_version}"
-pkg_shasum=5af07de982ba658fd91a03170c945f99c971f6955bc79df3266544373e39869c
+pkg_shasum=b9cd386e7cd22af6e0d2a0f06d0404951e1bef109e42ea06cc0450e10cd15550
 pkg_deps=(
   core/glibc
   core/readline
