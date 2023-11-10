aadbbbcd0bc871e1767e06a522d57814fc349d1e
Ravi Duddela <rduddela@progress.com>
2023-11-10 04:50:48 +0000

    upgrade libssh2,CHEF-8244
    Signed-off-by: Ravi Duddela <rduddela@progress.com>

--- libssh2/plan.sh
+++ libssh2/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=libssh2
 pkg_origin=core
-pkg_version=1.10.0
+pkg_version=1.11.0
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="libssh2 is a client-side C library implementing the SSH2 protocol"
 pkg_upstream_url="https://libssh2.org/"
 pkg_license=('BSD-3-Clause')
 pkg_source="https://libssh2.org/download/${pkg_name}-${pkg_version}.tar.gz"
-pkg_shasum="2d64e90f3ded394b91d3a2e774ca203a4179f69aebee03003e5a6fa621e41d51"
+pkg_shasum="3736161e41e2693324deb38c26cfdc3efe6209d634ba4258db1cecff6a5ad461"
 pkg_deps=(
   core/glibc
   core/openssl
