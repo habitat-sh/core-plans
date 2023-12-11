c5818a89384d8449397c807efd0bbc41707968af
Nimit <nimit.jyotiana@hotmail.com>
2023-12-11 16:23:12 +0000

    [tree] updated source valid and working url
    Signed-off-by: Nimit <nimit.jyotiana@hotmail.com>

--- tree/plan.sh
+++ tree/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=tree
 pkg_origin=core
 pkg_version=1.8.0
-pkg_upstream_url="http://mama.indstate.edu/users/ice/tree/"
+pkg_upstream_url="http://oldmanprogrammer.net/source.php?dir=projects/tree"
 pkg_description="A utility to display a tree view of directories."
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('gplv2+')
-pkg_source=http://mama.indstate.edu/users/ice/tree/src/tree-${pkg_version}.tgz
-pkg_shasum=715d5d4b434321ce74706d0dd067505bb60c5ea83b5f0b3655dae40aa6f9b7c2
+pkg_source="https://github.com/Old-Man-Programmer/tree/archive/refs/tags/${pkg_version}.tar.gz"
+pkg_shasum="eae2aaf21a272fa2416d43bccdc9eaee736dc3287bef92b2685f266940031c61"
 pkg_deps=(core/glibc)
 pkg_build_deps=(core/make core/gcc)
 pkg_bin_dirs=(bin)
