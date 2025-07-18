282cdd108ff61d477088c444a69bf6710db7e62a
Nimit <nimit.jyotiana@hotmail.com>
2025-07-18 10:05:39 +0000

    [opam] upgraded to v2.4.4 to fix build errors
    Signed-off-by: Nimit <nimit.jyotiana@hotmail.com>

--- opam/plan.sh
+++ opam/plan.sh
@@ -2,11 +2,11 @@ pkg_name=opam
 pkg_origin=core
 pkg_description="opam is a source-based package manager. It supports multiple simultaneous compiler installations, flexible package constraints, and a Git-friendly development workflow."
 pkg_upstream_url="https://opam.ocaml.org/"
-pkg_version="1.2.2"
+pkg_version="2.4.0"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('LGPL')
 pkg_source="https://github.com/ocaml/opam/releases/download/${pkg_version}/opam-full-${pkg_version}.tar.gz"
-pkg_shasum="15e617179251041f4bf3910257bbb8398db987d863dd3cfc288bdd958de58f00"
+pkg_shasum="119f41efb1192dad35f447fbf1c6202ffc331105e949d2980a75df8cb2c93282"
 pkg_dirname="opam-full-${pkg_version}"
 pkg_deps=(
   core/aspcud
@@ -28,10 +28,7 @@ pkg_deps=(
 pkg_bin_dirs=(bin)
 
 do_build() {
-  ./configure --prefix "${pkg_prefix}"
-  # This is a copy/paste from https://opam.ocaml.org/doc/Install.html#From-Sources
-  # to fix builds with ocaml >= 4.06.0
-  OCAMLPARAM="safe-string=0,_" make lib-ext
+  ./configure --prefix "${pkg_prefix}" --with-vendored-deps
   make
 }
 
