314cd7aed258456ba14ffc32459986fdf63cd9c2
Nimit <nimit.jyotiana@hotmail.com>
2025-07-28 09:36:23 +0000

    [libarchive] upgraded to v3.8.1
    Signed-off-by: Nimit <nimit.jyotiana@hotmail.com>

--- libarchive/plan.sh
+++ libarchive/plan.sh
@@ -1,13 +1,13 @@
 pkg_name=libarchive
 _distname=$pkg_name
 pkg_origin=core
-pkg_version="3.7.2"
+pkg_version="3.8.1"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="Multi-format archive and compression library"
 pkg_upstream_url="https://www.libarchive.org"
 pkg_license=('BSD')
 pkg_source="http://www.libarchive.org/downloads/${_distname}-${pkg_version}.tar.gz"
-pkg_shasum="df404eb7222cf30b4f8f93828677890a2986b66ff8bf39dac32a804e96ddf104"
+pkg_shasum="bde832a5e3344dc723cfe9cc37f8e54bde04565bfe6f136bc1bd31ab352e9fab"
 pkg_dirname="${_distname}-${pkg_version}"
 pkg_deps=(
   core/glibc
