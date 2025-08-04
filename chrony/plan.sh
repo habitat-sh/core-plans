ea87f8e6f6adc0586014c907a7542ce8336214ad
Nimit <nimit.jyotiana@hotmail.com>
2025-08-04 12:43:58 +0530

    [chrony] updated source url
    Signed-off-by: Nimit <nimit.jyotiana@hotmail.com>

--- chrony/plan.sh
+++ chrony/plan.sh
@@ -1,11 +1,11 @@
 pkg_name=chrony
 pkg_origin=core
-pkg_version=3.5.1
+pkg_version="3.5.1"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="Chrony is a versatile implementation of the Network Time Protocol (NTP)."
 pkg_upstream_url="https://chrony.tuxfamily.org/"
 pkg_license=("GPL-2.0")
-pkg_source="https://download.tuxfamily.org/chrony/${pkg_name}-${pkg_version}.tar.gz"
+pkg_source="https://chrony-project.org/releases/chrony-${pkg_version}.tar.gz"
 pkg_shasum="1ba82f70db85d414cd7420c39858e3ceca4b9eb8b028cbe869512c3a14a2dca7"
 pkg_deps=(
   core/glibc
