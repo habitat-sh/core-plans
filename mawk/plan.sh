7439ff4bbd5b30f237140720f62c94554d14a9f6
Nimit <nimit.jyotiana@hotmail.com>
2025-08-04 12:50:06 +0530

    [mawk] updated source url
    Signed-off-by: Nimit <nimit.jyotiana@hotmail.com>

--- mawk/plan.sh
+++ mawk/plan.sh
@@ -5,7 +5,7 @@ pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="An interpreter for the AWK Programming Language"
 pkg_upstream_url="http://invisible-island.net/mawk/mawk.html"
 pkg_license=('GPL-2.0')
-pkg_source=https://invisible-mirror.net/archives/${pkg_name}/${pkg_name}-${pkg_version}.tgz
+pkg_source=http://invisible-mirror.net/archives/${pkg_name}/${pkg_name}-${pkg_version}.tgz
 pkg_shasum=7fd4cd1e1fae9290fe089171181bbc6291dfd9bca939ca804f0ddb851c8b8237
 pkg_build_deps=(
   core/diffutils
