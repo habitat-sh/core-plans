ac635a7643c94db846f9c557e0be94c7ca07d783
muthuja <muthujabavaji.vempalli@progress.com>
2025-08-11 19:49:02 +0530

    update postgress repositories for 20.04 ubuntu
    Signed-off-by: muthuja <muthujabavaji.vempalli@progress.com>

--- bin/ci/check-pre-commit.sh
+++ bin/ci/check-pre-commit.sh
@@ -6,9 +6,13 @@ plan_path="$(basename "$1")"
 
 echo "--- :python: Install pre-commit"
 if [[ "${CI:-}" == "true" ]]; then
-  apt update
-  apt install -y python3-pip
-  pip3 install pre-commit
+  # for  Ubuntu 20.04 (Focal Fossa) and later we have to add the PostgreSQL repository
+  # to install the pre-commit package
+  sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt focal-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+  wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+  sudo apt update
+  sudo apt install -y python3-pip
+  sudo pip3 install pre-commit
 else
   echo "Not in CI! Skipping installation of pre-commit. Please install it manually if executing this on your workstation"
 fi
