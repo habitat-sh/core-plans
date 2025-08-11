ed81e74ca2934eea75def07cf4fe2b4726e7b8ad
Nimit <nimit.jyotiana@hotmail.com>
2025-08-11 13:38:16 +0530

    [libcap-ng] upgrade to v0.8.5 due to unavailability of v0.8.2
    Signed-off-by: Nimit <nimit.jyotiana@hotmail.com>

--- libcap-ng/plan.sh
+++ libcap-ng/plan.sh
@@ -1,8 +1,8 @@
 pkg_name=libcap-ng
 pkg_origin=core
-pkg_version=0.8.2
-pkg_source=http://people.redhat.com/sgrubb/$pkg_name/$pkg_name-$pkg_version.tar.gz
-pkg_shasum=52c083b77c2b0d8449dee141f9c3eba76e6d4c5ad44ef05df25891126cb85ae9
+pkg_version="0.8.5"
+pkg_source="https://github.com/stevegrubb/libcap-ng/archive/refs/tags/v${pkg_version}.tar.gz"
+pkg_shasum="e4be07fdd234f10b866433f224d183626003c65634ed0552b02e654a380244c2"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="The libcap-ng library is intended to make programming with posix capabilities much easier than the traditional libcap library"
 pkg_upstream_url="https://people.redhat.com/sgrubb/libcap-ng/"
@@ -10,10 +10,19 @@ pkg_license=('GPL-2.0' 'LGPL-2.1')
 pkg_bin_dirs=(bin)
 pkg_lib_dirs=(lib)
 pkg_include_dirs=(include)
-pkg_build_deps=(core/make core/gcc)
 pkg_deps=(core/glibc)
+pkg_build_deps=(
+	core/make
+	core/gcc
+	core/automake
+	core/autoconf
+	core/libtool
+	core/m4
+)
 
 do_build() {
-  ./configure --prefix="${pkg_prefix}" --enable-static=no --with-python=no
-  make
+	libtoolize
+	./autogen.sh
+	./configure --prefix="${pkg_prefix}" --enable-static=no --with-python=no
+	make
 }
