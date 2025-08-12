6725eaf19fe486b6fbf4a675c2a8019244623af8
muthuja <muthujabavaji.vempalli@progress.com>
2025-08-12 13:56:59 +0530

    update pre-commit script to support ubuntu focal
    Signed-off-by: muthuja <muthujabavaji.vempalli@progress.com>

--- bin/ci/check-pre-commit.sh
+++ bin/ci/check-pre-commit.sh
@@ -6,12 +6,14 @@ plan_path="$(basename "$1")"
 
 echo "--- :python: Install pre-commit"
 if [[ "${CI:-}" == "true" ]]; then
-  #sudo apt-get update
-  sudo apt-get install -y python3-pip
-  pip3 install --user pre-commit
+  # Run apt update but ignore errors (e.g., from PostgreSQL repo)
+  apt update || true
+  apt install -y python3-pip
+  pip3 install pre-commit
 else
   echo "Not in CI! Skipping installation of pre-commit. Please install it manually if executing this on your workstation"
 fi
+
 pre-commit --version
 
 echo "--- :git: [${plan_path}] Running checks provided by pre-commit hooks"
