06c354194a116c85b7e18b72e04a8e6dd4334f83
muthuja <muthujabavaji.vempalli@progress.com>
2025-08-12 14:05:47 +0530

    update pre-commit script to support ubuntu focal
    Signed-off-by: muthuja <muthujabavaji.vempalli@progress.com>

--- bin/ci/check-pre-commit.sh
+++ bin/ci/check-pre-commit.sh
@@ -1,20 +1,19 @@
-#!/usr/bin/env bash
-
-set -eu
-
-plan_path="$(basename "$1")"
-
 echo "--- :python: Install pre-commit"
 if [[ "${CI:-}" == "true" ]]; then
-  # Run apt update but ignore errors (e.g., from PostgreSQL repo)
-  apt update || true
+  # Temporarily disable PostgreSQL apt sources
+  sudo find /etc/apt/sources.list.d -type f -name "*postgres*" -exec mv {} {}.disabled \;
+
+  # Update package list without PostgreSQL repos
+  if ! apt update; then
+    echo "Warning: apt update had issues (excluding PostgreSQL repos)."
+  fi
+
+  # Re-enable PostgreSQL repos
+  sudo find /etc/apt/sources.list.d -type f -name "*.disabled" -exec sh -c 'mv "$1" "${1%.disabled}"' _ {} \;
+
+  # Install Python3 pip and pre-commit
   apt install -y python3-pip
   pip3 install pre-commit
 else
   echo "Not in CI! Skipping installation of pre-commit. Please install it manually if executing this on your workstation"
 fi
-
-pre-commit --version
-
-echo "--- :git: [${plan_path}] Running checks provided by pre-commit hooks"
-find "$plan_path" -type f -print0 | xargs -0 pre-commit run --files
